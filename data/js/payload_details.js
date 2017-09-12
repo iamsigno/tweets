40002624252fd609665cc7a00590f5e63a03687e
Signo <iamsigno@users.noreply.github.com>
2017-09-13 00:14:43 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26014,
-	"created_at": "Fri Sep 08 15:14:42 +0000 2017",
+	"tweets": 26015,
+	"created_at": "Tue Sep 12 15:14:42 +0000 2017",
 	"lang": "ko"
 }
--- data/js/tweets/2017_09.js
+++ data/js/tweets/2017_09.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2017_09 = 
 [
 	{
+		"created_at": "Tue Sep 12 08:32:15 +0000 2017",
+		"id": 907522232380993500,
+		"id_str": "907522232380993536",
+		"text": "【 #SMTOWN_NOW 】 STATION https://t.co/l6ykuVRTyw https://t.co/9F0G0CIRaS",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SMTOWN_NOW",
+					"indices": [
+						2,
+						13
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/l6ykuVRTyw",
+					"expanded_url": "http://now.smtown.com/Show/6290",
+					"display_url": "now.smtown.com/Show/6290",
+					"indices": [
+						24,
+						47
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 907522230443180000,
+					"id_str": "907522230443180032",
+					"indices": [
+						48,
+						71
+					],
+					"media_url": "http://pbs.twimg.com/media/DJgqqQtWAAASiaE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DJgqqQtWAAASiaE.jpg",
+					"url": "https://t.co/9F0G0CIRaS",
+					"display_url": "pic.twitter.com/9F0G0CIRaS",
+					"expanded_url": "https://twitter.com/dcsigno/status/907522232380993536/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 856,
+							"h": 600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 856,
+							"h": 600,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 477,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 907522230443180000,
+					"id_str": "907522230443180032",
+					"indices": [
+						48,
+						71
+					],
+					"media_url": "http://pbs.twimg.com/media/DJgqqQtWAAASiaE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DJgqqQtWAAASiaE.jpg",
+					"url": "https://t.co/9F0G0CIRaS",
+					"display_url": "pic.twitter.com/9F0G0CIRaS",
+					"expanded_url": "https://twitter.com/dcsigno/status/907522232380993536/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 856,
+							"h": 600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 856,
+							"h": 600,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 477,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 270405347,
+			"id_str": "270405347",
+			"name": "SIGNO",
+			"screen_name": "dcsigno",
+			"location": "",
+			"description": "Girls' Generation S❤NE",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 634,
+			"friends_count": 502,
+			"listed_count": 7,
+			"created_at": "Tue Mar 22 15:18:36 +0000 2011",
+			"favourites_count": 1981,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25895,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/690080728147738624/A9UadOVJ.png",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/690080728147738624/A9UadOVJ.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/602843767272386560/rOg4bt0i_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/602843767272386560/rOg4bt0i_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/270405347/1454603900",
+			"profile_link_color": "777777",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDE9D1",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Fri Sep 08 05:18:18 +0000 2017",
 		"id": 906023872251576300,
 		"id_str": "906023872251576321",
