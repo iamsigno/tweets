846fe3dda4d9043369497030d29cb2658714ed5a
Signo <iamsigno@users.noreply.github.com>
2019-09-02 00:14:41 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26152,
-	"created_at": "Sat Aug 31 15:14:40 +0000 2019",
+	"tweets": 26153,
+	"created_at": "Sun Sep 01 15:14:40 +0000 2019",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2019_09.js
@@ -0,0 +1,184 @@
+Grailbird.data.tweets_2019_09 = 
+[
+	{
+		"created_at": "Sun Sep 01 11:21:06 +0000 2019",
+		"id": 1168107480884211700,
+		"id_str": "1168121599733710848",
+		"full_text": "#독보적 #음색 소유자 #태연 의 #감성폭발곡 11:11\n#TAEYEON #1111 #소녀시대 #Girlsgeneration @GirlsGeneration #jtbc #비긴어게인3\n\nhttps://t.co/LQ1N5UXJuO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			125
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "독보적",
+					"indices": [
+						0,
+						4
+					]
+				},
+				{
+					"text": "음색",
+					"indices": [
+						5,
+						8
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						13,
+						16
+					]
+				},
+				{
+					"text": "감성폭발곡",
+					"indices": [
+						19,
+						25
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						32,
+						40
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						47,
+						52
+					]
+				},
+				{
+					"text": "Girlsgeneration",
+					"indices": [
+						53,
+						69
+					]
+				},
+				{
+					"text": "jtbc",
+					"indices": [
+						87,
+						92
+					]
+				},
+				{
+					"text": "비긴어게인3",
+					"indices": [
+						93,
+						100
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GirlsGeneration",
+					"name": "Girls' Generation",
+					"id": 391115625,
+					"id_str": "391115625",
+					"indices": [
+						70,
+						86
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/LQ1N5UXJuO",
+					"expanded_url": "https://youtu.be/Hst3gZt5Pww",
+					"display_url": "youtu.be/Hst3gZt5Pww",
+					"indices": [
+						102,
+						125
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "http://t.co/6dDRF9Jo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/6dDRF9Jo",
+							"expanded_url": "http://girlsgeneration.smtown.com/",
+							"display_url": "girlsgeneration.smtown.com",
+							"indices": [
+								0,
+								20
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3467651,
+			"friends_count": 4,
+			"listed_count": 5781,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 1738,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035111738041982977/z1sKPIX-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035111738041982977/z1sKPIX-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1563614074",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2329,
+		"favorite_count": 4541,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#독보적 #음색 소유자 #태연 의 #감성폭발곡 11:11\n#TAEYEON #1111 #소녀시대 #Girlsgeneration @GirlsGeneration #jtbc #비긴어게인3\n\nhttps://t.co/LQ1N5UXJuO"
+	}
+]
