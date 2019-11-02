2c7451fe397131d11ce6fe8ae3fb825ec2e7bee9
Signo <iamsigno@users.noreply.github.com>
2019-11-03 00:14:42 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26218,
-	"created_at": "Thu Oct 31 15:14:41 +0000 2019",
+	"tweets": 26219,
+	"created_at": "Sat Nov 02 15:14:41 +0000 2019",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2019_11.js
@@ -0,0 +1,493 @@
+Grailbird.data.tweets_2019_11 = 
+[
+	{
+		"created_at": "Sat Nov 02 01:21:08 +0000 2019",
+		"id": 1190438065904353300,
+		"id_str": "1190438658756755456",
+		"full_text": "‘#비긴어게인3’ #태연, 네덜란드 현지 관객들을 홀린 ‘보컬퀸’의 황홀한 버스킹을 다시 한번 확인해 보세요!\n\nhttps://t.co/zTZehhq8PF\nhttps://t.co/jMRW7Jg0Uc\nhttps://t.co/0sPouGSyj4\nhttps://t.co/5snqJIBCG2\nhttps://t.co/wz4Wi3oT6y\nhttps://t.co/1S3MpzF9yK\nhttps://t.co/JCmeV917OQ\n\n#TAEYEON https://t.co/yfaXfAlmuT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			240
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "비긴어게인3",
+					"indices": [
+						1,
+						8
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						10,
+						13
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						232,
+						240
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/zTZehhq8PF",
+					"expanded_url": "https://tv.naver.com/v/10696778",
+					"display_url": "tv.naver.com/v/10696778",
+					"indices": [
+						63,
+						86
+					]
+				},
+				{
+					"url": "https://t.co/jMRW7Jg0Uc",
+					"expanded_url": "https://tv.naver.com/v/10696956",
+					"display_url": "tv.naver.com/v/10696956",
+					"indices": [
+						87,
+						110
+					]
+				},
+				{
+					"url": "https://t.co/0sPouGSyj4",
+					"expanded_url": "https://tv.naver.com/v/10696958",
+					"display_url": "tv.naver.com/v/10696958",
+					"indices": [
+						111,
+						134
+					]
+				},
+				{
+					"url": "https://t.co/5snqJIBCG2",
+					"expanded_url": "https://tv.naver.com/v/10696959",
+					"display_url": "tv.naver.com/v/10696959",
+					"indices": [
+						135,
+						158
+					]
+				},
+				{
+					"url": "https://t.co/wz4Wi3oT6y",
+					"expanded_url": "https://tv.naver.com/v/10696961",
+					"display_url": "tv.naver.com/v/10696961",
+					"indices": [
+						159,
+						182
+					]
+				},
+				{
+					"url": "https://t.co/1S3MpzF9yK",
+					"expanded_url": "https://tv.naver.com/v/10697016",
+					"display_url": "tv.naver.com/v/10697016",
+					"indices": [
+						183,
+						206
+					]
+				},
+				{
+					"url": "https://t.co/JCmeV917OQ",
+					"expanded_url": "https://tv.naver.com/v/10697017",
+					"display_url": "tv.naver.com/v/10697017",
+					"indices": [
+						207,
+						230
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1190438051987837000,
+					"id_str": "1190438051987836930",
+					"indices": [
+						241,
+						264
+					],
+					"media_url": "http://pbs.twimg.com/media/EIVJGd_X0AIJjOW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EIVJGd_X0AIJjOW.jpg",
+					"url": "https://t.co/yfaXfAlmuT",
+					"display_url": "pic.twitter.com/yfaXfAlmuT",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1190438065904353281/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 306,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 541,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 923,
+							"h": 2048,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 32,
+									"y": 621,
+									"h": 33,
+									"w": 33
+								},
+								{
+									"x": 151,
+									"y": 339,
+									"h": 33,
+									"w": 33
+								},
+								{
+									"x": 125,
+									"y": 39,
+									"h": 56,
+									"w": 56
+								},
+								{
+									"x": 140,
+									"y": 184,
+									"h": 77,
+									"w": 77
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 57,
+									"y": 1098,
+									"h": 58,
+									"w": 58
+								},
+								{
+									"x": 268,
+									"y": 600,
+									"h": 59,
+									"w": 59
+								},
+								{
+									"x": 222,
+									"y": 70,
+									"h": 100,
+									"w": 100
+								},
+								{
+									"x": 248,
+									"y": 326,
+									"h": 136,
+									"w": 136
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 159,
+									"y": 3048,
+									"h": 162,
+									"w": 162
+								},
+								{
+									"x": 745,
+									"y": 1666,
+									"h": 165,
+									"w": 165
+								},
+								{
+									"x": 618,
+									"y": 195,
+									"h": 279,
+									"w": 279
+								},
+								{
+									"x": 689,
+									"y": 907,
+									"h": 380,
+									"w": 380
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 97,
+									"y": 1874,
+									"h": 99,
+									"w": 99
+								},
+								{
+									"x": 458,
+									"y": 1024,
+									"h": 101,
+									"w": 101
+								},
+								{
+									"x": 380,
+									"y": 119,
+									"h": 171,
+									"w": 171
+								},
+								{
+									"x": 423,
+									"y": 557,
+									"h": 233,
+									"w": 233
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1190438051987837000,
+					"id_str": "1190438051987836930",
+					"indices": [
+						241,
+						264
+					],
+					"media_url": "http://pbs.twimg.com/media/EIVJGd_X0AIJjOW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EIVJGd_X0AIJjOW.jpg",
+					"url": "https://t.co/yfaXfAlmuT",
+					"display_url": "pic.twitter.com/yfaXfAlmuT",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1190438065904353281/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 306,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 541,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 923,
+							"h": 2048,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 32,
+									"y": 621,
+									"h": 33,
+									"w": 33
+								},
+								{
+									"x": 151,
+									"y": 339,
+									"h": 33,
+									"w": 33
+								},
+								{
+									"x": 125,
+									"y": 39,
+									"h": 56,
+									"w": 56
+								},
+								{
+									"x": 140,
+									"y": 184,
+									"h": 77,
+									"w": 77
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 57,
+									"y": 1098,
+									"h": 58,
+									"w": 58
+								},
+								{
+									"x": 268,
+									"y": 600,
+									"h": 59,
+									"w": 59
+								},
+								{
+									"x": 222,
+									"y": 70,
+									"h": 100,
+									"w": 100
+								},
+								{
+									"x": 248,
+									"y": 326,
+									"h": 136,
+									"w": 136
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 159,
+									"y": 3048,
+									"h": 162,
+									"w": 162
+								},
+								{
+									"x": 745,
+									"y": 1666,
+									"h": 165,
+									"w": 165
+								},
+								{
+									"x": 618,
+									"y": 195,
+									"h": 279,
+									"w": 279
+								},
+								{
+									"x": 689,
+									"y": 907,
+									"h": 380,
+									"w": 380
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 97,
+									"y": 1874,
+									"h": 99,
+									"w": 99
+								},
+								{
+									"x": 458,
+									"y": 1024,
+									"h": 101,
+									"w": 101
+								},
+								{
+									"x": 380,
+									"y": 119,
+									"h": 171,
+									"w": 171
+								},
+								{
+									"x": 423,
+									"y": 557,
+									"h": 233,
+									"w": 233
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "http://t.co/6dDRF9Jo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/6dDRF9Jo",
+							"expanded_url": "http://girlsgeneration.smtown.com/",
+							"display_url": "girlsgeneration.smtown.com",
+							"indices": [
+								0,
+								20
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3505103,
+			"friends_count": 4,
+			"listed_count": 5880,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 6,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 1813,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035111738041982977/z1sKPIX-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035111738041982977/z1sKPIX-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1572253253",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4601,
+		"favorite_count": 11576,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "‘#비긴어게인3’ #태연, 네덜란드 현지 관객들을 홀린 ‘보컬퀸’의 황홀한 버스킹을 다시 한번 확인해 보세요!\n\nhttps://t.co/zTZehhq8PF\nhttps://t.co/jMRW7Jg0Uc\nhttps://t.co/0sPouGSyj4\nhttps://t.co/5snqJIBCG2\nhttps://t.co/wz4Wi3oT6y\nhttps://t.co/1S3MpzF9yK\nhttps://t.co/JCmeV917OQ\n\n#TAEYEON https://t.co/yfaXfAlmuT"
+	}
+]
