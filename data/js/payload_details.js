6d53691507184fa63ec3843214da457ebc3082bf
SIGNO <iamsigno@users.noreply.github.com>
2019-12-26 11:07:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26402,
-	"created_at": "Wed Dec 25 20:07:51 +0000 2019",
+	"tweets": 26403,
+	"created_at": "Thu Dec 26 02:07:50 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,418 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Thu Dec 26 01:45:37 +0000 2019",
+		"id": 1210008568708993000,
+		"id_str": "1210013768211738624",
+		"full_text": "써니가 JTBC 프로그램 ‘오늘부터 파티시에’에서 크리스마스 디저트 ‘오렌지 구겔호프’를 직접 제작, 출연자 중 가장 먼저 매진시키며 파티시에로 완벽 변신, 판매 수익금 전액을 기부해 마지막까지 안방극장에 훈훈함을 선사했습니다!\n\n#써니 #SUNNY #GirlsGeneration #소녀시대 #오늘부터파티시에 https://t.co/4kgqBnkS3z",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			172
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "써니",
+					"indices": [
+						129,
+						132
+					]
+				},
+				{
+					"text": "SUNNY",
+					"indices": [
+						133,
+						139
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						140,
+						156
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						157,
+						162
+					]
+				},
+				{
+					"text": "오늘부터파티시에",
+					"indices": [
+						163,
+						172
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1210008556038000600,
+					"id_str": "1210008556038000641",
+					"indices": [
+						173,
+						196
+					],
+					"media_url": "http://pbs.twimg.com/media/EMrQXu3UEAELf1R.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EMrQXu3UEAELf1R.jpg",
+					"url": "https://t.co/4kgqBnkS3z",
+					"display_url": "pic.twitter.com/4kgqBnkS3z",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1210008568708993026/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 428,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 242,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 730,
+							"h": 2048,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"all": {
+							"tags": [
+								{
+									"type": "user",
+									"user_id": "253415014",
+									"screen_name": "Sunnyday515",
+									"name": "Sunny"
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 137,
+									"y": 508,
+									"h": 86,
+									"w": 86
+								},
+								{
+									"x": 248,
+									"y": 75,
+									"h": 77,
+									"w": 77
+								},
+								{
+									"x": 123,
+									"y": 1001,
+									"h": 112,
+									"w": 112
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 77,
+									"y": 287,
+									"h": 49,
+									"w": 49
+								},
+								{
+									"x": 140,
+									"y": 42,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 69,
+									"y": 566,
+									"h": 63,
+									"w": 63
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 234,
+									"y": 868,
+									"h": 148,
+									"w": 148
+								},
+								{
+									"x": 424,
+									"y": 128,
+									"h": 132,
+									"w": 132
+								},
+								{
+									"x": 210,
+									"y": 1708,
+									"h": 192,
+									"w": 192
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 234,
+									"y": 868,
+									"h": 148,
+									"w": 148
+								},
+								{
+									"x": 424,
+									"y": 128,
+									"h": 132,
+									"w": 132
+								},
+								{
+									"x": 210,
+									"y": 1708,
+									"h": 192,
+									"w": 192
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1210008556038000600,
+					"id_str": "1210008556038000641",
+					"indices": [
+						173,
+						196
+					],
+					"media_url": "http://pbs.twimg.com/media/EMrQXu3UEAELf1R.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EMrQXu3UEAELf1R.jpg",
+					"url": "https://t.co/4kgqBnkS3z",
+					"display_url": "pic.twitter.com/4kgqBnkS3z",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1210008568708993026/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 428,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 242,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 730,
+							"h": 2048,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"all": {
+							"tags": [
+								{
+									"type": "user",
+									"user_id": "253415014",
+									"screen_name": "Sunnyday515",
+									"name": "Sunny"
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 137,
+									"y": 508,
+									"h": 86,
+									"w": 86
+								},
+								{
+									"x": 248,
+									"y": 75,
+									"h": 77,
+									"w": 77
+								},
+								{
+									"x": 123,
+									"y": 1001,
+									"h": 112,
+									"w": 112
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 77,
+									"y": 287,
+									"h": 49,
+									"w": 49
+								},
+								{
+									"x": 140,
+									"y": 42,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 69,
+									"y": 566,
+									"h": 63,
+									"w": 63
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 234,
+									"y": 868,
+									"h": 148,
+									"w": 148
+								},
+								{
+									"x": 424,
+									"y": 128,
+									"h": 132,
+									"w": 132
+								},
+								{
+									"x": 210,
+									"y": 1708,
+									"h": 192,
+									"w": 192
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 234,
+									"y": 868,
+									"h": 148,
+									"w": 148
+								},
+								{
+									"x": 424,
+									"y": 128,
+									"h": 132,
+									"w": 132
+								},
+								{
+									"x": 210,
+									"y": 1708,
+									"h": 192,
+									"w": 192
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "http://t.co/6dDRF9Jo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/6dDRF9Jo",
+							"expanded_url": "http://girlsgeneration.smtown.com/",
+							"display_url": "girlsgeneration.smtown.com",
+							"indices": [
+								0,
+								20
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3527192,
+			"friends_count": 4,
+			"listed_count": 5946,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 6,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 1847,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035111738041982977/z1sKPIX-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035111738041982977/z1sKPIX-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1572253253",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 708,
+		"favorite_count": 1784,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "써니가 JTBC 프로그램 ‘오늘부터 파티시에’에서 크리스마스 디저트 ‘오렌지 구겔호프’를 직접 제작, 출연자 중 가장 먼저 매진시키며 파티시에로 완벽 변신, 판매 수익금 전액을 기부해 마지막까지 안방극장에 훈훈함을 선사했습니다!\n\n#써니 #SUNNY #GirlsGeneration #소녀시대 #오늘부터파티시에 https://t.co/4kgqBnkS3z"
+	},
+	{
 		"created_at": "Wed Dec 25 20:02:21 +0000 2019",
 		"id": 1208743547630846000,
 		"id_str": "1209927380036308992",
