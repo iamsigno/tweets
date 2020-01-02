a7f77b23dcff6a9b3fa01e47c92d7d0270dfc786
SIGNO <iamsigno@users.noreply.github.com>
2020-01-02 13:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26421,
-	"created_at": "Tue Dec 31 16:07:50 +0000 2019",
+	"tweets": 26422,
+	"created_at": "Thu Jan 02 04:07:49 +0000 2020",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2020_01.js
@@ -0,0 +1,140 @@
+Grailbird.data.tweets_2020_01 = 
+[
+	{
+		"created_at": "Thu Jan 02 03:56:37 +0000 2020",
+		"id": 1212569206073348000,
+		"id_str": "1212583450424811520",
+		"full_text": "[V ORIGINAL] 제로와 함께하는 LA 힙스트리트에서의 쇼핑🔥✌️ 쇼핑하다가 그냥 막 찍혀두 화보 되는거 그게 바로 울 제로&amp;탱구 남매잖아요🥰 모든 순간이 화보 같았던 LA 햇살 아래 펫셔니스타 탱구를 지금 바로 만나보세요~💖\n\n▶ https://t.co/bwYVCvtrS5\n\n#펫셔니스타탱구 #태연 #VORIGINAL #VLIVE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			192
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "펫셔니스타탱구",
+					"indices": [
+						162,
+						170
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						171,
+						174
+					]
+				},
+				{
+					"text": "VORIGINAL",
+					"indices": [
+						175,
+						185
+					]
+				},
+				{
+					"text": "VLIVE",
+					"indices": [
+						186,
+						192
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/bwYVCvtrS5",
+					"expanded_url": "https://www.vlive.tv/video/167857/playlist/157709",
+					"display_url": "vlive.tv/video/167857/p…",
+					"indices": [
+						137,
+						160
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1459223742,
+			"id_str": "1459223742",
+			"name": "V LIVE",
+			"screen_name": "Vliveofficial",
+			"location": "Korea",
+			"description": "V LIVE lets celebs broadcast live videos to you. You can see what they are seeing in real time from anywhere in the world.",
+			"url": "https://t.co/Z7eVOHipm5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Z7eVOHipm5",
+							"expanded_url": "http://www.vlive.tv",
+							"display_url": "vlive.tv",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3442998,
+			"friends_count": 169,
+			"listed_count": 1913,
+			"created_at": "Sun May 26 09:50:22 +0000 2013",
+			"favourites_count": 7,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 12073,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "DBE9ED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme17/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme17/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/935805024059375616/rcj1NsPG_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/935805024059375616/rcj1NsPG_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1459223742/1511948291",
+			"profile_link_color": "CC3366",
+			"profile_sidebar_border_color": "DBE9ED",
+			"profile_sidebar_fill_color": "E6F6F9",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 216,
+		"favorite_count": 754,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[V ORIGINAL] 제로와 함께하는 LA 힙스트리트에서의 쇼핑🔥✌️ 쇼핑하다가 그냥 막 찍혀두 화보 되는거 그게 바로 울 제로&amp;탱구 남매잖아요🥰 모든 순간이 화보 같았던 LA 햇살 아래 펫셔니스타 탱구를 지금 바로 만나보세요~💖\n\n▶ https://t.co/bwYVCvtrS5\n\n#펫셔니스타탱구 #태연 #VORIGINAL #VLIVE"
+	}
+]
