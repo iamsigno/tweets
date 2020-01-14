b4e67aae493e4fd9bb0c6134a5174a8e753fc1ed
SIGNO <iamsigno@users.noreply.github.com>
2020-01-14 10:07:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26576,
-	"created_at": "Mon Jan 13 16:07:50 +0000 2020",
+	"tweets": 26577,
+	"created_at": "Tue Jan 14 01:07:50 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,139 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Tue Jan 14 00:45:38 +0000 2020",
+		"id": 1216878604199448600,
+		"id_str": "1216884038373380103",
+		"full_text": "‘D-1’ #태연, 타이틀 곡 ‘내게 들려주고 싶은 말’ MV 티저 영상 공개 화제!\n티저 이미지 속 신비하고 청초한 비주얼 눈길! 자신을 더욱 믿고 사랑하자는 메세지를 담은 발라드 타이틀 곡 ‘내게 들려주고 싶은 말 (Dear Me)’를 15일 오후 6시에 함께 들어요! 💌💕\n\n#믿듣탱 #TAEYEON #퍼포즈 #Purpose",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			184
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "태연",
+					"indices": [
+						6,
+						9
+					]
+				},
+				{
+					"text": "믿듣탱",
+					"indices": [
+						157,
+						161
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						162,
+						170
+					]
+				},
+				{
+					"text": "퍼포즈",
+					"indices": [
+						171,
+						175
+					]
+				},
+				{
+					"text": "Purpose",
+					"indices": [
+						176,
+						184
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "http://t.co/6dDRF9Jo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/6dDRF9Jo",
+							"expanded_url": "http://girlsgeneration.smtown.com/",
+							"display_url": "girlsgeneration.smtown.com",
+							"indices": [
+								0,
+								20
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3539864,
+			"friends_count": 4,
+			"listed_count": 5963,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 7,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 1884,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035111738041982977/z1sKPIX-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035111738041982977/z1sKPIX-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1572253253",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 933,
+		"favorite_count": 1934,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "‘D-1’ #태연, 타이틀 곡 ‘내게 들려주고 싶은 말’ MV 티저 영상 공개 화제!\n티저 이미지 속 신비하고 청초한 비주얼 눈길! 자신을 더욱 믿고 사랑하자는 메세지를 담은 발라드 타이틀 곡 ‘내게 들려주고 싶은 말 (Dear Me)’를 15일 오후 6시에 함께 들어요! 💌💕\n\n#믿듣탱 #TAEYEON #퍼포즈 #Purpose"
+	},
+	{
 		"created_at": "Mon Jan 13 16:04:32 +0000 2020",
 		"id": 1216752900979949600,
 		"id_str": "1216752900979949570",
