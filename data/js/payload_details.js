bbfb65ca40a51a5585ccad598ceaf1660f82cb8c
SIGNO <iamsigno@users.noreply.github.com>
2020-01-18 01:07:52 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26642,
-	"created_at": "Fri Jan 17 15:07:51 +0000 2020",
+	"tweets": 26643,
+	"created_at": "Fri Jan 17 16:07:51 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,116 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Fri Jan 17 15:45:38 +0000 2020",
+		"id": 1218187366348787700,
+		"id_str": "1218197694994440193",
+		"full_text": "역시..#믿듣탱 #믿보탱 자랑스러운 울 탱구언니 오늘도 최고였다고한다🥰\n간만에 보는 언니들 어제본것처럼 익숙하지만 또 애틋한💕\n (역시 우린 가끔봐야하나ㅏㅂㅘㅇㅛ..ㅋㅋㅋ) https://t.co/8U3ZKQDXI8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			120
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "믿듣탱",
+					"indices": [
+						4,
+						8
+					]
+				},
+				{
+					"text": "믿보탱",
+					"indices": [
+						9,
+						13
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/8U3ZKQDXI8",
+					"expanded_url": "https://www.instagram.com/p/B7bNVk7h_g2/?igshid=qg3md06e47n",
+					"display_url": "instagram.com/p/B7bNVk7h_g2/…",
+					"indices": [
+						97,
+						120
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://instagram.com\" rel=\"nofollow\">Instagram</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2173631072,
+			"id_str": "2173631072",
+			"name": "서현(서주현)",
+			"screen_name": "sjhsjh0628",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1395240,
+			"friends_count": 31,
+			"listed_count": 3342,
+			"created_at": "Mon Nov 04 08:58:47 +0000 2013",
+			"favourites_count": 6,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1199,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1146333352682647552/0h6dsbYz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1146333352682647552/0h6dsbYz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2173631072/1526914876",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 431,
+		"favorite_count": 747,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "역시..#믿듣탱 #믿보탱 자랑스러운 울 탱구언니 오늘도 최고였다고한다🥰\n간만에 보는 언니들 어제본것처럼 익숙하지만 또 애틋한💕\n (역시 우린 가끔봐야하나ㅏㅂㅘㅇㅛ..ㅋㅋㅋ) https://t.co/8U3ZKQDXI8"
+	},
+	{
 		"created_at": "Fri Jan 17 14:45:37 +0000 2020",
 		"id": 1218172450460053500,
 		"id_str": "1218182594778542085",
