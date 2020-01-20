f2cc2feaa802dc129bd08b14d34f302387108a04
SIGNO <iamsigno@users.noreply.github.com>
2020-01-20 12:07:52 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26660,
-	"created_at": "Mon Jan 20 02:07:51 +0000 2020",
+	"tweets": 26661,
+	"created_at": "Mon Jan 20 03:07:51 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,282 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Mon Jan 20 02:56:38 +0000 2020",
+		"id": 1219086482511519700,
+		"id_str": "1219091333161332737",
+		"full_text": "#Taeyeon's new album is like a beautiful, personal letter written to all of us 💌\n\nWhich is your favourite track off #PURPOSE?\nhttps://t.co/Iwmt909E1o\n\n#TAEYEON #태연 #소녀시대 #GirlsGeneration #Purpose #태연이가들려주고싶은말 https://t.co/RCC1Jwuseh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			208
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Taeyeon",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"text": "PURPOSE",
+					"indices": [
+						116,
+						124
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						151,
+						159
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						160,
+						163
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						164,
+						169
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						170,
+						186
+					]
+				},
+				{
+					"text": "Purpose",
+					"indices": [
+						187,
+						195
+					]
+				},
+				{
+					"text": "태연이가들려주고싶은말",
+					"indices": [
+						196,
+						208
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Iwmt909E1o",
+					"expanded_url": "http://spoti.fi/Taeyeon-DearMe",
+					"display_url": "spoti.fi/Taeyeon-DearMe",
+					"indices": [
+						126,
+						149
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1219084808594178000,
+					"id_str": "1219084808594178048",
+					"indices": [
+						209,
+						232
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1219084808594178048/pu/img/mY7-89onKo-1ti_A.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1219084808594178048/pu/img/mY7-89onKo-1ti_A.jpg",
+					"url": "https://t.co/RCC1Jwuseh",
+					"display_url": "pic.twitter.com/RCC1Jwuseh",
+					"expanded_url": "https://twitter.com/SpotifyKDaebak/status/1219086482511519745/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1219084808594178000,
+					"id_str": "1219084808594178048",
+					"indices": [
+						209,
+						232
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1219084808594178048/pu/img/mY7-89onKo-1ti_A.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1219084808594178048/pu/img/mY7-89onKo-1ti_A.jpg",
+					"url": "https://t.co/RCC1Jwuseh",
+					"display_url": "pic.twitter.com/RCC1Jwuseh",
+					"expanded_url": "https://twitter.com/SpotifyKDaebak/status/1219086482511519745/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 28042,
+						"variants": [
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1219084808594178048/pu/vid/320x320/V_AvgbwjsIJvBzHd.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1219084808594178048/pu/pl/v-yx_5xenAztm6PJ.m3u8?tag=10"
+							},
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1219084808594178048/pu/vid/720x720/XE-gB0a5dt78dN0z.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1219084808594178048/pu/vid/480x480/KqtcPqEniQNBmQeO.mp4?tag=10"
+							}
+						]
+					},
+					"features": {},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 978459450871697400,
+			"id_str": "978459450871697408",
+			"name": "Spotify ❤️ K-Pop",
+			"screen_name": "SpotifyKDaebak",
+			"location": "",
+			"description": "Do you eat, sleep, and breathe K-Pop like we do? For all the news, music, entertainment and more, follow our official Spotify K-pop profile right now.",
+			"url": "https://t.co/TSbcWtrINY",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/TSbcWtrINY",
+							"expanded_url": "http://spoti.fi/KPOP",
+							"display_url": "spoti.fi/KPOP",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 133394,
+			"friends_count": 189,
+			"listed_count": 202,
+			"created_at": "Tue Mar 27 02:31:26 +0000 2018",
+			"favourites_count": 629,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1747,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1108279680006709248/m7AM6Gyg_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1108279680006709248/m7AM6Gyg_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/978459450871697408/1526972671",
+			"profile_link_color": "19CF86",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 642,
+		"favorite_count": 1277,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#Taeyeon's new album is like a beautiful, personal letter written to all of us 💌\n\nWhich is your favourite track off #PURPOSE?\nhttps://t.co/Iwmt909E1o\n\n#TAEYEON #태연 #소녀시대 #GirlsGeneration #Purpose #태연이가들려주고싶은말 https://t.co/RCC1Jwuseh"
+	},
+	{
 		"created_at": "Mon Jan 20 01:56:41 +0000 2020",
 		"id": 1219068227801632800,
 		"id_str": "1219076246241456130",
