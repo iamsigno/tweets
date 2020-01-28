3ca478bb03ab9f9bdce364abb7530531e78da421
SIGNO <iamsigno@users.noreply.github.com>
2020-01-28 15:07:53 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26724,
-	"created_at": "Tue Jan 28 04:07:52 +0000 2020",
+	"tweets": 26725,
+	"created_at": "Tue Jan 28 06:07:52 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,130 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Tue Jan 28 05:45:18 +0000 2020",
+		"id": 1222030903289041000,
+		"id_str": "1222032882467790848",
+		"full_text": "자신만만하더니 이유있는 자신감이었어!ㅋㅋ진짜잘하더라~멋진 공연 초대해줘서 고마워 뮤배 김준면씨😆 막공까지 힘내!! kimjuncotton \n#뮤지컬웃는남자 #웃는남자 #수호 #김준면 https://t.co/TKXcrJsODd",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			125
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "뮤지컬웃는남자",
+					"indices": [
+						78,
+						86
+					]
+				},
+				{
+					"text": "웃는남자",
+					"indices": [
+						87,
+						92
+					]
+				},
+				{
+					"text": "수호",
+					"indices": [
+						93,
+						96
+					]
+				},
+				{
+					"text": "김준면",
+					"indices": [
+						97,
+						101
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/TKXcrJsODd",
+					"expanded_url": "https://www.instagram.com/p/B72hLMHBynS/?igshid=pb8jy5rgqycb",
+					"display_url": "instagram.com/p/B72hLMHBynS/…",
+					"indices": [
+						102,
+						125
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://instagram.com\" rel=\"nofollow\">Instagram</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2173631072,
+			"id_str": "2173631072",
+			"name": "서현(서주현)",
+			"screen_name": "sjhsjh0628",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1394938,
+			"friends_count": 31,
+			"listed_count": 3340,
+			"created_at": "Mon Nov 04 08:58:47 +0000 2013",
+			"favourites_count": 6,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1214,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1146333352682647552/0h6dsbYz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1146333352682647552/0h6dsbYz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2173631072/1526914876",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 295,
+		"favorite_count": 415,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "자신만만하더니 이유있는 자신감이었어!ㅋㅋ진짜잘하더라~멋진 공연 초대해줘서 고마워 뮤배 김준면씨😆 막공까지 힘내!! kimjuncotton \n#뮤지컬웃는남자 #웃는남자 #수호 #김준면 https://t.co/TKXcrJsODd"
+	},
+	{
 		"created_at": "Tue Jan 28 04:06:23 +0000 2020",
 		"id": 1222007990494154800,
 		"id_str": "1222007990494154753",
