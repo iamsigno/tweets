674cdf3f317b5c03f334496385cd0c00b102f3ff
SIGNO <iamsigno@users.noreply.github.com>
2020-01-30 16:07:52 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26734,
-	"created_at": "Thu Jan 30 05:07:52 +0000 2020",
+	"tweets": 26735,
+	"created_at": "Thu Jan 30 07:07:51 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,305 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Thu Jan 30 06:29:06 +0000 2020",
+		"id": 1222768681488388000,
+		"id_str": "1222768681488388096",
+		"full_text": "#TAEYEON #태연\n‘열일하고 이방인으로 떠난’ 윤종신 오늘(30일) 발매...”2CD로 담아”\nhttps://t.co/GyjEhOspZy\n#SMTOWN #Artist_News https://t.co/JtN19JooQC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			100
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TAEYEON",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						9,
+						12
+					]
+				},
+				{
+					"text": "SMTOWN",
+					"indices": [
+						80,
+						87
+					]
+				},
+				{
+					"text": "Artist_News",
+					"indices": [
+						88,
+						100
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/GyjEhOspZy",
+					"expanded_url": "https://www.smtown.com/artist/newsDetail/3138",
+					"display_url": "smtown.com/artist/newsDet…",
+					"indices": [
+						56,
+						79
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1222768680456654800,
+					"id_str": "1222768680456654849",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/EPgloukXkAE6pcS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EPgloukXkAE6pcS.jpg",
+					"url": "https://t.co/JtN19JooQC",
+					"display_url": "pic.twitter.com/JtN19JooQC",
+					"expanded_url": "https://twitter.com/IAMSIGNO/status/1222768681488388096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": [
+								{
+									"x": 157,
+									"y": 67,
+									"h": 81,
+									"w": 81
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 157,
+									"y": 67,
+									"h": 81,
+									"w": 81
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 157,
+									"y": 67,
+									"h": 81,
+									"w": 81
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 157,
+									"y": 67,
+									"h": 81,
+									"w": 81
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1222768680456654800,
+					"id_str": "1222768680456654849",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/EPgloukXkAE6pcS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EPgloukXkAE6pcS.jpg",
+					"url": "https://t.co/JtN19JooQC",
+					"display_url": "pic.twitter.com/JtN19JooQC",
+					"expanded_url": "https://twitter.com/IAMSIGNO/status/1222768681488388096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": [
+								{
+									"x": 157,
+									"y": 67,
+									"h": 81,
+									"w": 81
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 157,
+									"y": 67,
+									"h": 81,
+									"w": 81
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 157,
+									"y": 67,
+									"h": 81,
+									"w": 81
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 157,
+									"y": 67,
+									"h": 81,
+									"w": 81
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 270405347,
+			"id_str": "270405347",
+			"name": "SIGNO",
+			"screen_name": "IAMSIGNO",
+			"location": "",
+			"description": "소녀시대(Girls' Generation) S❤NE\n@GirlsGeneration @gg_official_jp\n@Sunnyday515 @sjhsjh0628 @tiffanyyoung @Hyoyeon_djhyo @sychoiofficial",
+			"url": "https://t.co/Y6UBFaq0CK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Y6UBFaq0CK",
+							"expanded_url": "http://girlsgeneration.xyz/",
+							"display_url": "girlsgeneration.xyz",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 543,
+			"friends_count": 490,
+			"listed_count": 7,
+			"created_at": "Tue Mar 22 15:18:36 +0000 2011",
+			"favourites_count": 1908,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 26553,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1201460639044005888/RTQPfRgI_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1201460639044005888/RTQPfRgI_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/270405347/1578714265",
+			"profile_link_color": "777777",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDE9D1",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#TAEYEON #태연\n‘열일하고 이방인으로 떠난’ 윤종신 오늘(30일) 발매...”2CD로 담아”\nhttps://t.co/GyjEhOspZy\n#SMTOWN #Artist_News https://t.co/JtN19JooQC"
+	},
+	{
 		"created_at": "Thu Jan 30 04:34:06 +0000 2020",
 		"id": 1222739740669239300,
 		"id_str": "1222739740669239297",
