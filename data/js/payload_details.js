f1603d2b4b0a9588568aad04d63fd2f5b3df241a
SIGNO <iamsigno@users.noreply.github.com>
2020-01-31 11:07:52 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26739,
-	"created_at": "Fri Jan 31 01:07:54 +0000 2020",
+	"tweets": 26740,
+	"created_at": "Fri Jan 31 02:07:51 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2020_01.js",
 		"year": 2020,
 		"var_name": "tweets_2020_01",
-		"tweet_count": 318,
+		"tweet_count": 319,
 		"month": 1
 	},
 	{
