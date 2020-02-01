cbb0936ec25a6148a6a3a4ab2f890d1f932cadd6
SIGNO <iamsigno@users.noreply.github.com>
2020-02-01 15:07:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26748,
-	"created_at": "Fri Jan 31 12:07:52 +0000 2020",
+	"tweets": 26749,
+	"created_at": "Sat Feb 01 06:07:50 +0000 2020",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2020_02.js
@@ -0,0 +1,249 @@
+Grailbird.data.tweets_2020_02 = 
+[
+	{
+		"created_at": "Sat Feb 01 05:35:38 +0000 2020",
+		"id": 1223380538875998200,
+		"id_str": "1223480003867758592",
+		"full_text": "&lt;본 대로 말하라&gt; 오늘 밤 첫방송! #DDAY\n\n서로의 눈과 발, 머리가 되어🏃‍♀️🏃‍♂️\n다시 돌아온 '그 놈'을 잡아야한다!\n\n오감 서스펜스 #본대로말하라\n오늘 밤 첫방송. 토일 밤 10시 50분\n#장혁 #최수영 #진서연 #장현성 #류승수 https://t.co/ps2FlNaZ4u",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			143
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DDAY",
+					"indices": [
+						27,
+						32
+					]
+				},
+				{
+					"text": "본대로말하라",
+					"indices": [
+						89,
+						96
+					]
+				},
+				{
+					"text": "장혁",
+					"indices": [
+						120,
+						123
+					]
+				},
+				{
+					"text": "최수영",
+					"indices": [
+						124,
+						128
+					]
+				},
+				{
+					"text": "진서연",
+					"indices": [
+						129,
+						133
+					]
+				},
+				{
+					"text": "장현성",
+					"indices": [
+						134,
+						138
+					]
+				},
+				{
+					"text": "류승수",
+					"indices": [
+						139,
+						143
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1223380508005957600,
+					"id_str": "1223380508005957632",
+					"indices": [
+						144,
+						167
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1223380508005957632/pu/img/WJCqGPzZzigRPw5s.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1223380508005957632/pu/img/WJCqGPzZzigRPw5s.jpg",
+					"url": "https://t.co/ps2FlNaZ4u",
+					"display_url": "pic.twitter.com/ps2FlNaZ4u",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1223380538875998208/video/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1223380508005957600,
+					"id_str": "1223380508005957632",
+					"indices": [
+						144,
+						167
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1223380508005957632/pu/img/WJCqGPzZzigRPw5s.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1223380508005957632/pu/img/WJCqGPzZzigRPw5s.jpg",
+					"url": "https://t.co/ps2FlNaZ4u",
+					"display_url": "pic.twitter.com/ps2FlNaZ4u",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1223380538875998208/video/1",
+					"type": "video",
+					"sizes": {
+						"large": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							4,
+							5
+						],
+						"duration_millis": 10611,
+						"variants": [
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1223380508005957632/pu/vid/320x400/-GaxCCMlal4mkWjj.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1223380508005957632/pu/vid/360x450/WHlwigzN5_bO08AB.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1223380508005957632/pu/pl/5hy8oLFoip5AIESs.m3u8?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1223380508005957632/pu/vid/720x900/ERtRmaKAC-iYYjRy.mp4?tag=10"
+							}
+						]
+					},
+					"features": {},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.smartpost.kr/\" rel=\"nofollow\">smartpost.kr</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 226483444,
+			"id_str": "226483444",
+			"name": "OCN ORIGINAL",
+			"screen_name": "OCN_ORIGINAL",
+			"location": "대한민국 서울",
+			"description": "🔜 2020 2/1 첫방송. 토일 밤 10:50 #장혁 #본대로말하라 \n🔜 2020 3/16 첫방송. 월화 밤 10:50 #최진혁 #루갈",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 44530,
+			"friends_count": 1,
+			"listed_count": 247,
+			"created_at": "Tue Dec 14 07:56:11 +0000 2010",
+			"favourites_count": 1601,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 14872,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1174583128486100993/b2ih4oQH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1174583128486100993/b2ih4oQH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/226483444/1579484461",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E8E8E8",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 276,
+		"favorite_count": 394,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "&lt;본 대로 말하라&gt; 오늘 밤 첫방송! #DDAY\n\n서로의 눈과 발, 머리가 되어🏃‍♀️🏃‍♂️\n다시 돌아온 '그 놈'을 잡아야한다!\n\n오감 서스펜스 #본대로말하라\n오늘 밤 첫방송. 토일 밤 10시 50분\n#장혁 #최수영 #진서연 #장현성 #류승수 https://t.co/ps2FlNaZ4u"
+	}
+]
