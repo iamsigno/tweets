d73fa16b52c3538f1b599cb6881019d2b7dd278a
SIGNO <iamsigno@users.noreply.github.com>
2020-02-04 00:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26755,
-	"created_at": "Mon Feb 03 08:07:48 +0000 2020",
+	"tweets": 26756,
+	"created_at": "Mon Feb 03 15:07:48 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,10 +1,105 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Mon Feb 03 14:44:57 +0000 2020",
+		"id": 1224330485775532000,
+		"id_str": "1224343019152953347",
+		"full_text": "안녕,드라큘라\n2월 17,18일 밤9:30! JTBC 많이 기대해주세요🖤 https://t.co/jaAPehiZJV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			64
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/jaAPehiZJV",
+					"expanded_url": "https://www.instagram.com/p/B8G25vnBmnQ/?igshid=154rb4ua7patu",
+					"display_url": "instagram.com/p/B8G25vnBmnQ/…",
+					"indices": [
+						41,
+						64
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://instagram.com\" rel=\"nofollow\">Instagram</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2173631072,
+			"id_str": "2173631072",
+			"name": "서현(서주현)",
+			"screen_name": "sjhsjh0628",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1395077,
+			"friends_count": 31,
+			"listed_count": 3345,
+			"created_at": "Mon Nov 04 08:58:47 +0000 2013",
+			"favourites_count": 6,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1217,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1146333352682647552/0h6dsbYz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1146333352682647552/0h6dsbYz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2173631072/1526914876",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 463,
+		"favorite_count": 842,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "안녕,드라큘라\n2월 17,18일 밤9:30! JTBC 많이 기대해주세요🖤 https://t.co/jaAPehiZJV"
+	},
+	{
 		"created_at": "Mon Feb 03 07:56:38 +0000 2020",
 		"id": 1224229884915929000,
 		"id_str": "1224240260378505217",
-		"full_text": "[#서현] 올 2월 강추위 이기는 방법 : 롱패딩, 핫팩 그리고 서현 배우의 &lt;안녕 드라큘라&gt;🧛‍\n2월 17일, 18일 밤 9시 30분에 만나요\nhttps://t.co/79eLrJZ6BW",
+		"full_text": "[#서현] 올 2월 강추위 이기는 방법 : 롱패딩, 핫팩 그리고 서현 배우의 &lt;안녕 드라큘라&gt;🧛\n2월 17일, 18일 밤 9시 30분에 만나요\nhttps://t.co/79eLrJZ6BW",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -141,7 +236,7 @@ Grailbird.data.tweets_2020_02 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "[#서현] 올 2월 강추위 이기는 방법 : 롱패딩, 핫팩 그리고 서현 배우의 &lt;안녕 드라큘라&gt;🧛‍\n2월 17일, 18일 밤 9시 30분에 만나요\nhttps://t.co/79eLrJZ6BW"
+		"text": "[#서현] 올 2월 강추위 이기는 방법 : 롱패딩, 핫팩 그리고 서현 배우의 &lt;안녕 드라큘라&gt;🧛\n2월 17일, 18일 밤 9시 30분에 만나요\nhttps://t.co/79eLrJZ6BW"
 	},
 	{
 		"created_at": "Sun Feb 02 17:18:28 +0000 2020",
