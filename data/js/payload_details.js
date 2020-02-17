3e03bf49b3672281d1567488bff6224bbd18382b
SIGNO <iamsigno@users.noreply.github.com>
2020-02-17 11:07:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26801,
-	"created_at": "Sun Feb 16 12:07:49 +0000 2020",
+	"tweets": 26802,
+	"created_at": "Mon Feb 17 02:07:49 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,128 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Mon Feb 17 01:44:56 +0000 2020",
+		"id": 1229219628737536000,
+		"id_str": "1229220153017208836",
+		"full_text": "#안녕드라큘라  D-day🖤\n오늘 밤 9시30분 JTBC에서 만나쟌나☺️\n#지안나 #고딩쟌나 #설레쟌나 https://t.co/d39KuSGIQX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			81
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "안녕드라큘라",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"text": "지안나",
+					"indices": [
+						41,
+						45
+					]
+				},
+				{
+					"text": "고딩쟌나",
+					"indices": [
+						46,
+						51
+					]
+				},
+				{
+					"text": "설레쟌나",
+					"indices": [
+						52,
+						57
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/d39KuSGIQX",
+					"expanded_url": "https://www.instagram.com/p/B8pmOizB8n1/?igshid=1w0vuxacx8j5u",
+					"display_url": "instagram.com/p/B8pmOizB8n1/…",
+					"indices": [
+						58,
+						81
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://instagram.com\" rel=\"nofollow\">Instagram</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2173631072,
+			"id_str": "2173631072",
+			"name": "서현(서주현)",
+			"screen_name": "sjhsjh0628",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1394913,
+			"friends_count": 31,
+			"listed_count": 3333,
+			"created_at": "Mon Nov 04 08:58:47 +0000 2013",
+			"favourites_count": 6,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1228,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1146333352682647552/0h6dsbYz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1146333352682647552/0h6dsbYz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2173631072/1526914876",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 175,
+		"favorite_count": 347,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#안녕드라큘라  D-day🖤\n오늘 밤 9시30분 JTBC에서 만나쟌나☺️\n#지안나 #고딩쟌나 #설레쟌나 https://t.co/d39KuSGIQX"
+	},
+	{
 		"created_at": "Sun Feb 16 11:44:57 +0000 2020",
 		"id": 1229000088246202400,
 		"id_str": "1229008760934457349",
