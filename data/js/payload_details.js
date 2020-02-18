e7858c5dc2ca8c7019d0904cfc039b7149b394a7
SIGNO <iamsigno@users.noreply.github.com>
2020-02-18 16:07:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26807,
-	"created_at": "Tue Feb 18 04:07:55 +0000 2020",
+	"tweets": 26808,
+	"created_at": "Tue Feb 18 07:07:50 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,332 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Tue Feb 18 06:41:09 +0000 2020",
+		"id": 1229647180463345700,
+		"id_str": "1229657086050607104",
+		"full_text": "서강준..우도환..많은 추측을 자아냈던👀\n&lt;본말&gt; 그놈의 정체는 바로 김바다 배우님!\n\n지금껏 꽁꽁 숨겨두었던 스틸 대방출!\n#고구마고구마고구마 #사이다~😍\n\n오감 서스펜스 #본대로말하라\n매주 토/일 밤 10시 50분 OCN 본방송\n#장혁 #최수영 #진서연 #장현성 #류승수 https://t.co/8uOQOt5vkJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			159
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "고구마고구마고구마",
+					"indices": [
+						76,
+						86
+					]
+				},
+				{
+					"text": "사이다",
+					"indices": [
+						87,
+						91
+					]
+				},
+				{
+					"text": "본대로말하라",
+					"indices": [
+						103,
+						110
+					]
+				},
+				{
+					"text": "장혁",
+					"indices": [
+						136,
+						139
+					]
+				},
+				{
+					"text": "최수영",
+					"indices": [
+						140,
+						144
+					]
+				},
+				{
+					"text": "진서연",
+					"indices": [
+						145,
+						149
+					]
+				},
+				{
+					"text": "장현성",
+					"indices": [
+						150,
+						154
+					]
+				},
+				{
+					"text": "류승수",
+					"indices": [
+						155,
+						159
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1229647170204098600,
+					"id_str": "1229647170204098560",
+					"indices": [
+						160,
+						183
+					],
+					"media_url": "http://pbs.twimg.com/media/ERCVlhsUYAAdIKn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERCVlhsUYAAdIKn.jpg",
+					"url": "https://t.co/8uOQOt5vkJ",
+					"display_url": "pic.twitter.com/8uOQOt5vkJ",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1229647180463345664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1229647170204098600,
+					"id_str": "1229647170204098560",
+					"indices": [
+						160,
+						183
+					],
+					"media_url": "http://pbs.twimg.com/media/ERCVlhsUYAAdIKn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERCVlhsUYAAdIKn.jpg",
+					"url": "https://t.co/8uOQOt5vkJ",
+					"display_url": "pic.twitter.com/8uOQOt5vkJ",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1229647180463345664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1229647173010067500,
+					"id_str": "1229647173010067456",
+					"indices": [
+						160,
+						183
+					],
+					"media_url": "http://pbs.twimg.com/media/ERCVlsJUEAAEN3D.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERCVlsJUEAAEN3D.jpg",
+					"url": "https://t.co/8uOQOt5vkJ",
+					"display_url": "pic.twitter.com/8uOQOt5vkJ",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1229647180463345664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1229647175824461800,
+					"id_str": "1229647175824461825",
+					"indices": [
+						160,
+						183
+					],
+					"media_url": "http://pbs.twimg.com/media/ERCVl2oUUAEaoI4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERCVl2oUUAEaoI4.jpg",
+					"url": "https://t.co/8uOQOt5vkJ",
+					"display_url": "pic.twitter.com/8uOQOt5vkJ",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1229647180463345664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1229647178303295500,
+					"id_str": "1229647178303295494",
+					"indices": [
+						160,
+						183
+					],
+					"media_url": "http://pbs.twimg.com/media/ERCVl_3UUAY31dF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERCVl_3UUAY31dF.jpg",
+					"url": "https://t.co/8uOQOt5vkJ",
+					"display_url": "pic.twitter.com/8uOQOt5vkJ",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1229647180463345664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.smartpost.kr/\" rel=\"nofollow\">smartpost.kr</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 226483444,
+			"id_str": "226483444",
+			"name": "OCN ORIGINAL",
+			"screen_name": "OCN_ORIGINAL",
+			"location": "대한민국 서울",
+			"description": "토/일 밤 10:50 #장혁 #본대로말하라 \n🔜 2020 3/16 첫방송. 월화 밤 10:50 #최진혁 #루갈",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 44709,
+			"friends_count": 0,
+			"listed_count": 249,
+			"created_at": "Tue Dec 14 07:56:11 +0000 2010",
+			"favourites_count": 1595,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 14947,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1174583128486100993/b2ih4oQH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1174583128486100993/b2ih4oQH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/226483444/1580714884",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E8E8E8",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 80,
+		"favorite_count": 74,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "서강준..우도환..많은 추측을 자아냈던👀\n&lt;본말&gt; 그놈의 정체는 바로 김바다 배우님!\n\n지금껏 꽁꽁 숨겨두었던 스틸 대방출!\n#고구마고구마고구마 #사이다~😍\n\n오감 서스펜스 #본대로말하라\n매주 토/일 밤 10시 50분 OCN 본방송\n#장혁 #최수영 #진서연 #장현성 #류승수 https://t.co/8uOQOt5vkJ"
+	},
+	{
 		"created_at": "Tue Feb 18 03:44:56 +0000 2020",
 		"id": 1229610861750366200,
 		"id_str": "1229612739884175360",
