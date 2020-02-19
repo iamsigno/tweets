c11ce9524e33f5c90e691716d8a63987bbbfa0b4
SIGNO <iamsigno@users.noreply.github.com>
2020-02-19 17:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26810,
-	"created_at": "Wed Feb 19 03:07:50 +0000 2020",
+	"tweets": 26811,
+	"created_at": "Wed Feb 19 08:07:49 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Wed Feb 19 07:41:09 +0000 2020",
+		"id": 1230025472907415600,
+		"id_str": "1230034573645537281",
+		"full_text": "TAEYEON – PURPOSE ✨\n\nTAEYEON – PURPOSE MD를 지금 SMTOWN &amp;STORE에서 만나보세요. 💖\n\nhttps://t.co/8SwzZwdzFA https://t.co/ifZWKEMfNp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			99
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/8SwzZwdzFA",
+					"expanded_url": "https://bit.ly/39RO6AN",
+					"display_url": "bit.ly/39RO6AN",
+					"indices": [
+						76,
+						99
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1230025464443392000,
+					"id_str": "1230025464443392000",
+					"indices": [
+						100,
+						123
+					],
+					"media_url": "http://pbs.twimg.com/media/ERHtpJjVUAAlVS-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERHtpJjVUAAlVS-.jpg",
+					"url": "https://t.co/ifZWKEMfNp",
+					"display_url": "pic.twitter.com/ifZWKEMfNp",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1230025472907415554/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1230025464443392000,
+					"id_str": "1230025464443392000",
+					"indices": [
+						100,
+						123
+					],
+					"media_url": "http://pbs.twimg.com/media/ERHtpJjVUAAlVS-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERHtpJjVUAAlVS-.jpg",
+					"url": "https://t.co/ifZWKEMfNp",
+					"display_url": "pic.twitter.com/ifZWKEMfNp",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1230025472907415554/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1113723889245278200,
+			"id_str": "1113723889245278209",
+			"name": "SMTOWN &STORE",
+			"screen_name": "SMTOWNandSTORE",
+			"location": "",
+			"description": "SMTOWN &STORE Official Twitter Page",
+			"url": "https://t.co/Ee3t6o418Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Ee3t6o418Q",
+							"expanded_url": "http://www.SMTOWNandSTORE.com",
+							"display_url": "SMTOWNandSTORE.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 32017,
+			"friends_count": 0,
+			"listed_count": 110,
+			"created_at": "Thu Apr 04 08:44:01 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 407,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1113723889245278209/1554705745",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 207,
+		"favorite_count": 303,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "TAEYEON – PURPOSE ✨\n\nTAEYEON – PURPOSE MD를 지금 SMTOWN &amp;STORE에서 만나보세요. 💖\n\nhttps://t.co/8SwzZwdzFA https://t.co/ifZWKEMfNp"
+	},
+	{
 		"created_at": "Wed Feb 19 02:41:10 +0000 2020",
 		"id": 1229951457220120600,
 		"id_str": "1229959076865548289",
