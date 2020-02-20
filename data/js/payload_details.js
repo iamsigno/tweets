bc271f9e821d9f46fba7a2ad3932554db8cf114e
SIGNO <iamsigno@users.noreply.github.com>
2020-02-20 17:07:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26813,
-	"created_at": "Thu Feb 20 06:07:49 +0000 2020",
+	"tweets": 26814,
+	"created_at": "Thu Feb 20 08:07:50 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Thu Feb 20 07:41:09 +0000 2020",
+		"id": 1230388132471685000,
+		"id_str": "1230396961666277382",
+		"full_text": "TAEYEON ARTIST CASE ❤️\n\nTAEYEON – PURPOSE 앨범의 무드를 느낄 수 있는 아티스트 케이스가 출시되었습니다. 😊\n\n✔출시 매장 :\nSMTOWN &amp;STORE @ DDP\nSMTOWN &amp;STORE @ coexartium 2F\nSMTOWN &amp;STORE @ SM communication center B1F (배송)\nSMTOWN &amp;STORE ONLINE\nhttps://t.co/THLLxkmG5V https://t.co/TqLRLguyxc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			248
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/THLLxkmG5V",
+					"expanded_url": "http://www.SMTOWNandSTORE.com",
+					"display_url": "SMTOWNandSTORE.com",
+					"indices": [
+						225,
+						248
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1230388120442392600,
+					"id_str": "1230388120442392577",
+					"indices": [
+						249,
+						272
+					],
+					"media_url": "http://pbs.twimg.com/media/ERM3egbUUAEZisd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERM3egbUUAEZisd.jpg",
+					"url": "https://t.co/TqLRLguyxc",
+					"display_url": "pic.twitter.com/TqLRLguyxc",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1230388132471685121/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 978,
+							"h": 978,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 978,
+							"h": 978,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1230388120442392600,
+					"id_str": "1230388120442392577",
+					"indices": [
+						249,
+						272
+					],
+					"media_url": "http://pbs.twimg.com/media/ERM3egbUUAEZisd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERM3egbUUAEZisd.jpg",
+					"url": "https://t.co/TqLRLguyxc",
+					"display_url": "pic.twitter.com/TqLRLguyxc",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1230388132471685121/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 978,
+							"h": 978,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 978,
+							"h": 978,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1113723889245278200,
+			"id_str": "1113723889245278209",
+			"name": "SMTOWN &STORE",
+			"screen_name": "SMTOWNandSTORE",
+			"location": "",
+			"description": "SMTOWN &STORE Official Twitter Page",
+			"url": "https://t.co/Ee3t6o418Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Ee3t6o418Q",
+							"expanded_url": "http://www.SMTOWNandSTORE.com",
+							"display_url": "SMTOWNandSTORE.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 32213,
+			"friends_count": 0,
+			"listed_count": 111,
+			"created_at": "Thu Apr 04 08:44:01 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 409,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1113723889245278209/1554705745",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 44,
+		"favorite_count": 70,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "TAEYEON ARTIST CASE ❤️\n\nTAEYEON – PURPOSE 앨범의 무드를 느낄 수 있는 아티스트 케이스가 출시되었습니다. 😊\n\n✔출시 매장 :\nSMTOWN &amp;STORE @ DDP\nSMTOWN &amp;STORE @ coexartium 2F\nSMTOWN &amp;STORE @ SM communication center B1F (배송)\nSMTOWN &amp;STORE ONLINE\nhttps://t.co/THLLxkmG5V https://t.co/TqLRLguyxc"
+	},
+	{
 		"created_at": "Thu Feb 20 05:41:09 +0000 2020",
 		"id": 1230361141664190500,
 		"id_str": "1230366762719420416",
