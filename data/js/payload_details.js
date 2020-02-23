e85b7261d081cefb8daca66bb8cb11bd4fa47731
SIGNO <iamsigno@users.noreply.github.com>
2020-02-23 19:07:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26826,
-	"created_at": "Sun Feb 23 07:07:49 +0000 2020",
+	"tweets": 26827,
+	"created_at": "Sun Feb 23 10:07:50 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,318 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Sun Feb 23 09:41:09 +0000 2020",
+		"id": 1231504131501654000,
+		"id_str": "1231514323769548801",
+		"full_text": "👉&lt;본 대로 말하라&gt; 8화 미리보기👉\n꼭꼭 숨어라 머리카락 보일라🤫\n\n아이를 구하기 위해 뭉친 광수대 1팀👥\n그 곳에서 충격적인 것을 목격한 현재?!\n\n오감 서스펜스 #본대로말하라\n매주 토/일 밤 10시 50분 OCN 본방송\n#장혁 #최수영 #진서연 #장현성 #류승수 https://t.co/rOAW4LFfGJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			155
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "본대로말하라",
+					"indices": [
+						99,
+						106
+					]
+				},
+				{
+					"text": "장혁",
+					"indices": [
+						132,
+						135
+					]
+				},
+				{
+					"text": "최수영",
+					"indices": [
+						136,
+						140
+					]
+				},
+				{
+					"text": "진서연",
+					"indices": [
+						141,
+						145
+					]
+				},
+				{
+					"text": "장현성",
+					"indices": [
+						146,
+						150
+					]
+				},
+				{
+					"text": "류승수",
+					"indices": [
+						151,
+						155
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1231504119610761200,
+					"id_str": "1231504119610761216",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/ERcueNTUYAAQIdo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERcueNTUYAAQIdo.jpg",
+					"url": "https://t.co/rOAW4LFfGJ",
+					"display_url": "pic.twitter.com/rOAW4LFfGJ",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1231504131501654016/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1231504119610761200,
+					"id_str": "1231504119610761216",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/ERcueNTUYAAQIdo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERcueNTUYAAQIdo.jpg",
+					"url": "https://t.co/rOAW4LFfGJ",
+					"display_url": "pic.twitter.com/rOAW4LFfGJ",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1231504131501654016/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1231504122873954300,
+					"id_str": "1231504122873954304",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/ERcueZdUwAA1ZrG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERcueZdUwAA1ZrG.jpg",
+					"url": "https://t.co/rOAW4LFfGJ",
+					"display_url": "pic.twitter.com/rOAW4LFfGJ",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1231504131501654016/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1231504125914779600,
+					"id_str": "1231504125914779648",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/ERcuekyUEAABdFo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERcuekyUEAABdFo.jpg",
+					"url": "https://t.co/rOAW4LFfGJ",
+					"display_url": "pic.twitter.com/rOAW4LFfGJ",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1231504131501654016/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1231504129136054300,
+					"id_str": "1231504129136054272",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/ERcuewyU0AA1dTo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERcuewyU0AA1dTo.jpg",
+					"url": "https://t.co/rOAW4LFfGJ",
+					"display_url": "pic.twitter.com/rOAW4LFfGJ",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1231504131501654016/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.smartpost.kr/\" rel=\"nofollow\">smartpost.kr</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 226483444,
+			"id_str": "226483444",
+			"name": "OCN ORIGINAL",
+			"screen_name": "OCN_ORIGINAL",
+			"location": "대한민국 서울",
+			"description": "토/일 밤 10:50 #장혁 #본대로말하라 \n🔜 2020 3/16 첫방송. 월화 밤 10:50 #최진혁 #루갈",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 44758,
+			"friends_count": 1,
+			"listed_count": 251,
+			"created_at": "Tue Dec 14 07:56:11 +0000 2010",
+			"favourites_count": 1595,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 14980,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1174583128486100993/b2ih4oQH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1174583128486100993/b2ih4oQH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/226483444/1580714884",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E8E8E8",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 57,
+		"favorite_count": 73,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "👉&lt;본 대로 말하라&gt; 8화 미리보기👉\n꼭꼭 숨어라 머리카락 보일라🤫\n\n아이를 구하기 위해 뭉친 광수대 1팀👥\n그 곳에서 충격적인 것을 목격한 현재?!\n\n오감 서스펜스 #본대로말하라\n매주 토/일 밤 10시 50분 OCN 본방송\n#장혁 #최수영 #진서연 #장현성 #류승수 https://t.co/rOAW4LFfGJ"
+	},
+	{
 		"created_at": "Sun Feb 23 06:44:56 +0000 2020",
 		"id": 1231464906219638800,
 		"id_str": "1231469977577951232",
