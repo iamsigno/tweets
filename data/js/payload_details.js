8f13925a121ccaac5f9ab09fac99ee459681ca09
SIGNO <iamsigno@users.noreply.github.com>
2020-03-01 12:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26840,
-	"created_at": "Sun Mar 01 02:07:48 +0000 2020",
+	"tweets": 26841,
+	"created_at": "Sun Mar 01 03:07:48 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,311 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Sun Mar 01 02:41:10 +0000 2020",
+		"id": 1233935187420930000,
+		"id_str": "1233945343433244672",
+		"full_text": "\"4789....!\"\n하태식이 죽기 전 남긴 또 하나의 단서,\n5년 전 사건의 실마리가 될 것인가!🧐\n\n비하인드 스틸로 9화 복습하기\n더 많은 고화질 스틸은▶️본말_공홈에서\n\n오감 서스펜스 &lt;본 대로 말하라&gt;\n매주 토/일 밤 10시 50분 OCN 본방송\n#장혁 #최수영 #진서연 #장현성 #류승수 https://t.co/l360XBLMAR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			171
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "장혁",
+					"indices": [
+						148,
+						151
+					]
+				},
+				{
+					"text": "최수영",
+					"indices": [
+						152,
+						156
+					]
+				},
+				{
+					"text": "진서연",
+					"indices": [
+						157,
+						161
+					]
+				},
+				{
+					"text": "장현성",
+					"indices": [
+						162,
+						166
+					]
+				},
+				{
+					"text": "류승수",
+					"indices": [
+						167,
+						171
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1233935176377323500,
+					"id_str": "1233935176377323520",
+					"indices": [
+						172,
+						195
+					],
+					"media_url": "http://pbs.twimg.com/media/ER_RgVfUUAA57u8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ER_RgVfUUAA57u8.jpg",
+					"url": "https://t.co/l360XBLMAR",
+					"display_url": "pic.twitter.com/l360XBLMAR",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1233935187420930048/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1233935176377323500,
+					"id_str": "1233935176377323520",
+					"indices": [
+						172,
+						195
+					],
+					"media_url": "http://pbs.twimg.com/media/ER_RgVfUUAA57u8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ER_RgVfUUAA57u8.jpg",
+					"url": "https://t.co/l360XBLMAR",
+					"display_url": "pic.twitter.com/l360XBLMAR",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1233935187420930048/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1233935179216904200,
+					"id_str": "1233935179216904192",
+					"indices": [
+						172,
+						195
+					],
+					"media_url": "http://pbs.twimg.com/media/ER_RggEU4AAU-DR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ER_RggEU4AAU-DR.jpg",
+					"url": "https://t.co/l360XBLMAR",
+					"display_url": "pic.twitter.com/l360XBLMAR",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1233935187420930048/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1233935182371016700,
+					"id_str": "1233935182371016704",
+					"indices": [
+						172,
+						195
+					],
+					"media_url": "http://pbs.twimg.com/media/ER_Rgr0U0AAzsik.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ER_Rgr0U0AAzsik.jpg",
+					"url": "https://t.co/l360XBLMAR",
+					"display_url": "pic.twitter.com/l360XBLMAR",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1233935187420930048/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1233935185281872000,
+					"id_str": "1233935185281871872",
+					"indices": [
+						172,
+						195
+					],
+					"media_url": "http://pbs.twimg.com/media/ER_Rg2qU8AAHL5j.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ER_Rg2qU8AAHL5j.jpg",
+					"url": "https://t.co/l360XBLMAR",
+					"display_url": "pic.twitter.com/l360XBLMAR",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1233935187420930048/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.smartpost.kr/\" rel=\"nofollow\">smartpost.kr</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 226483444,
+			"id_str": "226483444",
+			"name": "OCN ORIGINAL",
+			"screen_name": "OCN_ORIGINAL",
+			"location": "대한민국 서울",
+			"description": "토/일 밤 10:50 #장혁 #본대로말하라 \n🔜 2020 3/16 첫방송. 월화 밤 10:50 #최진혁 #루갈",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 44793,
+			"friends_count": 1,
+			"listed_count": 253,
+			"created_at": "Tue Dec 14 07:56:11 +0000 2010",
+			"favourites_count": 1595,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 14993,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1174583128486100993/b2ih4oQH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1174583128486100993/b2ih4oQH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/226483444/1580714884",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E8E8E8",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 43,
+		"favorite_count": 73,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "\"4789....!\"\n하태식이 죽기 전 남긴 또 하나의 단서,\n5년 전 사건의 실마리가 될 것인가!🧐\n\n비하인드 스틸로 9화 복습하기\n더 많은 고화질 스틸은▶️본말_공홈에서\n\n오감 서스펜스 &lt;본 대로 말하라&gt;\n매주 토/일 밤 10시 50분 OCN 본방송\n#장혁 #최수영 #진서연 #장현성 #류승수 https://t.co/l360XBLMAR"
+	},
+	{
 		"created_at": "Sun Mar 01 01:17:18 +0000 2020",
 		"id": 1233924240749027300,
 		"id_str": "1233924240749027328",
