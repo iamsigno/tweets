9eb67d5dcf266eb322ea88b583bda41e70e8b1ec
SIGNO <iamsigno@users.noreply.github.com>
2020-03-05 16:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26878,
-	"created_at": "Thu Mar 05 05:07:48 +0000 2020",
+	"tweets": 26879,
+	"created_at": "Thu Mar 05 07:07:49 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,255 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Thu Mar 05 06:41:10 +0000 2020",
+		"id": 1235447004391567400,
+		"id_str": "1235455292474785793",
+		"full_text": "#TAEYEON วง #GirlsGeneration เตรียมจัดไลฟ์ ‘Happy_TAEYEON_Day’ ทาง NAVER V LIVE เนื่องในโอกาสการปล่อยเพลงใหม่ #Happy 9 มี.ค.นี้!\n\nโดย TAEYEON จะมาแนะนำเพลงใหม่ พร้อมพูดคุยเรื่องต่าง ๆ กับแฟน ๆ อย่างใกล้ชิด\n\nแล้วพบกัน 9 มี.ค.นี้ 5 โมงเย็นไทยนะคะ!!\n\nhttps://t.co/m21IsvtxMC\n\n#태연 https://t.co/zVq7ccADwc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TAEYEON",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						12,
+						28
+					]
+				},
+				{
+					"text": "Happy",
+					"indices": [
+						110,
+						116
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						273,
+						276
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/m21IsvtxMC",
+					"expanded_url": "http://facebook.com/smtruethailand/posts/2831477246900205",
+					"display_url": "facebook.com/smtruethailand…",
+					"indices": [
+						248,
+						271
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1235441453259559000,
+					"id_str": "1235441453259558917",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/ESUrdMJUEAU6LcZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESUrdMJUEAU6LcZ.jpg",
+					"url": "https://t.co/zVq7ccADwc",
+					"display_url": "pic.twitter.com/zVq7ccADwc",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1235447004391567360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1235441453259559000,
+					"id_str": "1235441453259558917",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/ESUrdMJUEAU6LcZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESUrdMJUEAU6LcZ.jpg",
+					"url": "https://t.co/zVq7ccADwc",
+					"display_url": "pic.twitter.com/zVq7ccADwc",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1235447004391567360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1235441466685579300,
+					"id_str": "1235441466685579265",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/ESUrd-KU4AEtIfK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESUrd-KU4AEtIfK.jpg",
+					"url": "https://t.co/zVq7ccADwc",
+					"display_url": "pic.twitter.com/zVq7ccADwc",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1235447004391567360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1300,
+							"h": 867,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1093456514700828700,
+			"id_str": "1093456514700828672",
+			"name": "SM True",
+			"screen_name": "SMTrueThailand",
+			"location": "Bangkok, Thailand",
+			"description": "SM True Official Twitter Page",
+			"url": "https://t.co/fe1MwqbQdx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fe1MwqbQdx",
+							"expanded_url": "https://www.facebook.com/smtruethailand",
+							"display_url": "facebook.com/smtruethailand",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 65678,
+			"friends_count": 45,
+			"listed_count": 107,
+			"created_at": "Thu Feb 07 10:28:43 +0000 2019",
+			"favourites_count": 3,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2028,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1093456514700828672/1562743620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 330,
+		"favorite_count": 150,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "th",
+		"text": "#TAEYEON วง #GirlsGeneration เตรียมจัดไลฟ์ ‘Happy_TAEYEON_Day’ ทาง NAVER V LIVE เนื่องในโอกาสการปล่อยเพลงใหม่ #Happy 9 มี.ค.นี้!\n\nโดย TAEYEON จะมาแนะนำเพลงใหม่ พร้อมพูดคุยเรื่องต่าง ๆ กับแฟน ๆ อย่างใกล้ชิด\n\nแล้วพบกัน 9 มี.ค.นี้ 5 โมงเย็นไทยนะคะ!!\n\nhttps://t.co/m21IsvtxMC\n\n#태연 https://t.co/zVq7ccADwc"
+	},
+	{
 		"created_at": "Thu Mar 05 04:58:18 +0000 2020",
 		"id": 1235429408514740200,
 		"id_str": "1235429408514740224",
