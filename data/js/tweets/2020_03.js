75b2cacfc669ed02beb61d56d3252dc62fb5b891
SIGNO <iamsigno@users.noreply.github.com>
2020-03-08 18:07:50 +0900

    Added 1 tweets

--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,226 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Sun Mar 08 08:41:09 +0000 2020",
+		"id": 1236561270477697000,
+		"id_str": "1236572654028677120",
+		"full_text": "Happy Birthday TAEYEON🎀\n\nGirls' Generation 태연 생일을 맞아,ARTIST BIRTHDAY NECKLACE를 SMTOWN &amp;STORE에서 만나보세요😊\n3월의 탄생석인 아쿠아마린 컬러 크리스탈 스톤으로 포인트를 준 제품입니다.\n\n🔸3월 탄생석 의미 : 젊음, 행복\n🔸소재 : METAL, CRYSTAL STONE\n\nhttps://t.co/hz1gfVvJx4 https://t.co/4fAoWjVxGT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			220
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/hz1gfVvJx4",
+					"expanded_url": "http://SMTOWNandSTORE.com",
+					"display_url": "SMTOWNandSTORE.com",
+					"indices": [
+						197,
+						220
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1236561243604791300,
+					"id_str": "1236561243604791296",
+					"indices": [
+						221,
+						244
+					],
+					"media_url": "http://pbs.twimg.com/media/ESkl5kQUMAAsavx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESkl5kQUMAAsavx.jpg",
+					"url": "https://t.co/4fAoWjVxGT",
+					"display_url": "pic.twitter.com/4fAoWjVxGT",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1236561270477697024/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1236561243604791300,
+					"id_str": "1236561243604791296",
+					"indices": [
+						221,
+						244
+					],
+					"media_url": "http://pbs.twimg.com/media/ESkl5kQUMAAsavx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESkl5kQUMAAsavx.jpg",
+					"url": "https://t.co/4fAoWjVxGT",
+					"display_url": "pic.twitter.com/4fAoWjVxGT",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1236561270477697024/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1236561243751600000,
+					"id_str": "1236561243751600128",
+					"indices": [
+						221,
+						244
+					],
+					"media_url": "http://pbs.twimg.com/media/ESkl5kzUUAA2Puj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESkl5kzUUAA2Puj.jpg",
+					"url": "https://t.co/4fAoWjVxGT",
+					"display_url": "pic.twitter.com/4fAoWjVxGT",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1236561270477697024/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1280,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1113723889245278200,
+			"id_str": "1113723889245278209",
+			"name": "SMTOWN &STORE",
+			"screen_name": "SMTOWNandSTORE",
+			"location": "",
+			"description": "SMTOWN &STORE Official Twitter Page",
+			"url": "https://t.co/Ee3t6o418Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Ee3t6o418Q",
+							"expanded_url": "http://www.SMTOWNandSTORE.com",
+							"display_url": "SMTOWNandSTORE.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 33679,
+			"friends_count": 0,
+			"listed_count": 112,
+			"created_at": "Thu Apr 04 08:44:01 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 433,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1113723889245278209/1554705745",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 406,
+		"favorite_count": 580,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "Happy Birthday TAEYEON🎀\n\nGirls' Generation 태연 생일을 맞아,ARTIST BIRTHDAY NECKLACE를 SMTOWN &amp;STORE에서 만나보세요😊\n3월의 탄생석인 아쿠아마린 컬러 크리스탈 스톤으로 포인트를 준 제품입니다.\n\n🔸3월 탄생석 의미 : 젊음, 행복\n🔸소재 : METAL, CRYSTAL STONE\n\nhttps://t.co/hz1gfVvJx4 https://t.co/4fAoWjVxGT"
+	},
+	{
 		"created_at": "Sun Mar 08 01:41:10 +0000 2020",
 		"id": 1236456718743134200,
 		"id_str": "1236466959060996096",
