817f6d9dce8a468d6fbf1d745de53130b31d2fd8
SIGNO <iamsigno@users.noreply.github.com>
2020-03-09 09:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26903,
-	"created_at": "Sun Mar 08 16:07:50 +0000 2020",
+	"tweets": 26904,
+	"created_at": "Mon Mar 09 00:07:49 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,318 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Sun Mar 08 23:41:10 +0000 2020",
+		"id": 1236788876162658300,
+		"id_str": "1236799148185223168",
+		"full_text": "\"그놈... 경찰이군요.\"👨‍✈️\n\n비하인드 스틸로 12화 복습하기\n더 많은 고화질 스틸은▶️본말_공홈에서\n\n오감 서스펜스 #본대로말하라 \n매주 토/일 밤 10시 50분 OCN 본방송\n#장혁 #최수영 #진서연 #장현성 #류승수 https://t.co/Va4vaOYZmt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			126
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "본대로말하라",
+					"indices": [
+						69,
+						76
+					]
+				},
+				{
+					"text": "장혁",
+					"indices": [
+						103,
+						106
+					]
+				},
+				{
+					"text": "최수영",
+					"indices": [
+						107,
+						111
+					]
+				},
+				{
+					"text": "진서연",
+					"indices": [
+						112,
+						116
+					]
+				},
+				{
+					"text": "장현성",
+					"indices": [
+						117,
+						121
+					]
+				},
+				{
+					"text": "류승수",
+					"indices": [
+						122,
+						126
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1236788864036900900,
+					"id_str": "1236788864036900864",
+					"indices": [
+						127,
+						150
+					],
+					"media_url": "http://pbs.twimg.com/media/ESn060cUEAAkZEB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESn060cUEAAkZEB.jpg",
+					"url": "https://t.co/Va4vaOYZmt",
+					"display_url": "pic.twitter.com/Va4vaOYZmt",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1236788876162658306/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1236788864036900900,
+					"id_str": "1236788864036900864",
+					"indices": [
+						127,
+						150
+					],
+					"media_url": "http://pbs.twimg.com/media/ESn060cUEAAkZEB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESn060cUEAAkZEB.jpg",
+					"url": "https://t.co/Va4vaOYZmt",
+					"display_url": "pic.twitter.com/Va4vaOYZmt",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1236788876162658306/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1236788867417563100,
+					"id_str": "1236788867417563137",
+					"indices": [
+						127,
+						150
+					],
+					"media_url": "http://pbs.twimg.com/media/ESn07BCU4AEkC3d.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESn07BCU4AEkC3d.jpg",
+					"url": "https://t.co/Va4vaOYZmt",
+					"display_url": "pic.twitter.com/Va4vaOYZmt",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1236788876162658306/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1236788870210977800,
+					"id_str": "1236788870210977792",
+					"indices": [
+						127,
+						150
+					],
+					"media_url": "http://pbs.twimg.com/media/ESn07LcVAAAxA4m.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESn07LcVAAAxA4m.jpg",
+					"url": "https://t.co/Va4vaOYZmt",
+					"display_url": "pic.twitter.com/Va4vaOYZmt",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1236788876162658306/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1236788873025339400,
+					"id_str": "1236788873025339392",
+					"indices": [
+						127,
+						150
+					],
+					"media_url": "http://pbs.twimg.com/media/ESn07V7UwAAcmor.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESn07V7UwAAcmor.jpg",
+					"url": "https://t.co/Va4vaOYZmt",
+					"display_url": "pic.twitter.com/Va4vaOYZmt",
+					"expanded_url": "https://twitter.com/OCN_ORIGINAL/status/1236788876162658306/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 720,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.smartpost.kr/\" rel=\"nofollow\">smartpost.kr</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 226483444,
+			"id_str": "226483444",
+			"name": "OCN ORIGINAL",
+			"screen_name": "OCN_ORIGINAL",
+			"location": "대한민국 서울",
+			"description": "🔜 2020 3/28 첫방송. 토일 밤 10:50 #루갈\n⚠️🎥coming soon.. #번외수사 #트레인 #써치",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 44835,
+			"friends_count": 1,
+			"listed_count": 251,
+			"created_at": "Tue Dec 14 07:56:11 +0000 2010",
+			"favourites_count": 1596,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 15015,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1234360771728965632/RQw40aYk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1234360771728965632/RQw40aYk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/226483444/1583151463",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E8E8E8",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 16,
+		"favorite_count": 29,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "\"그놈... 경찰이군요.\"👨‍✈️\n\n비하인드 스틸로 12화 복습하기\n더 많은 고화질 스틸은▶️본말_공홈에서\n\n오감 서스펜스 #본대로말하라 \n매주 토/일 밤 10시 50분 OCN 본방송\n#장혁 #최수영 #진서연 #장현성 #류승수 https://t.co/Va4vaOYZmt"
+	},
+	{
 		"created_at": "Sun Mar 08 15:43:01 +0000 2020",
 		"id": 1236678819488182300,
 		"id_str": "1236678819488182272",
