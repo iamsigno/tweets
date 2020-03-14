a00292318b2e52f7d05c81bb38183ca040c4ed5b
SIGNO <iamsigno@users.noreply.github.com>
2020-03-15 00:07:51 +0900

    Added 1 tweets

--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,197 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Sat Mar 14 14:18:31 +0000 2020",
+		"id": 1238789376697483300,
+		"id_str": "1238831880184549376",
+		"full_text": "저번주에 맞은 뻥튀기를 품고 온 태연 https://t.co/sQRjWh4hWe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			20
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1238789054000324600,
+					"id_str": "1238789054000324611",
+					"indices": [
+						21,
+						44
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1238789054000324611/pu/img/SWdWbrEskhHsyra1.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1238789054000324611/pu/img/SWdWbrEskhHsyra1.jpg",
+					"url": "https://t.co/sQRjWh4hWe",
+					"display_url": "pic.twitter.com/sQRjWh4hWe",
+					"expanded_url": "https://twitter.com/rkskchKo4/status/1238789376697483264/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1238789054000324600,
+					"id_str": "1238789054000324611",
+					"indices": [
+						21,
+						44
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1238789054000324611/pu/img/SWdWbrEskhHsyra1.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1238789054000324611/pu/img/SWdWbrEskhHsyra1.jpg",
+					"url": "https://t.co/sQRjWh4hWe",
+					"display_url": "pic.twitter.com/sQRjWh4hWe",
+					"expanded_url": "https://twitter.com/rkskchKo4/status/1238789376697483264/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 31333,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1238789054000324611/pu/vid/1280x720/s3nGBzrRcF9Tb2sp.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1238789054000324611/pu/pl/7wtCg9ZQpnOLhCgd.m3u8?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1238789054000324611/pu/vid/480x270/d958mDKwj2yqJMz2.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1238789054000324611/pu/vid/640x360/pq9noGxChzXx3pfb.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1173855597453701000,
+			"id_str": "1173855597453701120",
+			"name": "ㄹㅋㅇㅂ",
+			"screen_name": "rkskchKo4",
+			"location": "",
+			"description": "'룰렷 아카이브' 라는 뜻 @I_Demeanor",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 35357,
+			"friends_count": 0,
+			"listed_count": 126,
+			"created_at": "Tue Sep 17 07:06:04 +0000 2019",
+			"favourites_count": 7,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1211,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+			"profile_image_url_https": "https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": true,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1792,
+		"favorite_count": 1215,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "저번주에 맞은 뻥튀기를 품고 온 태연 https://t.co/sQRjWh4hWe"
+	},
+	{
 		"created_at": "Sat Mar 14 09:41:10 +0000 2020",
 		"id": 1238751844102361000,
 		"id_str": "1238762082084032512",
