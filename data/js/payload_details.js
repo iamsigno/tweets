57b3cb6b2bdf62d6adbdf7fc12506877f3b07c51
SIGNO <iamsigno@users.noreply.github.com>
2020-03-27 19:07:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26946,
-	"created_at": "Thu Mar 26 12:07:50 +0000 2020",
+	"tweets": 26947,
+	"created_at": "Fri Mar 27 10:07:50 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Fri Mar 27 09:44:56 +0000 2020",
+		"id": 1243463256410226700,
+		"id_str": "1243474075835654146",
+		"full_text": "日本政府より発表されました入国者抑制策の方針、また新型コロナウイルス感染症拡大の影響を鑑み、「TAEYEON JAPAN TOUR 2020（仮）」の十分な準備期間を持つことができない為やむなく現状予定しておりました公演の開催を一旦見送らせて頂くことになりました。\n▼詳細はこちら\nhttps://t.co/2W5Sj6BZM3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			164
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2W5Sj6BZM3",
+					"expanded_url": "http://taeyeon.girls-generation.jp/news/japantour2020info",
+					"display_url": "taeyeon.girls-generation.jp/news/japantour…",
+					"indices": [
+						141,
+						164
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2585025200,
+			"id_str": "2585025200",
+			"name": "少女時代 JAPAN OFFICIAL",
+			"screen_name": "gg_official_jp",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/nUrgRiOH3R",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/nUrgRiOH3R",
+							"expanded_url": "http://www.girls-generation.jp/",
+							"display_url": "girls-generation.jp",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 103244,
+			"friends_count": 0,
+			"listed_count": 575,
+			"created_at": "Tue Jun 24 03:33:35 +0000 2014",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 311,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1222720577078611968/5QXkaKkC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1222720577078611968/5QXkaKkC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2585025200/1505360281",
+			"profile_link_color": "0024B3",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 926,
+		"favorite_count": 1151,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "日本政府より発表されました入国者抑制策の方針、また新型コロナウイルス感染症拡大の影響を鑑み、「TAEYEON JAPAN TOUR 2020（仮）」の十分な準備期間を持つことができない為やむなく現状予定しておりました公演の開催を一旦見送らせて頂くことになりました。\n▼詳細はこちら\nhttps://t.co/2W5Sj6BZM3"
+	},
+	{
 		"created_at": "Thu Mar 26 11:41:10 +0000 2020",
 		"id": 1243129585585098800,
 		"id_str": "1243140935283806209",
