60fff9edb056bed7899f42bf0b09a2ebf52e7884
SIGNO <iamsigno@users.noreply.github.com>
2020-04-17 15:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26984,
-	"created_at": "Thu Apr 16 10:07:49 +0000 2020",
+	"tweets": 26985,
+	"created_at": "Fri Apr 17 06:07:49 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,302 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Fri Apr 17 05:41:10 +0000 2020",
+		"id": 1251022674349809700,
+		"id_str": "1251022871083827206",
+		"full_text": "청순 서현도 좋고...🤔 시크 서현도 좋고...🤭\n우선 순위를 정할 수가 없을 만큼 서현 배우는 다 옳아현👍\n#서현 #Seohyun https://t.co/JHUv8qRPA1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			73
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "서현",
+					"indices": [
+						61,
+						64
+					]
+				},
+				{
+					"text": "Seohyun",
+					"indices": [
+						65,
+						73
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1251022613117202400,
+					"id_str": "1251022613117202433",
+					"indices": [
+						74,
+						97
+					],
+					"media_url": "http://pbs.twimg.com/media/EVyGcDzU8AESblw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EVyGcDzU8AESblw.jpg",
+					"url": "https://t.co/JHUv8qRPA1",
+					"display_url": "pic.twitter.com/JHUv8qRPA1",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1251022674349809670/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1909,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 943,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 534,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1251022613117202400,
+					"id_str": "1251022613117202433",
+					"indices": [
+						74,
+						97
+					],
+					"media_url": "http://pbs.twimg.com/media/EVyGcDzU8AESblw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EVyGcDzU8AESblw.jpg",
+					"url": "https://t.co/JHUv8qRPA1",
+					"display_url": "pic.twitter.com/JHUv8qRPA1",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1251022674349809670/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1909,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 943,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 534,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1251022612861345800,
+					"id_str": "1251022612861345793",
+					"indices": [
+						74,
+						97
+					],
+					"media_url": "http://pbs.twimg.com/media/EVyGcC2U4AEZ72P.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EVyGcC2U4AEZ72P.jpg",
+					"url": "https://t.co/JHUv8qRPA1",
+					"display_url": "pic.twitter.com/JHUv8qRPA1",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1251022674349809670/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1527,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 943,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 534,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1251022612190204000,
+					"id_str": "1251022612190203905",
+					"indices": [
+						74,
+						97
+					],
+					"media_url": "http://pbs.twimg.com/media/EVyGcAWUEAEP05z.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EVyGcAWUEAEP05z.jpg",
+					"url": "https://t.co/JHUv8qRPA1",
+					"display_url": "pic.twitter.com/JHUv8qRPA1",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1251022674349809670/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 534,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 943,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1909,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1251022612093735000,
+					"id_str": "1251022612093734912",
+					"indices": [
+						74,
+						97
+					],
+					"media_url": "http://pbs.twimg.com/media/EVyGb__UEAAdZqW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EVyGb__UEAAdZqW.jpg",
+					"url": "https://t.co/JHUv8qRPA1",
+					"display_url": "pic.twitter.com/JHUv8qRPA1",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1251022674349809670/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 943,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1527,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 534,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정\n4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. \n📸 고화질 아니면 죽음을 달라\n🌳주접은 그저 나무가 내뿜는 산소일 뿐\ninstagram: @namooactors\nyoutube: namooofficial",
+			"url": "https://t.co/pDrppn6MXI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pDrppn6MXI",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 29039,
+			"friends_count": 11,
+			"listed_count": 218,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 123,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 12862,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 217,
+		"favorite_count": 187,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "청순 서현도 좋고...🤔 시크 서현도 좋고...🤭\n우선 순위를 정할 수가 없을 만큼 서현 배우는 다 옳아현👍\n#서현 #Seohyun https://t.co/JHUv8qRPA1"
+	},
+	{
 		"created_at": "Thu Apr 16 09:28:10 +0000 2020",
 		"id": 1250717611714904000,
 		"id_str": "1250717611714904064",
