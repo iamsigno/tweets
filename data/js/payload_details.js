b15855fd7079ef9698c9745c963ea5c8d332d4c6
SIGNO <iamsigno@users.noreply.github.com>
2020-04-29 17:07:52 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27010,
-	"created_at": "Wed Apr 29 07:07:50 +0000 2020",
+	"tweets": 27011,
+	"created_at": "Wed Apr 29 08:07:50 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,205 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Wed Apr 29 07:41:10 +0000 2020",
+		"id": 1255400570631917600,
+		"id_str": "1255401725038559238",
+		"full_text": "นักร้องสุดน่ารักที่แฟนเพลงเชื่อใจ #TAEYEON จะปล่อยเพลงใหม่ #Happy 4 พ.ค.นี้ 4 โมงเย็นไทย!\n\n📌ไลฟ์ฉลองการปล่อยเพลงใหม่ จะมีขึ้นในวันเดียวกัน 5 โมงเย็นไทย ทาง NAVER V LIVE \n\nแล้วมารอรับฟังเพลงปลอบโยนสุดพิเศษที่เป็นของขวัญสำหรับแฟน ๆ อย่าง Happy ไปด้วยกัน!💜\n\nhttps://t.co/U0eOJqgsMy https://t.co/ZvZsOKs3Ki",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TAEYEON",
+					"indices": [
+						34,
+						42
+					]
+				},
+				{
+					"text": "Happy",
+					"indices": [
+						59,
+						65
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/U0eOJqgsMy",
+					"expanded_url": "http://facebook.com/212568845457738/posts/2957651750949420/",
+					"display_url": "facebook.com/21256884545773…",
+					"indices": [
+						255,
+						278
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1255400556442595300,
+					"id_str": "1255400556442595329",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/EWwUJ5mWsAEYSzI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EWwUJ5mWsAEYSzI.jpg",
+					"url": "https://t.co/ZvZsOKs3Ki",
+					"display_url": "pic.twitter.com/ZvZsOKs3Ki",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1255400570631917568/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 682,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 682,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1255400556442595300,
+					"id_str": "1255400556442595329",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/EWwUJ5mWsAEYSzI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EWwUJ5mWsAEYSzI.jpg",
+					"url": "https://t.co/ZvZsOKs3Ki",
+					"display_url": "pic.twitter.com/ZvZsOKs3Ki",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1255400570631917568/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 682,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 682,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1093456514700828700,
+			"id_str": "1093456514700828672",
+			"name": "SM True",
+			"screen_name": "SMTrueThailand",
+			"location": "Bangkok, Thailand",
+			"description": "SM True Official Twitter Page",
+			"url": "https://t.co/fe1MwqbQdx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fe1MwqbQdx",
+							"expanded_url": "https://www.facebook.com/smtruethailand",
+							"display_url": "facebook.com/smtruethailand",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 70577,
+			"friends_count": 45,
+			"listed_count": 128,
+			"created_at": "Thu Feb 07 10:28:43 +0000 2019",
+			"favourites_count": 3,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2376,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1093456514700828672/1562743620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 81,
+		"favorite_count": 23,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "th",
+		"text": "นักร้องสุดน่ารักที่แฟนเพลงเชื่อใจ #TAEYEON จะปล่อยเพลงใหม่ #Happy 4 พ.ค.นี้ 4 โมงเย็นไทย!\n\n📌ไลฟ์ฉลองการปล่อยเพลงใหม่ จะมีขึ้นในวันเดียวกัน 5 โมงเย็นไทย ทาง NAVER V LIVE \n\nแล้วมารอรับฟังเพลงปลอบโยนสุดพิเศษที่เป็นของขวัญสำหรับแฟน ๆ อย่าง Happy ไปด้วยกัน!💜\n\nhttps://t.co/U0eOJqgsMy https://t.co/ZvZsOKs3Ki"
+	},
+	{
 		"created_at": "Wed Apr 29 06:59:18 +0000 2020",
 		"id": 1255391189362192400,
 		"id_str": "1255391189362192386",
