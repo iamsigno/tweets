8a23b36094cd0f1ac4889e5a669bebb2ec9a2879
SIGNO <iamsigno@users.noreply.github.com>
2020-05-04 22:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27051,
-	"created_at": "Mon May 04 12:07:49 +0000 2020",
+	"tweets": 27052,
+	"created_at": "Mon May 04 13:07:49 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,233 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Mon May 04 12:46:29 +0000 2020",
+		"id": 1257290502094889000,
+		"id_str": "1257290502094888961",
+		"full_text": "#TAEYEON #태연\n[MV]Happy (03:46)\nhttps://t.co/qNcRCHk4J5\n#Melon #멜론 #비디오 #멜론비디오 https://t.co/Zq7iUxPTFY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			77
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TAEYEON",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						9,
+						12
+					]
+				},
+				{
+					"text": "Melon",
+					"indices": [
+						55,
+						61
+					]
+				},
+				{
+					"text": "멜론",
+					"indices": [
+						62,
+						65
+					]
+				},
+				{
+					"text": "비디오",
+					"indices": [
+						66,
+						70
+					]
+				},
+				{
+					"text": "멜론비디오",
+					"indices": [
+						71,
+						77
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/qNcRCHk4J5",
+					"expanded_url": "https://vod.melon.com/video/detail2.htm?mvId=50219869&menuId=27040101",
+					"display_url": "vod.melon.com/video/detail2.…",
+					"indices": [
+						31,
+						54
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1257290501067362300,
+					"id_str": "1257290501067362304",
+					"indices": [
+						78,
+						101
+					],
+					"media_url": "http://pbs.twimg.com/media/EXLLDJ-X0AAo1Su.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EXLLDJ-X0AAo1Su.jpg",
+					"url": "https://t.co/Zq7iUxPTFY",
+					"display_url": "pic.twitter.com/Zq7iUxPTFY",
+					"expanded_url": "https://twitter.com/IAMSIGNO/status/1257290502094888961/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1257290501067362300,
+					"id_str": "1257290501067362304",
+					"indices": [
+						78,
+						101
+					],
+					"media_url": "http://pbs.twimg.com/media/EXLLDJ-X0AAo1Su.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EXLLDJ-X0AAo1Su.jpg",
+					"url": "https://t.co/Zq7iUxPTFY",
+					"display_url": "pic.twitter.com/Zq7iUxPTFY",
+					"expanded_url": "https://twitter.com/IAMSIGNO/status/1257290502094888961/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 270405347,
+			"id_str": "270405347",
+			"name": "SIGNO",
+			"screen_name": "IAMSIGNO",
+			"location": "",
+			"description": "소녀시대(Girls' Generation) S❤NE\n@GirlsGeneration @gg_official_jp\n@Sunnyday515 @sjhsjh0628 @tiffanyyoung @Hyoyeon_djhyo @sychoiofficial",
+			"url": "https://t.co/Y6UBFaq0CK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Y6UBFaq0CK",
+							"expanded_url": "http://girlsgeneration.xyz/",
+							"display_url": "girlsgeneration.xyz",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 529,
+			"friends_count": 461,
+			"listed_count": 6,
+			"created_at": "Tue Mar 22 15:18:36 +0000 2011",
+			"favourites_count": 1906,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 26906,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1201460639044005888/RTQPfRgI_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1201460639044005888/RTQPfRgI_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/270405347/1578714265",
+			"profile_link_color": "777777",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDE9D1",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "et",
+		"text": "#TAEYEON #태연\n[MV]Happy (03:46)\nhttps://t.co/qNcRCHk4J5\n#Melon #멜론 #비디오 #멜론비디오 https://t.co/Zq7iUxPTFY"
+	},
+	{
 		"created_at": "Mon May 04 12:02:52 +0000 2020",
 		"id": 1257279523621609500,
 		"id_str": "1257279523621609473",
