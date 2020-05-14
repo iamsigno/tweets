9bf6b4765a382017bd852d02b6d4347817bd8ac3
SIGNO <iamsigno@users.noreply.github.com>
2020-05-15 00:07:53 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27111,
-	"created_at": "Thu May 14 10:07:50 +0000 2020",
+	"tweets": 27112,
+	"created_at": "Thu May 14 15:07:52 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,10 +1,103 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Thu May 14 14:44:57 +0000 2020",
+		"id": 1260934423597289500,
+		"id_str": "1260944192471289856",
+		"full_text": "현night🖤 https://t.co/lVLP3MEIbd",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			31
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/lVLP3MEIbd",
+					"expanded_url": "https://www.instagram.com/p/CAK8cw1grnE/?igshid=11ahtbczqsfla",
+					"display_url": "instagram.com/p/CAK8cw1grnE/…",
+					"indices": [
+						8,
+						31
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://instagram.com\" rel=\"nofollow\">Instagram</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2173631072,
+			"id_str": "2173631072",
+			"name": "서현(서주현)",
+			"screen_name": "sjhsjh0628",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1389028,
+			"friends_count": 31,
+			"listed_count": 2884,
+			"created_at": "Mon Nov 04 08:58:47 +0000 2013",
+			"favourites_count": 6,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1240,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1146333352682647552/0h6dsbYz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1146333352682647552/0h6dsbYz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2173631072/1526914876",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 334,
+		"favorite_count": 707,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "현night🖤 https://t.co/lVLP3MEIbd"
+	},
+	{
 		"created_at": "Thu May 14 09:41:19 +0000 2020",
 		"id": 1260843131680878600,
 		"id_str": "1260867780464775168",
-		"full_text": "From. #HYOYEON 💌\n\n#SMTOWN 여러분 오랜만이죠?🙋🏻‍♀️\n오늘 밤 오후 9시 30분 첫 방송되는 '굿걸 : 누가 방송국을 털었나'에서 만나요!💖\n\nHello SMTOWN friends, long time no see!🙋🏻‍♀️\nCheck me out at '굿걸 : 누가 방송국을 털었나' today at 9:30 PM KST!💖\n\n#효연 #소녀시대 #GirlsGeneration #굿걸 https://t.co/TfZBnoAydE",
+		"full_text": "From. #HYOYEON 💌\n\n#SMTOWN 여러분 오랜만이죠?🙋🏻♀️\n오늘 밤 오후 9시 30분 첫 방송되는 '굿걸 : 누가 방송국을 털었나'에서 만나요!💖\n\nHello SMTOWN friends, long time no see!🙋🏻♀️\nCheck me out at '굿걸 : 누가 방송국을 털었나' today at 9:30 PM KST!💖\n\n#효연 #소녀시대 #GirlsGeneration #굿걸 https://t.co/TfZBnoAydE",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -215,7 +308,7 @@ Grailbird.data.tweets_2020_05 =
 		"retweeted": true,
 		"possibly_sensitive": true,
 		"lang": "ko",
-		"text": "From. #HYOYEON 💌\n\n#SMTOWN 여러분 오랜만이죠?🙋🏻‍♀️\n오늘 밤 오후 9시 30분 첫 방송되는 '굿걸 : 누가 방송국을 털었나'에서 만나요!💖\n\nHello SMTOWN friends, long time no see!🙋🏻‍♀️\nCheck me out at '굿걸 : 누가 방송국을 털었나' today at 9:30 PM KST!💖\n\n#효연 #소녀시대 #GirlsGeneration #굿걸 https://t.co/TfZBnoAydE"
+		"text": "From. #HYOYEON 💌\n\n#SMTOWN 여러분 오랜만이죠?🙋🏻♀️\n오늘 밤 오후 9시 30분 첫 방송되는 '굿걸 : 누가 방송국을 털었나'에서 만나요!💖\n\nHello SMTOWN friends, long time no see!🙋🏻♀️\nCheck me out at '굿걸 : 누가 방송국을 털었나' today at 9:30 PM KST!💖\n\n#효연 #소녀시대 #GirlsGeneration #굿걸 https://t.co/TfZBnoAydE"
 	},
 	{
 		"created_at": "Thu May 14 09:41:17 +0000 2020",
