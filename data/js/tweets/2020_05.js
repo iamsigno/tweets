446ab0e1f98f24c3341bda362a58270cd1634a1b
SIGNO <iamsigno@users.noreply.github.com>
2020-05-24 22:07:52 +0900

    Added 1 tweets

--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,179 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sun May 24 12:43:27 +0000 2020",
+		"id": 1264507058717679600,
+		"id_str": "1264537493786910722",
+		"full_text": "https://t.co/YKRihKkQa7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1264507057014743000,
+					"id_str": "1264507057014743042",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/EYxud_1WoAI5m11.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EYxud_1WoAI5m11.jpg",
+					"url": "https://t.co/YKRihKkQa7",
+					"display_url": "pic.twitter.com/YKRihKkQa7",
+					"expanded_url": "https://twitter.com/yoongstagrm/status/1264507058717679616/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1125,
+							"h": 1125,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1125,
+							"h": 1125,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1264507057014743000,
+					"id_str": "1264507057014743042",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/EYxud_1WoAI5m11.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EYxud_1WoAI5m11.jpg",
+					"url": "https://t.co/YKRihKkQa7",
+					"display_url": "pic.twitter.com/YKRihKkQa7",
+					"expanded_url": "https://twitter.com/yoongstagrm/status/1264507058717679616/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1125,
+							"h": 1125,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1125,
+							"h": 1125,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://yoona.soshistagram.com\" rel=\"nofollow\">Yoongstagram</a>",
+		"in_reply_to_status_id": 1264507055102136300,
+		"in_reply_to_status_id_str": "1264507055102136320",
+		"in_reply_to_user_id": 2446652564,
+		"in_reply_to_user_id_str": "2446652564",
+		"in_reply_to_screen_name": "yoongstagrm",
+		"user": {
+			"id": 2446652564,
+			"id_str": "2446652564",
+			"name": "Yoongstagram",
+			"screen_name": "yoongstagrm",
+			"location": "",
+			"description": "Bot tweeting Yoona's updates and comments on Instagram. Built by @thegoguma. Also @yulstagrm @seostagrm @miyoungstagram @syoungstagram @sicafeed",
+			"url": "https://t.co/4tDvSU72JC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4tDvSU72JC",
+							"expanded_url": "https://yoongstagrm.tumblr.com/",
+							"display_url": "yoongstagrm.tumblr.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 39536,
+			"friends_count": 0,
+			"listed_count": 498,
+			"created_at": "Wed Apr 16 06:13:24 +0000 2014",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1410,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/648018860801585153/Uv9Gqcpc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/648018860801585153/Uv9Gqcpc_normal.jpg",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 228,
+		"favorite_count": 223,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/YKRihKkQa7"
+	},
+	{
 		"created_at": "Sun May 24 08:07:38 +0000 2020",
 		"id": 1264082439556575200,
 		"id_str": "1264468082870808577",
