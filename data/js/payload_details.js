2e4d922189a997ce6b62db0421c06ff74257c79c
SIGNO <iamsigno@users.noreply.github.com>
2020-06-01 17:07:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27190,
-	"created_at": "Sat May 30 14:07:51 +0000 2020",
+	"tweets": 27191,
+	"created_at": "Mon Jun 01 08:07:49 +0000 2020",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2020_06.js
@@ -0,0 +1,255 @@
+Grailbird.data.tweets_2020_06 = 
+[
+	{
+		"created_at": "Mon Jun 01 07:37:53 +0000 2020",
+		"id": 1266720898339401700,
+		"id_str": "1267359700418347008",
+		"full_text": "Did you guys have fun with DJ YOONA at ‘YOONA’s ♡NE Day Radio_90.530MHz’?🥰\nThank you all for being with me today!🦌💛\n\n#YOONA #윤아 #소녀시대 #GirlsGeneration\n#윤아의원데이라디오 #디제이융디 #융디 #융프로디테 #꽃사슴 #최고야 #사랑해 https://t.co/bgb1x1FBXY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			194
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "YOONA",
+					"indices": [
+						117,
+						123
+					]
+				},
+				{
+					"text": "윤아",
+					"indices": [
+						124,
+						127
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						128,
+						133
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						134,
+						150
+					]
+				},
+				{
+					"text": "윤아의원데이라디오",
+					"indices": [
+						151,
+						161
+					]
+				},
+				{
+					"text": "디제이융디",
+					"indices": [
+						162,
+						168
+					]
+				},
+				{
+					"text": "융디",
+					"indices": [
+						169,
+						172
+					]
+				},
+				{
+					"text": "융프로디테",
+					"indices": [
+						173,
+						179
+					]
+				},
+				{
+					"text": "꽃사슴",
+					"indices": [
+						180,
+						184
+					]
+				},
+				{
+					"text": "최고야",
+					"indices": [
+						185,
+						189
+					]
+				},
+				{
+					"text": "사랑해",
+					"indices": [
+						190,
+						194
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1266720889241919500,
+					"id_str": "1266720889241919488",
+					"indices": [
+						195,
+						218
+					],
+					"media_url": "http://pbs.twimg.com/media/EZRL7_jU4AAwqGX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZRL7_jU4AAwqGX.jpg",
+					"url": "https://t.co/bgb1x1FBXY",
+					"display_url": "pic.twitter.com/bgb1x1FBXY",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1266720898339401735/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1266720889241919500,
+					"id_str": "1266720889241919488",
+					"indices": [
+						195,
+						218
+					],
+					"media_url": "http://pbs.twimg.com/media/EZRL7_jU4AAwqGX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZRL7_jU4AAwqGX.jpg",
+					"url": "https://t.co/bgb1x1FBXY",
+					"display_url": "pic.twitter.com/bgb1x1FBXY",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1266720898339401735/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 884378714,
+			"id_str": "884378714",
+			"name": "SMTOWN",
+			"screen_name": "SMTOWNGLOBAL",
+			"location": "",
+			"description": "SMEntertainment Group Official Twitter",
+			"url": "https://t.co/vbjfZGuM9Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vbjfZGuM9Y",
+							"expanded_url": "http://www.smtown.com",
+							"display_url": "smtown.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8105560,
+			"friends_count": 49,
+			"listed_count": 7912,
+			"created_at": "Tue Oct 16 12:49:45 +0000 2012",
+			"favourites_count": 883,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 7818,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "F08BA0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/925281250792824832/dfj3nYUe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/925281250792824832/dfj3nYUe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/884378714/1509443118",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1998,
+		"favorite_count": 7936,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": true,
+		"lang": "en",
+		"text": "Did you guys have fun with DJ YOONA at ‘YOONA’s ♡NE Day Radio_90.530MHz’?🥰\nThank you all for being with me today!🦌💛\n\n#YOONA #윤아 #소녀시대 #GirlsGeneration\n#윤아의원데이라디오 #디제이융디 #융디 #융프로디테 #꽃사슴 #최고야 #사랑해 https://t.co/bgb1x1FBXY"
+	}
+]
