44c9d4fa2432c82093c7758bcf7b0a72a3ff5d86
SIGNO <iamsigno@users.noreply.github.com>
2020-06-02 09:07:49 +0900

    Added 1 tweets

--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,238 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Tue Jun 02 00:07:09 +0000 2020",
+		"id": 1267608397416824800,
+		"id_str": "1267608655446376455",
+		"full_text": "🔈#어퓨오프라인매장 #태연마스카라 입점!\n태연 포토카드 네 번째 게이트가 오픈되었습니다.\n\n♥어퓨 오프라인 매장에서 본투비 매드프루프 라인\n1개 구매 시 #태연 포토카드 1장 랜덤증정(1:1증정)\n➡선착순 한정수량\n➡ 매드멜팅 아이 리무버패드 제외 https://t.co/UfZbUDZVjN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			138
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "어퓨오프라인매장",
+					"indices": [
+						1,
+						10
+					]
+				},
+				{
+					"text": "태연마스카라",
+					"indices": [
+						11,
+						18
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						86,
+						89
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1267607830875435000,
+					"id_str": "1267607830875435008",
+					"indices": [
+						139,
+						162
+					],
+					"media_url": "http://pbs.twimg.com/media/EZdymynVcAAryiE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZdymynVcAAryiE.jpg",
+					"url": "https://t.co/UfZbUDZVjN",
+					"display_url": "pic.twitter.com/UfZbUDZVjN",
+					"expanded_url": "https://twitter.com/Apieu_official/status/1267608397416824833/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1267607830875435000,
+					"id_str": "1267607830875435008",
+					"indices": [
+						139,
+						162
+					],
+					"media_url": "http://pbs.twimg.com/media/EZdymynVcAAryiE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZdymynVcAAryiE.jpg",
+					"url": "https://t.co/UfZbUDZVjN",
+					"display_url": "pic.twitter.com/UfZbUDZVjN",
+					"expanded_url": "https://twitter.com/Apieu_official/status/1267608397416824833/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1267607802781958100,
+					"id_str": "1267607802781958144",
+					"indices": [
+						139,
+						162
+					],
+					"media_url": "http://pbs.twimg.com/media/EZdylJ9VAAANE-S.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZdylJ9VAAANE-S.jpg",
+					"url": "https://t.co/UfZbUDZVjN",
+					"display_url": "pic.twitter.com/UfZbUDZVjN",
+					"expanded_url": "https://twitter.com/Apieu_official/status/1267608397416824833/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ads-api.twitter.com\" rel=\"nofollow\">Twitter for Advertisers</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 64162171,
+			"id_str": "64162171",
+			"name": "어퓨",
+			"screen_name": "Apieu_official",
+			"location": "",
+			"description": "Everyday Beauty Challenge! / 어퓨 꿀 정보 모음 ٩( ᐖ ٩)❤️",
+			"url": "https://t.co/zB7aC2UFE9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zB7aC2UFE9",
+							"expanded_url": "http://m.beautynet.co.kr/main.do",
+							"display_url": "m.beautynet.co.kr/main.do",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7549,
+			"friends_count": 0,
+			"listed_count": 20,
+			"created_at": "Sun Aug 09 12:06:45 +0000 2009",
+			"favourites_count": 214,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 364,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "B2DFDA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1135800543245291520/79x6yOUc_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1135800543245291520/79x6yOUc_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/64162171/1589870791",
+			"profile_link_color": "93A644",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "🔈#어퓨오프라인매장 #태연마스카라 입점!\n태연 포토카드 네 번째 게이트가 오픈되었습니다.\n\n♥어퓨 오프라인 매장에서 본투비 매드프루프 라인\n1개 구매 시 #태연 포토카드 1장 랜덤증정(1:1증정)\n➡선착순 한정수량\n➡ 매드멜팅 아이 리무버패드 제외 https://t.co/UfZbUDZVjN"
+	},
+	{
 		"created_at": "Mon Jun 01 16:11:13 +0000 2020",
 		"id": 1265601407123460000,
 		"id_str": "1267488882330750976",
