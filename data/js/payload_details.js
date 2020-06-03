658d57072a87e4bc0000eabd35ecf955796db549
SIGNO <iamsigno@users.noreply.github.com>
2020-06-03 23:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27199,
-	"created_at": "Wed Jun 03 05:07:48 +0000 2020",
+	"tweets": 27200,
+	"created_at": "Wed Jun 03 14:07:48 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,162 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Wed Jun 03 14:07:09 +0000 2020",
+		"id": 1268169129439060000,
+		"id_str": "1268182438364282880",
+		"full_text": "[TMI NEWS] ‘주인이 바뀌고 대박 난 여자 아이돌 노래 BEST 7’ 1위!\n역대급 데뷔곡으로 역대급 걸그룹 등극! \n#소녀시대 ‘#다시_만난_세계’ \n\n연예계 정보 과부하 차트쇼 TMI NEWS \n매주 수요일 저녁 8시 본방사수\n#Mnet #엠넷 #전현무 #장도연 #TMI_NEWS \nhttps://t.co/nVg6y9ROjS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			187
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "소녀시대",
+					"indices": [
+						70,
+						75
+					]
+				},
+				{
+					"text": "다시_만난_세계",
+					"indices": [
+						77,
+						86
+					]
+				},
+				{
+					"text": "Mnet",
+					"indices": [
+						133,
+						138
+					]
+				},
+				{
+					"text": "엠넷",
+					"indices": [
+						139,
+						142
+					]
+				},
+				{
+					"text": "전현무",
+					"indices": [
+						143,
+						147
+					]
+				},
+				{
+					"text": "장도연",
+					"indices": [
+						148,
+						152
+					]
+				},
+				{
+					"text": "TMI_NEWS",
+					"indices": [
+						153,
+						162
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/nVg6y9ROjS",
+					"expanded_url": "https://m.tv.naver.com/v/14111716",
+					"display_url": "m.tv.naver.com/v/14111716",
+					"indices": [
+						164,
+						187
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 128466223,
+			"id_str": "128466223",
+			"name": "Mnet",
+			"screen_name": "MnetKR",
+			"location": "Seoul, Korea",
+			"description": "Official Twitter of Mnet, Asia`s #1 Music Channel.",
+			"url": "http://t.co/596v5W1yWN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/596v5W1yWN",
+							"expanded_url": "http://www.mnet.com",
+							"display_url": "mnet.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2720910,
+			"friends_count": 21791,
+			"listed_count": 2661,
+			"created_at": "Thu Apr 01 05:53:04 +0000 2010",
+			"favourites_count": 447,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 25809,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1231746734524485633/4fxo-s0U_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1231746734524485633/4fxo-s0U_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/128466223/1583761892",
+			"profile_link_color": "E40076",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "F5DCE7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 38,
+		"favorite_count": 237,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[TMI NEWS] ‘주인이 바뀌고 대박 난 여자 아이돌 노래 BEST 7’ 1위!\n역대급 데뷔곡으로 역대급 걸그룹 등극! \n#소녀시대 ‘#다시_만난_세계’ \n\n연예계 정보 과부하 차트쇼 TMI NEWS \n매주 수요일 저녁 8시 본방사수\n#Mnet #엠넷 #전현무 #장도연 #TMI_NEWS \nhttps://t.co/nVg6y9ROjS"
+	},
+	{
 		"created_at": "Wed Jun 03 05:07:08 +0000 2020",
 		"id": 1268028417745645600,
 		"id_str": "1268046540062498816",
