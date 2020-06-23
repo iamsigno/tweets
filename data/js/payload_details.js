6a32a4cead84175268468daede744805e3f0213e
SIGNO <iamsigno@users.noreply.github.com>
2020-06-23 20:07:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27255,
-	"created_at": "Tue Jun 23 05:07:49 +0000 2020",
+	"tweets": 27256,
+	"created_at": "Tue Jun 23 11:07:50 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,148 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Tue Jun 23 10:54:05 +0000 2020",
+		"id": 1275381610171502600,
+		"id_str": "1275381610171502592",
+		"full_text": "#SOOYOUNG #수영\n[The SOOTORY #11] 댕댕이와 드라이브 갔다 왔수영🐶🚘\nhttps://t.co/gO0pupz00K\n#the_sootory #더수토리 #YouTube",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			102
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SOOYOUNG",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"text": "수영",
+					"indices": [
+						10,
+						13
+					]
+				},
+				{
+					"text": "the_sootory",
+					"indices": [
+						75,
+						87
+					]
+				},
+				{
+					"text": "더수토리",
+					"indices": [
+						88,
+						93
+					]
+				},
+				{
+					"text": "YouTube",
+					"indices": [
+						94,
+						102
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/gO0pupz00K",
+					"expanded_url": "https://youtu.be/hWvTqY55g28",
+					"display_url": "youtu.be/hWvTqY55g28",
+					"indices": [
+						51,
+						74
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 270405347,
+			"id_str": "270405347",
+			"name": "SIGNO",
+			"screen_name": "IAMSIGNO",
+			"location": "",
+			"description": "소녀시대(Girls' Generation) S❤NE\n#소녀시대 #GirlsGeneration\n@GirlsGeneration @gg_official_jp\n@Sunnyday515 @sjhsjh0628 @tiffanyyoung @Hyoyeon_djhyo @sychoiofficial",
+			"url": "https://t.co/G70nXNDpSx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/G70nXNDpSx",
+							"expanded_url": "https://iamsigno.wordpress.com/",
+							"display_url": "iamsigno.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 521,
+			"friends_count": 457,
+			"listed_count": 5,
+			"created_at": "Tue Mar 22 15:18:36 +0000 2011",
+			"favourites_count": 1907,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 27149,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1201460639044005888/RTQPfRgI_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1201460639044005888/RTQPfRgI_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/270405347/1578714265",
+			"profile_link_color": "777777",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDE9D1",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#SOOYOUNG #수영\n[The SOOTORY #11] 댕댕이와 드라이브 갔다 왔수영🐶🚘\nhttps://t.co/gO0pupz00K\n#the_sootory #더수토리 #YouTube"
+	},
+	{
 		"created_at": "Tue Jun 23 05:07:08 +0000 2020",
 		"id": 1275284024135573500,
 		"id_str": "1275294297471029249",
@@ -109,7 +251,7 @@ Grailbird.data.tweets_2020_06 =
 			"created_at": "Tue Jun 23 02:29:37 +0000 2020",
 			"id": 1275254654549414000,
 			"id_str": "1275254654549413888",
-			"full_text": "올케다방의 띵곡팔이👋🏻\n⏰PM 2:00~3:30\n\n코디분 어느 방향에 계세요,,, 그쪽으로 절 한 번 올리겠습니다 🙇🏻‍♀️🙇🏻‍♂️이렇게 잘 입혀주셔서 넘 감사합니다 ˚✧₊⁎❝᷀ົཽ≀ˍ̮ ❝᷀ົཽ⁎⁺˳✧༚\n\n▶https://t.co/4FIZeqZCqy\n☝올케팝 유튜브 채널 알림설정하면 띵곡팔이 알림 딩동 https://t.co/7aBXWYfoZX",
+			"full_text": "올케다방의 띵곡팔이👋🏻\n⏰PM 2:00~3:30\n\n코디분 어느 방향에 계세요,,, 그쪽으로 절 한 번 올리겠습니다 🙇🏻♀️🙇🏻♂️이렇게 잘 입혀주셔서 넘 감사합니다 ˚✧₊⁎❝᷀ົཽ≀ˍ̮ ❝᷀ົཽ⁎⁺˳✧༚\n\n▶https://t.co/4FIZeqZCqy\n☝올케팝 유튜브 채널 알림설정하면 띵곡팔이 알림 딩동 https://t.co/7aBXWYfoZX",
 			"truncated": false,
 			"display_text_range": [
 				0,
