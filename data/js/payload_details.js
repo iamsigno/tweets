59dc040cf6233a5dcaf4f46dac1f281dda56f945
SIGNO <iamsigno@users.noreply.github.com>
2020-06-24 12:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27258,
-	"created_at": "Wed Jun 24 02:07:50 +0000 2020",
+	"tweets": 27259,
+	"created_at": "Wed Jun 24 03:07:49 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,223 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Wed Jun 24 03:07:16 +0000 2020",
+		"id": 1275624939807289300,
+		"id_str": "1275626516685246465",
+		"full_text": "[#STATION]\n\n🎬 태연 'Happy (Summer Version)' Live Video : 2020.06.26 6PM (KST)\n\n#TAEYEON #태연 #Happy #소녀시대 #GirlsGeneration https://t.co/rqTo4mPKmN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			119
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "STATION",
+					"indices": [
+						1,
+						9
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						77,
+						85
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						86,
+						89
+					]
+				},
+				{
+					"text": "Happy",
+					"indices": [
+						90,
+						96
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						97,
+						102
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						103,
+						119
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1275615385975324700,
+					"id_str": "1275615385975324672",
+					"indices": [
+						120,
+						143
+					],
+					"media_url": "http://pbs.twimg.com/media/EbPlb18VAAAeMCs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EbPlb18VAAAeMCs.jpg",
+					"url": "https://t.co/rqTo4mPKmN",
+					"display_url": "pic.twitter.com/rqTo4mPKmN",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1275624939807289344/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 483,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1455,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 853,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1275615385975324700,
+					"id_str": "1275615385975324672",
+					"indices": [
+						120,
+						143
+					],
+					"media_url": "http://pbs.twimg.com/media/EbPlb18VAAAeMCs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EbPlb18VAAAeMCs.jpg",
+					"url": "https://t.co/rqTo4mPKmN",
+					"display_url": "pic.twitter.com/rqTo4mPKmN",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1275624939807289344/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 483,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1455,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 853,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 884378714,
+			"id_str": "884378714",
+			"name": "SMTOWN",
+			"screen_name": "SMTOWNGLOBAL",
+			"location": "",
+			"description": "SMEntertainment Group Official Twitter",
+			"url": "https://t.co/vbjfZGuM9Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vbjfZGuM9Y",
+							"expanded_url": "http://www.smtown.com",
+							"display_url": "smtown.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8107496,
+			"friends_count": 49,
+			"listed_count": 7956,
+			"created_at": "Tue Oct 16 12:49:45 +0000 2012",
+			"favourites_count": 928,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 7940,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "F08BA0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/925281250792824832/dfj3nYUe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/925281250792824832/dfj3nYUe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/884378714/1509443118",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1420,
+		"favorite_count": 2336,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": true,
+		"lang": "en",
+		"text": "[#STATION]\n\n🎬 태연 'Happy (Summer Version)' Live Video : 2020.06.26 6PM (KST)\n\n#TAEYEON #태연 #Happy #소녀시대 #GirlsGeneration https://t.co/rqTo4mPKmN"
+	},
+	{
 		"created_at": "Wed Jun 24 02:07:12 +0000 2020",
 		"id": 1275598518225211400,
 		"id_str": "1275611401860591619",
