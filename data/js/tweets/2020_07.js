3ced122508e21af36abf79a9ae0191f7c8f38783
SIGNO <iamsigno@users.noreply.github.com>
2020-07-01 15:07:50 +0900

    Added 1 tweets

---
+++ data/js/tweets/2020_07.js
@@ -0,0 +1,245 @@
+Grailbird.data.tweets_2020_07 = 
+[
+	{
+		"created_at": "Wed Jul 01 06:07:09 +0000 2020",
+		"id": 1278202286636843000,
+		"id_str": "1278208500599005186",
+		"full_text": "[#굿걸] 스타 응원 메시지 I 소녀시대 수영 &amp; WOODZ(조승연) &amp; 오마이걸 승희\n\n스타들도 즐겨보고 응원하는\n＜GOOD GIRL : 누가 방송국을 털었나＞\n\n이번주 목요일 밤 11시 마지막회 Mnet 본/방/사/수 🔥🔥🔥\n\n#수영 #소녀시대_수영 #조승연 #WOODZ #승희 #오마이걸_승희 https://t.co/VBLLOgzCpW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			174
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "굿걸",
+					"indices": [
+						1,
+						4
+					]
+				},
+				{
+					"text": "수영",
+					"indices": [
+						137,
+						140
+					]
+				},
+				{
+					"text": "소녀시대_수영",
+					"indices": [
+						141,
+						149
+					]
+				},
+				{
+					"text": "조승연",
+					"indices": [
+						150,
+						154
+					]
+				},
+				{
+					"text": "WOODZ",
+					"indices": [
+						155,
+						161
+					]
+				},
+				{
+					"text": "승희",
+					"indices": [
+						162,
+						165
+					]
+				},
+				{
+					"text": "오마이걸_승희",
+					"indices": [
+						166,
+						174
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1278201268226232300,
+					"id_str": "1278201268226232321",
+					"indices": [
+						175,
+						198
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1278201268226232321/pu/img/yAnEVQLQkxVZncJI.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1278201268226232321/pu/img/yAnEVQLQkxVZncJI.jpg",
+					"url": "https://t.co/VBLLOgzCpW",
+					"display_url": "pic.twitter.com/VBLLOgzCpW",
+					"expanded_url": "https://twitter.com/mnet_hiphop/status/1278202286636843009/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1278201268226232300,
+					"id_str": "1278201268226232321",
+					"indices": [
+						175,
+						198
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1278201268226232321/pu/img/yAnEVQLQkxVZncJI.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1278201268226232321/pu/img/yAnEVQLQkxVZncJI.jpg",
+					"url": "https://t.co/VBLLOgzCpW",
+					"display_url": "pic.twitter.com/VBLLOgzCpW",
+					"expanded_url": "https://twitter.com/mnet_hiphop/status/1278202286636843009/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 127694,
+						"variants": [
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1278201268226232321/pu/vid/720x720/pCIEGDngLt3AhPQm.mp4?tag=10"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1278201268226232321/pu/vid/320x320/mEhSXAHSiqAwsN2-.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1278201268226232321/pu/vid/480x480/8DujWttW4AX5woKj.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1278201268226232321/pu/pl/sUlzcSDkPiend1LF.m3u8?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 600923170,
+			"id_str": "600923170",
+			"name": "Mnet I GOOD GIRL : 누가 방송국을 털었나",
+			"screen_name": "mnet_hiphop",
+			"location": "",
+			"description": "#GOODGIRL : #누가방송국을털었나\n매주 목요일 밤 11시 #Mnet",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 197129,
+			"friends_count": 2536,
+			"listed_count": 209,
+			"created_at": "Wed Jun 06 12:13:36 +0000 2012",
+			"favourites_count": 3,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5552,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1254695546050109442/pjbrSVmn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1254695546050109442/pjbrSVmn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/600923170/1592214877",
+			"profile_link_color": "19CF86",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 316,
+		"favorite_count": 352,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#굿걸] 스타 응원 메시지 I 소녀시대 수영 &amp; WOODZ(조승연) &amp; 오마이걸 승희\n\n스타들도 즐겨보고 응원하는\n＜GOOD GIRL : 누가 방송국을 털었나＞\n\n이번주 목요일 밤 11시 마지막회 Mnet 본/방/사/수 🔥🔥🔥\n\n#수영 #소녀시대_수영 #조승연 #WOODZ #승희 #오마이걸_승희 https://t.co/VBLLOgzCpW"
+	}
+]
