7dc0021a905fdfd42cf699381d53e81f37db853c
SIGNO <iamsigno@users.noreply.github.com>
2020-07-16 11:07:54 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27365,
-	"created_at": "Wed Jul 15 16:07:51 +0000 2020",
+	"tweets": 27366,
+	"created_at": "Thu Jul 16 02:07:52 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,223 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Thu Jul 16 02:07:09 +0000 2020",
+		"id": 1283569768675651600,
+		"id_str": "1283583922241187841",
+		"full_text": "💜 HYO(효연) 신곡 ‘DESSERT' 경쾌한 정글 팝 장르 곡!\n\n🎶 래퍼 루피∙(여자)아이들 소연 지원사격! 매력만점 컬래버 선사!\n\n💿 7월 22일 오후 6시에 만나보세요!\n\n#HYO #효연 #DESSERT #디저트 #소녀시대 #GirlsGeneration https://t.co/RcKHTEAmeQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			147
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HYO",
+					"indices": [
+						102,
+						106
+					]
+				},
+				{
+					"text": "효연",
+					"indices": [
+						107,
+						110
+					]
+				},
+				{
+					"text": "DESSERT",
+					"indices": [
+						111,
+						119
+					]
+				},
+				{
+					"text": "디저트",
+					"indices": [
+						120,
+						124
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						125,
+						130
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						131,
+						147
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1283569747066450000,
+					"id_str": "1283569747066449920",
+					"indices": [
+						148,
+						171
+					],
+					"media_url": "http://pbs.twimg.com/media/EdAn4mJVcAAZnc0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EdAn4mJVcAAZnc0.jpg",
+					"url": "https://t.co/RcKHTEAmeQ",
+					"display_url": "pic.twitter.com/RcKHTEAmeQ",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1283569768675651584/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2000,
+							"h": 1333,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1283569747066450000,
+					"id_str": "1283569747066449920",
+					"indices": [
+						148,
+						171
+					],
+					"media_url": "http://pbs.twimg.com/media/EdAn4mJVcAAZnc0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EdAn4mJVcAAZnc0.jpg",
+					"url": "https://t.co/RcKHTEAmeQ",
+					"display_url": "pic.twitter.com/RcKHTEAmeQ",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1283569768675651584/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2000,
+							"h": 1333,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "http://t.co/6dDRF9Jo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/6dDRF9Jo",
+							"expanded_url": "http://girlsgeneration.smtown.com/",
+							"display_url": "girlsgeneration.smtown.com",
+							"indices": [
+								0,
+								20
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3542758,
+			"friends_count": 4,
+			"listed_count": 5020,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 27,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2055,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1593746984",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1652,
+		"favorite_count": 3466,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": true,
+		"lang": "ko",
+		"text": "💜 HYO(효연) 신곡 ‘DESSERT' 경쾌한 정글 팝 장르 곡!\n\n🎶 래퍼 루피∙(여자)아이들 소연 지원사격! 매력만점 컬래버 선사!\n\n💿 7월 22일 오후 6시에 만나보세요!\n\n#HYO #효연 #DESSERT #디저트 #소녀시대 #GirlsGeneration https://t.co/RcKHTEAmeQ"
+	},
+	{
 		"created_at": "Wed Jul 15 15:52:23 +0000 2020",
 		"id": 1283429212389597200,
 		"id_str": "1283429212389597185",
