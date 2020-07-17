3930a47593914902c6db0ef8caa1bc0c4354e43a
SIGNO <iamsigno@users.noreply.github.com>
2020-07-18 01:07:52 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27378,
-	"created_at": "Fri Jul 17 12:07:51 +0000 2020",
+	"tweets": 27379,
+	"created_at": "Fri Jul 17 16:07:51 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,233 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Fri Jul 17 16:07:09 +0000 2020",
+		"id": 1284140803745443800,
+		"id_str": "1284157703821156352",
+		"full_text": "HYO 4th Single ['DESSERT (Feat. Loopy, 소연 ((여자)아이들))']\n\n🎧 2020.07.22. 6PM (KST)\n👉🏻 https://t.co/HRC5aJGhWJ\n\n#HYO #효연 #DESSERT #디저트 #소녀시대 #GirlsGeneration https://t.co/DgIKp4yoKG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			153
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HYO",
+					"indices": [
+						108,
+						112
+					]
+				},
+				{
+					"text": "효연",
+					"indices": [
+						113,
+						116
+					]
+				},
+				{
+					"text": "DESSERT",
+					"indices": [
+						117,
+						125
+					]
+				},
+				{
+					"text": "디저트",
+					"indices": [
+						126,
+						130
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						131,
+						136
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						137,
+						153
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/HRC5aJGhWJ",
+					"expanded_url": "http://hyoyeon.smtown.com",
+					"display_url": "hyoyeon.smtown.com",
+					"indices": [
+						83,
+						106
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1284093809249226800,
+					"id_str": "1284093809249226752",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/EdIEhB6UEAAHWgI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EdIEhB6UEAAHWgI.jpg",
+					"url": "https://t.co/DgIKp4yoKG",
+					"display_url": "pic.twitter.com/DgIKp4yoKG",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1284140803745443843/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1284093809249226800,
+					"id_str": "1284093809249226752",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/EdIEhB6UEAAHWgI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EdIEhB6UEAAHWgI.jpg",
+					"url": "https://t.co/DgIKp4yoKG",
+					"display_url": "pic.twitter.com/DgIKp4yoKG",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1284140803745443843/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "http://t.co/6dDRF9Jo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/6dDRF9Jo",
+							"expanded_url": "http://girlsgeneration.smtown.com/",
+							"display_url": "girlsgeneration.smtown.com",
+							"indices": [
+								0,
+								20
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3542449,
+			"friends_count": 4,
+			"listed_count": 5023,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 27,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2061,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1593746984",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3541,
+		"favorite_count": 6849,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": true,
+		"lang": "ko",
+		"text": "HYO 4th Single ['DESSERT (Feat. Loopy, 소연 ((여자)아이들))']\n\n🎧 2020.07.22. 6PM (KST)\n👉🏻 https://t.co/HRC5aJGhWJ\n\n#HYO #효연 #DESSERT #디저트 #소녀시대 #GirlsGeneration https://t.co/DgIKp4yoKG"
+	},
+	{
 		"created_at": "Fri Jul 17 11:52:22 +0000 2020",
 		"id": 1284093586414407700,
 		"id_str": "1284093586414407682",
