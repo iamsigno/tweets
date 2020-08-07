45a2c55d3b757fd6c53357651fc5a300a6994dd2
SIGNO <iamsigno@users.noreply.github.com>
2020-08-08 02:07:52 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27501,
-	"created_at": "Fri Aug 07 13:07:50 +0000 2020",
+	"tweets": 27502,
+	"created_at": "Fri Aug 07 17:07:51 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,179 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Fri Aug 07 16:44:57 +0000 2020",
+		"id": 1291769670618345500,
+		"id_str": "1291777361235648512",
+		"full_text": "#AskHYO💗\n@Hyoyeon_djhyo willl answer on #TwitterBlueroom LIVE Q&amp;A @GirlsGeneration (8/11, 7PM KST)\n\n#HYO #효연 #DJ_HYO #HYO_DESSERT #효연_디저트 #GirlsGeneration",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			158
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AskHYO",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"text": "TwitterBlueroom",
+					"indices": [
+						40,
+						56
+					]
+				},
+				{
+					"text": "HYO",
+					"indices": [
+						104,
+						108
+					]
+				},
+				{
+					"text": "효연",
+					"indices": [
+						109,
+						112
+					]
+				},
+				{
+					"text": "DJ_HYO",
+					"indices": [
+						113,
+						120
+					]
+				},
+				{
+					"text": "HYO_DESSERT",
+					"indices": [
+						121,
+						133
+					]
+				},
+				{
+					"text": "효연_디저트",
+					"indices": [
+						134,
+						141
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						142,
+						158
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Hyoyeon_djhyo",
+					"name": "Hyoyeon",
+					"id": 792687101842186200,
+					"id_str": "792687101842186240",
+					"indices": [
+						9,
+						23
+					]
+				},
+				{
+					"screen_name": "GirlsGeneration",
+					"name": "Girls' Generation",
+					"id": 391115625,
+					"id_str": "391115625",
+					"indices": [
+						70,
+						86
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://media.twitter.com\" rel=\"nofollow\">Twitter VIT App for iOS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "http://t.co/6dDRF9Jo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/6dDRF9Jo",
+							"expanded_url": "http://girlsgeneration.smtown.com/",
+							"display_url": "girlsgeneration.smtown.com",
+							"indices": [
+								0,
+								20
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3541399,
+			"friends_count": 4,
+			"listed_count": 5037,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 30,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2103,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1593746984",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 855,
+		"favorite_count": 1797,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "#AskHYO💗\n@Hyoyeon_djhyo willl answer on #TwitterBlueroom LIVE Q&amp;A @GirlsGeneration (8/11, 7PM KST)\n\n#HYO #효연 #DJ_HYO #HYO_DESSERT #효연_디저트 #GirlsGeneration"
+	},
+	{
 		"created_at": "Fri Aug 07 13:07:10 +0000 2020",
 		"id": 1291698781302321200,
 		"id_str": "1291722552679772160",
