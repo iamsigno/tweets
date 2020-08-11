065d2844673e77cd2348bdfa43f58b6912cd804b
SIGNO <iamsigno@users.noreply.github.com>
2020-08-11 16:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27508,
-	"created_at": "Tue Aug 11 04:07:50 +0000 2020",
+	"tweets": 27509,
+	"created_at": "Tue Aug 11 07:07:49 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,250 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Tue Aug 11 07:07:09 +0000 2020",
+		"id": 1293075234979897300,
+		"id_str": "1293081502364979200",
+		"full_text": "서현X효진 배우의 JTBC &lt;사생활&gt; 캐릭터 포스터 대공개🎉\n비주얼부터 '사기'인 두 배우의 만남, 9월 16일 밤 9시 30분에 만나요🙌🏻\n\n#서현 #김효진 #JTBC #사생활 #차주은 #정복기 #9월16일 #첫방송 #Seohyun #Kimhyojin https://t.co/8yZ6u972TX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			147
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "서현",
+					"indices": [
+						86,
+						89
+					]
+				},
+				{
+					"text": "김효진",
+					"indices": [
+						90,
+						94
+					]
+				},
+				{
+					"text": "JTBC",
+					"indices": [
+						95,
+						100
+					]
+				},
+				{
+					"text": "사생활",
+					"indices": [
+						101,
+						105
+					]
+				},
+				{
+					"text": "차주은",
+					"indices": [
+						106,
+						110
+					]
+				},
+				{
+					"text": "정복기",
+					"indices": [
+						111,
+						115
+					]
+				},
+				{
+					"text": "9월16일",
+					"indices": [
+						116,
+						122
+					]
+				},
+				{
+					"text": "첫방송",
+					"indices": [
+						123,
+						127
+					]
+				},
+				{
+					"text": "Seohyun",
+					"indices": [
+						128,
+						136
+					]
+				},
+				{
+					"text": "Kimhyojin",
+					"indices": [
+						137,
+						147
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1293075220794769400,
+					"id_str": "1293075220794769408",
+					"indices": [
+						148,
+						171
+					],
+					"media_url": "http://pbs.twimg.com/media/EfHtD_NVAAA7ApN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EfHtD_NVAAA7ApN.jpg",
+					"url": "https://t.co/8yZ6u972TX",
+					"display_url": "pic.twitter.com/8yZ6u972TX",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1293075234979897344/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 847,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 480,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1446,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1293075220794769400,
+					"id_str": "1293075220794769408",
+					"indices": [
+						148,
+						171
+					],
+					"media_url": "http://pbs.twimg.com/media/EfHtD_NVAAA7ApN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EfHtD_NVAAA7ApN.jpg",
+					"url": "https://t.co/8yZ6u972TX",
+					"display_url": "pic.twitter.com/8yZ6u972TX",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1293075234979897344/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 847,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 480,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1446,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정\n4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. \n📸 고화질 아니면 죽음을 달라\n🌳주접은 그저 나무가 내뿜는 산소일 뿐\ninstagram: @namooactors\nyoutube: namooofficial",
+			"url": "https://t.co/pDrppn6MXI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pDrppn6MXI",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 30464,
+			"friends_count": 11,
+			"listed_count": 182,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 124,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13042,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 231,
+		"favorite_count": 257,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "서현X효진 배우의 JTBC &lt;사생활&gt; 캐릭터 포스터 대공개🎉\n비주얼부터 '사기'인 두 배우의 만남, 9월 16일 밤 9시 30분에 만나요🙌🏻\n\n#서현 #김효진 #JTBC #사생활 #차주은 #정복기 #9월16일 #첫방송 #Seohyun #Kimhyojin https://t.co/8yZ6u972TX"
+	},
+	{
 		"created_at": "Tue Aug 11 04:07:12 +0000 2020",
 		"id": 1293034589791768600,
 		"id_str": "1293036216515014656",
