4ce0415331d60d411dc12b2a4de21161786c3507
SIGNO <iamsigno@users.noreply.github.com>
2020-08-12 12:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27514,
-	"created_at": "Tue Aug 11 13:07:49 +0000 2020",
+	"tweets": 27515,
+	"created_at": "Wed Aug 12 03:07:49 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,175 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Wed Aug 12 03:07:09 +0000 2020",
+		"id": 1293372107800232000,
+		"id_str": "1293383492315873283",
+		"full_text": "나무는 기다린다, 9월 16일 밤 9시 30분을....\n&lt;사생활&gt;을 보고 행복해지기 위해서🥳\n\n▶️https://t.co/dOiKhWGonl\n\n#서현 #Seohyun #김효진 #Kimhyojin\n#JTBC #사생활 #9월16일 #밤9시30분 #첫방송",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			145
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "서현",
+					"indices": [
+						86,
+						89
+					]
+				},
+				{
+					"text": "Seohyun",
+					"indices": [
+						90,
+						98
+					]
+				},
+				{
+					"text": "김효진",
+					"indices": [
+						99,
+						103
+					]
+				},
+				{
+					"text": "Kimhyojin",
+					"indices": [
+						104,
+						114
+					]
+				},
+				{
+					"text": "JTBC",
+					"indices": [
+						115,
+						120
+					]
+				},
+				{
+					"text": "사생활",
+					"indices": [
+						121,
+						125
+					]
+				},
+				{
+					"text": "9월16일",
+					"indices": [
+						126,
+						132
+					]
+				},
+				{
+					"text": "밤9시30분",
+					"indices": [
+						133,
+						140
+					]
+				},
+				{
+					"text": "첫방송",
+					"indices": [
+						141,
+						145
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/dOiKhWGonl",
+					"expanded_url": "https://youtu.be/-rQ_7VDhnik",
+					"display_url": "youtu.be/-rQ_7VDhnik",
+					"indices": [
+						61,
+						84
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정\n4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. \n📸 고화질 아니면 죽음을 달라\n🌳주접은 그저 나무가 내뿜는 산소일 뿐\ninstagram: @namooactors\nyoutube: namooofficial",
+			"url": "https://t.co/pDrppn6MXI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pDrppn6MXI",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 30479,
+			"friends_count": 11,
+			"listed_count": 182,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 124,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13046,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 225,
+		"favorite_count": 206,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "나무는 기다린다, 9월 16일 밤 9시 30분을....\n&lt;사생활&gt;을 보고 행복해지기 위해서🥳\n\n▶️https://t.co/dOiKhWGonl\n\n#서현 #Seohyun #김효진 #Kimhyojin\n#JTBC #사생활 #9월16일 #밤9시30분 #첫방송"
+	},
+	{
 		"created_at": "Tue Aug 11 13:07:11 +0000 2020",
 		"id": 1293169505284157400,
 		"id_str": "1293172108412018689",
