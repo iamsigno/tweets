d8b1197aa090f914e455e6a6307bd7f2a474b7b1
SIGNO <iamsigno@users.noreply.github.com>
2020-08-29 18:07:53 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27582,
-	"created_at": "Fri Aug 28 13:07:51 +0000 2020",
+	"tweets": 27583,
+	"created_at": "Sat Aug 29 09:07:51 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,243 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Sat Aug 29 09:07:09 +0000 2020",
+		"id": 1299632884081471500,
+		"id_str": "1299634685253910528",
+		"full_text": "✨환!상!적!인!✨ 그녀 권유리의 프로필 촬영 현장 📸\n\n#권유리 #KWONYURI #소녀시대 #GirlsGeneration https://t.co/9bqru6ZcTz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			69
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "권유리",
+					"indices": [
+						32,
+						36
+					]
+				},
+				{
+					"text": "KWONYURI",
+					"indices": [
+						37,
+						46
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						47,
+						52
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						53,
+						69
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1299246078496108500,
+					"id_str": "1299246078496108544",
+					"indices": [
+						70,
+						93
+					],
+					"media_url": "http://pbs.twimg.com/media/EgfjXYsUcAAQc30.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EgfjXYsUcAAQc30.jpg",
+					"url": "https://t.co/9bqru6ZcTz",
+					"display_url": "pic.twitter.com/9bqru6ZcTz",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1299632884081471488/video/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 382,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1299246078496108500,
+					"id_str": "1299246078496108544",
+					"indices": [
+						70,
+						93
+					],
+					"media_url": "http://pbs.twimg.com/media/EgfjXYsUcAAQc30.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EgfjXYsUcAAQc30.jpg",
+					"url": "https://t.co/9bqru6ZcTz",
+					"display_url": "pic.twitter.com/9bqru6ZcTz",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1299632884081471488/video/1",
+					"type": "video",
+					"sizes": {
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 382,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							9,
+							16
+						],
+						"duration_millis": 232699,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1299246078496108544/pl/mNd9xXGiR0Y719AL.m3u8?tag=13"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1299246078496108544/vid/320x568/sKJFsOLa9CDS8dWm.mp4?tag=13"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1299246078496108544/vid/360x640/QVFVwD6JdaEkDiaF.mp4?tag=13"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1299246078496108544/vid/720x1280/4BSfmS6D8wOjdcXq.mp4?tag=13"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "http://t.co/6dDRF9Jo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/6dDRF9Jo",
+							"expanded_url": "http://girlsgeneration.smtown.com/",
+							"display_url": "girlsgeneration.smtown.com",
+							"indices": [
+								0,
+								20
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3546787,
+			"friends_count": 4,
+			"listed_count": 5025,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 35,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2125,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1593746984",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 812,
+		"favorite_count": 2029,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "✨환!상!적!인!✨ 그녀 권유리의 프로필 촬영 현장 📸\n\n#권유리 #KWONYURI #소녀시대 #GirlsGeneration https://t.co/9bqru6ZcTz"
+	},
+	{
 		"created_at": "Fri Aug 28 12:45:04 +0000 2020",
 		"id": 1299322422693421000,
 		"id_str": "1299327137086873603",
