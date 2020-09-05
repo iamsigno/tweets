08dd2c6ddf80046ec98bf99e34415db11b31b9de
SIGNO <iamsigno@users.noreply.github.com>
2020-09-05 23:07:51 +0900

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27593,
-	"created_at": "Fri Sep 04 11:07:48 +0000 2020",
+	"tweets": 27596,
+	"created_at": "Sat Sep 05 14:07:49 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,668 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Sat Sep 05 13:56:35 +0000 2020",
+		"id": 1301822315148107800,
+		"id_str": "1302244239011491840",
+		"full_text": "[#청담Key친 I CHEONGDAM KEYchin] ep3-4 Key와 태연의 라떼 시절 추억 이야기\n\nKey와 #태연 의 추억 여행\n연습생 시절 이야기부터 그룹 활동 비하인드 스토리까지!\n\n👉https://t.co/y7ZmQAXk0u\n\n매주 금요일 SM C&amp;C STUDIO 에서 만나요🍽\n\n#CHEONGDAM #Key #샤이니 #SHINee #SMCC_STUDIO https://t.co/29gjoFM2NA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			209
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "청담Key친",
+					"indices": [
+						1,
+						8
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						64,
+						67
+					]
+				},
+				{
+					"text": "CHEONGDAM",
+					"indices": [
+						168,
+						178
+					]
+				},
+				{
+					"text": "Key",
+					"indices": [
+						179,
+						183
+					]
+				},
+				{
+					"text": "샤이니",
+					"indices": [
+						184,
+						188
+					]
+				},
+				{
+					"text": "SHINee",
+					"indices": [
+						189,
+						196
+					]
+				},
+				{
+					"text": "SMCC_STUDIO",
+					"indices": [
+						197,
+						209
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/y7ZmQAXk0u",
+					"expanded_url": "https://youtu.be/ekshKzxgHog",
+					"display_url": "youtu.be/ekshKzxgHog",
+					"indices": [
+						109,
+						132
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1300660139448361000,
+					"id_str": "1300660139448360961",
+					"indices": [
+						210,
+						233
+					],
+					"media_url": "http://pbs.twimg.com/media/EgzfgXFUYAEf0WW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EgzfgXFUYAEf0WW.jpg",
+					"url": "https://t.co/29gjoFM2NA",
+					"display_url": "pic.twitter.com/29gjoFM2NA",
+					"expanded_url": "https://twitter.com/smcc_studio/status/1301822315148107776/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1300660139448361000,
+					"id_str": "1300660139448360961",
+					"indices": [
+						210,
+						233
+					],
+					"media_url": "http://pbs.twimg.com/media/EgzfgXFUYAEf0WW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EgzfgXFUYAEf0WW.jpg",
+					"url": "https://t.co/29gjoFM2NA",
+					"display_url": "pic.twitter.com/29gjoFM2NA",
+					"expanded_url": "https://twitter.com/smcc_studio/status/1301822315148107776/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1192288122756681700,
+			"id_str": "1192288122756681728",
+			"name": "SM C&C STUDIO",
+			"screen_name": "smcc_studio",
+			"location": "",
+			"description": "SM C&C STUDIO Official Twitter Page\n\n💙슈주 리턴즈4 \n🟢NCT LIFE: DREAM in Wonderland \n♥️레벨업 아슬한 프로젝트",
+			"url": "https://t.co/vODnwmZcgP",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vODnwmZcgP",
+							"expanded_url": "https://www.youtube.com/SMCCStudio",
+							"display_url": "youtube.com/SMCCStudio",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11862,
+			"friends_count": 0,
+			"listed_count": 35,
+			"created_at": "Thu Nov 07 03:50:32 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 362,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1298213648922316801/cK_qY6if_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1298213648922316801/cK_qY6if_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1192288122756681728/1598353231",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 405,
+		"favorite_count": 724,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#청담Key친 I CHEONGDAM KEYchin] ep3-4 Key와 태연의 라떼 시절 추억 이야기\n\nKey와 #태연 의 추억 여행\n연습생 시절 이야기부터 그룹 활동 비하인드 스토리까지!\n\n👉https://t.co/y7ZmQAXk0u\n\n매주 금요일 SM C&amp;C STUDIO 에서 만나요🍽\n\n#CHEONGDAM #Key #샤이니 #SHINee #SMCC_STUDIO https://t.co/29gjoFM2NA"
+	},
+	{
+		"created_at": "Sat Sep 05 13:56:13 +0000 2020",
+		"id": 1301784563358658600,
+		"id_str": "1302244147022053376",
+		"full_text": "💜 HYO V-LOG : 효연의 #DESSERT 음악방송 브이로그 💗  \n\n#HYO #효연 #HYO_DESSERT #효연_디저트 #DJ_HYO #디저트 #소녀시대 #GirlsGeneration #HYO_VLOG https://t.co/IbRXNWP4vf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			117
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DESSERT",
+					"indices": [
+						18,
+						26
+					]
+				},
+				{
+					"text": "HYO",
+					"indices": [
+						42,
+						46
+					]
+				},
+				{
+					"text": "효연",
+					"indices": [
+						47,
+						50
+					]
+				},
+				{
+					"text": "HYO_DESSERT",
+					"indices": [
+						51,
+						63
+					]
+				},
+				{
+					"text": "효연_디저트",
+					"indices": [
+						64,
+						71
+					]
+				},
+				{
+					"text": "DJ_HYO",
+					"indices": [
+						72,
+						79
+					]
+				},
+				{
+					"text": "디저트",
+					"indices": [
+						80,
+						84
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						85,
+						90
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						91,
+						107
+					]
+				},
+				{
+					"text": "HYO_VLOG",
+					"indices": [
+						108,
+						117
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1301741903134691300,
+					"id_str": "1301741903134691329",
+					"indices": [
+						118,
+						141
+					],
+					"media_url": "http://pbs.twimg.com/media/EhDJIx7VkAEwXPs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EhDJIx7VkAEwXPs.jpg",
+					"url": "https://t.co/IbRXNWP4vf",
+					"display_url": "pic.twitter.com/IbRXNWP4vf",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1301784563358658561/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1301741903134691300,
+					"id_str": "1301741903134691329",
+					"indices": [
+						118,
+						141
+					],
+					"media_url": "http://pbs.twimg.com/media/EhDJIx7VkAEwXPs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EhDJIx7VkAEwXPs.jpg",
+					"url": "https://t.co/IbRXNWP4vf",
+					"display_url": "pic.twitter.com/IbRXNWP4vf",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1301784563358658561/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 375742,
+						"variants": [
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1301741903134691329/vid/480x270/UUPNdKNyTOWPQOzh.mp4?tag=13"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1301741903134691329/vid/1280x720/OotgS_7ETYPC5sXB.mp4?tag=13"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1301741903134691329/vid/640x360/AZhctDt8dmPhNLJc.mp4?tag=13"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1301741903134691329/pl/cfQBgczHsoKb7Nq7.m3u8?tag=13"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "http://t.co/6dDRF9Jo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/6dDRF9Jo",
+							"expanded_url": "http://girlsgeneration.smtown.com/",
+							"display_url": "girlsgeneration.smtown.com",
+							"indices": [
+								0,
+								20
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3551070,
+			"friends_count": 4,
+			"listed_count": 5043,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 35,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2128,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1593746984",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4283,
+		"favorite_count": 9929,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "💜 HYO V-LOG : 효연의 #DESSERT 음악방송 브이로그 💗  \n\n#HYO #효연 #HYO_DESSERT #효연_디저트 #DJ_HYO #디저트 #소녀시대 #GirlsGeneration #HYO_VLOG https://t.co/IbRXNWP4vf"
+	},
+	{
+		"created_at": "Sat Sep 05 13:56:02 +0000 2020",
+		"id": 1301083864299835400,
+		"id_str": "1302244101446799360",
+		"full_text": "[4K] [ENG] [유리한 식탁] EP12-1. 찐친과 청담에서 밥 먹다가 연예인 영접?! \n\n👉🏻https://t.co/INLZr0lcU0 \n\n#유리 #YURI #유리한TV #유리한식탁 #소녀시대 #GirlsGeneration",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			128
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "유리",
+					"indices": [
+						82,
+						85
+					]
+				},
+				{
+					"text": "YURI",
+					"indices": [
+						86,
+						91
+					]
+				},
+				{
+					"text": "유리한TV",
+					"indices": [
+						92,
+						98
+					]
+				},
+				{
+					"text": "유리한식탁",
+					"indices": [
+						99,
+						105
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						106,
+						111
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						112,
+						128
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/INLZr0lcU0",
+					"expanded_url": "https://youtu.be/hHoWgJLFkis",
+					"display_url": "youtu.be/hHoWgJLFkis",
+					"indices": [
+						56,
+						79
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "http://t.co/6dDRF9Jo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/6dDRF9Jo",
+							"expanded_url": "http://girlsgeneration.smtown.com/",
+							"display_url": "girlsgeneration.smtown.com",
+							"indices": [
+								0,
+								20
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3551070,
+			"friends_count": 4,
+			"listed_count": 5043,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 35,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2128,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1593746984",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1554,
+		"favorite_count": 3755,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[4K] [ENG] [유리한 식탁] EP12-1. 찐친과 청담에서 밥 먹다가 연예인 영접?! \n\n👉🏻https://t.co/INLZr0lcU0 \n\n#유리 #YURI #유리한TV #유리한식탁 #소녀시대 #GirlsGeneration"
+	},
+	{
 		"created_at": "Fri Sep 04 10:10:24 +0000 2020",
 		"id": 1301824928086454300,
 		"id_str": "1301824928086454273",
