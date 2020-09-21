1843c7acd249d2882a786e5e75f339f243a94b6d
SIGNO <iamsigno@users.noreply.github.com>
2020-09-21 19:07:52 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27629,
-	"created_at": "Mon Sep 21 07:07:50 +0000 2020",
+	"tweets": 27630,
+	"created_at": "Mon Sep 21 10:07:50 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,226 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Mon Sep 21 10:07:09 +0000 2020",
+		"id": 1307970200872968200,
+		"id_str": "1307984706206564353",
+		"full_text": "Happy Birthday HYOYEON 💓\n\nGirls' Generation 효연 생일을 맞아, ARTIST BIRTHDAY NECKLACE를 SMTOWN &amp;STORE에서 만나 보실 수 있습니다. \n9월 탄생석 사파이어 컬러 크리스탈 스톤으로 포인트를 주었어요.\n\n🔸9월 탄생석 의미 : 성실, 진실\n🔸소재 : METAL, CRYSTAL STONE\n\nhttps://t.co/THLLxkmG5V https://t.co/6SsXFxx79e",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			224
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/THLLxkmG5V",
+					"expanded_url": "http://www.SMTOWNandSTORE.com",
+					"display_url": "SMTOWNandSTORE.com",
+					"indices": [
+						201,
+						224
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1307969978352545800,
+					"id_str": "1307969978352545792",
+					"indices": [
+						225,
+						248
+					],
+					"media_url": "http://pbs.twimg.com/media/EibXw_CUMAAXctM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EibXw_CUMAAXctM.jpg",
+					"url": "https://t.co/6SsXFxx79e",
+					"display_url": "pic.twitter.com/6SsXFxx79e",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1307970200872968192/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1199,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1773,
+							"h": 1772,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1307969978352545800,
+					"id_str": "1307969978352545792",
+					"indices": [
+						225,
+						248
+					],
+					"media_url": "http://pbs.twimg.com/media/EibXw_CUMAAXctM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EibXw_CUMAAXctM.jpg",
+					"url": "https://t.co/6SsXFxx79e",
+					"display_url": "pic.twitter.com/6SsXFxx79e",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1307970200872968192/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1199,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1773,
+							"h": 1772,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1307969982471442400,
+					"id_str": "1307969982471442432",
+					"indices": [
+						225,
+						248
+					],
+					"media_url": "http://pbs.twimg.com/media/EibXxOYVkAAdBO5.png",
+					"media_url_https": "https://pbs.twimg.com/media/EibXxOYVkAAdBO5.png",
+					"url": "https://t.co/6SsXFxx79e",
+					"display_url": "pic.twitter.com/6SsXFxx79e",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1307970200872968192/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1772,
+							"h": 1772,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1113723889245278200,
+			"id_str": "1113723889245278209",
+			"name": "SMTOWN &STORE",
+			"screen_name": "SMTOWNandSTORE",
+			"location": "",
+			"description": "SMTOWN &STORE Official Twitter Page",
+			"url": "https://t.co/ls3iEMlnuV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ls3iEMlnuV",
+							"expanded_url": "https://booking.naver.com/booking/12/bizes/405725",
+							"display_url": "booking.naver.com/booking/12/biz…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 77396,
+			"friends_count": 0,
+			"listed_count": 140,
+			"created_at": "Thu Apr 04 08:44:01 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 798,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1113723889245278209/1554705745",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 207,
+		"favorite_count": 442,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "Happy Birthday HYOYEON 💓\n\nGirls' Generation 효연 생일을 맞아, ARTIST BIRTHDAY NECKLACE를 SMTOWN &amp;STORE에서 만나 보실 수 있습니다. \n9월 탄생석 사파이어 컬러 크리스탈 스톤으로 포인트를 주었어요.\n\n🔸9월 탄생석 의미 : 성실, 진실\n🔸소재 : METAL, CRYSTAL STONE\n\nhttps://t.co/THLLxkmG5V https://t.co/6SsXFxx79e"
+	},
+	{
 		"created_at": "Mon Sep 21 06:15:59 +0000 2020",
 		"id": 1307857130381172700,
 		"id_str": "1307926529108668416",
