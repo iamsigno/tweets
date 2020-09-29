bc914d1eaa5e873e7718db4e4a82d4ad88cd6429
SIGNO <iamsigno@users.noreply.github.com>
2020-09-29 21:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27660,
-	"created_at": "Mon Sep 28 16:07:51 +0000 2020",
+	"tweets": 27661,
+	"created_at": "Tue Sep 29 12:07:49 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,246 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Tue Sep 29 12:07:09 +0000 2020",
+		"id": 1310897112092160000,
+		"id_str": "1310914006723555329",
+		"full_text": "팔찌 장인 #태연 첫 ASMR에 도전했다? 소원(S♡NE)을 위한 깜짝 선물을 만들면서 소근소근 나눈 미니 인터뷰 ▶️ https://t.co/9ocOye5PDA #TAEYEON #GirlsGeneration https://t.co/cF0eReW1qY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			116
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "태연",
+					"indices": [
+						6,
+						9
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						91,
+						99
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						100,
+						116
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/9ocOye5PDA",
+					"expanded_url": "http://bit.ly/2FZKweB",
+					"display_url": "bit.ly/2FZKweB",
+					"indices": [
+						67,
+						90
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1310875568468967400,
+					"id_str": "1310875568468967425",
+					"indices": [
+						117,
+						140
+					],
+					"media_url": "http://pbs.twimg.com/media/EjEqt-DU8AEELBv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjEqt-DU8AEELBv.jpg",
+					"url": "https://t.co/cF0eReW1qY",
+					"display_url": "pic.twitter.com/cF0eReW1qY",
+					"expanded_url": "https://twitter.com/wkorea/status/1310897112092160000/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1310875568468967400,
+					"id_str": "1310875568468967425",
+					"indices": [
+						117,
+						140
+					],
+					"media_url": "http://pbs.twimg.com/media/EjEqt-DU8AEELBv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjEqt-DU8AEELBv.jpg",
+					"url": "https://t.co/cF0eReW1qY",
+					"display_url": "pic.twitter.com/cF0eReW1qY",
+					"expanded_url": "https://twitter.com/wkorea/status/1310897112092160000/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 33167,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1310875568468967425/vid/1280x720/Ky6v07SXFFcM-uu4.mp4?tag=13"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1310875568468967425/pl/G8by6S1zWX5Yc2yI.m3u8?tag=13"
+							},
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1310875568468967425/vid/480x270/LI9kARxjC9JgO3at.mp4?tag=13"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1310875568468967425/vid/640x360/chb0lne1YuKiZvoK.mp4?tag=13"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": true
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 78262636,
+			"id_str": "78262636",
+			"name": "W Korea",
+			"screen_name": "wkorea",
+			"location": "",
+			"description": "Conceptual Trend Magazine 'W Korea' 공식 트위터 계정입니다.",
+			"url": "https://t.co/m6zPKg47IM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/m6zPKg47IM",
+							"expanded_url": "http://www.wkorea.com",
+							"display_url": "wkorea.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 248835,
+			"friends_count": 0,
+			"listed_count": 763,
+			"created_at": "Tue Sep 29 09:21:19 +0000 2009",
+			"favourites_count": 3706,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 11372,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "DD2E44",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1227868024499097600/goQ9_G6Q_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1227868024499097600/goQ9_G6Q_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/78262636/1600333350",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "333333",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1159,
+		"favorite_count": 1218,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "팔찌 장인 #태연 첫 ASMR에 도전했다? 소원(S♡NE)을 위한 깜짝 선물을 만들면서 소근소근 나눈 미니 인터뷰 ▶️ https://t.co/9ocOye5PDA #TAEYEON #GirlsGeneration https://t.co/cF0eReW1qY"
+	},
+	{
 		"created_at": "Mon Sep 28 16:03:12 +0000 2020",
 		"id": 1310611024865898500,
 		"id_str": "1310611024865898496",
