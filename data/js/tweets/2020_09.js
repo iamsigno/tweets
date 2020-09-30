69602b2b05188ea062998b80842d2180b8191be2
SIGNO <iamsigno@users.noreply.github.com>
2020-10-01 00:07:53 +0900

    Added 1 tweets

--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,166 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Wed Sep 30 15:07:18 +0000 2020",
+		"id": 1311320939338948600,
+		"id_str": "1311321731097911297",
+		"full_text": "テヨン最新曲「#GirlsSpkOut ft.ちゃんみな」のミュージックビデオを公開しました！また先行配信もスタート！\nぜひチェックしてください🎉\n\n🎬https://t.co/D3NiveC26S\n🎧https://t.co/vkvEPi20Lf\n🔗https://t.co/zFQBpg4doX\n\n#テヨン #TAEYEON #태연 #ちゃんみな",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			175
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GirlsSpkOut",
+					"indices": [
+						7,
+						19
+					]
+				},
+				{
+					"text": "テヨン",
+					"indices": [
+						151,
+						155
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						156,
+						164
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						165,
+						168
+					]
+				},
+				{
+					"text": "ちゃんみな",
+					"indices": [
+						169,
+						175
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/D3NiveC26S",
+					"expanded_url": "https://youtu.be/XT7Oh6vTY1Q",
+					"display_url": "youtu.be/XT7Oh6vTY1Q",
+					"indices": [
+						76,
+						99
+					]
+				},
+				{
+					"url": "https://t.co/vkvEPi20Lf",
+					"expanded_url": "https://lnk.to/GSODL",
+					"display_url": "lnk.to/GSODL",
+					"indices": [
+						101,
+						124
+					]
+				},
+				{
+					"url": "https://t.co/zFQBpg4doX",
+					"expanded_url": "http://taeyeon.girls-generation.jp/news/girlsspkout_mv/",
+					"display_url": "taeyeon.girls-generation.jp/news/girlsspko…",
+					"indices": [
+						126,
+						149
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2585025200,
+			"id_str": "2585025200",
+			"name": "少女時代 JAPAN OFFICIAL",
+			"screen_name": "gg_official_jp",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/nUrgRiOH3R",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/nUrgRiOH3R",
+							"expanded_url": "http://www.girls-generation.jp/",
+							"display_url": "girls-generation.jp",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 104668,
+			"friends_count": 0,
+			"listed_count": 479,
+			"created_at": "Tue Jun 24 03:33:35 +0000 2014",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 327,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1222720577078611968/5QXkaKkC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1222720577078611968/5QXkaKkC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2585025200/1505360281",
+			"profile_link_color": "0024B3",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 616,
+		"favorite_count": 547,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "テヨン最新曲「#GirlsSpkOut ft.ちゃんみな」のミュージックビデオを公開しました！また先行配信もスタート！\nぜひチェックしてください🎉\n\n🎬https://t.co/D3NiveC26S\n🎧https://t.co/vkvEPi20Lf\n🔗https://t.co/zFQBpg4doX\n\n#テヨン #TAEYEON #태연 #ちゃんみな"
+	},
+	{
 		"created_at": "Wed Sep 30 13:07:09 +0000 2020",
 		"id": 1311290160194048000,
 		"id_str": "1311291492804292608",
