20d408e05c20161eec995dac342828f5f5ad3237
SIGNO <iamsigno@users.noreply.github.com>
2020-10-03 09:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27675,
-	"created_at": "Fri Oct 02 08:07:49 +0000 2020",
+	"tweets": 27676,
+	"created_at": "Sat Oct 03 00:07:48 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Fri Oct 02 23:44:57 +0000 2020",
+		"id": 1312169251394863000,
+		"id_str": "1312176777003855872",
+		"full_text": "#사생활 D-4\n사기캐들의 통쾌한 사기 플레이👊\n\n사기캐들의 현실생활 &lt;사생활&gt;\n10/7(수) 밤9:30 첫 방송\n#JTBC #새수목드라마 #사생활 #PrivateLives\n#서현 #고경표 #김효진 #김영민 #태원석\n#ComingSoonJTBC #10월7일_밤_9시30분 #첫방송 https://t.co/CGXkk3UZl4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			186
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "사생활",
+					"indices": [
+						0,
+						4
+					]
+				},
+				{
+					"text": "JTBC",
+					"indices": [
+						70,
+						75
+					]
+				},
+				{
+					"text": "새수목드라마",
+					"indices": [
+						76,
+						83
+					]
+				},
+				{
+					"text": "사생활",
+					"indices": [
+						84,
+						88
+					]
+				},
+				{
+					"text": "PrivateLives",
+					"indices": [
+						89,
+						102
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						103,
+						106
+					]
+				},
+				{
+					"text": "고경표",
+					"indices": [
+						107,
+						111
+					]
+				},
+				{
+					"text": "김효진",
+					"indices": [
+						112,
+						116
+					]
+				},
+				{
+					"text": "김영민",
+					"indices": [
+						117,
+						121
+					]
+				},
+				{
+					"text": "태원석",
+					"indices": [
+						122,
+						126
+					]
+				},
+				{
+					"text": "ComingSoonJTBC",
+					"indices": [
+						127,
+						142
+					]
+				},
+				{
+					"text": "10월7일_밤_9시30분",
+					"indices": [
+						143,
+						157
+					]
+				},
+				{
+					"text": "첫방송",
+					"indices": [
+						158,
+						162
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/CGXkk3UZl4",
+					"expanded_url": "https://www.instagram.com/p/CF2_WHlB-ea/?igshid=10xwr6mwa4oiw",
+					"display_url": "instagram.com/p/CF2_WHlB-ea/…",
+					"indices": [
+						163,
+						186
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://instagram.com\" rel=\"nofollow\">Instagram</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2173631072,
+			"id_str": "2173631072",
+			"name": "서현(서주현)",
+			"screen_name": "sjhsjh0628",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1384076,
+			"friends_count": 31,
+			"listed_count": 2856,
+			"created_at": "Mon Nov 04 08:58:47 +0000 2013",
+			"favourites_count": 6,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1269,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1146333352682647552/0h6dsbYz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1146333352682647552/0h6dsbYz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2173631072/1526914876",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 152,
+		"favorite_count": 355,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#사생활 D-4\n사기캐들의 통쾌한 사기 플레이👊\n\n사기캐들의 현실생활 &lt;사생활&gt;\n10/7(수) 밤9:30 첫 방송\n#JTBC #새수목드라마 #사생활 #PrivateLives\n#서현 #고경표 #김효진 #김영민 #태원석\n#ComingSoonJTBC #10월7일_밤_9시30분 #첫방송 https://t.co/CGXkk3UZl4"
+	},
+	{
 		"created_at": "Fri Oct 02 08:07:09 +0000 2020",
 		"id": 1311938978417766400,
 		"id_str": "1311940772804132865",
