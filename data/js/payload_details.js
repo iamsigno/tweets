802687217d858f4a44736b758d01aa2e17c22a43
SIGNO <iamsigno@users.noreply.github.com>
2020-10-09 00:07:52 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27697,
-	"created_at": "Thu Oct 08 09:07:50 +0000 2020",
+	"tweets": 27698,
+	"created_at": "Thu Oct 08 15:07:50 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,218 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Thu Oct 08 15:07:09 +0000 2020",
+		"id": 1314213267514949600,
+		"id_str": "1314220794835337221",
+		"full_text": "말해 뭐해.. 나의 심장은 이미 서현 배우 꺼💘\n\n▶️ https://t.co/XoteGDUZ93\n\n#서현 #JTBC #사생활 #Seohyun https://t.co/9KPvMfIkNA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			79
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "서현",
+					"indices": [
+						56,
+						59
+					]
+				},
+				{
+					"text": "JTBC",
+					"indices": [
+						60,
+						65
+					]
+				},
+				{
+					"text": "사생활",
+					"indices": [
+						66,
+						70
+					]
+				},
+				{
+					"text": "Seohyun",
+					"indices": [
+						71,
+						79
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/XoteGDUZ93",
+					"expanded_url": "http://naver.me/xKJ845TC",
+					"display_url": "naver.me/xKJ845TC",
+					"indices": [
+						31,
+						54
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1314213256441917400,
+					"id_str": "1314213256441917440",
+					"indices": [
+						80,
+						103
+					],
+					"media_url": "http://pbs.twimg.com/media/Ej0F_mVUcAAiuhp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ej0F_mVUcAAiuhp.jpg",
+					"url": "https://t.co/9KPvMfIkNA",
+					"display_url": "pic.twitter.com/9KPvMfIkNA",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1314213267514949637/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1200,
+							"h": 1800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1314213256441917400,
+					"id_str": "1314213256441917440",
+					"indices": [
+						80,
+						103
+					],
+					"media_url": "http://pbs.twimg.com/media/Ej0F_mVUcAAiuhp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ej0F_mVUcAAiuhp.jpg",
+					"url": "https://t.co/9KPvMfIkNA",
+					"display_url": "pic.twitter.com/9KPvMfIkNA",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1314213267514949637/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1200,
+							"h": 1800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정\n4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. \n📸 고화질 아니면 죽음을 달라\n🌳주접은 그저 나무가 내뿜는 산소일 뿐\ninstagram: @namooactors\nyoutube: namooofficial",
+			"url": "https://t.co/pDrppn6MXI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pDrppn6MXI",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 32599,
+			"friends_count": 12,
+			"listed_count": 185,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 124,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13212,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 178,
+		"favorite_count": 199,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "말해 뭐해.. 나의 심장은 이미 서현 배우 꺼💘\n\n▶️ https://t.co/XoteGDUZ93\n\n#서현 #JTBC #사생활 #Seohyun https://t.co/9KPvMfIkNA"
+	},
+	{
 		"created_at": "Thu Oct 08 09:07:09 +0000 2020",
 		"id": 1314129154078576600,
 		"id_str": "1314130200590987264",
