33c89a47e3b3e4677b4696f0c09873f7154e4969
SIGNO <iamsigno@users.noreply.github.com>
2020-10-14 11:07:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27714,
-	"created_at": "Tue Oct 13 09:07:50 +0000 2020",
+	"tweets": 27715,
+	"created_at": "Wed Oct 14 02:07:49 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,241 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Wed Oct 14 02:07:09 +0000 2020",
+		"id": 1316186113476710400,
+		"id_str": "1316198828610117637",
+		"full_text": "[#아이돌챔프_투표]\n\n강남역 와이드 칼라 광고를 진행할\n🌟90년생 대표아이돌🌟에게 투표해주세요💕\n\n🏆예선전 진행 중\n#SNSD #SOOYOUNG #YOONA\n#2PM #JUNHO #CHANSUNG\n#EXO #XIUMIN \n\n☑Preliminaries(예선): 10/14~10/19 6PM \n☑Final(결선): 10/20 10AM~10/29 6PM \n\nVote▶https://t.co/eZIaWwtR5Q https://t.co/qRaLHuyEJL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			226
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "아이돌챔프_투표",
+					"indices": [
+						1,
+						10
+					]
+				},
+				{
+					"text": "SNSD",
+					"indices": [
+						66,
+						71
+					]
+				},
+				{
+					"text": "SOOYOUNG",
+					"indices": [
+						72,
+						81
+					]
+				},
+				{
+					"text": "YOONA",
+					"indices": [
+						82,
+						88
+					]
+				},
+				{
+					"text": "2PM",
+					"indices": [
+						89,
+						93
+					]
+				},
+				{
+					"text": "JUNHO",
+					"indices": [
+						94,
+						100
+					]
+				},
+				{
+					"text": "CHANSUNG",
+					"indices": [
+						101,
+						110
+					]
+				},
+				{
+					"text": "EXO",
+					"indices": [
+						111,
+						115
+					]
+				},
+				{
+					"text": "XIUMIN",
+					"indices": [
+						116,
+						123
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/eZIaWwtR5Q",
+					"expanded_url": "https://bit.ly/3jXC7qG",
+					"display_url": "bit.ly/3jXC7qG",
+					"indices": [
+						203,
+						226
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1316186104475779000,
+					"id_str": "1316186104475779072",
+					"indices": [
+						227,
+						250
+					],
+					"media_url": "http://pbs.twimg.com/media/EkQISd-VgAAb_f_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EkQISd-VgAAb_f_.jpg",
+					"url": "https://t.co/qRaLHuyEJL",
+					"display_url": "pic.twitter.com/qRaLHuyEJL",
+					"expanded_url": "https://twitter.com/idolchamp1/status/1316186113476710400/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1316186104475779000,
+					"id_str": "1316186104475779072",
+					"indices": [
+						227,
+						250
+					],
+					"media_url": "http://pbs.twimg.com/media/EkQISd-VgAAb_f_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EkQISd-VgAAb_f_.jpg",
+					"url": "https://t.co/qRaLHuyEJL",
+					"display_url": "pic.twitter.com/qRaLHuyEJL",
+					"expanded_url": "https://twitter.com/idolchamp1/status/1316186113476710400/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 842271014520148000,
+			"id_str": "842271014520147968",
+			"name": "아이돌챔프(아챔이)",
+			"screen_name": "idolchamp1",
+			"location": "아이돌 맴 속",
+			"description": "MBC M 쇼챔피언 사전투표, 주간아이돌 출연진 싸인사진 이벤트 진행 중",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 69814,
+			"friends_count": 622,
+			"listed_count": 127,
+			"created_at": "Thu Mar 16 07:07:13 +0000 2017",
+			"favourites_count": 271,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3684,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1224930538693120002/l0ij6CAt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1224930538693120002/l0ij6CAt_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/842271014520147968/1580881175",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 11,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#아이돌챔프_투표]\n\n강남역 와이드 칼라 광고를 진행할\n🌟90년생 대표아이돌🌟에게 투표해주세요💕\n\n🏆예선전 진행 중\n#SNSD #SOOYOUNG #YOONA\n#2PM #JUNHO #CHANSUNG\n#EXO #XIUMIN \n\n☑Preliminaries(예선): 10/14~10/19 6PM \n☑Final(결선): 10/20 10AM~10/29 6PM \n\nVote▶https://t.co/eZIaWwtR5Q https://t.co/qRaLHuyEJL"
+	},
+	{
 		"created_at": "Tue Oct 13 09:07:09 +0000 2020",
 		"id": 1315942101549379600,
 		"id_str": "1315942138249609217",
