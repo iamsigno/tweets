e9d2eee1b965586ed146576cbe5cac4390958d0f
SIGNO <iamsigno@users.noreply.github.com>
2020-10-29 11:07:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27781,
-	"created_at": "Wed Oct 28 15:07:50 +0000 2020",
+	"tweets": 27782,
+	"created_at": "Thu Oct 29 02:07:49 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,135 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Thu Oct 29 01:44:56 +0000 2020",
+		"id": 1321618972824010800,
+		"id_str": "1321629058577375233",
+		"full_text": "사기꾼들의 현실생활 #사생활\n4기꾼즈✌️\n#이정환 #차주은 #정복기 #김재욱\n오늘 밤 9시30분 JTBC에서 만나💋 https://t.co/NiNnBkqJUm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			88
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "사생활",
+					"indices": [
+						11,
+						15
+					]
+				},
+				{
+					"text": "이정환",
+					"indices": [
+						23,
+						27
+					]
+				},
+				{
+					"text": "차주은",
+					"indices": [
+						28,
+						32
+					]
+				},
+				{
+					"text": "정복기",
+					"indices": [
+						33,
+						37
+					]
+				},
+				{
+					"text": "김재욱",
+					"indices": [
+						38,
+						42
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/NiNnBkqJUm",
+					"expanded_url": "https://www.instagram.com/p/CG6IllgBBsf/?igshid=a5d2ak9upphg",
+					"display_url": "instagram.com/p/CG6IllgBBsf/…",
+					"indices": [
+						65,
+						88
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://instagram.com\" rel=\"nofollow\">Instagram</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2173631072,
+			"id_str": "2173631072",
+			"name": "서현(서주현)",
+			"screen_name": "sjhsjh0628",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1384341,
+			"friends_count": 31,
+			"listed_count": 2853,
+			"created_at": "Mon Nov 04 08:58:47 +0000 2013",
+			"favourites_count": 6,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1291,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1146333352682647552/0h6dsbYz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1146333352682647552/0h6dsbYz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2173631072/1526914876",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 178,
+		"favorite_count": 482,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "사기꾼들의 현실생활 #사생활\n4기꾼즈✌️\n#이정환 #차주은 #정복기 #김재욱\n오늘 밤 9시30분 JTBC에서 만나💋 https://t.co/NiNnBkqJUm"
+	},
+	{
 		"created_at": "Wed Oct 28 14:44:57 +0000 2020",
 		"id": 1321450904961405000,
 		"id_str": "1321462965502156800",
