2ba7e47f0d66c6495fc98d44d60e296c7c500b38
SIGNO <iamsigno@users.noreply.github.com>
2020-11-12 16:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27871,
-	"created_at": "Thu Nov 12 04:07:50 +0000 2020",
+	"tweets": 27872,
+	"created_at": "Thu Nov 12 07:07:49 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Thu Nov 12 06:44:56 +0000 2020",
+		"id": 1326768273137037300,
+		"id_str": "1326777986096443393",
+		"full_text": "스파이흥신소 Family✌️\n#차주은 #장민정 #고혜원 #박태주 \nJTBC수목드라마 #사생활 #PrivateLives https://t.co/bYQOQlZUjH",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			89
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "차주은",
+					"indices": [
+						16,
+						20
+					]
+				},
+				{
+					"text": "장민정",
+					"indices": [
+						21,
+						25
+					]
+				},
+				{
+					"text": "고혜원",
+					"indices": [
+						26,
+						30
+					]
+				},
+				{
+					"text": "박태주",
+					"indices": [
+						31,
+						35
+					]
+				},
+				{
+					"text": "사생활",
+					"indices": [
+						47,
+						51
+					]
+				},
+				{
+					"text": "PrivateLives",
+					"indices": [
+						52,
+						65
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/bYQOQlZUjH",
+					"expanded_url": "https://www.instagram.com/p/CHeuN54B1fG/?igshid=vgzuo4jb831k",
+					"display_url": "instagram.com/p/CHeuN54B1fG/…",
+					"indices": [
+						66,
+						89
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://instagram.com\" rel=\"nofollow\">Instagram</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2173631072,
+			"id_str": "2173631072",
+			"name": "서현(서주현)",
+			"screen_name": "sjhsjh0628",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1384697,
+			"friends_count": 31,
+			"listed_count": 2842,
+			"created_at": "Mon Nov 04 08:58:47 +0000 2013",
+			"favourites_count": 6,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1295,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1146333352682647552/0h6dsbYz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1146333352682647552/0h6dsbYz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2173631072/1526914876",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 179,
+		"favorite_count": 481,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "스파이흥신소 Family✌️\n#차주은 #장민정 #고혜원 #박태주 \nJTBC수목드라마 #사생활 #PrivateLives https://t.co/bYQOQlZUjH"
+	},
+	{
 		"created_at": "Thu Nov 12 04:07:09 +0000 2020",
 		"id": 1326729014740803600,
 		"id_str": "1326738274942070787",
