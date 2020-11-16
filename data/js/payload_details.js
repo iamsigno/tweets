999eb85963173eba80ac172ad99e01c7ec4965ad
SIGNO <iamsigno@users.noreply.github.com>
2020-11-17 00:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27889,
-	"created_at": "Mon Nov 16 09:07:49 +0000 2020",
+	"tweets": 27890,
+	"created_at": "Mon Nov 16 15:07:49 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,134 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Mon Nov 16 15:07:08 +0000 2020",
+		"id": 1328352124254654500,
+		"id_str": "1328353920478031872",
+		"full_text": "テヨンが、2012年に少女時代として登場して以来8年ぶりに「anan」に登場📕❣️\n撮り下ろしグラビア＆インタビューの掲載、更にananWEBでも本誌とは異なるテヨンの撮り下ろし写真や動画、インタビューも掲載予定🎉\n#テヨン #TAEYEON #태연 \n\n🔻anan(2227号) 11月25日発売\nhttps://t.co/SpgNShjNjj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			173
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "テヨン",
+					"indices": [
+						108,
+						112
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						113,
+						121
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						122,
+						125
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SpgNShjNjj",
+					"expanded_url": "https://ananweb.jp/",
+					"display_url": "ananweb.jp",
+					"indices": [
+						150,
+						173
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2585025200,
+			"id_str": "2585025200",
+			"name": "少女時代 JAPAN OFFICIAL",
+			"screen_name": "gg_official_jp",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/nUrgRiOH3R",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/nUrgRiOH3R",
+							"expanded_url": "http://www.girls-generation.jp/",
+							"display_url": "girls-generation.jp",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 105294,
+			"friends_count": 0,
+			"listed_count": 487,
+			"created_at": "Tue Jun 24 03:33:35 +0000 2014",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 350,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1222720577078611968/5QXkaKkC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1222720577078611968/5QXkaKkC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2585025200/1505360281",
+			"profile_link_color": "0024B3",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 250,
+		"favorite_count": 444,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "テヨンが、2012年に少女時代として登場して以来8年ぶりに「anan」に登場📕❣️\n撮り下ろしグラビア＆インタビューの掲載、更にananWEBでも本誌とは異なるテヨンの撮り下ろし写真や動画、インタビューも掲載予定🎉\n#テヨン #TAEYEON #태연 \n\n🔻anan(2227号) 11月25日発売\nhttps://t.co/SpgNShjNjj"
+	},
+	{
 		"created_at": "Mon Nov 16 09:07:14 +0000 2020",
 		"id": 1328261132864213000,
 		"id_str": "1328263346425131008",
