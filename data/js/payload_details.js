7ca7209a7a7fe8b41050d34b5c9883eae4abe1d6
SIGNO <iamsigno@users.noreply.github.com>
2020-11-18 11:07:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27894,
-	"created_at": "Tue Nov 17 14:08:13 +0000 2020",
+	"tweets": 27895,
+	"created_at": "Wed Nov 18 02:07:50 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,317 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Wed Nov 18 01:44:57 +0000 2020",
+		"id": 1328874374494003200,
+		"id_str": "1328876816971558915",
+		"full_text": "‘에스티 로더(ESTÉE LAUDER)’의 새로운 뮤즈 ‘임윤아’가 함께한 뷰티 화보!\n마리끌레르 12월호에서 공개!💛\n\nESTÉE LAUDER’s new muse LIM YOONA’s beauty pictorial to be released via Marie Claire DEC. issue!💛\n\n#LIMYOONA #임윤아 #소녀시대 #GirlsGeneration https://t.co/iDHkI1tVRX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			204
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LIMYOONA",
+					"indices": [
+						167,
+						176
+					]
+				},
+				{
+					"text": "임윤아",
+					"indices": [
+						177,
+						181
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						182,
+						187
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						188,
+						204
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1328874351072985000,
+					"id_str": "1328874351072985088",
+					"indices": [
+						205,
+						228
+					],
+					"media_url": "http://pbs.twimg.com/media/EnEcLn7VQAAyr5x.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnEcLn7VQAAyr5x.jpg",
+					"url": "https://t.co/iDHkI1tVRX",
+					"display_url": "pic.twitter.com/iDHkI1tVRX",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1328874374494003200/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1616,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 537,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 947,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1328874351072985000,
+					"id_str": "1328874351072985088",
+					"indices": [
+						205,
+						228
+					],
+					"media_url": "http://pbs.twimg.com/media/EnEcLn7VQAAyr5x.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnEcLn7VQAAyr5x.jpg",
+					"url": "https://t.co/iDHkI1tVRX",
+					"display_url": "pic.twitter.com/iDHkI1tVRX",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1328874374494003200/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1616,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 537,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 947,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1328874351072952300,
+					"id_str": "1328874351072952327",
+					"indices": [
+						205,
+						228
+					],
+					"media_url": "http://pbs.twimg.com/media/EnEcLn7UwAcOLQm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnEcLn7UwAcOLQm.jpg",
+					"url": "https://t.co/iDHkI1tVRX",
+					"display_url": "pic.twitter.com/iDHkI1tVRX",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1328874374494003200/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1616,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 947,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 537,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1328874351073001500,
+					"id_str": "1328874351073001473",
+					"indices": [
+						205,
+						228
+					],
+					"media_url": "http://pbs.twimg.com/media/EnEcLn7VgAE1d91.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnEcLn7VgAE1d91.jpg",
+					"url": "https://t.co/iDHkI1tVRX",
+					"display_url": "pic.twitter.com/iDHkI1tVRX",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1328874374494003200/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 537,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1616,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 947,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1328874351072923600,
+					"id_str": "1328874351072923649",
+					"indices": [
+						205,
+						228
+					],
+					"media_url": "http://pbs.twimg.com/media/EnEcLn7UUAEvrhS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnEcLn7UUAEvrhS.jpg",
+					"url": "https://t.co/iDHkI1tVRX",
+					"display_url": "pic.twitter.com/iDHkI1tVRX",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1328874374494003200/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 947,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 537,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1616,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "http://t.co/6dDRF9Jo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/6dDRF9Jo",
+							"expanded_url": "http://girlsgeneration.smtown.com/",
+							"display_url": "girlsgeneration.smtown.com",
+							"indices": [
+								0,
+								20
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3597986,
+			"friends_count": 4,
+			"listed_count": 5075,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 50,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2186,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1593746984",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1710,
+		"favorite_count": 4414,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "‘에스티 로더(ESTÉE LAUDER)’의 새로운 뮤즈 ‘임윤아’가 함께한 뷰티 화보!\n마리끌레르 12월호에서 공개!💛\n\nESTÉE LAUDER’s new muse LIM YOONA’s beauty pictorial to be released via Marie Claire DEC. issue!💛\n\n#LIMYOONA #임윤아 #소녀시대 #GirlsGeneration https://t.co/iDHkI1tVRX"
+	},
+	{
 		"created_at": "Tue Nov 17 13:44:56 +0000 2020",
 		"id": 1328681848692760600,
 		"id_str": "1328695621834911749",
