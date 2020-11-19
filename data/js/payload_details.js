77257bcb973a9804e4da96dc174023811576091c
SIGNO <iamsigno@users.noreply.github.com>
2020-11-19 09:07:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27900,
-	"created_at": "Wed Nov 18 10:07:51 +0000 2020",
+	"tweets": 27901,
+	"created_at": "Thu Nov 19 00:07:50 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Wed Nov 18 23:45:46 +0000 2020",
+		"id": 1329001412940820500,
+		"id_str": "1329209212535250947",
+		"full_text": "テヨン（少女時代）、ソロデビュー５年で見せた“ガールクラッシュ”な新境地。 - 取材、文・尹秀姫 | ananweb - マガジンハウス https://t.co/tttm4W2RCs https://t.co/98ApQeTozJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			92
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/tttm4W2RCs",
+					"expanded_url": "http://dlvr.it/RlwhkR",
+					"display_url": "dlvr.it/RlwhkR",
+					"indices": [
+						69,
+						92
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1329001411275735000,
+					"id_str": "1329001411275735040",
+					"indices": [
+						93,
+						116
+					],
+					"media_url": "http://pbs.twimg.com/media/EnGPvgEVQAAwS2K.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnGPvgEVQAAwS2K.jpg",
+					"url": "https://t.co/98ApQeTozJ",
+					"display_url": "pic.twitter.com/98ApQeTozJ",
+					"expanded_url": "https://twitter.com/anan_web/status/1329001412940820480/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 666,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 666,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1329001411275735000,
+					"id_str": "1329001411275735040",
+					"indices": [
+						93,
+						116
+					],
+					"media_url": "http://pbs.twimg.com/media/EnGPvgEVQAAwS2K.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnGPvgEVQAAwS2K.jpg",
+					"url": "https://t.co/98ApQeTozJ",
+					"display_url": "pic.twitter.com/98ApQeTozJ",
+					"expanded_url": "https://twitter.com/anan_web/status/1329001412940820480/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 666,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 666,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://dlvrit.com/\" rel=\"nofollow\">dlvr.it</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2254500937,
+			"id_str": "2254500937",
+			"name": "アンアンウェブ",
+			"screen_name": "anan_web",
+			"location": "東京都 中央区",
+			"description": "アンアンウェブは、雑誌「anan」のウェブメディアです。雑誌だけでなく、ウェブオリジナルのコンテンツが盛りだくさん。恋愛・美容・メイク・占い・芸能・カルチャーなどの記事が集まっています。恋愛現役女子が満足する情報を毎日更新します！",
+			"url": "https://t.co/RCTqZDDs8M",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/RCTqZDDs8M",
+							"expanded_url": "http://ananweb.jp/",
+							"display_url": "ananweb.jp",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 16645,
+			"friends_count": 9764,
+			"listed_count": 136,
+			"created_at": "Fri Dec 20 04:59:14 +0000 2013",
+			"favourites_count": 2920,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 23116,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/898392312711061504/UFU6enWA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/898392312711061504/UFU6enWA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2254500937/1473841580",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 274,
+		"favorite_count": 874,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "テヨン（少女時代）、ソロデビュー５年で見せた“ガールクラッシュ”な新境地。 - 取材、文・尹秀姫 | ananweb - マガジンハウス https://t.co/tttm4W2RCs https://t.co/98ApQeTozJ"
+	},
+	{
 		"created_at": "Wed Nov 18 10:07:15 +0000 2020",
 		"id": 1329002524255240200,
 		"id_str": "1329003226478354432",
