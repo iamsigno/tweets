c9f5b447e3866499764370c5930331ed58b37268
SIGNO <iamsigno@users.noreply.github.com>
2020-11-20 16:07:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27905,
-	"created_at": "Fri Nov 20 03:07:50 +0000 2020",
+	"tweets": 27906,
+	"created_at": "Fri Nov 20 07:07:50 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,209 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Fri Nov 20 06:44:57 +0000 2020",
+		"id": 1329667763724562400,
+		"id_str": "1329677089793052673",
+		"full_text": "#임윤아, 화보 촬영 비하인드 사진 공개! \n‘무결점 비주얼’\n‘겨울 여신’다운 압도적 분위기❤️\n\n#LIMYOONA released her behind-the-scene photos of the pictorial photoshoot!\nShowing off her 'Flawless visual' and the overwhelming vibes like 'Winter Goddess'❤️\n\n#소녀시대 #GirlsGeneration https://t.co/naFyPsOQWT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			240
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "임윤아",
+					"indices": [
+						0,
+						4
+					]
+				},
+				{
+					"text": "LIMYOONA",
+					"indices": [
+						56,
+						65
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						218,
+						223
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						224,
+						240
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1329667755197358000,
+					"id_str": "1329667755197358080",
+					"indices": [
+						241,
+						264
+					],
+					"media_url": "http://pbs.twimg.com/media/EnPtx0nUYAAJubS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnPtx0nUYAAJubS.jpg",
+					"url": "https://t.co/naFyPsOQWT",
+					"display_url": "pic.twitter.com/naFyPsOQWT",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1329667763724562432/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 340,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 600,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1329667755197358000,
+					"id_str": "1329667755197358080",
+					"indices": [
+						241,
+						264
+					],
+					"media_url": "http://pbs.twimg.com/media/EnPtx0nUYAAJubS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnPtx0nUYAAJubS.jpg",
+					"url": "https://t.co/naFyPsOQWT",
+					"display_url": "pic.twitter.com/naFyPsOQWT",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1329667763724562432/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 340,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 600,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "http://t.co/6dDRF9Jo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/6dDRF9Jo",
+							"expanded_url": "http://girlsgeneration.smtown.com/",
+							"display_url": "girlsgeneration.smtown.com",
+							"indices": [
+								0,
+								20
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3598697,
+			"friends_count": 4,
+			"listed_count": 5080,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 50,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2193,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1593746984",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1589,
+		"favorite_count": 4126,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#임윤아, 화보 촬영 비하인드 사진 공개! \n‘무결점 비주얼’\n‘겨울 여신’다운 압도적 분위기❤️\n\n#LIMYOONA released her behind-the-scene photos of the pictorial photoshoot!\nShowing off her 'Flawless visual' and the overwhelming vibes like 'Winter Goddess'❤️\n\n#소녀시대 #GirlsGeneration https://t.co/naFyPsOQWT"
+	},
+	{
 		"created_at": "Fri Nov 20 03:07:15 +0000 2020",
 		"id": 1329618837898031000,
 		"id_str": "1329622304289120256",
