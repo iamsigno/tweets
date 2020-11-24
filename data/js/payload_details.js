8744807bd1116c53337d5a77a01086ed5e0b6b68
SIGNO <iamsigno@users.noreply.github.com>
2020-11-24 15:07:52 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27928,
-	"created_at": "Tue Nov 24 04:07:53 +0000 2020",
+	"tweets": 27929,
+	"created_at": "Tue Nov 24 06:07:51 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,333 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Tue Nov 24 06:07:09 +0000 2020",
+		"id": 1331102430457598000,
+		"id_str": "1331117128527863809",
+		"full_text": "[#NEWS] '사생활' 서현X고경표, 화끈한 '꾼' 활약...반격도 사랑도 열일\n▶ https://t.co/1COBrSST3k\n\n#서현 #사생활 #SEOHYUN #PrivateLives https://t.co/aFeepXIBrQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			104
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						73,
+						76
+					]
+				},
+				{
+					"text": "사생활",
+					"indices": [
+						77,
+						81
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						82,
+						90
+					]
+				},
+				{
+					"text": "PrivateLives",
+					"indices": [
+						91,
+						104
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/1COBrSST3k",
+					"expanded_url": "http://naver.me/IGAcdn3v",
+					"display_url": "naver.me/IGAcdn3v",
+					"indices": [
+						48,
+						71
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1331102402976440300,
+					"id_str": "1331102402976440322",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/EnkGlUEUYAIcC-o.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnkGlUEUYAIcC-o.jpg",
+					"url": "https://t.co/aFeepXIBrQ",
+					"display_url": "pic.twitter.com/aFeepXIBrQ",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1331102430457597952/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1365,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1331102402976440300,
+					"id_str": "1331102402976440322",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/EnkGlUEUYAIcC-o.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnkGlUEUYAIcC-o.jpg",
+					"url": "https://t.co/aFeepXIBrQ",
+					"display_url": "pic.twitter.com/aFeepXIBrQ",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1331102430457597952/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1365,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1331102403085488000,
+					"id_str": "1331102403085488130",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/EnkGlUeUUAI2W3F.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnkGlUeUUAI2W3F.jpg",
+					"url": "https://t.co/aFeepXIBrQ",
+					"display_url": "pic.twitter.com/aFeepXIBrQ",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1331102430457597952/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1365,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1331102403186159600,
+					"id_str": "1331102403186159616",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/EnkGlU2UcAAQZ7r.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnkGlU2UcAAQZ7r.jpg",
+					"url": "https://t.co/aFeepXIBrQ",
+					"display_url": "pic.twitter.com/aFeepXIBrQ",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1331102430457597952/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1366,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1331102403253289000,
+					"id_str": "1331102403253288962",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/EnkGlVGUwAI8o9P.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnkGlVGUwAI8o9P.jpg",
+					"url": "https://t.co/aFeepXIBrQ",
+					"display_url": "pic.twitter.com/aFeepXIBrQ",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1331102430457597952/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1366,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정\n4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. \n📸 고화질 아니면 죽음을 달라\n🌳주접은 그저 나무가 내뿜는 산소일 뿐\ninstagram: @namooactors\nyoutube: namooofficial",
+			"url": "https://t.co/pDrppn6MXI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pDrppn6MXI",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 33128,
+			"friends_count": 12,
+			"listed_count": 188,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 124,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13281,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 244,
+		"favorite_count": 380,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] '사생활' 서현X고경표, 화끈한 '꾼' 활약...반격도 사랑도 열일\n▶ https://t.co/1COBrSST3k\n\n#서현 #사생활 #SEOHYUN #PrivateLives https://t.co/aFeepXIBrQ"
+	},
+	{
 		"created_at": "Tue Nov 24 04:07:09 +0000 2020",
 		"id": 1331086071648505900,
 		"id_str": "1331086930570866688",
