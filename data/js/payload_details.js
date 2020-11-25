2a57d4e224ad34c8b314a7b4516294b680c436d9
SIGNO <iamsigno@users.noreply.github.com>
2020-11-25 16:07:52 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27932,
-	"created_at": "Wed Nov 25 06:07:50 +0000 2020",
+	"tweets": 27933,
+	"created_at": "Wed Nov 25 07:07:50 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,275 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Wed Nov 25 07:07:09 +0000 2020",
+		"id": 1331480103260283000,
+		"id_str": "1331494616756051968",
+		"full_text": "[#NEWS] '사생활' 서현X고경표X김효진X김영민, 최후의 야망 레이스 \n▶ https://t.co/NOlBIXZtg0\n　\n#서현 #김효진 #사생활 #SEOHYUN #KIMHYOJIN #PrivateLives https://t.co/ctJtoERuRP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			117
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						70,
+						73
+					]
+				},
+				{
+					"text": "김효진",
+					"indices": [
+						74,
+						78
+					]
+				},
+				{
+					"text": "사생활",
+					"indices": [
+						79,
+						83
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						84,
+						92
+					]
+				},
+				{
+					"text": "KIMHYOJIN",
+					"indices": [
+						93,
+						103
+					]
+				},
+				{
+					"text": "PrivateLives",
+					"indices": [
+						104,
+						117
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/NOlBIXZtg0",
+					"expanded_url": "http://naver.me/xhsw9dZZ",
+					"display_url": "naver.me/xhsw9dZZ",
+					"indices": [
+						44,
+						67
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1331480055273246700,
+					"id_str": "1331480055273246720",
+					"indices": [
+						118,
+						141
+					],
+					"media_url": "http://pbs.twimg.com/media/EnpeDkgUUAAd6xQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnpeDkgUUAAd6xQ.jpg",
+					"url": "https://t.co/ctJtoERuRP",
+					"display_url": "pic.twitter.com/ctJtoERuRP",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1331480103260282880/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1331480055273246700,
+					"id_str": "1331480055273246720",
+					"indices": [
+						118,
+						141
+					],
+					"media_url": "http://pbs.twimg.com/media/EnpeDkgUUAAd6xQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnpeDkgUUAAd6xQ.jpg",
+					"url": "https://t.co/ctJtoERuRP",
+					"display_url": "pic.twitter.com/ctJtoERuRP",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1331480103260282880/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1331480071131918300,
+					"id_str": "1331480071131918336",
+					"indices": [
+						118,
+						141
+					],
+					"media_url": "http://pbs.twimg.com/media/EnpeEflUcAA-z-c.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnpeEflUcAA-z-c.jpg",
+					"url": "https://t.co/ctJtoERuRP",
+					"display_url": "pic.twitter.com/ctJtoERuRP",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1331480103260282880/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정\n4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. \n📸 고화질 아니면 죽음을 달라\n🌳주접은 그저 나무가 내뿜는 산소일 뿐\ninstagram: @namooactors\nyoutube: namooofficial",
+			"url": "https://t.co/pDrppn6MXI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pDrppn6MXI",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 33157,
+			"friends_count": 12,
+			"listed_count": 189,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 124,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13285,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 148,
+		"favorite_count": 300,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] '사생활' 서현X고경표X김효진X김영민, 최후의 야망 레이스 \n▶ https://t.co/NOlBIXZtg0\n　\n#서현 #김효진 #사생활 #SEOHYUN #KIMHYOJIN #PrivateLives https://t.co/ctJtoERuRP"
+	},
+	{
 		"created_at": "Wed Nov 25 06:07:09 +0000 2020",
 		"id": 1331472007238598700,
 		"id_str": "1331479517664260096",
