1669322251a2b478aaf229b57756e03e8fd7d95f
SIGNO <iamsigno@users.noreply.github.com>
2020-12-02 16:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27954,
-	"created_at": "Tue Dec 01 07:07:50 +0000 2020",
+	"tweets": 27955,
+	"created_at": "Wed Dec 02 07:07:48 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,250 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Wed Dec 02 06:44:56 +0000 2020",
+		"id": 1334021982459883500,
+		"id_str": "1334025743869894656",
+		"full_text": "권유리 주연 멜로 드라마 &lt;이별유예, 일주일&gt; 메인 예고편 🎬\n2020년 12월 대개봉🎉\n\n#권유리 #KWONYURI #이별유예일주일 #소녀시대 #GirlsGeneration https://t.co/68XdhJVcxf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			103
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "권유리",
+					"indices": [
+						57,
+						61
+					]
+				},
+				{
+					"text": "KWONYURI",
+					"indices": [
+						62,
+						71
+					]
+				},
+				{
+					"text": "이별유예일주일",
+					"indices": [
+						72,
+						80
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						81,
+						86
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						87,
+						103
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1333694904640311300,
+					"id_str": "1333694904640311296",
+					"indices": [
+						104,
+						127
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1333694904640311296/img/SYXIQYb-Yt6b_f1K.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1333694904640311296/img/SYXIQYb-Yt6b_f1K.jpg",
+					"url": "https://t.co/68XdhJVcxf",
+					"display_url": "pic.twitter.com/68XdhJVcxf",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1334021982459883521/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1333694904640311300,
+					"id_str": "1333694904640311296",
+					"indices": [
+						104,
+						127
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1333694904640311296/img/SYXIQYb-Yt6b_f1K.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1333694904640311296/img/SYXIQYb-Yt6b_f1K.jpg",
+					"url": "https://t.co/68XdhJVcxf",
+					"display_url": "pic.twitter.com/68XdhJVcxf",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1334021982459883521/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 77661,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1333694904640311296/vid/1280x720/7dM0NZC3ekt9Xbvi.mp4?tag=13"
+							},
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1333694904640311296/vid/480x270/CxgGpERtlGuoo-Hw.mp4?tag=13"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1333694904640311296/pl/W3kLFhtAXhWuj6j0.m3u8?tag=13"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1333694904640311296/vid/640x360/MyglKxEC7f-V-Rk7.mp4?tag=13"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "http://t.co/6dDRF9Jo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/6dDRF9Jo",
+							"expanded_url": "http://girlsgeneration.smtown.com/",
+							"display_url": "girlsgeneration.smtown.com",
+							"indices": [
+								0,
+								20
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3604318,
+			"friends_count": 4,
+			"listed_count": 5095,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 50,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2203,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1593746984",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1056,
+		"favorite_count": 2841,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "권유리 주연 멜로 드라마 &lt;이별유예, 일주일&gt; 메인 예고편 🎬\n2020년 12월 대개봉🎉\n\n#권유리 #KWONYURI #이별유예일주일 #소녀시대 #GirlsGeneration https://t.co/68XdhJVcxf"
+	},
+	{
 		"created_at": "Tue Dec 01 07:07:09 +0000 2020",
 		"id": 1333668394168709000,
 		"id_str": "1333668944662716419",
