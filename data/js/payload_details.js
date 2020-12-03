372394b384c2595214ac45dc50fc64b19d139a08
SIGNO <iamsigno@users.noreply.github.com>
2020-12-03 11:07:49 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27959,
-	"created_at": "Thu Dec 03 01:07:49 +0000 2020",
+	"tweets": 27961,
+	"created_at": "Thu Dec 03 02:07:48 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,443 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Thu Dec 03 02:07:37 +0000 2020",
+		"id": 1334315666262777900,
+		"id_str": "1334318341461340167",
+		"full_text": "권유리, 웹예능 ‘유리한 식탁’ 성공적 마무리…만능 셰프 등극\n\n👉🏻https://t.co/7ILZrMxd66\n\n#유리 #YURI #유리한TV #유리한식탁 #소녀시대 #GirlsGeneration https://t.co/xlMudYjg1W",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			109
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "유리",
+					"indices": [
+						63,
+						66
+					]
+				},
+				{
+					"text": "YURI",
+					"indices": [
+						67,
+						72
+					]
+				},
+				{
+					"text": "유리한TV",
+					"indices": [
+						73,
+						79
+					]
+				},
+				{
+					"text": "유리한식탁",
+					"indices": [
+						80,
+						86
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						87,
+						92
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						93,
+						109
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/7ILZrMxd66",
+					"expanded_url": "http://naver.me/5dfJf80h",
+					"display_url": "naver.me/5dfJf80h",
+					"indices": [
+						38,
+						61
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1334314969798566000,
+					"id_str": "1334314969798565888",
+					"indices": [
+						110,
+						133
+					],
+					"media_url": "http://pbs.twimg.com/media/EoRwZUFUYAAhZ6N.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoRwZUFUYAAhZ6N.jpg",
+					"url": "https://t.co/xlMudYjg1W",
+					"display_url": "pic.twitter.com/xlMudYjg1W",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1334315666262777856/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1280,
+							"h": 1600,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1334314969798566000,
+					"id_str": "1334314969798565888",
+					"indices": [
+						110,
+						133
+					],
+					"media_url": "http://pbs.twimg.com/media/EoRwZUFUYAAhZ6N.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoRwZUFUYAAhZ6N.jpg",
+					"url": "https://t.co/xlMudYjg1W",
+					"display_url": "pic.twitter.com/xlMudYjg1W",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1334315666262777856/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1280,
+							"h": 1600,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "http://t.co/6dDRF9Jo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/6dDRF9Jo",
+							"expanded_url": "http://girlsgeneration.smtown.com/",
+							"display_url": "girlsgeneration.smtown.com",
+							"indices": [
+								0,
+								20
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3604558,
+			"friends_count": 4,
+			"listed_count": 5097,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 50,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2207,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1593746984",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 415,
+		"favorite_count": 1461,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "권유리, 웹예능 ‘유리한 식탁’ 성공적 마무리…만능 셰프 등극\n\n👉🏻https://t.co/7ILZrMxd66\n\n#유리 #YURI #유리한TV #유리한식탁 #소녀시대 #GirlsGeneration https://t.co/xlMudYjg1W"
+	},
+	{
+		"created_at": "Thu Dec 03 02:07:28 +0000 2020",
+		"id": 1334308605479149600,
+		"id_str": "1334318303163150340",
+		"full_text": "#TAEYEON เตรียมปล่อยมินิอัลบั้มชุดที่ 4 ‘What Do I Call You’ ในวันที่ 15 ธันวาคมนี้ เวลา 16.00 น. ตามเวลาไทย!\n\nโดยจะประกอบไปด้วยเพลงหลากหลายแนวทั้งหมด 6 เพลง!\n\n-\n\nTAEYEON 태연 The 4th mini Album [#What_Do_I_Call_You] \n\n🎧 2020.12.15. 4PM BKK\n\n#태연 #소녀시대 #GirlsGeneration https://t.co/cj9XViObsM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			266
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TAEYEON",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"text": "What_Do_I_Call_You",
+					"indices": [
+						194,
+						213
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						240,
+						243
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						244,
+						249
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						250,
+						266
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1334308597950386200,
+					"id_str": "1334308597950386177",
+					"indices": [
+						267,
+						290
+					],
+					"media_url": "http://pbs.twimg.com/media/EoRqmbGVEAExxXz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoRqmbGVEAExxXz.jpg",
+					"url": "https://t.co/cj9XViObsM",
+					"display_url": "pic.twitter.com/cj9XViObsM",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1334308605479149569/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1638,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1334308597950386200,
+					"id_str": "1334308597950386177",
+					"indices": [
+						267,
+						290
+					],
+					"media_url": "http://pbs.twimg.com/media/EoRqmbGVEAExxXz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoRqmbGVEAExxXz.jpg",
+					"url": "https://t.co/cj9XViObsM",
+					"display_url": "pic.twitter.com/cj9XViObsM",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1334308605479149569/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1638,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1093456514700828700,
+			"id_str": "1093456514700828672",
+			"name": "SM True",
+			"screen_name": "SMTrueThailand",
+			"location": "Bangkok, Thailand",
+			"description": "SM True Official Twitter Page",
+			"url": "https://t.co/fe1MwqbQdx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fe1MwqbQdx",
+							"expanded_url": "https://www.facebook.com/smtruethailand",
+							"display_url": "facebook.com/smtruethailand",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 82591,
+			"friends_count": 48,
+			"listed_count": 147,
+			"created_at": "Thu Feb 07 10:28:43 +0000 2019",
+			"favourites_count": 5,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 5344,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1093456514700828672/1562743620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 112,
+		"favorite_count": 52,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "th",
+		"text": "#TAEYEON เตรียมปล่อยมินิอัลบั้มชุดที่ 4 ‘What Do I Call You’ ในวันที่ 15 ธันวาคมนี้ เวลา 16.00 น. ตามเวลาไทย!\n\nโดยจะประกอบไปด้วยเพลงหลากหลายแนวทั้งหมด 6 เพลง!\n\n-\n\nTAEYEON 태연 The 4th mini Album [#What_Do_I_Call_You] \n\n🎧 2020.12.15. 4PM BKK\n\n#태연 #소녀시대 #GirlsGeneration https://t.co/cj9XViObsM"
+	},
+	{
 		"created_at": "Thu Dec 03 01:07:09 +0000 2020",
 		"id": 1334301824162082800,
 		"id_str": "1334303121695252482",
