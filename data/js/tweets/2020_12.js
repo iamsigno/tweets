9fed4717d5223d31df7359cc2c7991715332e23e
SIGNO <iamsigno@users.noreply.github.com>
2020-12-04 20:07:51 +0900

    Added 1 tweets

--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Fri Dec 04 11:07:09 +0000 2020",
+		"id": 1334806190740525000,
+		"id_str": "1334816504643268609",
+		"full_text": "TAEYEON The 4th Mini Album - What Do I Call You (Random cover ver.) ✨\n\n‘믿듣탱’ 태연 네 번째 미니앨범 ‘What Do I Call You’ 12월 15일 발매됩니다.\n지금 SMTOWN &amp;STORE에서 만나보세요!\n\nhttps://t.co/vmjyWeXtyw https://t.co/BNIDTqKPGo",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			180
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/vmjyWeXtyw",
+					"expanded_url": "https://bit.ly/37y68Zc",
+					"display_url": "bit.ly/37y68Zc",
+					"indices": [
+						157,
+						180
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1334805748262396000,
+					"id_str": "1334805748262395904",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/media/EoYuwYVUYAASlom.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoYuwYVUYAASlom.jpg",
+					"url": "https://t.co/BNIDTqKPGo",
+					"display_url": "pic.twitter.com/BNIDTqKPGo",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1334806190740525058/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1334805748262396000,
+					"id_str": "1334805748262395904",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/media/EoYuwYVUYAASlom.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoYuwYVUYAASlom.jpg",
+					"url": "https://t.co/BNIDTqKPGo",
+					"display_url": "pic.twitter.com/BNIDTqKPGo",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1334806190740525058/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1113723889245278200,
+			"id_str": "1113723889245278209",
+			"name": "SMTOWN &STORE",
+			"screen_name": "SMTOWNandSTORE",
+			"location": "",
+			"description": "SMTOWN &STORE Official Twitter Page",
+			"url": "https://t.co/Ee3t6o418Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Ee3t6o418Q",
+							"expanded_url": "http://www.SMTOWNandSTORE.com",
+							"display_url": "SMTOWNandSTORE.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 91124,
+			"friends_count": 0,
+			"listed_count": 166,
+			"created_at": "Thu Apr 04 08:44:01 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 992,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1113723889245278209/1554705745",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 126,
+		"favorite_count": 441,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "TAEYEON The 4th Mini Album - What Do I Call You (Random cover ver.) ✨\n\n‘믿듣탱’ 태연 네 번째 미니앨범 ‘What Do I Call You’ 12월 15일 발매됩니다.\n지금 SMTOWN &amp;STORE에서 만나보세요!\n\nhttps://t.co/vmjyWeXtyw https://t.co/BNIDTqKPGo"
+	},
+	{
 		"created_at": "Fri Dec 04 06:07:17 +0000 2020",
 		"id": 1334740936094404600,
 		"id_str": "1334741040826363904",
