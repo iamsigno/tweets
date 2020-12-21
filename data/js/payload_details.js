e3a73dd6f49b77db3f8254754cbfc67c941d8bd2
SIGNO <iamsigno@users.noreply.github.com>
2020-12-21 22:07:53 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28080,
-	"created_at": "Mon Dec 21 09:07:51 +0000 2020",
+	"tweets": 28081,
+	"created_at": "Mon Dec 21 13:07:52 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,281 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Mon Dec 21 13:07:09 +0000 2020",
+		"id": 1341001227149009000,
+		"id_str": "1341007298710401030",
+		"full_text": "#퀘스천마크 를 통해 나우에 첫 방문한 킹태연👑 제로도 함께 놀러왔어요✌🏻 호스트 #라비 와 물흐르듯 편안한 대화 넘나리 좋아따리~~ 명곡 가득한 새 앨범 비하인드도 직접 얘기해주고! 알차게 수다 떨고 간 울 탱구,,,언제든지 나우에 또 놀러와요💜 #태연 #NOW온에어 https://t.co/Ps943ZZ6Fp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			150
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "퀘스천마크",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "라비",
+					"indices": [
+						46,
+						49
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						139,
+						142
+					]
+				},
+				{
+					"text": "NOW온에어",
+					"indices": [
+						143,
+						150
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1341001220660478000,
+					"id_str": "1341001220660477954",
+					"indices": [
+						151,
+						174
+					],
+					"media_url": "http://pbs.twimg.com/media/EpwxgVjVQAITeIt.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EpwxgVjVQAITeIt.jpg",
+					"url": "https://t.co/Ps943ZZ6Fp",
+					"display_url": "pic.twitter.com/Ps943ZZ6Fp",
+					"expanded_url": "https://twitter.com/kr_now/status/1341001227149008898/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1638,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1341001220660478000,
+					"id_str": "1341001220660477954",
+					"indices": [
+						151,
+						174
+					],
+					"media_url": "http://pbs.twimg.com/media/EpwxgVjVQAITeIt.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EpwxgVjVQAITeIt.jpg",
+					"url": "https://t.co/Ps943ZZ6Fp",
+					"display_url": "pic.twitter.com/Ps943ZZ6Fp",
+					"expanded_url": "https://twitter.com/kr_now/status/1341001227149008898/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1638,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1341001220647882800,
+					"id_str": "1341001220647882752",
+					"indices": [
+						151,
+						174
+					],
+					"media_url": "http://pbs.twimg.com/media/EpwxgVgVEAA1SBb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EpwxgVgVEAA1SBb.jpg",
+					"url": "https://t.co/Ps943ZZ6Fp",
+					"display_url": "pic.twitter.com/Ps943ZZ6Fp",
+					"expanded_url": "https://twitter.com/kr_now/status/1341001227149008898/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1638,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1341001220656226300,
+					"id_str": "1341001220656226305",
+					"indices": [
+						151,
+						174
+					],
+					"media_url": "http://pbs.twimg.com/media/EpwxgViUYAER-FM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EpwxgViUYAER-FM.jpg",
+					"url": "https://t.co/Ps943ZZ6Fp",
+					"display_url": "pic.twitter.com/Ps943ZZ6Fp",
+					"expanded_url": "https://twitter.com/kr_now/status/1341001227149008898/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1638,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1159646410414817300,
+			"id_str": "1159646410414817281",
+			"name": "NOW.",
+			"screen_name": "kr_now",
+			"location": "",
+			"description": "네이버앱은 지금 방송 중 #네이버NOW",
+			"url": "https://t.co/PTf4ELLDJt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PTf4ELLDJt",
+							"expanded_url": "https://bit.ly/35YqTh2",
+							"display_url": "bit.ly/35YqTh2",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 72556,
+			"friends_count": 1,
+			"listed_count": 135,
+			"created_at": "Fri Aug 09 02:03:44 +0000 2019",
+			"favourites_count": 86,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2708,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1164016940198666242/ElqZ-oIg_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1164016940198666242/ElqZ-oIg_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1159646410414817281/1603177066",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 920,
+		"favorite_count": 1168,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#퀘스천마크 를 통해 나우에 첫 방문한 킹태연👑 제로도 함께 놀러왔어요✌🏻 호스트 #라비 와 물흐르듯 편안한 대화 넘나리 좋아따리~~ 명곡 가득한 새 앨범 비하인드도 직접 얘기해주고! 알차게 수다 떨고 간 울 탱구,,,언제든지 나우에 또 놀러와요💜 #태연 #NOW온에어 https://t.co/Ps943ZZ6Fp"
+	},
+	{
 		"created_at": "Mon Dec 21 09:07:09 +0000 2020",
 		"id": 1340945103628583000,
 		"id_str": "1340946900275638273",
