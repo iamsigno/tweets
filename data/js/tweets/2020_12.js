e5b7fb3ca1e718e2c3791d7aad0549861430fa1e
SIGNO <iamsigno@users.noreply.github.com>
2020-12-22 22:07:51 +0900

    Added 1 tweets

--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,240 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Tue Dec 22 13:07:09 +0000 2020",
+		"id": 1341367893036961800,
+		"id_str": "1341369688169848834",
+		"full_text": "TAEYEON 태연 'Playlist' Live Clip\n\n🎬 https://t.co/ZtLeMQxJ1X\n\n#TAEYEON #태연 #What_Do_I_Call_You #Playlist #소녀시대 #GirlsGeneration #이럴때엔_탱구노래가_역시적당할까 https://t.co/3dFyzfmiTS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			144
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TAEYEON",
+					"indices": [
+						60,
+						68
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						69,
+						72
+					]
+				},
+				{
+					"text": "What_Do_I_Call_You",
+					"indices": [
+						73,
+						92
+					]
+				},
+				{
+					"text": "Playlist",
+					"indices": [
+						93,
+						102
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						103,
+						108
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						109,
+						125
+					]
+				},
+				{
+					"text": "이럴때엔_탱구노래가_역시적당할까",
+					"indices": [
+						126,
+						144
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ZtLeMQxJ1X",
+					"expanded_url": "https://youtu.be/fNYxBpBo050",
+					"display_url": "youtu.be/fNYxBpBo050",
+					"indices": [
+						35,
+						58
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1341313830257750000,
+					"id_str": "1341313830257750016",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/Ep1N0nDU8AAnzaL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ep1N0nDU8AAnzaL.jpg",
+					"url": "https://t.co/3dFyzfmiTS",
+					"display_url": "pic.twitter.com/3dFyzfmiTS",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1341367893036961792/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1341313830257750000,
+					"id_str": "1341313830257750016",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/Ep1N0nDU8AAnzaL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ep1N0nDU8AAnzaL.jpg",
+					"url": "https://t.co/3dFyzfmiTS",
+					"display_url": "pic.twitter.com/3dFyzfmiTS",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1341367893036961792/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "http://t.co/6dDRF9Jo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/6dDRF9Jo",
+							"expanded_url": "http://girlsgeneration.smtown.com/",
+							"display_url": "girlsgeneration.smtown.com",
+							"indices": [
+								0,
+								20
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3617002,
+			"friends_count": 4,
+			"listed_count": 5162,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 54,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2278,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1278893652085895168/rWHaUUOr_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1593746984",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1804,
+		"favorite_count": 2311,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "TAEYEON 태연 'Playlist' Live Clip\n\n🎬 https://t.co/ZtLeMQxJ1X\n\n#TAEYEON #태연 #What_Do_I_Call_You #Playlist #소녀시대 #GirlsGeneration #이럴때엔_탱구노래가_역시적당할까 https://t.co/3dFyzfmiTS"
+	},
+	{
 		"created_at": "Tue Dec 22 09:07:09 +0000 2020",
 		"id": 1341307519507959800,
 		"id_str": "1341309287440773120",
