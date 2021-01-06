83e31783d0544a1bb94d9109998b49f5166e7922
SIGNO <iamsigno@users.noreply.github.com>
2021-01-06 16:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28131,
-	"created_at": "Wed Jan 06 04:07:49 +0000 2021",
+	"tweets": 28132,
+	"created_at": "Wed Jan 06 07:07:49 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Wed Jan 06 07:07:09 +0000 2021",
+		"id": 1346704906665693200,
+		"id_str": "1346714906964455424",
+		"full_text": "TAEYEON - The 4th Mini Album - What Do I Call You (LP Ver.) 🌼\n\nTAEYEON - The 4th Mini Album - What Do I Call You (LP Ver.)을 SMTOWN &amp;STORE 온라인 예약판매로 만나보세요! \n2월 25일 발매됩니다. \n- 예약판매기간 : 21. 01. 06(WED) ~ 01. 10(SUN)\n\nhttps://t.co/jJgT6zB1mU https://t.co/IXfzhZuDkx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			240
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/jJgT6zB1mU",
+					"expanded_url": "https://bit.ly/38hVR4U",
+					"display_url": "bit.ly/38hVR4U",
+					"indices": [
+						217,
+						240
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1346704486748672000,
+					"id_str": "1346704486748672005",
+					"indices": [
+						241,
+						264
+					],
+					"media_url": "http://pbs.twimg.com/media/ErB0mHYUUAUyGXL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ErB0mHYUUAUyGXL.jpg",
+					"url": "https://t.co/IXfzhZuDkx",
+					"display_url": "pic.twitter.com/IXfzhZuDkx",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1346704906665693187/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1346704486748672000,
+					"id_str": "1346704486748672005",
+					"indices": [
+						241,
+						264
+					],
+					"media_url": "http://pbs.twimg.com/media/ErB0mHYUUAUyGXL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ErB0mHYUUAUyGXL.jpg",
+					"url": "https://t.co/IXfzhZuDkx",
+					"display_url": "pic.twitter.com/IXfzhZuDkx",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1346704906665693187/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1113723889245278200,
+			"id_str": "1113723889245278209",
+			"name": "SMTOWN &STORE",
+			"screen_name": "SMTOWNandSTORE",
+			"location": "",
+			"description": "SMTOWN &STORE Official Twitter Page",
+			"url": "https://t.co/Ee3t6o418Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Ee3t6o418Q",
+							"expanded_url": "http://www.SMTOWNandSTORE.com",
+							"display_url": "SMTOWNandSTORE.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 102580,
+			"friends_count": 0,
+			"listed_count": 180,
+			"created_at": "Thu Apr 04 08:44:01 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1074,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1113723889245278209/1554705745",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 293,
+		"favorite_count": 670,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "TAEYEON - The 4th Mini Album - What Do I Call You (LP Ver.) 🌼\n\nTAEYEON - The 4th Mini Album - What Do I Call You (LP Ver.)을 SMTOWN &amp;STORE 온라인 예약판매로 만나보세요! \n2월 25일 발매됩니다. \n- 예약판매기간 : 21. 01. 06(WED) ~ 01. 10(SUN)\n\nhttps://t.co/jJgT6zB1mU https://t.co/IXfzhZuDkx"
+	},
+	{
 		"created_at": "Wed Jan 06 04:07:09 +0000 2021",
 		"id": 1346667864510513200,
 		"id_str": "1346669607801810949",
