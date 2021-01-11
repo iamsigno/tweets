90e86197bcf3a25b6779726e0daeb14a7b98023b
SIGNO <iamsigno@users.noreply.github.com>
2021-01-11 16:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28135,
-	"created_at": "Sat Jan 09 05:07:49 +0000 2021",
+	"tweets": 28136,
+	"created_at": "Mon Jan 11 07:07:49 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,232 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Mon Jan 11 07:07:09 +0000 2021",
+		"id": 1348520128166600700,
+		"id_str": "1348526846942523392",
+		"full_text": "[#NEWS] ‘런 온’ 임시완-신세경-최수영-강태오, 누구랑 있어도 케미 가득\n▶ https://t.co/K5sau2kBD7\n(사진제공=메이스 엔터테인먼트, 콘텐츠 지음)\n\n#신세경 #런온 #SHINSAEKYEONG #SHINSEKYUNG #RunOn https://t.co/m7wCLXkmtB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			141
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "신세경",
+					"indices": [
+						98,
+						102
+					]
+				},
+				{
+					"text": "런온",
+					"indices": [
+						103,
+						106
+					]
+				},
+				{
+					"text": "SHINSAEKYEONG",
+					"indices": [
+						107,
+						121
+					]
+				},
+				{
+					"text": "SHINSEKYUNG",
+					"indices": [
+						122,
+						134
+					]
+				},
+				{
+					"text": "RunOn",
+					"indices": [
+						135,
+						141
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/K5sau2kBD7",
+					"expanded_url": "http://naver.me/GttV3YnY",
+					"display_url": "naver.me/GttV3YnY",
+					"indices": [
+						47,
+						70
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1348520119333388300,
+					"id_str": "1348520119333388291",
+					"indices": [
+						142,
+						165
+					],
+					"media_url": "http://pbs.twimg.com/media/Erbn511VgAMRlvI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Erbn511VgAMRlvI.jpg",
+					"url": "https://t.co/m7wCLXkmtB",
+					"display_url": "pic.twitter.com/m7wCLXkmtB",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1348520128166600704/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1348520119333388300,
+					"id_str": "1348520119333388291",
+					"indices": [
+						142,
+						165
+					],
+					"media_url": "http://pbs.twimg.com/media/Erbn511VgAMRlvI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Erbn511VgAMRlvI.jpg",
+					"url": "https://t.co/m7wCLXkmtB",
+					"display_url": "pic.twitter.com/m7wCLXkmtB",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1348520128166600704/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정\n4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. \n📸 고화질 아니면 죽음을 달라\n🌳주접은 그저 나무가 내뿜는 산소일 뿐\ninstagram: @namooactors\nyoutube: namooofficial",
+			"url": "https://t.co/pDrppn6MXI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pDrppn6MXI",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 36499,
+			"friends_count": 12,
+			"listed_count": 210,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 124,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13448,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 16,
+		"favorite_count": 36,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] ‘런 온’ 임시완-신세경-최수영-강태오, 누구랑 있어도 케미 가득\n▶ https://t.co/K5sau2kBD7\n(사진제공=메이스 엔터테인먼트, 콘텐츠 지음)\n\n#신세경 #런온 #SHINSAEKYEONG #SHINSEKYUNG #RunOn https://t.co/m7wCLXkmtB"
+	},
+	{
 		"created_at": "Sat Jan 09 05:07:09 +0000 2021",
 		"id": 1347769444181790700,
 		"id_str": "1347771870809096202",
