7f256296d6604624bc4180ab7604b0f23024d767
SIGNO <iamsigno@users.noreply.github.com>
2021-01-15 15:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28138,
-	"created_at": "Fri Jan 15 05:07:48 +0000 2021",
+	"tweets": 28139,
+	"created_at": "Fri Jan 15 06:07:49 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,130 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Fri Jan 15 05:44:57 +0000 2021",
+		"id": 1349951491700879400,
+		"id_str": "1349955710784831489",
+		"full_text": "2021 Season's greetings🖤\n\nhttps://t.co/nA7yAXB1vb\n#2021시즌그리팅 #서현 #Seohyun https://t.co/Tpwg9s7fLE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "2021시즌그리팅",
+					"indices": [
+						50,
+						60
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						61,
+						64
+					]
+				},
+				{
+					"text": "Seohyun",
+					"indices": [
+						65,
+						73
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/nA7yAXB1vb",
+					"expanded_url": "http://namooshop.co.kr/shop/shopdetail.html?branduid=3349889&xcode=003&mcode=002&scode=&special=3&GfDT=a2h3UQ%3D%3D",
+					"display_url": "namooshop.co.kr/shop/shopdetai…",
+					"indices": [
+						26,
+						49
+					]
+				},
+				{
+					"url": "https://t.co/Tpwg9s7fLE",
+					"expanded_url": "https://www.instagram.com/p/CKDcuMsh3qj/?igshid=g6i0tdbypcxd",
+					"display_url": "instagram.com/p/CKDcuMsh3qj/…",
+					"indices": [
+						74,
+						97
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://instagram.com\" rel=\"nofollow\">Instagram</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2173631072,
+			"id_str": "2173631072",
+			"name": "서현(서주현)",
+			"screen_name": "sjhsjh0628",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1384573,
+			"friends_count": 31,
+			"listed_count": 2832,
+			"created_at": "Mon Nov 04 08:58:47 +0000 2013",
+			"favourites_count": 6,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1307,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1146333352682647552/0h6dsbYz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1146333352682647552/0h6dsbYz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2173631072/1526914876",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 344,
+		"favorite_count": 606,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "2021 Season's greetings🖤\n\nhttps://t.co/nA7yAXB1vb\n#2021시즌그리팅 #서현 #Seohyun https://t.co/Tpwg9s7fLE"
+	},
+	{
 		"created_at": "Fri Jan 15 05:07:09 +0000 2021",
 		"id": 1349945712402788400,
 		"id_str": "1349946198392770560",
