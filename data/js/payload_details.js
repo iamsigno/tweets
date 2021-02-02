685bf61d861bb2c72f4f8a8f7ea6b19e9cfb0b96
SIGNO <iamsigno@users.noreply.github.com>
2021-02-02 18:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28161,
-	"created_at": "Mon Feb 01 15:07:48 +0000 2021",
+	"tweets": 28162,
+	"created_at": "Tue Feb 02 09:07:48 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Tue Feb 02 09:07:18 +0000 2021",
+		"id": 1356528744941248500,
+		"id_str": "1356529616509403137",
+		"full_text": "TAEYEON - FILM KEYRING - What Do I Call You 💓\n\nTAEYEON - FILM KEYRING - What Do I Call You를 SMTOWN &amp;STORE 온라인 예약판매로 만나보세요!\n3월 26일 발매됩니다. \n\nhttps://t.co/Jp29Q6xuh3 https://t.co/FK2lNkIAcw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			166
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Jp29Q6xuh3",
+					"expanded_url": "https://bit.ly/2MimyhT",
+					"display_url": "bit.ly/2MimyhT",
+					"indices": [
+						143,
+						166
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1356528733851553800,
+					"id_str": "1356528733851553792",
+					"indices": [
+						167,
+						190
+					],
+					"media_url": "http://pbs.twimg.com/media/EtNbsjzVEAADZu6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EtNbsjzVEAADZu6.jpg",
+					"url": "https://t.co/FK2lNkIAcw",
+					"display_url": "pic.twitter.com/FK2lNkIAcw",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1356528744941248512/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1356528733851553800,
+					"id_str": "1356528733851553792",
+					"indices": [
+						167,
+						190
+					],
+					"media_url": "http://pbs.twimg.com/media/EtNbsjzVEAADZu6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EtNbsjzVEAADZu6.jpg",
+					"url": "https://t.co/FK2lNkIAcw",
+					"display_url": "pic.twitter.com/FK2lNkIAcw",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1356528744941248512/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1113723889245278200,
+			"id_str": "1113723889245278209",
+			"name": "SMTOWN &STORE",
+			"screen_name": "SMTOWNandSTORE",
+			"location": "",
+			"description": "SMTOWN &STORE Official Twitter Page",
+			"url": "https://t.co/Ee3t6o418Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Ee3t6o418Q",
+							"expanded_url": "http://www.SMTOWNandSTORE.com",
+							"display_url": "SMTOWNandSTORE.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 114893,
+			"friends_count": 0,
+			"listed_count": 202,
+			"created_at": "Thu Apr 04 08:44:01 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1160,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1113723889245278209/1554705745",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 18,
+		"favorite_count": 71,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "TAEYEON - FILM KEYRING - What Do I Call You 💓\n\nTAEYEON - FILM KEYRING - What Do I Call You를 SMTOWN &amp;STORE 온라인 예약판매로 만나보세요!\n3월 26일 발매됩니다. \n\nhttps://t.co/Jp29Q6xuh3 https://t.co/FK2lNkIAcw"
+	},
+	{
 		"created_at": "Mon Feb 01 14:44:56 +0000 2021",
 		"id": 1356242893334306800,
 		"id_str": "1356252199274352646",
