32274fca1899de2053a13924d017e3ec719a4a07
SIGNO <iamsigno@users.noreply.github.com>
2021-06-29 17:07:57 +0900

    Added 1 tweets

--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,224 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Tue Jun 29 08:07:14 +0000 2021",
+		"id": 1409780159427670000,
+		"id_str": "1409785517055545350",
+		"full_text": "TAEYEON Rilis Single ‘Weekend’ Tanggal 6 Juli 2021!\n\nCek informasi lengkapnya di Official Facebook SMTOWN Indonesia\n🔗https://t.co/QYjdWTu4EK\n\n#TAEYEON #Weekend\n#GirlsGeneration @GirlsGeneration https://t.co/lsqWxRhiiv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			193
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TAEYEON",
+					"indices": [
+						142,
+						150
+					]
+				},
+				{
+					"text": "Weekend",
+					"indices": [
+						151,
+						159
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						160,
+						176
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GirlsGeneration",
+					"name": "Girls' Generation",
+					"id": 391115625,
+					"id_str": "391115625",
+					"indices": [
+						177,
+						193
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/QYjdWTu4EK",
+					"expanded_url": "https://www.facebook.com/299527763997578/posts/842623249688024/",
+					"display_url": "facebook.com/29952776399757…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1409779882238742500,
+					"id_str": "1409779882238742531",
+					"indices": [
+						194,
+						217
+					],
+					"media_url": "http://pbs.twimg.com/media/E5CLVWRVEAM8QY2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5CLVWRVEAM8QY2.jpg",
+					"url": "https://t.co/lsqWxRhiiv",
+					"display_url": "pic.twitter.com/lsqWxRhiiv",
+					"expanded_url": "https://twitter.com/SMTOWN_Idn/status/1409780159427670016/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1409779882238742500,
+					"id_str": "1409779882238742531",
+					"indices": [
+						194,
+						217
+					],
+					"media_url": "http://pbs.twimg.com/media/E5CLVWRVEAM8QY2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5CLVWRVEAM8QY2.jpg",
+					"url": "https://t.co/lsqWxRhiiv",
+					"display_url": "pic.twitter.com/lsqWxRhiiv",
+					"expanded_url": "https://twitter.com/SMTOWN_Idn/status/1409780159427670016/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1064828006487609300,
+			"id_str": "1064828006487609345",
+			"name": "SMTOWN Indonesia",
+			"screen_name": "SMTOWN_Idn",
+			"location": "FX Sudirman Mall F5 #18B",
+			"description": "SM Entertainment Indonesia Official Twitter Page",
+			"url": "https://t.co/vbjfZGuM9Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vbjfZGuM9Y",
+							"expanded_url": "http://www.smtown.com",
+							"display_url": "smtown.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 563606,
+			"friends_count": 34,
+			"listed_count": 391,
+			"created_at": "Tue Nov 20 10:29:15 +0000 2018",
+			"favourites_count": 87,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2176,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1100927655610744832/D4iRym3b_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1100927655610744832/D4iRym3b_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1064828006487609345/1551316595",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 140,
+		"favorite_count": 614,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "TAEYEON Rilis Single ‘Weekend’ Tanggal 6 Juli 2021!\n\nCek informasi lengkapnya di Official Facebook SMTOWN Indonesia\n🔗https://t.co/QYjdWTu4EK\n\n#TAEYEON #Weekend\n#GirlsGeneration @GirlsGeneration https://t.co/lsqWxRhiiv"
+	},
+	{
 		"created_at": "Tue Jun 29 03:07:11 +0000 2021",
 		"id": 1409707623977406500,
 		"id_str": "1409710005897416710",
