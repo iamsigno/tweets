841febeddc3b5ebe27d7057d43db66eac3d63f62
SIGNO <iamsigno@users.noreply.github.com>
2021-07-01 13:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28230,
-	"created_at": "Wed Jun 30 16:07:48 +0000 2021",
+	"tweets": 28231,
+	"created_at": "Thu Jul 01 04:07:48 +0000 2021",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2021_07.js
@@ -0,0 +1,250 @@
+Grailbird.data.tweets_2021_07 = 
+[
+	{
+		"created_at": "Thu Jul 01 03:44:57 +0000 2021",
+		"id": 1410435935209021400,
+		"id_str": "1410444286018064387",
+		"full_text": "#태연, 신곡 ‘Weekend’ 발표 기념 스페셜 생방송 6일 오후5시 진행! 팬들과 컴백 카운트다운!\n\n#TAEYEON’s NAVER V LIVE, celebrating the release of her new song ‘Weekend’, to be livestreamed on 7/6 at 5PM KST! To have a comeback countdown with fans! \n\n#Weekend #소녀시대 #GirlsGeneration https://t.co/svM8Qj709e",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			246
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "태연",
+					"indices": [
+						0,
+						3
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						59,
+						67
+					]
+				},
+				{
+					"text": "Weekend",
+					"indices": [
+						215,
+						223
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						224,
+						229
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						230,
+						246
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1410435896701190100,
+					"id_str": "1410435896701190151",
+					"indices": [
+						247,
+						270
+					],
+					"media_url": "http://pbs.twimg.com/media/E5Lf-alVgAcrIO7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5Lf-alVgAcrIO7.jpg",
+					"url": "https://t.co/svM8Qj709e",
+					"display_url": "pic.twitter.com/svM8Qj709e",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1410435935209021443/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1649,
+							"h": 1100,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1410435896701190100,
+					"id_str": "1410435896701190151",
+					"indices": [
+						247,
+						270
+					],
+					"media_url": "http://pbs.twimg.com/media/E5Lf-alVgAcrIO7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5Lf-alVgAcrIO7.jpg",
+					"url": "https://t.co/svM8Qj709e",
+					"display_url": "pic.twitter.com/svM8Qj709e",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1410435935209021443/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1649,
+							"h": 1100,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1410435896696995800,
+					"id_str": "1410435896696995842",
+					"indices": [
+						247,
+						270
+					],
+					"media_url": "http://pbs.twimg.com/media/E5Lf-akVgAIke9W.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5Lf-akVgAIke9W.jpg",
+					"url": "https://t.co/svM8Qj709e",
+					"display_url": "pic.twitter.com/svM8Qj709e",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1410435935209021443/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 454,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1100,
+							"h": 1649,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "http://t.co/6dDRF9Jo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/6dDRF9Jo",
+							"expanded_url": "http://girlsgeneration.smtown.com/",
+							"display_url": "girlsgeneration.smtown.com",
+							"indices": [
+								0,
+								20
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3638403,
+			"friends_count": 4,
+			"listed_count": 5440,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2371,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1409693453227724807/jbaH6lvs_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1409693453227724807/jbaH6lvs_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1593746984",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2452,
+		"favorite_count": 5628,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#태연, 신곡 ‘Weekend’ 발표 기념 스페셜 생방송 6일 오후5시 진행! 팬들과 컴백 카운트다운!\n\n#TAEYEON’s NAVER V LIVE, celebrating the release of her new song ‘Weekend’, to be livestreamed on 7/6 at 5PM KST! To have a comeback countdown with fans! \n\n#Weekend #소녀시대 #GirlsGeneration https://t.co/svM8Qj709e"
+	}
+]
