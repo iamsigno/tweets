054b2449424449b9990faf35acd489eba901d5b9
SIGNO <iamsigno@users.noreply.github.com>
2021-07-11 17:07:50 +0900

    Added 1 tweets

--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,260 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Sun Jul 11 08:07:13 +0000 2021",
+		"id": 1414132353379733500,
+		"id_str": "1414134165973442565",
+		"full_text": "☁️💿💗인간 주말 탱구랑 주말엔 Weekend💗💿☁️\n\n#태연 #TAEYEON #Weekend\n#소녀시대 #GirlsGeneration\n#인기가요 https://t.co/rpnRIz7W36",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			81
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "태연",
+					"indices": [
+						31,
+						34
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						35,
+						43
+					]
+				},
+				{
+					"text": "Weekend",
+					"indices": [
+						44,
+						52
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						53,
+						58
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						59,
+						75
+					]
+				},
+				{
+					"text": "인기가요",
+					"indices": [
+						76,
+						81
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1414073766028611600,
+					"id_str": "1414073766028611587",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/E5_MmR5VkAMW5BY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5_MmR5VkAMW5BY.jpg",
+					"url": "https://t.co/rpnRIz7W36",
+					"display_url": "pic.twitter.com/rpnRIz7W36",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1414132353379733504/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 797,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 452,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1361,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1414073766028611600,
+					"id_str": "1414073766028611587",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/E5_MmR5VkAMW5BY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5_MmR5VkAMW5BY.jpg",
+					"url": "https://t.co/rpnRIz7W36",
+					"display_url": "pic.twitter.com/rpnRIz7W36",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1414132353379733504/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 797,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 452,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1361,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1414073766028529700,
+					"id_str": "1414073766028529667",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/E5_MmR5UUAMam7Z.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5_MmR5UUAMam7Z.jpg",
+					"url": "https://t.co/rpnRIz7W36",
+					"display_url": "pic.twitter.com/rpnRIz7W36",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1414132353379733504/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1318,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 438,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 772,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "http://t.co/6dDRF9Jo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/6dDRF9Jo",
+							"expanded_url": "http://girlsgeneration.smtown.com/",
+							"display_url": "girlsgeneration.smtown.com",
+							"indices": [
+								0,
+								20
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3645650,
+			"friends_count": 4,
+			"listed_count": 5447,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2415,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1409693453227724807/jbaH6lvs_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1409693453227724807/jbaH6lvs_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1593746984",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1935,
+		"favorite_count": 3241,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "☁️💿💗인간 주말 탱구랑 주말엔 Weekend💗💿☁️\n\n#태연 #TAEYEON #Weekend\n#소녀시대 #GirlsGeneration\n#인기가요 https://t.co/rpnRIz7W36"
+	},
+	{
 		"created_at": "Sun Jul 11 05:07:09 +0000 2021",
 		"id": 1414077861846151200,
 		"id_str": "1414088850549379075",
