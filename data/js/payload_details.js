9352f716db0ab131040418892a9e1073a4329100
SIGNO <iamsigno@users.noreply.github.com>
2021-08-01 16:07:53 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28335,
-	"created_at": "Sat Jul 31 03:07:50 +0000 2021",
+	"tweets": 28336,
+	"created_at": "Sun Aug 01 07:07:51 +0000 2021",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2021_08.js
@@ -0,0 +1,221 @@
+Grailbird.data.tweets_2021_08 = 
+[
+	{
+		"created_at": "Sun Aug 01 07:07:08 +0000 2021",
+		"id": 1421726598093361200,
+		"id_str": "1421729193646231555",
+		"full_text": "NAVER NOW.최초 #소녀시대 #효연 #댸니쇼 출격🥳 #동해 #은혁 과 찐 남매 케미 폭발할 예정🔥 게스트 맞춤 #생색정보통 👉 나 이만큼 효연 아낀다!!! 사랑한다 소시💖 \n사진과 함께 사연을 보내주세요💖 donkon486@naver.com 많관잘부😘 https://t.co/iqi1W0MGrk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			143
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "소녀시대",
+					"indices": [
+						13,
+						18
+					]
+				},
+				{
+					"text": "효연",
+					"indices": [
+						19,
+						22
+					]
+				},
+				{
+					"text": "댸니쇼",
+					"indices": [
+						23,
+						27
+					]
+				},
+				{
+					"text": "동해",
+					"indices": [
+						32,
+						35
+					]
+				},
+				{
+					"text": "은혁",
+					"indices": [
+						36,
+						39
+					]
+				},
+				{
+					"text": "생색정보통",
+					"indices": [
+						65,
+						71
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1421726554804015000,
+					"id_str": "1421726554804015104",
+					"indices": [
+						144,
+						167
+					],
+					"media_url": "http://pbs.twimg.com/media/E7r8xNrVUAA8uAC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7r8xNrVUAA8uAC.jpg",
+					"url": "https://t.co/iqi1W0MGrk",
+					"display_url": "pic.twitter.com/iqi1W0MGrk",
+					"expanded_url": "https://twitter.com/kr_now/status/1421726598093361155/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1366,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1421726554804015000,
+					"id_str": "1421726554804015104",
+					"indices": [
+						144,
+						167
+					],
+					"media_url": "http://pbs.twimg.com/media/E7r8xNrVUAA8uAC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7r8xNrVUAA8uAC.jpg",
+					"url": "https://t.co/iqi1W0MGrk",
+					"display_url": "pic.twitter.com/iqi1W0MGrk",
+					"expanded_url": "https://twitter.com/kr_now/status/1421726598093361155/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1366,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1159646410414817300,
+			"id_str": "1159646410414817281",
+			"name": "NOW.",
+			"screen_name": "kr_now",
+			"location": "",
+			"description": "네이버앱은 지금 방송 중 #네이버NOW",
+			"url": "https://t.co/PTf4ELLDJt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PTf4ELLDJt",
+							"expanded_url": "https://bit.ly/35YqTh2",
+							"display_url": "bit.ly/35YqTh2",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 143628,
+			"friends_count": 2,
+			"listed_count": 241,
+			"created_at": "Fri Aug 09 02:03:44 +0000 2019",
+			"favourites_count": 174,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 4185,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1164016940198666242/ElqZ-oIg_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1164016940198666242/ElqZ-oIg_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1159646410414817281/1603177066",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 39,
+		"favorite_count": 91,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "NAVER NOW.최초 #소녀시대 #효연 #댸니쇼 출격🥳 #동해 #은혁 과 찐 남매 케미 폭발할 예정🔥 게스트 맞춤 #생색정보통 👉 나 이만큼 효연 아낀다!!! 사랑한다 소시💖 \n사진과 함께 사연을 보내주세요💖 donkon486@naver.com 많관잘부😘 https://t.co/iqi1W0MGrk"
+	}
+]
