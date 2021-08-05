5531b2bb1efa5e96d2144514156cd4275256f6f2
SIGNO <iamsigno@users.noreply.github.com>
2021-08-05 18:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28353,
-	"created_at": "Thu Aug 05 07:07:47 +0000 2021",
+	"tweets": 28354,
+	"created_at": "Thu Aug 05 09:07:48 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,270 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Thu Aug 05 08:44:57 +0000 2021",
+		"id": 1423199292584456200,
+		"id_str": "1423203358718971908",
+		"full_text": "(ENG) 어머 얘 좀 봐라 얘 무슨 일이 있었길래 이름 틀렸대 어? 소시 데뷔 14주년 기념 파틴데 인물 퀴즈가 장악한 현장ㅋㅋㅋㅋㅋ / [문명특급 EP.203] \n\n👉🏻 https://t.co/bHvJULvDpS\n\n#효연 #HYO #Second \n#소녀시대 #GirlsGeneration\n#문명특급 https://t.co/5ZT3Z0ECjb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			168
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "효연",
+					"indices": [
+						122,
+						125
+					]
+				},
+				{
+					"text": "HYO",
+					"indices": [
+						126,
+						130
+					]
+				},
+				{
+					"text": "Second",
+					"indices": [
+						131,
+						138
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						140,
+						145
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						146,
+						162
+					]
+				},
+				{
+					"text": "문명특급",
+					"indices": [
+						163,
+						168
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/bHvJULvDpS",
+					"expanded_url": "https://youtu.be/2p-YJWs-THI",
+					"display_url": "youtu.be/2p-YJWs-THI",
+					"indices": [
+						97,
+						120
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1423199271130517500,
+					"id_str": "1423199271130517506",
+					"indices": [
+						169,
+						192
+					],
+					"media_url": "http://pbs.twimg.com/media/E8A4MlhUUAI_oar.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8A4MlhUUAI_oar.jpg",
+					"url": "https://t.co/5ZT3Z0ECjb",
+					"display_url": "pic.twitter.com/5ZT3Z0ECjb",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1423199292584456193/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 961,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 511,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 901,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1423199271130517500,
+					"id_str": "1423199271130517506",
+					"indices": [
+						169,
+						192
+					],
+					"media_url": "http://pbs.twimg.com/media/E8A4MlhUUAI_oar.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8A4MlhUUAI_oar.jpg",
+					"url": "https://t.co/5ZT3Z0ECjb",
+					"display_url": "pic.twitter.com/5ZT3Z0ECjb",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1423199292584456193/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 961,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 511,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 901,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1423199271134785500,
+					"id_str": "1423199271134785543",
+					"indices": [
+						169,
+						192
+					],
+					"media_url": "http://pbs.twimg.com/media/E8A4MliVcAc6iUb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8A4MliVcAc6iUb.jpg",
+					"url": "https://t.co/5ZT3Z0ECjb",
+					"display_url": "pic.twitter.com/5ZT3Z0ECjb",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1423199292584456193/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 961,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 901,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 511,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBUL3Ez",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBUL3Ez",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3655212,
+			"friends_count": 4,
+			"listed_count": 5490,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2442,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1422754170503794688/NQRUTTC6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1422754170503794688/NQRUTTC6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1628046003",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1780,
+		"favorite_count": 3775,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "(ENG) 어머 얘 좀 봐라 얘 무슨 일이 있었길래 이름 틀렸대 어? 소시 데뷔 14주년 기념 파틴데 인물 퀴즈가 장악한 현장ㅋㅋㅋㅋㅋ / [문명특급 EP.203] \n\n👉🏻 https://t.co/bHvJULvDpS\n\n#효연 #HYO #Second \n#소녀시대 #GirlsGeneration\n#문명특급 https://t.co/5ZT3Z0ECjb"
+	},
+	{
 		"created_at": "Thu Aug 05 06:44:56 +0000 2021",
 		"id": 1423165335092400000,
 		"id_str": "1423173158304104448",
