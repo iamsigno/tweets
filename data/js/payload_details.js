8f769d30a17b3bf543d75ce5fa68e8cffdc467c3
SIGNO <iamsigno@users.noreply.github.com>
2021-08-05 16:07:48 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28352,
-	"created_at": "Thu Aug 05 06:07:48 +0000 2021",
+	"tweets": 28353,
+	"created_at": "Thu Aug 05 07:07:47 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Thu Aug 05 06:44:56 +0000 2021",
+		"id": 1423165335092400000,
+		"id_str": "1423173158304104448",
+		"full_text": "효연 (HYO) 신곡 ‘Second’ 카운트다운 생방송 ‘Second’ COUNTDOWN LIVE” 9일 오후 5시 진행!\n\nHYO’s new song ‘Second’ release countdown LIVE to be aired on 8/9 5PM KST on NAVER V LIVE SMTOWN Channel!\n\n#효연 #HYO #Second \n#소녀시대 #GirlsGeneration https://t.co/ShIZYBC5fN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			217
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "효연",
+					"indices": [
+						177,
+						180
+					]
+				},
+				{
+					"text": "HYO",
+					"indices": [
+						181,
+						185
+					]
+				},
+				{
+					"text": "Second",
+					"indices": [
+						186,
+						193
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						195,
+						200
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						201,
+						217
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1423165329635569700,
+					"id_str": "1423165329635569674",
+					"indices": [
+						218,
+						241
+					],
+					"media_url": "http://pbs.twimg.com/media/E8AZU7mUcAo_KSo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8AZU7mUcAo_KSo.jpg",
+					"url": "https://t.co/ShIZYBC5fN",
+					"display_url": "pic.twitter.com/ShIZYBC5fN",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1423165335092400130/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1423165329635569700,
+					"id_str": "1423165329635569674",
+					"indices": [
+						218,
+						241
+					],
+					"media_url": "http://pbs.twimg.com/media/E8AZU7mUcAo_KSo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8AZU7mUcAo_KSo.jpg",
+					"url": "https://t.co/ShIZYBC5fN",
+					"display_url": "pic.twitter.com/ShIZYBC5fN",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1423165335092400130/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBUL3Ez",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBUL3Ez",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3655099,
+			"friends_count": 4,
+			"listed_count": 5489,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2441,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1422754170503794688/NQRUTTC6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1422754170503794688/NQRUTTC6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1628046003",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1682,
+		"favorite_count": 3704,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "효연 (HYO) 신곡 ‘Second’ 카운트다운 생방송 ‘Second’ COUNTDOWN LIVE” 9일 오후 5시 진행!\n\nHYO’s new song ‘Second’ release countdown LIVE to be aired on 8/9 5PM KST on NAVER V LIVE SMTOWN Channel!\n\n#효연 #HYO #Second \n#소녀시대 #GirlsGeneration https://t.co/ShIZYBC5fN"
+	},
+	{
 		"created_at": "Thu Aug 05 06:07:17 +0000 2021",
 		"id": 1423149527758827500,
 		"id_str": "1423163679923703809",
