22cb80d8dc7379967d10244bcfc2accf57663b7f
SIGNO <iamsigno@users.noreply.github.com>
2021-08-13 23:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28404,
-	"created_at": "Fri Aug 13 11:07:49 +0000 2021",
+	"tweets": 28405,
+	"created_at": "Fri Aug 13 14:07:49 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,150 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Fri Aug 13 13:44:56 +0000 2021",
+		"id": 1426163854199992300,
+		"id_str": "1426177958079832066",
+		"full_text": "영화 #거룩한밤_데몬헌터스 🔥 #HOLYNIGHT_DEMONHUNTERS\n#마동석 #서현 #이다윗 #경수진 #정지소\n(바우/샤론/김군/정원/은서) https://t.co/42NDePWVXX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			105
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "거룩한밤_데몬헌터스",
+					"indices": [
+						3,
+						14
+					]
+				},
+				{
+					"text": "HOLYNIGHT_DEMONHUNTERS",
+					"indices": [
+						17,
+						40
+					]
+				},
+				{
+					"text": "마동석",
+					"indices": [
+						41,
+						45
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						46,
+						49
+					]
+				},
+				{
+					"text": "이다윗",
+					"indices": [
+						50,
+						54
+					]
+				},
+				{
+					"text": "경수진",
+					"indices": [
+						55,
+						59
+					]
+				},
+				{
+					"text": "정지소",
+					"indices": [
+						60,
+						64
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/42NDePWVXX",
+					"expanded_url": "https://www.instagram.com/p/CSg-GFfB_Hz/?utm_medium=twitter",
+					"display_url": "instagram.com/p/CSg-GFfB_Hz/…",
+					"indices": [
+						82,
+						105
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://instagram.com\" rel=\"nofollow\">Instagram</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2173631072,
+			"id_str": "2173631072",
+			"name": "서현(Seohyun)",
+			"screen_name": "sjhsjh0628",
+			"location": "",
+			"description": "Singer / Actor",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1388228,
+			"friends_count": 31,
+			"listed_count": 2879,
+			"created_at": "Mon Nov 04 08:58:47 +0000 2013",
+			"favourites_count": 5,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1364,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1350274158945398785/6B2gOd8K_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1350274158945398785/6B2gOd8K_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2173631072/1617691854",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 327,
+		"favorite_count": 808,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "영화 #거룩한밤_데몬헌터스 🔥 #HOLYNIGHT_DEMONHUNTERS\n#마동석 #서현 #이다윗 #경수진 #정지소\n(바우/샤론/김군/정원/은서) https://t.co/42NDePWVXX"
+	},
+	{
 		"created_at": "Fri Aug 13 11:07:09 +0000 2021",
 		"id": 1426136624480473000,
 		"id_str": "1426138246988316674",
