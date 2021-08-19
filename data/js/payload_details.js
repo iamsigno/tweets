e7f642b36b01e9fcbce5bdc29e3cca82215f6015
SIGNO <iamsigno@users.noreply.github.com>
2021-08-19 20:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28428,
-	"created_at": "Thu Aug 19 09:07:48 +0000 2021",
+	"tweets": 28429,
+	"created_at": "Thu Aug 19 11:07:49 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,219 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Thu Aug 19 11:07:08 +0000 2021",
+		"id": 1428309966545248300,
+		"id_str": "1428312572583989250",
+		"full_text": "[#NEWS] 김희철X태연X홍현희X강기영의 반려견 동반 예능 ‘펫키지’.. 26일 첫방송\n▶ https://t.co/NRfKGAxUJr\n\n#강기영 #펫키지 #KANGKIYOUNG https://t.co/riwwbfUGiP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			99
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "강기영",
+					"indices": [
+						77,
+						81
+					]
+				},
+				{
+					"text": "펫키지",
+					"indices": [
+						82,
+						86
+					]
+				},
+				{
+					"text": "KANGKIYOUNG",
+					"indices": [
+						87,
+						99
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/NRfKGAxUJr",
+					"expanded_url": "http://naver.me/IgTwiPux",
+					"display_url": "naver.me/IgTwiPux",
+					"indices": [
+						52,
+						75
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1428309951550611500,
+					"id_str": "1428309951550611467",
+					"indices": [
+						100,
+						123
+					],
+					"media_url": "http://pbs.twimg.com/media/E9JgVVtUYAs8ZYp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9JgVVtUYAs8ZYp.jpg",
+					"url": "https://t.co/riwwbfUGiP",
+					"display_url": "pic.twitter.com/riwwbfUGiP",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1428309966545248287/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1448,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 848,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 481,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1428309951550611500,
+					"id_str": "1428309951550611467",
+					"indices": [
+						100,
+						123
+					],
+					"media_url": "http://pbs.twimg.com/media/E9JgVVtUYAs8ZYp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9JgVVtUYAs8ZYp.jpg",
+					"url": "https://t.co/riwwbfUGiP",
+					"display_url": "pic.twitter.com/riwwbfUGiP",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1428309966545248287/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1448,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 848,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 481,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정\n4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. \n📸 고화질 아니면 죽음을 달라\n🌳주접은 그저 나무가 내뿜는 산소일 뿐\ninstagram: @namooactors\nyoutube: namooofficial",
+			"url": "https://t.co/pDrppn6MXI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pDrppn6MXI",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 49929,
+			"friends_count": 13,
+			"listed_count": 249,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 124,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14342,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 24,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] 김희철X태연X홍현희X강기영의 반려견 동반 예능 ‘펫키지’.. 26일 첫방송\n▶ https://t.co/NRfKGAxUJr\n\n#강기영 #펫키지 #KANGKIYOUNG https://t.co/riwwbfUGiP"
+	},
+	{
 		"created_at": "Thu Aug 19 09:07:09 +0000 2021",
 		"id": 1428278092242428000,
 		"id_str": "1428282374844989443",
