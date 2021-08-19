ec24175a826ec6181ae4305ad9145d01e0d1c069
SIGNO <iamsigno@users.noreply.github.com>
2021-08-19 17:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28426,
-	"created_at": "Thu Aug 19 06:07:49 +0000 2021",
+	"tweets": 28427,
+	"created_at": "Thu Aug 19 08:07:49 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,262 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Thu Aug 19 08:07:16 +0000 2021",
+		"id": 1428265964731203600,
+		"id_str": "1428267306103316480",
+		"full_text": "[🎥#항공캠] 효연 - Second (Feat. 비비)\n\n#효연 #HYO #Second #세컨드 #소녀시대 #GirlsGeneration @Hyoyeon_djhyo @GirlsGeneration\n\n📌https://t.co/ZJybsa1K68 https://t.co/wUzh6uOVEB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			134
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "항공캠",
+					"indices": [
+						2,
+						6
+					]
+				},
+				{
+					"text": "효연",
+					"indices": [
+						32,
+						35
+					]
+				},
+				{
+					"text": "HYO",
+					"indices": [
+						36,
+						40
+					]
+				},
+				{
+					"text": "Second",
+					"indices": [
+						41,
+						48
+					]
+				},
+				{
+					"text": "세컨드",
+					"indices": [
+						49,
+						53
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						54,
+						59
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						60,
+						76
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Hyoyeon_djhyo",
+					"name": "Hyoyeon",
+					"id": 792687101842186200,
+					"id_str": "792687101842186240",
+					"indices": [
+						77,
+						91
+					]
+				},
+				{
+					"screen_name": "GirlsGeneration",
+					"name": "Girls' Generation",
+					"id": 391115625,
+					"id_str": "391115625",
+					"indices": [
+						92,
+						108
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/ZJybsa1K68",
+					"expanded_url": "https://youtu.be/8hkJ2sBubMA",
+					"display_url": "youtu.be/8hkJ2sBubMA",
+					"indices": [
+						111,
+						134
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1428265939280154600,
+					"id_str": "1428265939280154624",
+					"indices": [
+						135,
+						158
+					],
+					"media_url": "http://pbs.twimg.com/media/E9I4TfOVUAAyGMA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9I4TfOVUAAyGMA.jpg",
+					"url": "https://t.co/wUzh6uOVEB",
+					"display_url": "pic.twitter.com/wUzh6uOVEB",
+					"expanded_url": "https://twitter.com/kpop_sbs/status/1428265964731203585/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1428265939280154600,
+					"id_str": "1428265939280154624",
+					"indices": [
+						135,
+						158
+					],
+					"media_url": "http://pbs.twimg.com/media/E9I4TfOVUAAyGMA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9I4TfOVUAAyGMA.jpg",
+					"url": "https://t.co/wUzh6uOVEB",
+					"display_url": "pic.twitter.com/wUzh6uOVEB",
+					"expanded_url": "https://twitter.com/kpop_sbs/status/1428265964731203585/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1131475077650772000,
+			"id_str": "1131475077650771968",
+			"name": "스브스케이팝 / SBS KPOP",
+			"screen_name": "kpop_sbs",
+			"location": "",
+			"description": "♡ 스브스케이팝 / SBS KPOP 공식 트위터 ♡ \nSBS의 예능/음악방송을 여러분께 더 많이 공유하고 소통하기 위한 채널입니다 :)",
+			"url": "https://t.co/qapZ7nOCns",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qapZ7nOCns",
+							"expanded_url": "https://www.youtube.com/user/SBSMusic1",
+							"display_url": "youtube.com/user/SBSMusic1",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 492025,
+			"friends_count": 77,
+			"listed_count": 508,
+			"created_at": "Thu May 23 08:20:55 +0000 2019",
+			"favourites_count": 3531,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3471,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1232179341775138818/Ry7b4M3d_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1232179341775138818/Ry7b4M3d_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1131475077650771968/1628997571",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 34,
+		"favorite_count": 73,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[🎥#항공캠] 효연 - Second (Feat. 비비)\n\n#효연 #HYO #Second #세컨드 #소녀시대 #GirlsGeneration @Hyoyeon_djhyo @GirlsGeneration\n\n📌https://t.co/ZJybsa1K68 https://t.co/wUzh6uOVEB"
+	},
+	{
 		"created_at": "Thu Aug 19 06:07:09 +0000 2021",
 		"id": 1428224291938599000,
 		"id_str": "1428237077779517448",
