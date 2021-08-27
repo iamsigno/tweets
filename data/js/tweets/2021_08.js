2c768b0473081f4d38711589307d13d32a4344a8
SIGNO <iamsigno@users.noreply.github.com>
2021-08-27 19:07:51 +0900

    Added 1 tweets

--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,248 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Fri Aug 27 09:44:57 +0000 2021",
+		"id": 1431189303690813400,
+		"id_str": "1431190991566954497",
+		"full_text": "【#SONEJAPAN】メンバーの個性を特集したデジタルマガジン『SONE NOTE LIVE』のVol.33を更新致しました！今回のメンバーはサニー💛\nそんなサニーからのメッセージ動画をお届け！\nぜひチェックしてください♪\n※会員様限定コンテンツのため、転載禁止です。\n\n#少女時代\n#SUNNY\n#サニー\n#SONENOTELIVE https://t.co/e0udZi4G0r",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			168
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SONEJAPAN",
+					"indices": [
+						1,
+						11
+					]
+				},
+				{
+					"text": "少女時代",
+					"indices": [
+						137,
+						142
+					]
+				},
+				{
+					"text": "SUNNY",
+					"indices": [
+						143,
+						149
+					]
+				},
+				{
+					"text": "サニー",
+					"indices": [
+						150,
+						154
+					]
+				},
+				{
+					"text": "SONENOTELIVE",
+					"indices": [
+						155,
+						168
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1431189248774848500,
+					"id_str": "1431189248774848514",
+					"indices": [
+						169,
+						192
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1431189248774848514/pu/img/-FyoB952d8nEtNHG.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1431189248774848514/pu/img/-FyoB952d8nEtNHG.jpg",
+					"url": "https://t.co/e0udZi4G0r",
+					"display_url": "pic.twitter.com/e0udZi4G0r",
+					"expanded_url": "https://twitter.com/gg_official_jp/status/1431189303690813441/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1431189248774848500,
+					"id_str": "1431189248774848514",
+					"indices": [
+						169,
+						192
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1431189248774848514/pu/img/-FyoB952d8nEtNHG.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1431189248774848514/pu/img/-FyoB952d8nEtNHG.jpg",
+					"url": "https://t.co/e0udZi4G0r",
+					"display_url": "pic.twitter.com/e0udZi4G0r",
+					"expanded_url": "https://twitter.com/gg_official_jp/status/1431189303690813441/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 11100,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1431189248774848514/pu/pl/_wiBGPivZGey_zo3.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1431189248774848514/pu/vid/1280x720/LB1lJ4DgYkqgPw6w.mp4?tag=12"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1431189248774848514/pu/vid/480x270/FQvBj-FUbn44XuXX.mp4?tag=12"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1431189248774848514/pu/vid/640x360/nrhLR1oMhYcGOA2q.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2585025200,
+			"id_str": "2585025200",
+			"name": "少女時代 JAPAN OFFICIAL",
+			"screen_name": "gg_official_jp",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/nUrgRiOH3R",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/nUrgRiOH3R",
+							"expanded_url": "http://www.girls-generation.jp/",
+							"display_url": "girls-generation.jp",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 106946,
+			"friends_count": 0,
+			"listed_count": 499,
+			"created_at": "Tue Jun 24 03:33:35 +0000 2014",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 388,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1222720577078611968/5QXkaKkC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1222720577078611968/5QXkaKkC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2585025200/1505360281",
+			"profile_link_color": "0024B3",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 425,
+		"favorite_count": 724,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "【#SONEJAPAN】メンバーの個性を特集したデジタルマガジン『SONE NOTE LIVE』のVol.33を更新致しました！今回のメンバーはサニー💛\nそんなサニーからのメッセージ動画をお届け！\nぜひチェックしてください♪\n※会員様限定コンテンツのため、転載禁止です。\n\n#少女時代\n#SUNNY\n#サニー\n#SONENOTELIVE https://t.co/e0udZi4G0r"
+	},
+	{
 		"created_at": "Fri Aug 27 09:07:09 +0000 2021",
 		"id": 1431179687242485800,
 		"id_str": "1431181477841154053",
