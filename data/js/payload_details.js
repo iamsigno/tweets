0d3a07b189fa83e9b056bf699bb433476ba956f8
SIGNO <iamsigno@users.noreply.github.com>
2021-08-28 01:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28449,
-	"created_at": "Fri Aug 27 10:07:50 +0000 2021",
+	"tweets": 28450,
+	"created_at": "Fri Aug 27 16:07:48 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,238 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Fri Aug 27 15:07:15 +0000 2021",
+		"id": 1431270322297139200,
+		"id_str": "1431272100451557378",
+		"full_text": "KEY 키 Pre-release Single ‘Hate that… (Feat. TAEYEON)’\n\n➫ 2021.08.30 6PM (KST)\n\n#KEY #키 #SHINee #샤이니\n#TAEYEON #태연 \n#KEY_HateThat #HateThat https://t.co/GMm8Freswd",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			137
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "KEY",
+					"indices": [
+						79,
+						83
+					]
+				},
+				{
+					"text": "키",
+					"indices": [
+						84,
+						86
+					]
+				},
+				{
+					"text": "SHINee",
+					"indices": [
+						87,
+						94
+					]
+				},
+				{
+					"text": "샤이니",
+					"indices": [
+						95,
+						99
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						100,
+						108
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						109,
+						112
+					]
+				},
+				{
+					"text": "KEY_HateThat",
+					"indices": [
+						114,
+						127
+					]
+				},
+				{
+					"text": "HateThat",
+					"indices": [
+						128,
+						137
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1431262178145828900,
+					"id_str": "1431262178145828870",
+					"indices": [
+						138,
+						161
+					],
+					"media_url": "http://pbs.twimg.com/media/E9zdXizVgAY5iJM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9zdXizVgAY5iJM.jpg",
+					"url": "https://t.co/GMm8Freswd",
+					"display_url": "pic.twitter.com/GMm8Freswd",
+					"expanded_url": "https://twitter.com/SHINee/status/1431270322297139202/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1431262178145828900,
+					"id_str": "1431262178145828870",
+					"indices": [
+						138,
+						161
+					],
+					"media_url": "http://pbs.twimg.com/media/E9zdXizVgAY5iJM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9zdXizVgAY5iJM.jpg",
+					"url": "https://t.co/GMm8Freswd",
+					"display_url": "pic.twitter.com/GMm8Freswd",
+					"expanded_url": "https://twitter.com/SHINee/status/1431270322297139202/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 887973863824306200,
+			"id_str": "887973863824306176",
+			"name": "SHINee",
+			"screen_name": "SHINee",
+			"location": "SHINeeisBACK",
+			"description": "KEY 키 Pre-release Single ‘Hate that…’",
+			"url": "https://t.co/vhjnrVMUsq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vhjnrVMUsq",
+							"expanded_url": "https://www.youtube.com/SHINee",
+							"display_url": "youtube.com/SHINee",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2077604,
+			"friends_count": 2,
+			"listed_count": 5364,
+			"created_at": "Thu Jul 20 09:54:01 +0000 2017",
+			"favourites_count": 64,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1959,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1429820747787079680/jUQFbhCL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1429820747787079680/jUQFbhCL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/887973863824306176/1629730807",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10797,
+		"favorite_count": 22217,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "KEY 키 Pre-release Single ‘Hate that… (Feat. TAEYEON)’\n\n➫ 2021.08.30 6PM (KST)\n\n#KEY #키 #SHINee #샤이니\n#TAEYEON #태연 \n#KEY_HateThat #HateThat https://t.co/GMm8Freswd"
+	},
+	{
 		"created_at": "Fri Aug 27 09:44:57 +0000 2021",
 		"id": 1431189303690813400,
 		"id_str": "1431190991566954497",
