49186715f8c6898554dcb5d95e5e37498ea6e105
SIGNO <iamsigno@users.noreply.github.com>
2021-09-14 15:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28489,
-	"created_at": "Tue Sep 14 02:07:48 +0000 2021",
+	"tweets": 28490,
+	"created_at": "Tue Sep 14 06:07:48 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,255 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Tue Sep 14 06:07:08 +0000 2021",
+		"id": 1437651449622634500,
+		"id_str": "1437659160624119812",
+		"full_text": "[#NEWS] 마동석·서현·경수진 ‘거룩한 밤: 데몬 헌터스’ 크랭크업 [공식]\n▶ https://t.co/ZXxUUSJdZY\n\n#서현 #거룩한밤_데몬헌터스 #SEOHYUN https://t.co/CzhS99QJNY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			96
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						72,
+						75
+					]
+				},
+				{
+					"text": "거룩한밤_데몬헌터스",
+					"indices": [
+						76,
+						87
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						88,
+						96
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ZXxUUSJdZY",
+					"expanded_url": "http://naver.me/GlVdDUqB",
+					"display_url": "naver.me/GlVdDUqB",
+					"indices": [
+						47,
+						70
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1437651440290320400,
+					"id_str": "1437651440290320390",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/E_OQXjFVUAYa6S5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_OQXjFVUAYa6S5.jpg",
+					"url": "https://t.co/CzhS99QJNY",
+					"display_url": "pic.twitter.com/CzhS99QJNY",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1437651449622634499/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1437651440290320400,
+					"id_str": "1437651440290320390",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/E_OQXjFVUAYa6S5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_OQXjFVUAYa6S5.jpg",
+					"url": "https://t.co/CzhS99QJNY",
+					"display_url": "pic.twitter.com/CzhS99QJNY",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1437651449622634499/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1437651440302919700,
+					"id_str": "1437651440302919680",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/E_OQXjIVkAAu70v.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_OQXjIVkAAu70v.jpg",
+					"url": "https://t.co/CzhS99QJNY",
+					"display_url": "pic.twitter.com/CzhS99QJNY",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1437651449622634499/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1685,
+							"h": 1124,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정\n4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. \n📸 고화질 아니면 죽음을 달라\n🌳주접은 그저 나무가 내뿜는 산소일 뿐\ninstagram: @namooactors\nyoutube: namooofficial",
+			"url": "https://t.co/pDrppn6MXI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pDrppn6MXI",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 52002,
+			"friends_count": 13,
+			"listed_count": 1,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 124,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14461,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 118,
+		"favorite_count": 137,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] 마동석·서현·경수진 ‘거룩한 밤: 데몬 헌터스’ 크랭크업 [공식]\n▶ https://t.co/ZXxUUSJdZY\n\n#서현 #거룩한밤_데몬헌터스 #SEOHYUN https://t.co/CzhS99QJNY"
+	},
+	{
 		"created_at": "Tue Sep 14 02:07:08 +0000 2021",
 		"id": 1437594782553886700,
 		"id_str": "1437598762638139392",
