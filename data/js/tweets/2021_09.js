b90201ad3cf3c8fc4d10636aca1f2c2211c444f5
SIGNO <iamsigno@users.noreply.github.com>
2021-09-17 13:07:49 +0900

    Added 1 tweets

--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Fri Sep 17 04:07:08 +0000 2021",
+		"id": 1438705743780221000,
+		"id_str": "1438716125211201536",
+		"full_text": "Happy Birthday HYOYEON 💕\n\nGIRLS GENERATION-Oh!GG HYOYEON의 생일을 기념하는 ARTIST BIRTHDAY KEYRING을 SMTOWN &amp;STORE에서 만나 보실 수 있습니다.\n아티스트의 손글씨와 생일 달력참, 9월의 탄생석 컬러의 케이크 참으로 포인트를 준 제품입니다.\n로듐, 핑크 골드, 골드 세가지컬러로 만나보세요. https://t.co/xlwka0OhzP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			206
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1438705735735537700,
+					"id_str": "1438705735735537667",
+					"indices": [
+						207,
+						230
+					],
+					"media_url": "http://pbs.twimg.com/media/E_dPPnrVcAMwBFQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_dPPnrVcAMwBFQ.jpg",
+					"url": "https://t.co/xlwka0OhzP",
+					"display_url": "pic.twitter.com/xlwka0OhzP",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1438705743780220932/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1438705735735537700,
+					"id_str": "1438705735735537667",
+					"indices": [
+						207,
+						230
+					],
+					"media_url": "http://pbs.twimg.com/media/E_dPPnrVcAMwBFQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_dPPnrVcAMwBFQ.jpg",
+					"url": "https://t.co/xlwka0OhzP",
+					"display_url": "pic.twitter.com/xlwka0OhzP",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1438705743780220932/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1438705737195090000,
+					"id_str": "1438705737195089921",
+					"indices": [
+						207,
+						230
+					],
+					"media_url": "http://pbs.twimg.com/media/E_dPPtHUcAE11qs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_dPPtHUcAE11qs.jpg",
+					"url": "https://t.co/xlwka0OhzP",
+					"display_url": "pic.twitter.com/xlwka0OhzP",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1438705743780220932/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1113723889245278200,
+			"id_str": "1113723889245278209",
+			"name": "SMTOWN &STORE",
+			"screen_name": "SMTOWNandSTORE",
+			"location": "",
+			"description": "",
+			"url": "https://t.co/Ee3t6o418Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Ee3t6o418Q",
+							"expanded_url": "http://www.SMTOWNandSTORE.com",
+							"display_url": "SMTOWNandSTORE.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 167740,
+			"friends_count": 0,
+			"listed_count": 280,
+			"created_at": "Thu Apr 04 08:44:01 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1833,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1113723889245278209/1554705745",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 197,
+		"favorite_count": 401,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "Happy Birthday HYOYEON 💕\n\nGIRLS GENERATION-Oh!GG HYOYEON의 생일을 기념하는 ARTIST BIRTHDAY KEYRING을 SMTOWN &amp;STORE에서 만나 보실 수 있습니다.\n아티스트의 손글씨와 생일 달력참, 9월의 탄생석 컬러의 케이크 참으로 포인트를 준 제품입니다.\n로듐, 핑크 골드, 골드 세가지컬러로 만나보세요. https://t.co/xlwka0OhzP"
+	},
+	{
 		"created_at": "Fri Sep 17 03:07:11 +0000 2021",
 		"id": 1438689466311319600,
 		"id_str": "1438701034923249667",
