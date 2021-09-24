c2ddc0db9b3bac8fb7c1d55164401b755c648824
SIGNO <iamsigno@users.noreply.github.com>
2021-09-24 12:07:53 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28507,
-	"created_at": "Thu Sep 23 05:07:48 +0000 2021",
+	"tweets": 28508,
+	"created_at": "Fri Sep 24 03:07:52 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,235 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Fri Sep 24 03:07:09 +0000 2021",
+		"id": 1441235954086264800,
+		"id_str": "1441237742860390415",
+		"full_text": "평범하게 그리지 않는\n탱티스트 #태연 의 제로 머그잔( •̀ᴗ•́ )و ̑̑ \n\n▶️매주 목요일 밤 𝟭𝟬시 𝟯𝟬분\n&lt;#펫키지&gt; 본방사수🐶 https://t.co/5nuaJHrWyL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			82
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "태연",
+					"indices": [
+						17,
+						20
+					]
+				},
+				{
+					"text": "펫키지",
+					"indices": [
+						68,
+						72
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1441203014996402200,
+					"id_str": "1441203014996402186",
+					"indices": [
+						83,
+						106
+					],
+					"media_url": "http://pbs.twimg.com/media/FAAupaFVcAIyOlR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FAAupaFVcAIyOlR.jpg",
+					"url": "https://t.co/5nuaJHrWyL",
+					"display_url": "pic.twitter.com/5nuaJHrWyL",
+					"expanded_url": "https://twitter.com/jtbclove/status/1441235954086264837/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1441203014996402200,
+					"id_str": "1441203014996402186",
+					"indices": [
+						83,
+						106
+					],
+					"media_url": "http://pbs.twimg.com/media/FAAupaFVcAIyOlR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FAAupaFVcAIyOlR.jpg",
+					"url": "https://t.co/5nuaJHrWyL",
+					"display_url": "pic.twitter.com/5nuaJHrWyL",
+					"expanded_url": "https://twitter.com/jtbclove/status/1441235954086264837/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 64865,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1441203014996402186/vid/640x360/8CoY2VoQLDiT_56D.mp4?tag=14"
+							},
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1441203014996402186/vid/480x270/T3v-WcxqwEZAam3v.mp4?tag=14"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1441203014996402186/pl/4Ny3xB6SdVSdaTRs.m3u8?tag=14"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1441203014996402186/vid/1280x720/cIm7euP5PU42NNbW.mp4?tag=14"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "[펫키지] 평범하게 그리지 않는 탱티스트의 제로 머그잔( •̀ᴗ•́ )و ̑̑ | JTBC 210923 방송",
+						"description": "",
+						"call_to_actions": {
+							"visit_site": {
+								"url": "https://tv.naver.com/v/22585111"
+							}
+						},
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 418488729,
+			"id_str": "418488729",
+			"name": "@jtbclove",
+			"screen_name": "jtbclove",
+			"location": "Korea",
+			"description": "JTBC 공식 트위터",
+			"url": "https://t.co/320ZFCs3nQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/320ZFCs3nQ",
+							"expanded_url": "http://www.jtbc.co.kr",
+							"display_url": "jtbc.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 455740,
+			"friends_count": 111,
+			"listed_count": 753,
+			"created_at": "Tue Nov 22 07:18:13 +0000 2011",
+			"favourites_count": 595,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 23261,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1381488444128432131/05xHSfgm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1381488444128432131/05xHSfgm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/418488729/1630977575",
+			"profile_link_color": "2785BB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 28,
+		"favorite_count": 39,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "평범하게 그리지 않는\n탱티스트 #태연 의 제로 머그잔( •̀ᴗ•́ )و ̑̑ \n\n▶️매주 목요일 밤 𝟭𝟬시 𝟯𝟬분\n&lt;#펫키지&gt; 본방사수🐶 https://t.co/5nuaJHrWyL"
+	},
+	{
 		"created_at": "Thu Sep 23 04:45:05 +0000 2021",
 		"id": 1440893291076145200,
 		"id_str": "1440900001006227456",
