4612cb31b533fb7ae94e7fa3a4d491ec99cb5072
SIGNO <iamsigno@users.noreply.github.com>
2021-10-02 14:07:49 +0900

    Added 1 tweets

--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,169 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Sat Oct 02 05:07:09 +0000 2021",
+		"id": 1444159014514749400,
+		"id_str": "1444167043452899330",
+		"full_text": "[#VIDEO] [ENG SOON] 언니 같은 막내👶 #서현 의 인싸력 점수는 몇점? (feat. 전화 찬스)\n▶ https://t.co/2PcBDsjUpb\n\n#서현 #퍼스트룩 #메트로시티 #SEOHYUN #1stLook #Metrocity",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			134
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "VIDEO",
+					"indices": [
+						1,
+						7
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						30,
+						33
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						89,
+						92
+					]
+				},
+				{
+					"text": "퍼스트룩",
+					"indices": [
+						93,
+						98
+					]
+				},
+				{
+					"text": "메트로시티",
+					"indices": [
+						99,
+						105
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						106,
+						114
+					]
+				},
+				{
+					"text": "1stLook",
+					"indices": [
+						115,
+						123
+					]
+				},
+				{
+					"text": "Metrocity",
+					"indices": [
+						124,
+						134
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2PcBDsjUpb",
+					"expanded_url": "https://youtu.be/X0H1hfDikjY",
+					"display_url": "youtu.be/X0H1hfDikjY",
+					"indices": [
+						64,
+						87
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정\n4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. \n📸 고화질 아니면 죽음을 달라\n🌳주접은 그저 나무가 내뿜는 산소일 뿐\ninstagram: @namooactors\nyoutube: namooofficial",
+			"url": "https://t.co/pDrppn6MXI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pDrppn6MXI",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 52646,
+			"friends_count": 13,
+			"listed_count": 251,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 124,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14531,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 73,
+		"favorite_count": 93,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#VIDEO] [ENG SOON] 언니 같은 막내👶 #서현 의 인싸력 점수는 몇점? (feat. 전화 찬스)\n▶ https://t.co/2PcBDsjUpb\n\n#서현 #퍼스트룩 #메트로시티 #SEOHYUN #1stLook #Metrocity"
+	},
+	{
 		"created_at": "Fri Oct 01 03:07:23 +0000 2021",
 		"id": 1443772996917760000,
 		"id_str": "1443774515998339073",
