680f452c353fa8c14f729dd3e95cb049072fdce8
SIGNO <iamsigno@users.noreply.github.com>
2021-11-28 11:07:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28585,
-	"created_at": "Sat Nov 27 07:07:49 +0000 2021",
+	"tweets": 28586,
+	"created_at": "Sun Nov 28 02:07:50 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Sun Nov 28 02:07:09 +0000 2021",
+		"id": 1464773537802199000,
+		"id_str": "1464777853728444421",
+		"full_text": "[#🌳] 화면에서 과일 향 나요\n▶ https://t.co/glwB9oPTdy\n\n#서현 #SEOHYUN #NAMOOCONTENTS https://t.co/aDjIOo8aws",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			72
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "서현",
+					"indices": [
+						45,
+						48
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						49,
+						57
+					]
+				},
+				{
+					"text": "NAMOOCONTENTS",
+					"indices": [
+						58,
+						72
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/glwB9oPTdy",
+					"expanded_url": "http://naver.me/GVSyXObx",
+					"display_url": "naver.me/GVSyXObx",
+					"indices": [
+						20,
+						43
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1464169839401246700,
+					"id_str": "1464169839401246723",
+					"indices": [
+						73,
+						96
+					],
+					"media_url": "http://pbs.twimg.com/media/FFHGtiBaIAM0HMT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FFHGtiBaIAM0HMT.jpg",
+					"url": "https://t.co/aDjIOo8aws",
+					"display_url": "pic.twitter.com/aDjIOo8aws",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1464773537802199047/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1157,
+							"h": 1129,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1157,
+							"h": 1129,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 664,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1464169839401246700,
+					"id_str": "1464169839401246723",
+					"indices": [
+						73,
+						96
+					],
+					"media_url": "http://pbs.twimg.com/media/FFHGtiBaIAM0HMT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FFHGtiBaIAM0HMT.jpg",
+					"url": "https://t.co/aDjIOo8aws",
+					"display_url": "pic.twitter.com/aDjIOo8aws",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1464773537802199047/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1157,
+							"h": 1129,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1157,
+							"h": 1129,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 664,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 54667,
+			"friends_count": 13,
+			"listed_count": 264,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 124,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14763,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 83,
+		"favorite_count": 114,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#🌳] 화면에서 과일 향 나요\n▶ https://t.co/glwB9oPTdy\n\n#서현 #SEOHYUN #NAMOOCONTENTS https://t.co/aDjIOo8aws"
+	},
+	{
 		"created_at": "Sat Nov 27 07:07:08 +0000 2021",
 		"id": 1464489163793830000,
 		"id_str": "1464490961476767747",
