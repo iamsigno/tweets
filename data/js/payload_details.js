c30786bed46a66fc7e0c9f6379a9242425dbfe3c
SIGNO <iamsigno@users.noreply.github.com>
2021-12-01 09:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28592,
-	"created_at": "Tue Nov 30 09:07:48 +0000 2021",
+	"tweets": 28593,
+	"created_at": "Wed Dec 01 00:07:48 +0000 2021",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2021_12.js
@@ -0,0 +1,235 @@
+Grailbird.data.tweets_2021_12 = 
+[
+	{
+		"created_at": "Wed Dec 01 00:07:08 +0000 2021",
+		"id": 1465833113259364400,
+		"id_str": "1465834816872624129",
+		"full_text": "영화 &lt;해피뉴이어&gt; 포스터 공개 🎞\n\n#임윤아 #LIMYOONA\n#윤아 #YOONA\n#해피뉴이어 #happynewyear \n#소녀시대 #GirlsGeneration https://t.co/jRDHlnycAh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "임윤아",
+					"indices": [
+						27,
+						31
+					]
+				},
+				{
+					"text": "LIMYOONA",
+					"indices": [
+						32,
+						41
+					]
+				},
+				{
+					"text": "윤아",
+					"indices": [
+						42,
+						45
+					]
+				},
+				{
+					"text": "YOONA",
+					"indices": [
+						46,
+						52
+					]
+				},
+				{
+					"text": "해피뉴이어",
+					"indices": [
+						53,
+						59
+					]
+				},
+				{
+					"text": "happynewyear",
+					"indices": [
+						60,
+						73
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						75,
+						80
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						81,
+						97
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1465833086822666200,
+					"id_str": "1465833086822666242",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/FFevbRgUUAIUHpJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FFevbRgUUAIUHpJ.jpg",
+					"url": "https://t.co/jRDHlnycAh",
+					"display_url": "pic.twitter.com/jRDHlnycAh",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1465833113259364352/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1429,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 837,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 474,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1465833086822666200,
+					"id_str": "1465833086822666242",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/FFevbRgUUAIUHpJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FFevbRgUUAIUHpJ.jpg",
+					"url": "https://t.co/jRDHlnycAh",
+					"display_url": "pic.twitter.com/jRDHlnycAh",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1465833113259364352/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1429,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 837,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 474,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBUL3Ez",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBUL3Ez",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3673124,
+			"friends_count": 4,
+			"listed_count": 5592,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2518,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1422754170503794688/NQRUTTC6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1422754170503794688/NQRUTTC6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1628046003",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 441,
+		"favorite_count": 880,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "영화 &lt;해피뉴이어&gt; 포스터 공개 🎞\n\n#임윤아 #LIMYOONA\n#윤아 #YOONA\n#해피뉴이어 #happynewyear \n#소녀시대 #GirlsGeneration https://t.co/jRDHlnycAh"
+	}
+]
