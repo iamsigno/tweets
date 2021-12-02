1f616c51a595b93dc527485e6cd04beb925620ab
SIGNO <iamsigno@users.noreply.github.com>
2021-12-02 20:07:49 +0900

    Added 1 tweets

--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Thu Dec 02 11:07:09 +0000 2021",
+		"id": 1466361512176455700,
+		"id_str": "1466363299927310343",
+		"full_text": "[Weekly Playlist l 세로캠 Ver.] 위키미키(Weki Meki) 지수연&amp;엘리 - ＜사계 (Four Seasons)＞ (원곡 : 태연)♬\n\n세로캠으로 더 크게 만나는 위키미키(Weki Meki)의 감성보컬🎙 \n📽️https://t.co/xIGXaCJYeA\n\n#주간아이돌 #WeeklyPlaylist #위키미키 #세로캠 https://t.co/SZ4rPlkv6d",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			189
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "주간아이돌",
+					"indices": [
+						156,
+						162
+					]
+				},
+				{
+					"text": "WeeklyPlaylist",
+					"indices": [
+						163,
+						178
+					]
+				},
+				{
+					"text": "위키미키",
+					"indices": [
+						179,
+						184
+					]
+				},
+				{
+					"text": "세로캠",
+					"indices": [
+						185,
+						189
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/xIGXaCJYeA",
+					"expanded_url": "https://youtu.be/NrJSZpaJYOs",
+					"display_url": "youtu.be/NrJSZpaJYOs",
+					"indices": [
+						131,
+						154
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1466279651131609000,
+					"id_str": "1466279651131609090",
+					"indices": [
+						190,
+						213
+					],
+					"media_url": "http://pbs.twimg.com/media/FFlFkvMVUAITwRY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FFlFkvMVUAITwRY.jpg",
+					"url": "https://t.co/SZ4rPlkv6d",
+					"display_url": "pic.twitter.com/SZ4rPlkv6d",
+					"expanded_url": "https://twitter.com/ALLTHEKPOP/status/1466361512176455687/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1466279651131609000,
+					"id_str": "1466279651131609090",
+					"indices": [
+						190,
+						213
+					],
+					"media_url": "http://pbs.twimg.com/media/FFlFkvMVUAITwRY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FFlFkvMVUAITwRY.jpg",
+					"url": "https://t.co/SZ4rPlkv6d",
+					"display_url": "pic.twitter.com/SZ4rPlkv6d",
+					"expanded_url": "https://twitter.com/ALLTHEKPOP/status/1466361512176455687/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 744928926,
+			"id_str": "744928926",
+			"name": "ALL THE K-POP 올더케이팝",
+			"screen_name": "ALLTHEKPOP",
+			"location": "",
+			"description": "✨ALL about K-POP, ALL THE K-POP✨\n\n🎶 24/7 K-POP LIVE STREAMING\n\n💕 POPPIN' WEDNESDAY\n      📌 SHOW CHAMPION, 5PM KST\n      📌 WEEKLY IDOL, 8PM KST",
+			"url": "https://t.co/0kbjbCne8R",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0kbjbCne8R",
+							"expanded_url": "http://www.youtube.com/allthekpop",
+							"display_url": "youtube.com/allthekpop",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 99413,
+			"friends_count": 341,
+			"listed_count": 349,
+			"created_at": "Wed Aug 08 10:28:32 +0000 2012",
+			"favourites_count": 7887,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 8216,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035092488409632769/sDzylGOZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035092488409632769/sDzylGOZ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/744928926/1638145427",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[Weekly Playlist l 세로캠 Ver.] 위키미키(Weki Meki) 지수연&amp;엘리 - ＜사계 (Four Seasons)＞ (원곡 : 태연)♬\n\n세로캠으로 더 크게 만나는 위키미키(Weki Meki)의 감성보컬🎙 \n📽️https://t.co/xIGXaCJYeA\n\n#주간아이돌 #WeeklyPlaylist #위키미키 #세로캠 https://t.co/SZ4rPlkv6d"
+	},
+	{
 		"created_at": "Thu Dec 02 05:07:08 +0000 2021",
 		"id": 1466263355379777500,
 		"id_str": "1466272702231195651",
