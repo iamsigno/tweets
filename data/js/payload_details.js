b58065c5eaf52c7b7d599677d5524b7bf7b4a8b4
SIGNO <iamsigno@users.noreply.github.com>
2021-12-03 16:09:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28602,
-	"created_at": "Fri Dec 03 06:07:48 +0000 2021",
+	"tweets": 28603,
+	"created_at": "Fri Dec 03 07:08:49 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,227 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Fri Dec 03 07:07:08 +0000 2021",
+		"id": 1466664298244374500,
+		"id_str": "1466665288347361284",
+		"full_text": "'러브캐쳐 인 서울' TVING에서 만나요💘\n\n👉🏻https://t.co/kbNphvoAIY \n\n#써니 #SUNNY \n#소녀시대 #GirlsGeneration\n#러브캐쳐인서울 https://t.co/7riS8oE3cM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "써니",
+					"indices": [
+						54,
+						57
+					]
+				},
+				{
+					"text": "SUNNY",
+					"indices": [
+						58,
+						64
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						66,
+						71
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						72,
+						88
+					]
+				},
+				{
+					"text": "러브캐쳐인서울",
+					"indices": [
+						89,
+						97
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/kbNphvoAIY",
+					"expanded_url": "http://www.tving.com/vod/player/E003649968",
+					"display_url": "tving.com/vod/player/E00…",
+					"indices": [
+						28,
+						51
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1466664284130144300,
+					"id_str": "1466664284130144260",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/FFqjZUxUcAQ7xkP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FFqjZUxUcAQ7xkP.jpg",
+					"url": "https://t.co/7riS8oE3cM",
+					"display_url": "pic.twitter.com/7riS8oE3cM",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1466664298244374529/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1466664284130144300,
+					"id_str": "1466664284130144260",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/FFqjZUxUcAQ7xkP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FFqjZUxUcAQ7xkP.jpg",
+					"url": "https://t.co/7riS8oE3cM",
+					"display_url": "pic.twitter.com/7riS8oE3cM",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1466664298244374529/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBUL3Ez",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBUL3Ez",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3673020,
+			"friends_count": 4,
+			"listed_count": 5591,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2521,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1422754170503794688/NQRUTTC6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1422754170503794688/NQRUTTC6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1628046003",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 411,
+		"favorite_count": 864,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "'러브캐쳐 인 서울' TVING에서 만나요💘\n\n👉🏻https://t.co/kbNphvoAIY \n\n#써니 #SUNNY \n#소녀시대 #GirlsGeneration\n#러브캐쳐인서울 https://t.co/7riS8oE3cM"
+	},
+	{
 		"created_at": "Fri Dec 03 06:07:08 +0000 2021",
 		"id": 1466637358821159000,
 		"id_str": "1466650189528289283",
