cdf02561785b7df12d4ad3b232b81fd779138add
SIGNO <iamsigno@users.noreply.github.com>
2021-12-05 11:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28607,
-	"created_at": "Sun Dec 05 01:08:07 +0000 2021",
+	"tweets": 28608,
+	"created_at": "Sun Dec 05 02:07:48 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,266 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Sun Dec 05 02:07:09 +0000 2021",
+		"id": 1467312769590517800,
+		"id_str": "1467314568724688897",
+		"full_text": "[#IMAGE] SEOHYUN 2022 SEASON’S GREETINGS &lt;LIKE A SHADOW&gt; Teaser Image\n2021. 12. 06 (Mon) 11am(KST)\nComing Soon!\n\n#서현 #2022시즌그리팅 #LIKE_A_SHADOW \n#SEOHYUN #2022SEASONSGREETINGS #NAMOOACTORS https://t.co/9Rk3oGm4Aj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			193
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "IMAGE",
+					"indices": [
+						1,
+						7
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						119,
+						122
+					]
+				},
+				{
+					"text": "2022시즌그리팅",
+					"indices": [
+						123,
+						133
+					]
+				},
+				{
+					"text": "LIKE_A_SHADOW",
+					"indices": [
+						134,
+						148
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						150,
+						158
+					]
+				},
+				{
+					"text": "2022SEASONSGREETINGS",
+					"indices": [
+						159,
+						180
+					]
+				},
+				{
+					"text": "NAMOOACTORS",
+					"indices": [
+						181,
+						193
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1466698697719566300,
+					"id_str": "1466698697719566336",
+					"indices": [
+						194,
+						217
+					],
+					"media_url": "http://pbs.twimg.com/media/FFrCsdYaUAAk6Ss.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FFrCsdYaUAAk6Ss.jpg",
+					"url": "https://t.co/9Rk3oGm4Aj",
+					"display_url": "pic.twitter.com/9Rk3oGm4Aj",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1467312769590517762/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1600,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1466698697719566300,
+					"id_str": "1466698697719566336",
+					"indices": [
+						194,
+						217
+					],
+					"media_url": "http://pbs.twimg.com/media/FFrCsdYaUAAk6Ss.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FFrCsdYaUAAk6Ss.jpg",
+					"url": "https://t.co/9Rk3oGm4Aj",
+					"display_url": "pic.twitter.com/9Rk3oGm4Aj",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1467312769590517762/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1600,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1466698697849569300,
+					"id_str": "1466698697849569284",
+					"indices": [
+						194,
+						217
+					],
+					"media_url": "http://pbs.twimg.com/media/FFrCsd3aAAQ-Anj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FFrCsd3aAAQ-Anj.jpg",
+					"url": "https://t.co/9Rk3oGm4Aj",
+					"display_url": "pic.twitter.com/9Rk3oGm4Aj",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1467312769590517762/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 55154,
+			"friends_count": 13,
+			"listed_count": 262,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 124,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14800,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 76,
+		"favorite_count": 81,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "[#IMAGE] SEOHYUN 2022 SEASON’S GREETINGS &lt;LIKE A SHADOW&gt; Teaser Image\n2021. 12. 06 (Mon) 11am(KST)\nComing Soon!\n\n#서현 #2022시즌그리팅 #LIKE_A_SHADOW \n#SEOHYUN #2022SEASONSGREETINGS #NAMOOACTORS https://t.co/9Rk3oGm4Aj"
+	},
+	{
 		"created_at": "Sun Dec 05 01:07:08 +0000 2021",
 		"id": 1467297671840813000,
 		"id_str": "1467299467875467273",
