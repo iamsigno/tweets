0d0a8faabb7167a458f0ef14b8993fe75ebdc1d0
SIGNO <iamsigno@users.noreply.github.com>
2021-12-26 21:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28659,
-	"created_at": "Sun Dec 26 08:07:50 +0000 2021",
+	"tweets": 28660,
+	"created_at": "Sun Dec 26 12:07:49 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,262 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Sun Dec 26 12:07:08 +0000 2021",
+		"id": 1475074421631389700,
+		"id_str": "1475075707592224773",
+		"full_text": "#GirlsGeneration's Pink Blood Is Running!💗｜Pink Blood Quiz Show @ SMTOWN LIVE 2022\n\n👉🏻 https://t.co/COCaFSztnL \n\n#SMTOWN_LIVE #SMCU_EXPRESS #PinkBlood\n#SMTOWN #SMCU #KWANGYA\n#GirlsGeneration #TAEYEON #HYO https://t.co/ZIz1zkjNqk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			204
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"text": "SMTOWN_LIVE",
+					"indices": [
+						113,
+						125
+					]
+				},
+				{
+					"text": "SMCU_EXPRESS",
+					"indices": [
+						126,
+						139
+					]
+				},
+				{
+					"text": "PinkBlood",
+					"indices": [
+						140,
+						150
+					]
+				},
+				{
+					"text": "SMTOWN",
+					"indices": [
+						151,
+						158
+					]
+				},
+				{
+					"text": "SMCU",
+					"indices": [
+						159,
+						164
+					]
+				},
+				{
+					"text": "KWANGYA",
+					"indices": [
+						165,
+						173
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						174,
+						190
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						191,
+						199
+					]
+				},
+				{
+					"text": "HYO",
+					"indices": [
+						200,
+						204
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/COCaFSztnL",
+					"expanded_url": "https://youtu.be/fGTHILrxUM8",
+					"display_url": "youtu.be/fGTHILrxUM8",
+					"indices": [
+						87,
+						110
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1475074401402253300,
+					"id_str": "1475074401402253315",
+					"indices": [
+						205,
+						228
+					],
+					"media_url": "http://pbs.twimg.com/media/FHiEWlDacAM3g8f.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FHiEWlDacAM3g8f.jpg",
+					"url": "https://t.co/ZIz1zkjNqk",
+					"display_url": "pic.twitter.com/ZIz1zkjNqk",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1475074421631389697/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1475074401402253300,
+					"id_str": "1475074401402253315",
+					"indices": [
+						205,
+						228
+					],
+					"media_url": "http://pbs.twimg.com/media/FHiEWlDacAM3g8f.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FHiEWlDacAM3g8f.jpg",
+					"url": "https://t.co/ZIz1zkjNqk",
+					"display_url": "pic.twitter.com/ZIz1zkjNqk",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1475074421631389697/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBV3cSH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBV3cSH",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3688986,
+			"friends_count": 4,
+			"listed_count": 5625,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2552,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1469321995682660354/RuPtHUXm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1469321995682660354/RuPtHUXm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1638950614",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 864,
+		"favorite_count": 1121,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#GirlsGeneration's Pink Blood Is Running!💗｜Pink Blood Quiz Show @ SMTOWN LIVE 2022\n\n👉🏻 https://t.co/COCaFSztnL \n\n#SMTOWN_LIVE #SMCU_EXPRESS #PinkBlood\n#SMTOWN #SMCU #KWANGYA\n#GirlsGeneration #TAEYEON #HYO https://t.co/ZIz1zkjNqk"
+	},
+	{
 		"created_at": "Sun Dec 26 08:07:09 +0000 2021",
 		"id": 1475013763254284300,
 		"id_str": "1475015310533181442",
