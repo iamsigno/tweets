2ebc396741cbd8743cc8a10b1911fabec6959139
SIGNO <iamsigno@users.noreply.github.com>
2021-12-28 16:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28671,
-	"created_at": "Mon Dec 27 15:07:49 +0000 2021",
+	"tweets": 28672,
+	"created_at": "Tue Dec 28 07:07:50 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,342 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Tue Dec 28 07:07:26 +0000 2021",
+		"id": 1475724973884907500,
+		"id_str": "1475725061487308802",
+		"full_text": "ยูนิต GOT the beat โปรเจกต์ใหม่ของ SM เตรียมปล่อยเพลงใหม่ ‘Step Back’ วันที่ 3 มกราคมนี้ เวลา 16.00 น. (ไทย)\n\nเปิดเผยภาพทีเซอร์ BoA, TAEYEON, SEULGI, WINTER ที่ได้รับการพูดถึงอย่างร้อนแรง!\n\n💥 https://t.co/GwJwnoWuIG\n\n#GirlsOnTop #GOT #걸스온탑\n#GOT_the_beat #갓더비트  \n#StepBack https://t.co/R0yVY5VNfu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GirlsOnTop",
+					"indices": [
+						217,
+						228
+					]
+				},
+				{
+					"text": "GOT",
+					"indices": [
+						229,
+						233
+					]
+				},
+				{
+					"text": "걸스온탑",
+					"indices": [
+						234,
+						239
+					]
+				},
+				{
+					"text": "GOT_the_beat",
+					"indices": [
+						240,
+						253
+					]
+				},
+				{
+					"text": "갓더비트",
+					"indices": [
+						254,
+						259
+					]
+				},
+				{
+					"text": "StepBack",
+					"indices": [
+						262,
+						271
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/GwJwnoWuIG",
+					"expanded_url": "http://www.facebook.com/smtruethailand/posts/4727950787252832",
+					"display_url": "facebook.com/smtruethailand…",
+					"indices": [
+						192,
+						215
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1475723339905650700,
+					"id_str": "1475723339905650688",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/FHrSjxXUUAA6AlW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FHrSjxXUUAA6AlW.jpg",
+					"url": "https://t.co/R0yVY5VNfu",
+					"display_url": "pic.twitter.com/R0yVY5VNfu",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1475724973884907520/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1475723339905650700,
+					"id_str": "1475723339905650688",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/FHrSjxXUUAA6AlW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FHrSjxXUUAA6AlW.jpg",
+					"url": "https://t.co/R0yVY5VNfu",
+					"display_url": "pic.twitter.com/R0yVY5VNfu",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1475724973884907520/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1475723356280275000,
+					"id_str": "1475723356280274946",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/FHrSkuXVQAIU3Aq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FHrSkuXVQAIU3Aq.jpg",
+					"url": "https://t.co/R0yVY5VNfu",
+					"display_url": "pic.twitter.com/R0yVY5VNfu",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1475724973884907520/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1475723377708978200,
+					"id_str": "1475723377708978178",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/FHrSl-MVUAIBd8A.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FHrSl-MVUAIBd8A.jpg",
+					"url": "https://t.co/R0yVY5VNfu",
+					"display_url": "pic.twitter.com/R0yVY5VNfu",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1475724973884907520/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1475723392464478200,
+					"id_str": "1475723392464478208",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/FHrSm1KUYAA6bxr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FHrSm1KUYAA6bxr.jpg",
+					"url": "https://t.co/R0yVY5VNfu",
+					"display_url": "pic.twitter.com/R0yVY5VNfu",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1475724973884907520/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1093456514700828700,
+			"id_str": "1093456514700828672",
+			"name": "SM True",
+			"screen_name": "SMTrueThailand",
+			"location": "Bangkok, Thailand",
+			"description": "SM True Official Twitter Page",
+			"url": "https://t.co/fe1MwqbQdx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fe1MwqbQdx",
+							"expanded_url": "https://www.facebook.com/smtruethailand",
+							"display_url": "facebook.com/smtruethailand",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 90170,
+			"friends_count": 49,
+			"listed_count": 156,
+			"created_at": "Thu Feb 07 10:28:43 +0000 2019",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 10019,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1093456514700828672/1562743620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "th",
+		"text": "ยูนิต GOT the beat โปรเจกต์ใหม่ของ SM เตรียมปล่อยเพลงใหม่ ‘Step Back’ วันที่ 3 มกราคมนี้ เวลา 16.00 น. (ไทย)\n\nเปิดเผยภาพทีเซอร์ BoA, TAEYEON, SEULGI, WINTER ที่ได้รับการพูดถึงอย่างร้อนแรง!\n\n💥 https://t.co/GwJwnoWuIG\n\n#GirlsOnTop #GOT #걸스온탑\n#GOT_the_beat #갓더비트  \n#StepBack https://t.co/R0yVY5VNfu"
+	},
+	{
 		"created_at": "Mon Dec 27 15:07:25 +0000 2021",
 		"id": 1475482192168222700,
 		"id_str": "1475483463952343046",
