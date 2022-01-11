5cb975df4060eab50cd292f0e8f25a67e446af0a
SIGNO <iamsigno@users.noreply.github.com>
2022-01-11 16:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28702,
-	"created_at": "Tue Jan 11 04:07:48 +0000 2022",
+	"tweets": 28703,
+	"created_at": "Tue Jan 11 07:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,234 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Tue Jan 11 07:07:16 +0000 2022",
+		"id": 1480798093717115000,
+		"id_str": "1480798446193979398",
+		"full_text": "‘โวคอล ควีน’ #TAEYEON วง Girls’ Generation ส่งสัญญาณคัมแบ็ก ด้วยการปล่อยซิงเกิล ‘Can’t Control Myself’ จากอัลบั้มเต็มชุดที่ 3 ออกมาก่อน ในวันที่ 17 มกราคมนี้!!\n\n🔥 https://t.co/WPvUA0E1lm\n\n#태연\n#소녀시대 #GirlsGeneration\n#CantControlMyself #TAEYEON_CantControlMyself https://t.co/hsr2xPayRY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			260
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TAEYEON",
+					"indices": [
+						13,
+						21
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						188,
+						191
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						192,
+						197
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						198,
+						214
+					]
+				},
+				{
+					"text": "CantControlMyself",
+					"indices": [
+						215,
+						233
+					]
+				},
+				{
+					"text": "TAEYEON_CantControlMyself",
+					"indices": [
+						234,
+						260
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/WPvUA0E1lm",
+					"expanded_url": "http://www.facebook.com/smtruethailand/posts/4769290326452211",
+					"display_url": "facebook.com/smtruethailand…",
+					"indices": [
+						163,
+						186
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1480797785356050400,
+					"id_str": "1480797785356050432",
+					"indices": [
+						261,
+						284
+					],
+					"media_url": "http://pbs.twimg.com/media/FIzZvXxVIAAS13U.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FIzZvXxVIAAS13U.jpg",
+					"url": "https://t.co/hsr2xPayRY",
+					"display_url": "pic.twitter.com/hsr2xPayRY",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1480798093717114881/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1480797785356050400,
+					"id_str": "1480797785356050432",
+					"indices": [
+						261,
+						284
+					],
+					"media_url": "http://pbs.twimg.com/media/FIzZvXxVIAAS13U.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FIzZvXxVIAAS13U.jpg",
+					"url": "https://t.co/hsr2xPayRY",
+					"display_url": "pic.twitter.com/hsr2xPayRY",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1480798093717114881/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1093456514700828700,
+			"id_str": "1093456514700828672",
+			"name": "SM True",
+			"screen_name": "SMTrueThailand",
+			"location": "Bangkok, Thailand",
+			"description": "SM True Official Twitter Page",
+			"url": "https://t.co/fe1MwqbQdx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fe1MwqbQdx",
+							"expanded_url": "https://www.facebook.com/smtruethailand",
+							"display_url": "facebook.com/smtruethailand",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 90620,
+			"friends_count": 49,
+			"listed_count": 156,
+			"created_at": "Thu Feb 07 10:28:43 +0000 2019",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 10143,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1093456514700828672/1562743620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 22,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "th",
+		"text": "‘โวคอล ควีน’ #TAEYEON วง Girls’ Generation ส่งสัญญาณคัมแบ็ก ด้วยการปล่อยซิงเกิล ‘Can’t Control Myself’ จากอัลบั้มเต็มชุดที่ 3 ออกมาก่อน ในวันที่ 17 มกราคมนี้!!\n\n🔥 https://t.co/WPvUA0E1lm\n\n#태연\n#소녀시대 #GirlsGeneration\n#CantControlMyself #TAEYEON_CantControlMyself https://t.co/hsr2xPayRY"
+	},
+	{
 		"created_at": "Tue Jan 11 04:07:08 +0000 2022",
 		"id": 1480749828674138000,
 		"id_str": "1480753117226483714",
