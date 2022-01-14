aebd29d08052863e388f9ca30652ff6b197b9bab
SIGNO <iamsigno@users.noreply.github.com>
2022-01-14 20:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28718,
-	"created_at": "Fri Jan 14 07:07:48 +0000 2022",
+	"tweets": 28719,
+	"created_at": "Fri Jan 14 11:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,270 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Fri Jan 14 11:07:08 +0000 2022",
+		"id": 1481938111727042600,
+		"id_str": "1481945977040814082",
+		"full_text": "❤️‍🩹 MV เพลงใหม่ ‘#CantControlMyself’ ของ #TAEYEON เวอร์ชัน 30 วินาที จะฉายเป็นโฆษณาในโรงภาพยนตร์เกาหลีใต้ MEGABOX ทั่วประเทศ 87 สาขา ตลอดระยะเวลาหนึ่งเดือน เริ่มตั้งแต่วันที่ 18 มกราคมนี้!\n\nhttps://t.co/luiVUEoSer\n\n#태연\n#소녀시대 #GirlsGeneration\n #TAEYEON_CantControlMyself https://t.co/CQ54GFCt5p",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			270
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "CantControlMyself",
+					"indices": [
+						18,
+						36
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						42,
+						50
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						216,
+						219
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						220,
+						225
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						226,
+						242
+					]
+				},
+				{
+					"text": "TAEYEON_CantControlMyself",
+					"indices": [
+						244,
+						270
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/luiVUEoSer",
+					"expanded_url": "http://www.facebook.com/smtruethailand/posts/4780536215327622",
+					"display_url": "facebook.com/smtruethailand…",
+					"indices": [
+						191,
+						214
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1481937702853705700,
+					"id_str": "1481937702853705730",
+					"indices": [
+						271,
+						294
+					],
+					"media_url": "http://pbs.twimg.com/media/FJDmfTXagAISbKt.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJDmfTXagAISbKt.jpg",
+					"url": "https://t.co/CQ54GFCt5p",
+					"display_url": "pic.twitter.com/CQ54GFCt5p",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1481938111727042567/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1366,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 454,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1481937702853705700,
+					"id_str": "1481937702853705730",
+					"indices": [
+						271,
+						294
+					],
+					"media_url": "http://pbs.twimg.com/media/FJDmfTXagAISbKt.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJDmfTXagAISbKt.jpg",
+					"url": "https://t.co/CQ54GFCt5p",
+					"display_url": "pic.twitter.com/CQ54GFCt5p",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1481938111727042567/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1366,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 454,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1481937702908215300,
+					"id_str": "1481937702908215297",
+					"indices": [
+						271,
+						294
+					],
+					"media_url": "http://pbs.twimg.com/media/FJDmfTkaQAE3PMD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJDmfTkaQAE3PMD.jpg",
+					"url": "https://t.co/CQ54GFCt5p",
+					"display_url": "pic.twitter.com/CQ54GFCt5p",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1481938111727042567/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1365,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1093456514700828700,
+			"id_str": "1093456514700828672",
+			"name": "SM True",
+			"screen_name": "SMTrueThailand",
+			"location": "Bangkok, Thailand",
+			"description": "SM True Official Twitter Page",
+			"url": "https://t.co/fe1MwqbQdx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fe1MwqbQdx",
+							"expanded_url": "https://www.facebook.com/smtruethailand",
+							"display_url": "facebook.com/smtruethailand",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 90659,
+			"friends_count": 49,
+			"listed_count": 157,
+			"created_at": "Thu Feb 07 10:28:43 +0000 2019",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 10199,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1093456514700828672/1562743620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 96,
+		"favorite_count": 32,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "th",
+		"text": "❤️‍🩹 MV เพลงใหม่ ‘#CantControlMyself’ ของ #TAEYEON เวอร์ชัน 30 วินาที จะฉายเป็นโฆษณาในโรงภาพยนตร์เกาหลีใต้ MEGABOX ทั่วประเทศ 87 สาขา ตลอดระยะเวลาหนึ่งเดือน เริ่มตั้งแต่วันที่ 18 มกราคมนี้!\n\nhttps://t.co/luiVUEoSer\n\n#태연\n#소녀시대 #GirlsGeneration\n #TAEYEON_CantControlMyself https://t.co/CQ54GFCt5p"
+	},
+	{
 		"created_at": "Fri Jan 14 07:07:17 +0000 2022",
 		"id": 1481873762043961300,
 		"id_str": "1481885614031224834",
