3645458c813af2fdbec69e472fa457282c48deb9
SIGNO <iamsigno@users.noreply.github.com>
2022-02-02 20:07:48 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28779,
-	"created_at": "Tue Feb 01 11:07:48 +0000 2022",
+	"tweets": 28780,
+	"created_at": "Wed Feb 02 11:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,171 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Wed Feb 02 11:07:09 +0000 2022",
+		"id": 1488804386713530400,
+		"id_str": "1488831347573600256",
+		"full_text": "TAEYEON &amp; WINTER can’t take eyes off bigm_ss9 ‘s dance💥💥\n\nhttps://t.co/vu1PNhgKdB\n\n#GirlsOnTop #GOT #걸스온탑\n#GOT_the_beat #갓더비트 \n#StepBack #GOT_StepBack\n#StepBackChallenge\n#TAEYEON #WINTER",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			190
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GirlsOnTop",
+					"indices": [
+						87,
+						98
+					]
+				},
+				{
+					"text": "GOT",
+					"indices": [
+						99,
+						103
+					]
+				},
+				{
+					"text": "걸스온탑",
+					"indices": [
+						104,
+						109
+					]
+				},
+				{
+					"text": "GOT_the_beat",
+					"indices": [
+						110,
+						123
+					]
+				},
+				{
+					"text": "갓더비트",
+					"indices": [
+						124,
+						129
+					]
+				},
+				{
+					"text": "StepBack",
+					"indices": [
+						131,
+						140
+					]
+				},
+				{
+					"text": "GOT_StepBack",
+					"indices": [
+						141,
+						154
+					]
+				},
+				{
+					"text": "StepBackChallenge",
+					"indices": [
+						155,
+						173
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						174,
+						182
+					]
+				},
+				{
+					"text": "WINTER",
+					"indices": [
+						183,
+						190
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/vu1PNhgKdB",
+					"expanded_url": "https://vt.tiktok.com/ZSepRhkxE/",
+					"display_url": "vt.tiktok.com/ZSepRhkxE/",
+					"indices": [
+						62,
+						85
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1472876868416475100,
+			"id_str": "1472876868416475141",
+			"name": "Girls On Top",
+			"screen_name": "GirlsOnTop_SM",
+			"location": "",
+			"description": "Girls, bring it on",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 318813,
+			"friends_count": 5,
+			"listed_count": 670,
+			"created_at": "Mon Dec 20 10:29:56 +0000 2021",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 65,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1477124862812749824/ppJpxOL1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1477124862812749824/ppJpxOL1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1472876868416475141/1640530827",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3417,
+		"favorite_count": 8345,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "TAEYEON &amp; WINTER can’t take eyes off bigm_ss9 ‘s dance💥💥\n\nhttps://t.co/vu1PNhgKdB\n\n#GirlsOnTop #GOT #걸스온탑\n#GOT_the_beat #갓더비트 \n#StepBack #GOT_StepBack\n#StepBackChallenge\n#TAEYEON #WINTER"
+	},
+	{
 		"created_at": "Tue Feb 01 11:07:08 +0000 2022",
 		"id": 1488465114798125000,
 		"id_str": "1488468959209062400",
