0c013e763fd3feab4d635e6ded94a141c67e5af8
SIGNO <iamsigno@users.noreply.github.com>
2022-02-03 12:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28785,
-	"created_at": "Thu Feb 03 02:07:49 +0000 2022",
+	"tweets": 28786,
+	"created_at": "Thu Feb 03 03:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,334 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Thu Feb 03 03:07:14 +0000 2022",
+		"id": 1489064752030486500,
+		"id_str": "1489072964289146881",
+		"full_text": "[#NEWS] '모럴센스' 서현X이준영, 아슬아슬 심쿵 모먼트\n▶ https://t.co/hg1cznH82t\n\n#서현 #모럴센스 #SEOHYUN #LoveandLeashes https://t.co/HSqFBGo8Rf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			96
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						62,
+						65
+					]
+				},
+				{
+					"text": "모럴센스",
+					"indices": [
+						66,
+						71
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						72,
+						80
+					]
+				},
+				{
+					"text": "LoveandLeashes",
+					"indices": [
+						81,
+						96
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/hg1cznH82t",
+					"expanded_url": "http://naver.me/FRrX40lg",
+					"display_url": "naver.me/FRrX40lg",
+					"indices": [
+						37,
+						60
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1489064741343760400,
+					"id_str": "1489064741343760384",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/FKo4fitakAARlDr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKo4fitakAARlDr.jpg",
+					"url": "https://t.co/HSqFBGo8Rf",
+					"display_url": "pic.twitter.com/HSqFBGo8Rf",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1489064752030486536/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 799,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1364,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1489064741343760400,
+					"id_str": "1489064741343760384",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/FKo4fitakAARlDr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKo4fitakAARlDr.jpg",
+					"url": "https://t.co/HSqFBGo8Rf",
+					"display_url": "pic.twitter.com/HSqFBGo8Rf",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1489064752030486536/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 799,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1364,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1489064741339562000,
+					"id_str": "1489064741339561984",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/FKo4fisagAAYqgM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKo4fisagAAYqgM.jpg",
+					"url": "https://t.co/HSqFBGo8Rf",
+					"display_url": "pic.twitter.com/HSqFBGo8Rf",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1489064752030486536/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1364,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 799,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1489064741624778800,
+					"id_str": "1489064741624778754",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/FKo4fjwakAIPSDt.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKo4fjwakAIPSDt.jpg",
+					"url": "https://t.co/HSqFBGo8Rf",
+					"display_url": "pic.twitter.com/HSqFBGo8Rf",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1489064752030486536/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 799,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1364,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1489064741628969000,
+					"id_str": "1489064741628968961",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/FKo4fjxagAEaXmU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKo4fjxagAEaXmU.jpg",
+					"url": "https://t.co/HSqFBGo8Rf",
+					"display_url": "pic.twitter.com/HSqFBGo8Rf",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1489064752030486536/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1364,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 799,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 56873,
+			"friends_count": 13,
+			"listed_count": 267,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 124,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15021,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 193,
+		"favorite_count": 228,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] '모럴센스' 서현X이준영, 아슬아슬 심쿵 모먼트\n▶ https://t.co/hg1cznH82t\n\n#서현 #모럴센스 #SEOHYUN #LoveandLeashes https://t.co/HSqFBGo8Rf"
+	},
+	{
 		"created_at": "Thu Feb 03 02:07:24 +0000 2022",
 		"id": 1489047606319743000,
 		"id_str": "1489057904024076288",
