2801e1048515106ead6b97f403b9db462c97c167
SIGNO <iamsigno@users.noreply.github.com>
2022-02-04 12:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28796,
-	"created_at": "Fri Feb 04 02:07:48 +0000 2022",
+	"tweets": 28797,
+	"created_at": "Fri Feb 04 03:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,156 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Fri Feb 04 02:44:57 +0000 2022",
+		"id": 1489422192429666300,
+		"id_str": "1489429740582850561",
+		"full_text": "태연 정규 3집 ‘INVU’ 발매 기념 전시 ‘INVU : THE EXHIBIT’ 2월 7일 오픈!\n\nTAEYEON’s 3rd Album ‘INVU’ exhibition ‘INVU : THE EXHIBIT’ will open on February 7th!\n\nhttps://t.co/HlKTBbt8vv\n\n#태연 #TAEYEON \n#INVU #TAEYEON_INVU\n#소녀시대 #GirlsGeneration",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			225
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "태연",
+					"indices": [
+						169,
+						172
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						173,
+						181
+					]
+				},
+				{
+					"text": "INVU",
+					"indices": [
+						183,
+						188
+					]
+				},
+				{
+					"text": "TAEYEON_INVU",
+					"indices": [
+						189,
+						202
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						203,
+						208
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						209,
+						225
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/HlKTBbt8vv",
+					"expanded_url": "https://bit.ly/3L8wK5N",
+					"display_url": "bit.ly/3L8wK5N",
+					"indices": [
+						144,
+						167
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBV3cSH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBV3cSH",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3711295,
+			"friends_count": 4,
+			"listed_count": 5737,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2626,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1485990971917877258/ZyKbbdwA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1485990971917877258/ZyKbbdwA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1643122832",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1211,
+		"favorite_count": 2591,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "태연 정규 3집 ‘INVU’ 발매 기념 전시 ‘INVU : THE EXHIBIT’ 2월 7일 오픈!\n\nTAEYEON’s 3rd Album ‘INVU’ exhibition ‘INVU : THE EXHIBIT’ will open on February 7th!\n\nhttps://t.co/HlKTBbt8vv\n\n#태연 #TAEYEON \n#INVU #TAEYEON_INVU\n#소녀시대 #GirlsGeneration"
+	},
+	{
 		"created_at": "Fri Feb 04 02:07:18 +0000 2022",
 		"id": 1489414891895398400,
 		"id_str": "1489420267793309700",
