cd14df759166a3c5475fb0d9544482f5e7468018
SIGNO <iamsigno@users.noreply.github.com>
2022-02-08 16:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28828,
-	"created_at": "Tue Feb 08 04:07:48 +0000 2022",
+	"tweets": 28829,
+	"created_at": "Tue Feb 08 07:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,270 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Tue Feb 08 07:07:09 +0000 2022",
+		"id": 1490938871072260000,
+		"id_str": "1490945278358216707",
+		"full_text": "อัลบั้มใหม่ ‘INVU’ ของ TAEYEON ประกอบไปด้วย 13 เพลงหลากหลายแนวที่สร้างสรรค์ออกมาอย่างยอดเยี่ยม!\n\n💖 https://t.co/GHKgZufztJ\n\n#태연 #TAEYEON \n#INVU #TAEYEON_INVU\n#소녀시대 #GirlsGeneration https://t.co/5btEPVVbVO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			180
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "태연",
+					"indices": [
+						124,
+						127
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						128,
+						136
+					]
+				},
+				{
+					"text": "INVU",
+					"indices": [
+						138,
+						143
+					]
+				},
+				{
+					"text": "TAEYEON_INVU",
+					"indices": [
+						144,
+						157
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						158,
+						163
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						164,
+						180
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/GHKgZufztJ",
+					"expanded_url": "https://www.facebook.com/smtruethailand/posts/4879316942116215",
+					"display_url": "facebook.com/smtruethailand…",
+					"indices": [
+						99,
+						122
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1490938595326120000,
+					"id_str": "1490938595326119936",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/media/FLDgwMxVUAADm6V.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLDgwMxVUAADm6V.jpg",
+					"url": "https://t.co/5btEPVVbVO",
+					"display_url": "pic.twitter.com/5btEPVVbVO",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1490938871072260096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1365,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1490938595326120000,
+					"id_str": "1490938595326119936",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/media/FLDgwMxVUAADm6V.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLDgwMxVUAADm6V.jpg",
+					"url": "https://t.co/5btEPVVbVO",
+					"display_url": "pic.twitter.com/5btEPVVbVO",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1490938871072260096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1365,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1490938595334525000,
+					"id_str": "1490938595334524929",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/media/FLDgwMzVkAEHeNm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLDgwMzVkAEHeNm.jpg",
+					"url": "https://t.co/5btEPVVbVO",
+					"display_url": "pic.twitter.com/5btEPVVbVO",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1490938871072260096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1365,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1093456514700828700,
+			"id_str": "1093456514700828672",
+			"name": "SM True",
+			"screen_name": "SMTrueThailand",
+			"location": "Bangkok, Thailand",
+			"description": "SM True Official Twitter Page",
+			"url": "https://t.co/fe1MwqbQdx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fe1MwqbQdx",
+							"expanded_url": "https://www.facebook.com/smtruethailand",
+							"display_url": "facebook.com/smtruethailand",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 91027,
+			"friends_count": 49,
+			"listed_count": 157,
+			"created_at": "Thu Feb 07 10:28:43 +0000 2019",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 10340,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1093456514700828672/1562743620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 40,
+		"favorite_count": 18,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "th",
+		"text": "อัลบั้มใหม่ ‘INVU’ ของ TAEYEON ประกอบไปด้วย 13 เพลงหลากหลายแนวที่สร้างสรรค์ออกมาอย่างยอดเยี่ยม!\n\n💖 https://t.co/GHKgZufztJ\n\n#태연 #TAEYEON \n#INVU #TAEYEON_INVU\n#소녀시대 #GirlsGeneration https://t.co/5btEPVVbVO"
+	},
+	{
 		"created_at": "Tue Feb 08 04:07:17 +0000 2022",
 		"id": 1490898939994075100,
 		"id_str": "1490900012330622978",
