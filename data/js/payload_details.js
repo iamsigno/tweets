435e2f5c1695a08e179f45dcf2ff1b0862c2ebe1
SIGNO <iamsigno@users.noreply.github.com>
2022-02-11 13:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28853,
-	"created_at": "Fri Feb 11 03:07:49 +0000 2022",
+	"tweets": 28854,
+	"created_at": "Fri Feb 11 04:07:49 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Fri Feb 11 03:44:59 +0000 2022",
+		"id": 1491980429276205000,
+		"id_str": "1491981563785105423",
+		"full_text": "D-day!!\n오늘 오후 5시에 영화 #모럴센스 가  넷플릭스에서 공개됩니다 이따만나요👠\n#Netflix #LoveAndLeashes https://t.co/QtfoGTr1jj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			98
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "모럴센스",
+					"indices": [
+						21,
+						26
+					]
+				},
+				{
+					"text": "Netflix",
+					"indices": [
+						50,
+						58
+					]
+				},
+				{
+					"text": "LoveAndLeashes",
+					"indices": [
+						59,
+						74
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/QtfoGTr1jj",
+					"expanded_url": "https://www.instagram.com/p/CZ0oAbBB39n/?utm_medium=twitter",
+					"display_url": "instagram.com/p/CZ0oAbBB39n/…",
+					"indices": [
+						75,
+						98
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://instagram.com\" rel=\"nofollow\">Instagram</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2173631072,
+			"id_str": "2173631072",
+			"name": "서현(Seohyun)",
+			"screen_name": "sjhsjh0628",
+			"location": "",
+			"description": "Singer / Actor",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1397033,
+			"friends_count": 31,
+			"listed_count": 2912,
+			"created_at": "Mon Nov 04 08:58:47 +0000 2013",
+			"favourites_count": 5,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1385,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1350274158945398785/6B2gOd8K_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1350274158945398785/6B2gOd8K_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2173631072/1617691854",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 326,
+		"favorite_count": 666,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "D-day!!\n오늘 오후 5시에 영화 #모럴센스 가  넷플릭스에서 공개됩니다 이따만나요👠\n#Netflix #LoveAndLeashes https://t.co/QtfoGTr1jj"
+	},
+	{
 		"created_at": "Fri Feb 11 03:07:14 +0000 2022",
 		"id": 1491971406799978500,
 		"id_str": "1491972064131141649",
