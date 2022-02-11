4de9368242be0fd904fe6adcc65d1a648998cc7e
SIGNO <iamsigno@users.noreply.github.com>
2022-02-11 12:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28852,
-	"created_at": "Fri Feb 11 02:07:50 +0000 2022",
+	"tweets": 28853,
+	"created_at": "Fri Feb 11 03:07:49 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,259 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Fri Feb 11 03:07:14 +0000 2022",
+		"id": 1491971406799978500,
+		"id_str": "1491972064131141649",
+		"full_text": "💜#INVU : THE MESSAGE💜\n \n#TAEYEON has left special messages for S♡NEs! Don't forget to follow [This Is TAEYEON] playlist and pre-save ‘INVU’!\n \nhttps://t.co/QgOE91xMj3\nhttps://t.co/2o4S1fYe8m\nhttps://t.co/VudjlthN9z\n \n#TAEYEON_INVU\n#GirlsGeneration\n@SpotifyKDaebak @SpotifyKR https://t.co/V0st0n5g3E",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			274
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "INVU",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						24,
+						32
+					]
+				},
+				{
+					"text": "TAEYEON_INVU",
+					"indices": [
+						217,
+						230
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						231,
+						247
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SpotifyKDaebak",
+					"name": "Spotify ❤️ K-Pop",
+					"id": 978459450871697400,
+					"id_str": "978459450871697408",
+					"indices": [
+						248,
+						263
+					]
+				},
+				{
+					"screen_name": "SpotifyKR",
+					"name": "Spotify Korea",
+					"id": 1207908245118963700,
+					"id_str": "1207908245118963712",
+					"indices": [
+						264,
+						274
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/QgOE91xMj3",
+					"expanded_url": "http://spoti.fi/3Lmi1EN",
+					"display_url": "spoti.fi/3Lmi1EN",
+					"indices": [
+						143,
+						166
+					]
+				},
+				{
+					"url": "https://t.co/2o4S1fYe8m",
+					"expanded_url": "http://spoti.fi/35V9Y1e",
+					"display_url": "spoti.fi/35V9Y1e",
+					"indices": [
+						167,
+						190
+					]
+				},
+				{
+					"url": "https://t.co/VudjlthN9z",
+					"expanded_url": "http://smarturl.it/TAEYEON_INVU",
+					"display_url": "smarturl.it/TAEYEON_INVU",
+					"indices": [
+						191,
+						214
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1491971397627019300,
+					"id_str": "1491971397627019264",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/media/FLSMFNKaAAAuFMJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLSMFNKaAAAuFMJ.jpg",
+					"url": "https://t.co/V0st0n5g3E",
+					"display_url": "pic.twitter.com/V0st0n5g3E",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1491971406799978498/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1491971397627019300,
+					"id_str": "1491971397627019264",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/media/FLSMFNKaAAAuFMJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLSMFNKaAAAuFMJ.jpg",
+					"url": "https://t.co/V0st0n5g3E",
+					"display_url": "pic.twitter.com/V0st0n5g3E",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1491971406799978498/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBV3cSH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBV3cSH",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3721877,
+			"friends_count": 4,
+			"listed_count": 5763,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2683,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1485990971917877258/ZyKbbdwA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1485990971917877258/ZyKbbdwA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1643122832",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 544,
+		"favorite_count": 1002,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "💜#INVU : THE MESSAGE💜\n \n#TAEYEON has left special messages for S♡NEs! Don't forget to follow [This Is TAEYEON] playlist and pre-save ‘INVU’!\n \nhttps://t.co/QgOE91xMj3\nhttps://t.co/2o4S1fYe8m\nhttps://t.co/VudjlthN9z\n \n#TAEYEON_INVU\n#GirlsGeneration\n@SpotifyKDaebak @SpotifyKR https://t.co/V0st0n5g3E"
+	},
+	{
 		"created_at": "Fri Feb 11 02:07:18 +0000 2022",
 		"id": 1491956602483921000,
 		"id_str": "1491956981552144390",
