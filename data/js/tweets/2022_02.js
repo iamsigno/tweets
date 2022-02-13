ec8e71efaa75f4f8d8357c29c1e7ce58bdede061
SIGNO <iamsigno@users.noreply.github.com>
2022-02-13 13:07:50 +0900

    Added 1 tweets

--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,145 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Sun Feb 13 04:07:16 +0000 2022",
+		"id": 1492697070410494000,
+		"id_str": "1492711947128774657",
+		"full_text": "'Comeback D-1' TAEYEON to show mature vocal charm with her 3rd album 'INVU'! \nA rhythmical song 'Toddler' -&gt; a sincere song 'Heart' -&gt; a sentimental song ‘Ending Credits’! \n\n#태연 #TAEYEON \n#INVU #TAEYEON_INVU\n#소녀시대 #GirlsGeneration",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			236
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "태연",
+					"indices": [
+						180,
+						183
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						184,
+						192
+					]
+				},
+				{
+					"text": "INVU",
+					"indices": [
+						194,
+						199
+					]
+				},
+				{
+					"text": "TAEYEON_INVU",
+					"indices": [
+						200,
+						213
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						214,
+						219
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						220,
+						236
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1492697068577517600,
+		"in_reply_to_status_id_str": "1492697068577517570",
+		"in_reply_to_user_id": 391115625,
+		"in_reply_to_user_id_str": "391115625",
+		"in_reply_to_screen_name": "GirlsGeneration",
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBV3cSH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBV3cSH",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3724627,
+			"friends_count": 4,
+			"listed_count": 5767,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2701,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1485990971917877258/ZyKbbdwA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1485990971917877258/ZyKbbdwA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1643122832",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 547,
+		"favorite_count": 1378,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "'Comeback D-1' TAEYEON to show mature vocal charm with her 3rd album 'INVU'! \nA rhythmical song 'Toddler' -&gt; a sincere song 'Heart' -&gt; a sentimental song ‘Ending Credits’! \n\n#태연 #TAEYEON \n#INVU #TAEYEON_INVU\n#소녀시대 #GirlsGeneration"
+	},
+	{
 		"created_at": "Sat Feb 12 15:07:35 +0000 2022",
 		"id": 1492514437953372200,
 		"id_str": "1492515735872098309",
