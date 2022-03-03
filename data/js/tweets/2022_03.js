26242730d272f4033335d53994ed23dec9827a65
SIGNO <iamsigno@users.noreply.github.com>
2022-03-03 20:07:50 +0900

    Added 1 tweets

--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,162 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Thu Mar 03 11:07:14 +0000 2022",
+		"id": 1499329766784061400,
+		"id_str": "1499340620502126594",
+		"full_text": "[#VIDEO] 이 조합 찬성해~ 알고보니 둘은 모럴센스 실사판? 서현, 이준영의 MBTI와 밸런스 게임까지 모두 공개합니다! | 얼루어코리아 Allure Korea\n▶ https://t.co/Bsl6xoPUys\n\n#서현 #얼루어 #모럴센스 #SEOHYUN #allurekorea #LoveandLeashes",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			172
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "VIDEO",
+					"indices": [
+						1,
+						7
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						120,
+						123
+					]
+				},
+				{
+					"text": "얼루어",
+					"indices": [
+						124,
+						128
+					]
+				},
+				{
+					"text": "모럴센스",
+					"indices": [
+						129,
+						134
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						135,
+						143
+					]
+				},
+				{
+					"text": "allurekorea",
+					"indices": [
+						144,
+						156
+					]
+				},
+				{
+					"text": "LoveandLeashes",
+					"indices": [
+						157,
+						172
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Bsl6xoPUys",
+					"expanded_url": "https://youtu.be/8sDLXMhA9gY",
+					"display_url": "youtu.be/8sDLXMhA9gY",
+					"indices": [
+						95,
+						118
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 58014,
+			"friends_count": 13,
+			"listed_count": 269,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 125,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15175,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 95,
+		"favorite_count": 120,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#VIDEO] 이 조합 찬성해~ 알고보니 둘은 모럴센스 실사판? 서현, 이준영의 MBTI와 밸런스 게임까지 모두 공개합니다! | 얼루어코리아 Allure Korea\n▶ https://t.co/Bsl6xoPUys\n\n#서현 #얼루어 #모럴센스 #SEOHYUN #allurekorea #LoveandLeashes"
+	},
+	{
 		"created_at": "Thu Mar 03 09:44:57 +0000 2022",
 		"id": 1499310425049022500,
 		"id_str": "1499319909179961345",
