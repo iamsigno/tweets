e1f60e801ba96f0e95dfc27190bba30c7883ddb2
SIGNO <iamsigno@users.noreply.github.com>
2022-03-09 00:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28999,
-	"created_at": "Tue Mar 08 13:07:49 +0000 2022",
+	"tweets": 29000,
+	"created_at": "Tue Mar 08 15:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Tue Mar 08 15:07:08 +0000 2022",
+		"id": 1501211136880005000,
+		"id_str": "1501212932444729359",
+		"full_text": "#HappyTAEYEONDay 🎂🎉\n\n220309 #태연 #TAEYEON #소녀시대 #GirlsGeneration https://t.co/Y7dsVYvIiB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			63
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HappyTAEYEONDay",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						28,
+						31
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						32,
+						40
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						41,
+						46
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						47,
+						63
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1501189637104365600,
+					"id_str": "1501189637104365568",
+					"indices": [
+						64,
+						87
+					],
+					"media_url": "http://pbs.twimg.com/media/FNVMBXQacAAuiF_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNVMBXQacAAuiF_.jpg",
+					"url": "https://t.co/Y7dsVYvIiB",
+					"display_url": "pic.twitter.com/Y7dsVYvIiB",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1501211136880005127/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1199,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 679,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1199,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1501189637104365600,
+					"id_str": "1501189637104365568",
+					"indices": [
+						64,
+						87
+					],
+					"media_url": "http://pbs.twimg.com/media/FNVMBXQacAAuiF_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNVMBXQacAAuiF_.jpg",
+					"url": "https://t.co/Y7dsVYvIiB",
+					"display_url": "pic.twitter.com/Y7dsVYvIiB",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1501211136880005127/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1199,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 679,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1199,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBV3cSH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBV3cSH",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3740964,
+			"friends_count": 5,
+			"listed_count": 5797,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2757,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1497557135445880832/K_1Hu8uu_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1497557135445880832/K_1Hu8uu_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1646751605",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6489,
+		"favorite_count": 7885,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "#HappyTAEYEONDay 🎂🎉\n\n220309 #태연 #TAEYEON #소녀시대 #GirlsGeneration https://t.co/Y7dsVYvIiB"
+	},
+	{
 		"created_at": "Tue Mar 08 13:07:08 +0000 2022",
 		"id": 1501182394308907000,
 		"id_str": "1501182732440854536",
