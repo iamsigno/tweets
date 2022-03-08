a0459de0adb705472ff1db522d3e15704b743e96
SIGNO <iamsigno@users.noreply.github.com>
2022-03-08 22:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28998,
-	"created_at": "Tue Mar 08 12:07:48 +0000 2022",
+	"tweets": 28999,
+	"created_at": "Tue Mar 08 13:07:49 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,234 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Tue Mar 08 13:07:08 +0000 2022",
+		"id": 1501182394308907000,
+		"id_str": "1501182732440854536",
+		"full_text": "금주의 #KPop100 차트가 공개되었습니다.👐\n⠀\n태연의 'INVU'가 정상을 지키며 2주 연속 1위를 기록합니다.✌️\n\nFull chart available now\nhttps://t.co/OF7kWnIyXN\n\n#소코도모 #sokodomo\n#태연 #TAEYEON #INVU https://t.co/OcItg4M2Nv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			153
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "KPop100",
+					"indices": [
+						4,
+						12
+					]
+				},
+				{
+					"text": "소코도모",
+					"indices": [
+						119,
+						124
+					]
+				},
+				{
+					"text": "sokodomo",
+					"indices": [
+						125,
+						134
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						135,
+						138
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						139,
+						147
+					]
+				},
+				{
+					"text": "INVU",
+					"indices": [
+						148,
+						153
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/OF7kWnIyXN",
+					"expanded_url": "http://billboard.co.kr/chart/week/",
+					"display_url": "billboard.co.kr/chart/week/",
+					"indices": [
+						94,
+						117
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1501182303187644400,
+					"id_str": "1501182303187644419",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/FNVFWeSaMAMtfae.png",
+					"media_url_https": "https://pbs.twimg.com/media/FNVFWeSaMAMtfae.png",
+					"url": "https://t.co/OcItg4M2Nv",
+					"display_url": "pic.twitter.com/OcItg4M2Nv",
+					"expanded_url": "https://twitter.com/billboardkorea/status/1501182394308907009/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 711,
+							"h": 719,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 711,
+							"h": 719,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 672,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1501182303187644400,
+					"id_str": "1501182303187644419",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/FNVFWeSaMAMtfae.png",
+					"media_url_https": "https://pbs.twimg.com/media/FNVFWeSaMAMtfae.png",
+					"url": "https://t.co/OcItg4M2Nv",
+					"display_url": "pic.twitter.com/OcItg4M2Nv",
+					"expanded_url": "https://twitter.com/billboardkorea/status/1501182394308907009/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 711,
+							"h": 719,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 711,
+							"h": 719,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 672,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 996592973964984300,
+			"id_str": "996592973964984320",
+			"name": "빌보드 코리아 Billboard Korea",
+			"screen_name": "billboardkorea",
+			"location": "대한민국 서울",
+			"description": "빌보드 코리아 공식트위터입니다. 팔로우를 통해 더 많은 KPOP 소식을 받아보세요! Follow us to get more KPOP news, chart and events! #빌보드코리아 #BillboardKorea",
+			"url": "https://t.co/Rv5FgNzJFu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Rv5FgNzJFu",
+							"expanded_url": "http://billboard.co.kr",
+							"display_url": "billboard.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 60101,
+			"friends_count": 30,
+			"listed_count": 133,
+			"created_at": "Wed May 16 03:27:35 +0000 2018",
+			"favourites_count": 44,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2891,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/999581468971024384/Qvmvzk0r_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/999581468971024384/Qvmvzk0r_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/996592973964984320/1640250238",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "금주의 #KPop100 차트가 공개되었습니다.👐\n⠀\n태연의 'INVU'가 정상을 지키며 2주 연속 1위를 기록합니다.✌️\n\nFull chart available now\nhttps://t.co/OF7kWnIyXN\n\n#소코도모 #sokodomo\n#태연 #TAEYEON #INVU https://t.co/OcItg4M2Nv"
+	},
+	{
 		"created_at": "Tue Mar 08 12:07:08 +0000 2022",
 		"id": 1501150916250341400,
 		"id_str": "1501167634016051201",
