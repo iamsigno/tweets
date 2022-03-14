51699aae86dd987c1782cdb528e278f939dd27d6
SIGNO <iamsigno@users.noreply.github.com>
2022-03-14 11:07:48 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29004,
-	"created_at": "Fri Mar 11 15:07:48 +0000 2022",
+	"tweets": 29005,
+	"created_at": "Mon Mar 14 02:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,226 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Mon Mar 14 02:07:09 +0000 2022",
+		"id": 1503189169039806500,
+		"id_str": "1503190967083573253",
+		"full_text": "[#NEWS] [공식] 김남길·서현→이호정 넷플릭스 '도적 : 칼의 소리' 캐스팅 확정\n▶ https://t.co/d94dxSJqE4\n\n#서현 #도적_칼의소리 #SEOHYUN #SongoftheBandits https://t.co/pGpmrDjElF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			115
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						76,
+						79
+					]
+				},
+				{
+					"text": "도적_칼의소리",
+					"indices": [
+						80,
+						88
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						89,
+						97
+					]
+				},
+				{
+					"text": "SongoftheBandits",
+					"indices": [
+						98,
+						115
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/d94dxSJqE4",
+					"expanded_url": "http://naver.me/5bRnRmcU",
+					"display_url": "naver.me/5bRnRmcU",
+					"indices": [
+						51,
+						74
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1503187535949795300,
+					"id_str": "1503187535949795329",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/FNxlGYcVEAEN3RR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNxlGYcVEAEN3RR.jpg",
+					"url": "https://t.co/pGpmrDjElF",
+					"display_url": "pic.twitter.com/pGpmrDjElF",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1503189169039806465/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 785,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 554,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 785,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1503187535949795300,
+					"id_str": "1503187535949795329",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/FNxlGYcVEAEN3RR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNxlGYcVEAEN3RR.jpg",
+					"url": "https://t.co/pGpmrDjElF",
+					"display_url": "pic.twitter.com/pGpmrDjElF",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1503189169039806465/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 785,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 554,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 785,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 58411,
+			"friends_count": 13,
+			"listed_count": 271,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 125,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15208,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 80,
+		"favorite_count": 105,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] [공식] 김남길·서현→이호정 넷플릭스 '도적 : 칼의 소리' 캐스팅 확정\n▶ https://t.co/d94dxSJqE4\n\n#서현 #도적_칼의소리 #SEOHYUN #SongoftheBandits https://t.co/pGpmrDjElF"
+	},
+	{
 		"created_at": "Fri Mar 11 15:07:09 +0000 2022",
 		"id": 1502294279833944000,
 		"id_str": "1502300096297373704",
