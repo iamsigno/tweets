2ebd141dee7b1ed0644d25abb9145005d1f757e2
SIGNO <iamsigno@users.noreply.github.com>
2022-03-15 19:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29007,
-	"created_at": "Tue Mar 15 02:07:48 +0000 2022",
+	"tweets": 29008,
+	"created_at": "Tue Mar 15 10:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Tue Mar 15 10:07:08 +0000 2022",
+		"id": 1503667318684934100,
+		"id_str": "1503674149931307008",
+		"full_text": "[#🌳] 우린 이걸 보석이라고 부르기로 했어요\n▶ https://t.co/cLUPE9Mkb2\n\n#서현 #SEOHYUN #NAMOOCONTENTS https://t.co/bhdj0c8ftO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			80
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "서현",
+					"indices": [
+						53,
+						56
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						57,
+						65
+					]
+				},
+				{
+					"text": "NAMOOCONTENTS",
+					"indices": [
+						66,
+						80
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/cLUPE9Mkb2",
+					"expanded_url": "http://naver.me/G6iccG2j",
+					"display_url": "naver.me/G6iccG2j",
+					"indices": [
+						28,
+						51
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1503665731786842000,
+					"id_str": "1503665731786842114",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/FN4YBCwVIAImeQ3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FN4YBCwVIAImeQ3.jpg",
+					"url": "https://t.co/bhdj0c8ftO",
+					"display_url": "pic.twitter.com/bhdj0c8ftO",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1503667318684934149/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1199,
+							"h": 927,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 526,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1199,
+							"h": 927,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1503665731786842000,
+					"id_str": "1503665731786842114",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/FN4YBCwVIAImeQ3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FN4YBCwVIAImeQ3.jpg",
+					"url": "https://t.co/bhdj0c8ftO",
+					"display_url": "pic.twitter.com/bhdj0c8ftO",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1503667318684934149/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1199,
+							"h": 927,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 526,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1199,
+							"h": 927,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 58449,
+			"friends_count": 13,
+			"listed_count": 269,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 125,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15217,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 203,
+		"favorite_count": 251,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#🌳] 우린 이걸 보석이라고 부르기로 했어요\n▶ https://t.co/cLUPE9Mkb2\n\n#서현 #SEOHYUN #NAMOOCONTENTS https://t.co/bhdj0c8ftO"
+	},
+	{
 		"created_at": "Tue Mar 15 02:07:08 +0000 2022",
 		"id": 1503549055615795200,
 		"id_str": "1503553354290671621",
