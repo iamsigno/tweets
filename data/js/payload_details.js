e10a974fe277f0a4728c56edcc8696008d6b20a1
SIGNO <iamsigno@users.noreply.github.com>
2022-03-17 18:07:48 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29009,
-	"created_at": "Tue Mar 15 14:07:48 +0000 2022",
+	"tweets": 29010,
+	"created_at": "Thu Mar 17 09:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,244 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Thu Mar 17 09:07:08 +0000 2022",
+		"id": 1504382031337341000,
+		"id_str": "1504383825572507652",
+		"full_text": "무진 음색을 더한 완벽함💕\n#이무진 의 'Fine' 커버🎼 (원곡: #태연 #taeyeon)\n\n&lt;#전지적참견시점&gt; 매주 토요일 밤 11시 10분 방송\nMBC220312방송 https://t.co/nJl1IX78XJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			101
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이무진",
+					"indices": [
+						15,
+						19
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						38,
+						41
+					]
+				},
+				{
+					"text": "taeyeon",
+					"indices": [
+						42,
+						50
+					]
+				},
+				{
+					"text": "전지적참견시점",
+					"indices": [
+						57,
+						65
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1504361327468290000,
+					"id_str": "1504361327468290049",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/FOCRFECUYAAS8Vq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FOCRFECUYAAS8Vq.jpg",
+					"url": "https://t.co/nJl1IX78XJ",
+					"display_url": "pic.twitter.com/nJl1IX78XJ",
+					"expanded_url": "https://twitter.com/withMBC/status/1504382031337340935/video/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 800,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1504361327468290000,
+					"id_str": "1504361327468290049",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/FOCRFECUYAAS8Vq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FOCRFECUYAAS8Vq.jpg",
+					"url": "https://t.co/nJl1IX78XJ",
+					"display_url": "pic.twitter.com/nJl1IX78XJ",
+					"expanded_url": "https://twitter.com/withMBC/status/1504382031337340935/video/1",
+					"type": "video",
+					"sizes": {
+						"large": {
+							"w": 800,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 59926,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1504361327468290049/pl/_Yf0MDgc5qdLkRVi.m3u8?tag=14&container=fmp4"
+							},
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1504361327468290049/vid/720x720/vSzEfCaIhvhkhHQ4.mp4?tag=14"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1504361327468290049/vid/320x320/a8emyor_H9UDSBUh.mp4?tag=14"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1504361327468290049/vid/540x540/teXz5as23AQSWggt.mp4?tag=14"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 104673579,
+			"id_str": "104673579",
+			"name": "withMBC",
+			"screen_name": "withMBC",
+			"location": "상암동",
+			"description": "📺𝖮𝖿𝖿𝗂𝖼𝗂𝖺𝗅 Twitter 𝗈𝖿 𝖬𝗎𝗇𝗁𝗐𝖺 𝖡𝗋𝗈𝖺𝖽𝖼𝖺𝗌𝗍𝗂𝗇𝗀 𝖢𝗈𝗋𝗉.",
+			"url": "https://t.co/6dtNDiKuXn",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6dtNDiKuXn",
+							"expanded_url": "http://www.imbc.com",
+							"display_url": "imbc.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1010992,
+			"friends_count": 9941,
+			"listed_count": 4226,
+			"created_at": "Thu Jan 14 01:40:58 +0000 2010",
+			"favourites_count": 128,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 47409,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FCFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1326085413245612034/W0IFxoZo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1326085413245612034/W0IFxoZo_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/104673579/1645422305",
+			"profile_link_color": "1B3E8F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FCFCFC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 13,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "무진 음색을 더한 완벽함💕\n#이무진 의 'Fine' 커버🎼 (원곡: #태연 #taeyeon)\n\n&lt;#전지적참견시점&gt; 매주 토요일 밤 11시 10분 방송\nMBC220312방송 https://t.co/nJl1IX78XJ"
+	},
+	{
 		"created_at": "Tue Mar 15 14:07:09 +0000 2022",
 		"id": 1503726691369119700,
 		"id_str": "1503734548407934983",
