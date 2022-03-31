06ee1262b45e471a5342bf7ab834488e7d636fc3
SIGNO <iamsigno@users.noreply.github.com>
2022-03-31 21:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29019,
-	"created_at": "Thu Mar 31 08:07:48 +0000 2022",
+	"tweets": 29020,
+	"created_at": "Thu Mar 31 12:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Thu Mar 31 12:07:09 +0000 2022",
+		"id": 1509496595255283700,
+		"id_str": "1509502556078280704",
+		"full_text": "👑 TAEYEON จะปรากฏตัวในฐานะแกรนด์มาสเตอร์ของรายการ ‘Queendom 2’ ออกอากาศตอนแรกคืนนี้ เวลาไทย 19.20 น. ทางช่อง Mnet!\n\n💗 https://t.co/yvNK4q3WFk\n\n#태연 #TAEYEON \n#퀸덤2 #QUEENDOM2 https://t.co/d0WIOSbNtJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			172
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "태연",
+					"indices": [
+						143,
+						146
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						147,
+						155
+					]
+				},
+				{
+					"text": "퀸덤2",
+					"indices": [
+						157,
+						161
+					]
+				},
+				{
+					"text": "QUEENDOM2",
+					"indices": [
+						162,
+						172
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/yvNK4q3WFk",
+					"expanded_url": "http://facebook.com/smtruethailand/posts/5023211031060138",
+					"display_url": "facebook.com/smtruethailand…",
+					"indices": [
+						118,
+						141
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1509495556984688600,
+					"id_str": "1509495556984688641",
+					"indices": [
+						173,
+						196
+					],
+					"media_url": "http://pbs.twimg.com/media/FPLONiAaUAEdjsb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPLONiAaUAEdjsb.jpg",
+					"url": "https://t.co/d0WIOSbNtJ",
+					"display_url": "pic.twitter.com/d0WIOSbNtJ",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1509496595255283724/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 857,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 486,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1463,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1509495556984688600,
+					"id_str": "1509495556984688641",
+					"indices": [
+						173,
+						196
+					],
+					"media_url": "http://pbs.twimg.com/media/FPLONiAaUAEdjsb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPLONiAaUAEdjsb.jpg",
+					"url": "https://t.co/d0WIOSbNtJ",
+					"display_url": "pic.twitter.com/d0WIOSbNtJ",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1509496595255283724/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 857,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 486,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1463,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1093456514700828700,
+			"id_str": "1093456514700828672",
+			"name": "SM True",
+			"screen_name": "SMTrueThailand",
+			"location": "Bangkok, Thailand",
+			"description": "SM True Official Twitter Page",
+			"url": "https://t.co/fe1MwqbQdx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fe1MwqbQdx",
+							"expanded_url": "https://www.facebook.com/smtruethailand",
+							"display_url": "facebook.com/smtruethailand",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 92401,
+			"friends_count": 50,
+			"listed_count": 155,
+			"created_at": "Thu Feb 07 10:28:43 +0000 2019",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 10754,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1093456514700828672/1562743620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 74,
+		"favorite_count": 53,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "th",
+		"text": "👑 TAEYEON จะปรากฏตัวในฐานะแกรนด์มาสเตอร์ของรายการ ‘Queendom 2’ ออกอากาศตอนแรกคืนนี้ เวลาไทย 19.20 น. ทางช่อง Mnet!\n\n💗 https://t.co/yvNK4q3WFk\n\n#태연 #TAEYEON \n#퀸덤2 #QUEENDOM2 https://t.co/d0WIOSbNtJ"
+	},
+	{
 		"created_at": "Thu Mar 31 07:44:56 +0000 2022",
 		"id": 1509432808850137000,
 		"id_str": "1509436569354129409",
