0cd6645ed8a8fe44d8124ddb2a9befd55b555530
SIGNO <iamsigno@users.noreply.github.com>
2022-04-01 21:07:48 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29023,
-	"created_at": "Fri Apr 01 10:07:48 +0000 2022",
+	"tweets": 29024,
+	"created_at": "Fri Apr 01 12:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,210 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Fri Apr 01 12:07:09 +0000 2022",
+		"id": 1509860480600273000,
+		"id_str": "1509864943121846320",
+		"full_text": "매주 기쁜 소식으로 축하받은 #INVU 활동🏹🥰\n\n2022년 3월 멜론 월간 차트 1위, 음악방송 8관왕, 가온차트 Can’t Control Myself 1월, INVU 2월 음원상 등 많은 기록을 세우며 꾸준히 사랑받고 있습니다.\n\n소원들의 사랑과 응원으로 선물 받은 좋은 기록들 감사합니다💜\n#태연 #TAEYEON \n#TAEYEON_INVU https://t.co/TXvBF8AZl7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			192
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "INVU",
+					"indices": [
+						16,
+						21
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						165,
+						168
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						169,
+						177
+					]
+				},
+				{
+					"text": "TAEYEON_INVU",
+					"indices": [
+						179,
+						192
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1509860475764314000,
+					"id_str": "1509860475764314119",
+					"indices": [
+						193,
+						216
+					],
+					"media_url": "http://pbs.twimg.com/media/FPQaGmZVkAc8vcf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPQaGmZVkAc8vcf.jpg",
+					"url": "https://t.co/TXvBF8AZl7",
+					"display_url": "pic.twitter.com/TXvBF8AZl7",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1509860480600272902/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1509860475764314000,
+					"id_str": "1509860475764314119",
+					"indices": [
+						193,
+						216
+					],
+					"media_url": "http://pbs.twimg.com/media/FPQaGmZVkAc8vcf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPQaGmZVkAc8vcf.jpg",
+					"url": "https://t.co/TXvBF8AZl7",
+					"display_url": "pic.twitter.com/TXvBF8AZl7",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1509860480600272902/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBV3cSH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBV3cSH",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3742992,
+			"friends_count": 5,
+			"listed_count": 5827,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2763,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1497557135445880832/K_1Hu8uu_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1497557135445880832/K_1Hu8uu_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1646751605",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1230,
+		"favorite_count": 3485,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "매주 기쁜 소식으로 축하받은 #INVU 활동🏹🥰\n\n2022년 3월 멜론 월간 차트 1위, 음악방송 8관왕, 가온차트 Can’t Control Myself 1월, INVU 2월 음원상 등 많은 기록을 세우며 꾸준히 사랑받고 있습니다.\n\n소원들의 사랑과 응원으로 선물 받은 좋은 기록들 감사합니다💜\n#태연 #TAEYEON \n#TAEYEON_INVU https://t.co/TXvBF8AZl7"
+	},
+	{
 		"created_at": "Fri Apr 01 09:44:56 +0000 2022",
 		"id": 1509816826838270000,
 		"id_str": "1509829156930043929",
