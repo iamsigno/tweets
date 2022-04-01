12c3938ba22dac5b51b4a9671a91acaebd7d7dba
SIGNO <iamsigno@users.noreply.github.com>
2022-04-01 22:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29024,
-	"created_at": "Fri Apr 01 12:07:48 +0000 2022",
+	"tweets": 29025,
+	"created_at": "Fri Apr 01 13:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,210 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Fri Apr 01 12:44:57 +0000 2022",
+		"id": 1509860332495589400,
+		"id_str": "1509874456856088634",
+		"full_text": "매주 기쁜 소식으로 축하받은 #INVU 활동🏹🥰\n\n2022년 3월 멜론 월간 차트 1위, 음악방송 8관왕, 가온차트 Can’t Control Myself 1월, INVU 2월 음원상 등 많은 기록을 세우며 꾸준히 사랑받고 있습니다.\n\n소원들의 사랑과 응원으로 선물 받은 좋은 기록들 감사합니다💜\n#태연 #TAEYEON \n#TAEYEON_INVU https://t.co/e7LyjZaRZr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			192
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "INVU",
+					"indices": [
+						16,
+						21
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						165,
+						168
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						169,
+						177
+					]
+				},
+				{
+					"text": "TAEYEON_INVU",
+					"indices": [
+						179,
+						192
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1509860317702279200,
+					"id_str": "1509860317702279169",
+					"indices": [
+						193,
+						216
+					],
+					"media_url": "http://pbs.twimg.com/media/FPQZ9ZkaUAEJw85.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPQZ9ZkaUAEJw85.jpg",
+					"url": "https://t.co/e7LyjZaRZr",
+					"display_url": "pic.twitter.com/e7LyjZaRZr",
+					"expanded_url": "https://twitter.com/TAEYEONsmtown/status/1509860332495589377/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1509860317702279200,
+					"id_str": "1509860317702279169",
+					"indices": [
+						193,
+						216
+					],
+					"media_url": "http://pbs.twimg.com/media/FPQZ9ZkaUAEJw85.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPQZ9ZkaUAEJw85.jpg",
+					"url": "https://t.co/e7LyjZaRZr",
+					"display_url": "pic.twitter.com/e7LyjZaRZr",
+					"expanded_url": "https://twitter.com/TAEYEONsmtown/status/1509860332495589377/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1496499777965822000,
+			"id_str": "1496499777965821956",
+			"name": "TAEYEON Official",
+			"screen_name": "TAEYEONsmtown",
+			"location": "",
+			"description": "TAEYEON The 3rd Album 'INVU'",
+			"url": "https://t.co/c1nmkKMaGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/c1nmkKMaGi",
+							"expanded_url": "https://youtu.be/AbZH7XWDW_k",
+							"display_url": "youtu.be/AbZH7XWDW_k",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 88276,
+			"friends_count": 2,
+			"listed_count": 249,
+			"created_at": "Wed Feb 23 14:59:02 +0000 2022",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 34,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1496687188041678849/ngLIpota_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1496687188041678849/ngLIpota_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1496499777965821956/1645673016",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1815,
+		"favorite_count": 3649,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "매주 기쁜 소식으로 축하받은 #INVU 활동🏹🥰\n\n2022년 3월 멜론 월간 차트 1위, 음악방송 8관왕, 가온차트 Can’t Control Myself 1월, INVU 2월 음원상 등 많은 기록을 세우며 꾸준히 사랑받고 있습니다.\n\n소원들의 사랑과 응원으로 선물 받은 좋은 기록들 감사합니다💜\n#태연 #TAEYEON \n#TAEYEON_INVU https://t.co/e7LyjZaRZr"
+	},
+	{
 		"created_at": "Fri Apr 01 12:07:09 +0000 2022",
 		"id": 1509860480600273000,
 		"id_str": "1509864943121846320",
