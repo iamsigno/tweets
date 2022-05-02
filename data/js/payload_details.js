3190dd282eb6a7ed8411c5af21880a30ab4c9e10
SIGNO <iamsigno@users.noreply.github.com>
2022-05-02 12:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29060,
-	"created_at": "Mon May 02 02:07:48 +0000 2022",
+	"tweets": 29061,
+	"created_at": "Mon May 02 03:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,156 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Mon May 02 02:44:57 +0000 2022",
+		"id": 1520949730733224000,
+		"id_str": "1520957484973895680",
+		"full_text": "'퍼포먼스 퀸' 효연 (HYO), 첫 미니앨범 '휩 (DEEP)' 5월 16일 공개!\n오늘부터 예약 판매 시작!\n\n‘Performance Queen’ HYO’s 1st mini album ‘휩 (DEEP)’ will be released on 5/16!  Pre-order starts today!\n\nhttps://t.co/SixzJfeIvy\n\n#HYO #효연\n#휩_DEEP #HYO_휩_DEEP\n#GirlsGeneration #소녀시대",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			244
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HYO",
+					"indices": [
+						193,
+						197
+					]
+				},
+				{
+					"text": "효연",
+					"indices": [
+						198,
+						201
+					]
+				},
+				{
+					"text": "휩_DEEP",
+					"indices": [
+						202,
+						209
+					]
+				},
+				{
+					"text": "HYO_휩_DEEP",
+					"indices": [
+						210,
+						221
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						222,
+						238
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						239,
+						244
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SixzJfeIvy",
+					"expanded_url": "https://bit.ly/375qRHD",
+					"display_url": "bit.ly/375qRHD",
+					"indices": [
+						168,
+						191
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBV3cSH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBV3cSH",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3756019,
+			"friends_count": 5,
+			"listed_count": 5870,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2780,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1520784620396367872/n-mDvaB1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1520784620396367872/n-mDvaB1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1651417198",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1968,
+		"favorite_count": 3795,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "'퍼포먼스 퀸' 효연 (HYO), 첫 미니앨범 '휩 (DEEP)' 5월 16일 공개!\n오늘부터 예약 판매 시작!\n\n‘Performance Queen’ HYO’s 1st mini album ‘휩 (DEEP)’ will be released on 5/16!  Pre-order starts today!\n\nhttps://t.co/SixzJfeIvy\n\n#HYO #효연\n#휩_DEEP #HYO_휩_DEEP\n#GirlsGeneration #소녀시대"
+	},
+	{
 		"created_at": "Mon May 02 02:07:09 +0000 2022",
 		"id": 1520942181225091000,
 		"id_str": "1520947972493787141",
