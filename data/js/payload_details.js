6f6b67d5593c080fa5b17145edf0518f0bd48816
SIGNO <iamsigno@users.noreply.github.com>
2022-05-02 17:07:48 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29063,
-	"created_at": "Mon May 02 07:07:48 +0000 2022",
+	"tweets": 29064,
+	"created_at": "Mon May 02 08:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Mon May 02 08:07:09 +0000 2022",
+		"id": 1521031621733924900,
+		"id_str": "1521038569095192576",
+		"full_text": "HYOYEON - The 1st Mini Album '휩 (DEEP)'💖\n\nHYOYEON의 첫 번째 미니 앨범 '휩 (DEEP)'을 지금 바로 SMTOWN &amp;STORE 온라인에서 만나보세요!🎧\n\n🔹2만원 이상 구매 시 무료배송 : ~5/6 09:59 까지\n\n👉🏻https://t.co/Y68T19WBJH https://t.co/f7o1yOHEa1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			173
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Y68T19WBJH",
+					"expanded_url": "https://bit.ly/3ONBG23",
+					"display_url": "bit.ly/3ONBG23",
+					"indices": [
+						150,
+						173
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1521030732684136400,
+					"id_str": "1521030732684136452",
+					"indices": [
+						174,
+						197
+					],
+					"media_url": "http://pbs.twimg.com/media/FRvJZIIXEAQJLkv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRvJZIIXEAQJLkv.jpg",
+					"url": "https://t.co/f7o1yOHEa1",
+					"display_url": "pic.twitter.com/f7o1yOHEa1",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1521031621733924864/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1521030732684136400,
+					"id_str": "1521030732684136452",
+					"indices": [
+						174,
+						197
+					],
+					"media_url": "http://pbs.twimg.com/media/FRvJZIIXEAQJLkv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRvJZIIXEAQJLkv.jpg",
+					"url": "https://t.co/f7o1yOHEa1",
+					"display_url": "pic.twitter.com/f7o1yOHEa1",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1521031621733924864/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1113723889245278200,
+			"id_str": "1113723889245278209",
+			"name": "SMTOWN &STORE",
+			"screen_name": "SMTOWNandSTORE",
+			"location": "",
+			"description": "",
+			"url": "https://t.co/Ee3t6o418Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Ee3t6o418Q",
+							"expanded_url": "http://www.SMTOWNandSTORE.com",
+							"display_url": "SMTOWNandSTORE.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 217416,
+			"friends_count": 0,
+			"listed_count": 353,
+			"created_at": "Thu Apr 04 08:44:01 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2528,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1113723889245278209/1554705745",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 221,
+		"favorite_count": 283,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "HYOYEON - The 1st Mini Album '휩 (DEEP)'💖\n\nHYOYEON의 첫 번째 미니 앨범 '휩 (DEEP)'을 지금 바로 SMTOWN &amp;STORE 온라인에서 만나보세요!🎧\n\n🔹2만원 이상 구매 시 무료배송 : ~5/6 09:59 까지\n\n👉🏻https://t.co/Y68T19WBJH https://t.co/f7o1yOHEa1"
+	},
+	{
 		"created_at": "Mon May 02 07:07:09 +0000 2022",
 		"id": 1521022767067385900,
 		"id_str": "1521023470141902851",
