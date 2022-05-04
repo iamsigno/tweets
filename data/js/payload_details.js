80a36ff17e6aac42502ad69d79a95b570b95572b
SIGNO <iamsigno@users.noreply.github.com>
2022-05-04 18:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29068,
-	"created_at": "Wed May 04 04:07:49 +0000 2022",
+	"tweets": 29069,
+	"created_at": "Wed May 04 09:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,238 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Wed May 04 09:07:08 +0000 2022",
+		"id": 1521766371641938000,
+		"id_str": "1521778443633020928",
+		"full_text": "&lt;징크스의 연인&gt; 핑크빛 무드 피어오르는\n설렘 가득한 포스터 공개!✨\n\n따뜻하면서도 신비로운\n로맨스의 시작💖\n\nKBS 2TV 새 수목드라마 #징크스의연인\n6월 첫 방송\n\n#KBS #새수목드라마 #징크스의연인 #서현 #나인우 #KBS드라마 #KBSdrama https://t.co/JhJXxVF3se",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			148
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "징크스의연인",
+					"indices": [
+						84,
+						91
+					]
+				},
+				{
+					"text": "KBS",
+					"indices": [
+						101,
+						105
+					]
+				},
+				{
+					"text": "새수목드라마",
+					"indices": [
+						106,
+						113
+					]
+				},
+				{
+					"text": "징크스의연인",
+					"indices": [
+						114,
+						121
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						122,
+						125
+					]
+				},
+				{
+					"text": "나인우",
+					"indices": [
+						126,
+						130
+					]
+				},
+				{
+					"text": "KBS드라마",
+					"indices": [
+						131,
+						138
+					]
+				},
+				{
+					"text": "KBSdrama",
+					"indices": [
+						139,
+						148
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1521766325399736300,
+					"id_str": "1521766325399736320",
+					"indices": [
+						149,
+						172
+					],
+					"media_url": "http://pbs.twimg.com/media/FR5maQiaUAAtWnF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FR5maQiaUAAtWnF.jpg",
+					"url": "https://t.co/JhJXxVF3se",
+					"display_url": "pic.twitter.com/JhJXxVF3se",
+					"expanded_url": "https://twitter.com/MyloveKBS/status/1521766371641937920/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 454,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 999,
+							"h": 1495,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 802,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1521766325399736300,
+					"id_str": "1521766325399736320",
+					"indices": [
+						149,
+						172
+					],
+					"media_url": "http://pbs.twimg.com/media/FR5maQiaUAAtWnF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FR5maQiaUAAtWnF.jpg",
+					"url": "https://t.co/JhJXxVF3se",
+					"display_url": "pic.twitter.com/JhJXxVF3se",
+					"expanded_url": "https://twitter.com/MyloveKBS/status/1521766371641937920/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 454,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 999,
+							"h": 1495,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 802,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 158284325,
+			"id_str": "158284325",
+			"name": "KBS 한국방송",
+			"screen_name": "MyloveKBS",
+			"location": "",
+			"description": "대한민국 대표 공영방송 KBS 공식 트위터",
+			"url": "https://t.co/QZdiw0Ykps",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/QZdiw0Ykps",
+							"expanded_url": "http://twtkr.com/MyloveKBS",
+							"display_url": "twtkr.com/MyloveKBS",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1106755,
+			"friends_count": 56992,
+			"listed_count": 2108,
+			"created_at": "Tue Jun 22 07:45:48 +0000 2010",
+			"favourites_count": 100,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 54016,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1520993049010065408/83_NUsR2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1520993049010065408/83_NUsR2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/158284325/1651211070",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 196,
+		"favorite_count": 286,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "&lt;징크스의 연인&gt; 핑크빛 무드 피어오르는\n설렘 가득한 포스터 공개!✨\n\n따뜻하면서도 신비로운\n로맨스의 시작💖\n\nKBS 2TV 새 수목드라마 #징크스의연인\n6월 첫 방송\n\n#KBS #새수목드라마 #징크스의연인 #서현 #나인우 #KBS드라마 #KBSdrama https://t.co/JhJXxVF3se"
+	},
+	{
 		"created_at": "Wed May 04 04:07:08 +0000 2022",
 		"id": 1521701148431769600,
 		"id_str": "1521702946030956546",
