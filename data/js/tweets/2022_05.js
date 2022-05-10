6705787c164b2aff3db703401a63bed78b406ba4
SIGNO <iamsigno@users.noreply.github.com>
2022-05-10 20:07:49 +0900

    Added 1 tweets

--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,270 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Tue May 10 11:07:09 +0000 2022",
+		"id": 1523970757709414400,
+		"id_str": "1523982971212029953",
+		"full_text": "🔥 HYO จะกลับมาพร้อมเพลงแดนซ์ EDM สุดหนักหน่วง ‘DEEP’ ไตเติลจากมินิอัลบั้มแรก!\n\nhttps://t.co/v2oTKSXDkg\n\n#HYO #효연\n#DEEP #HYO_DEEP\n#GirlsGeneration #소녀시대 https://t.co/mcdnbPm0Mq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			151
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HYO",
+					"indices": [
+						104,
+						108
+					]
+				},
+				{
+					"text": "효연",
+					"indices": [
+						109,
+						112
+					]
+				},
+				{
+					"text": "DEEP",
+					"indices": [
+						113,
+						118
+					]
+				},
+				{
+					"text": "HYO_DEEP",
+					"indices": [
+						119,
+						128
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						129,
+						145
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						146,
+						151
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/v2oTKSXDkg",
+					"expanded_url": "http://facebook.com/212568845457738/posts/5128845863829987/",
+					"display_url": "facebook.com/21256884545773…",
+					"indices": [
+						79,
+						102
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1523970470735118300,
+					"id_str": "1523970470735118337",
+					"indices": [
+						152,
+						175
+					],
+					"media_url": "http://pbs.twimg.com/media/FSY7EZxaAAErbLs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSY7EZxaAAErbLs.jpg",
+					"url": "https://t.co/mcdnbPm0Mq",
+					"display_url": "pic.twitter.com/mcdnbPm0Mq",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1523970757709414400/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1523970470735118300,
+					"id_str": "1523970470735118337",
+					"indices": [
+						152,
+						175
+					],
+					"media_url": "http://pbs.twimg.com/media/FSY7EZxaAAErbLs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSY7EZxaAAErbLs.jpg",
+					"url": "https://t.co/mcdnbPm0Mq",
+					"display_url": "pic.twitter.com/mcdnbPm0Mq",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1523970757709414400/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1523970470777094100,
+					"id_str": "1523970470777094144",
+					"indices": [
+						152,
+						175
+					],
+					"media_url": "http://pbs.twimg.com/media/FSY7EZ7agAA87Ls.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSY7EZ7agAA87Ls.jpg",
+					"url": "https://t.co/mcdnbPm0Mq",
+					"display_url": "pic.twitter.com/mcdnbPm0Mq",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1523970757709414400/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 486,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 857,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1463,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1093456514700828700,
+			"id_str": "1093456514700828672",
+			"name": "SM True",
+			"screen_name": "SMTrueThailand",
+			"location": "Bangkok, Thailand",
+			"description": "SM True Official Twitter Page",
+			"url": "https://t.co/fe1MwqbQdx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fe1MwqbQdx",
+							"expanded_url": "https://www.facebook.com/smtruethailand",
+							"display_url": "facebook.com/smtruethailand",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 93081,
+			"friends_count": 50,
+			"listed_count": 150,
+			"created_at": "Thu Feb 07 10:28:43 +0000 2019",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 11083,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1093456514700828672/1562743620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 43,
+		"favorite_count": 23,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "th",
+		"text": "🔥 HYO จะกลับมาพร้อมเพลงแดนซ์ EDM สุดหนักหน่วง ‘DEEP’ ไตเติลจากมินิอัลบั้มแรก!\n\nhttps://t.co/v2oTKSXDkg\n\n#HYO #효연\n#DEEP #HYO_DEEP\n#GirlsGeneration #소녀시대 https://t.co/mcdnbPm0Mq"
+	},
+	{
 		"created_at": "Tue May 10 06:07:12 +0000 2022",
 		"id": 1523896471929557000,
 		"id_str": "1523907488839614469",
