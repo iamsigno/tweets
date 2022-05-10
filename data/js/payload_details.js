471fc42b3f6b295d7cb47da22460da210e32fa02
SIGNO <iamsigno@users.noreply.github.com>
2022-05-10 21:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29089,
-	"created_at": "Tue May 10 11:07:49 +0000 2022",
+	"tweets": 29090,
+	"created_at": "Tue May 10 12:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,344 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Tue May 10 12:07:08 +0000 2022",
+		"id": 1523996012708515800,
+		"id_str": "1523998068449894400",
+		"full_text": "🎉 KWON YURI เปิดบัญชีโซเชียลมีเดียอย่างเป็นทางการ (https://t.co/ntScrZ3ICf) พร้อมโปรเจกต์ภาพถ่าย ‘유리한 봄’!\n\nแฟน ๆ อย่าลืมไปกดติดตามกันเยอะ ๆ และรอชมหลากหลายคอนเทนต์ที่จะปล่อยออกมานะคะ 🥰🌸\n\nhttps://t.co/ykN1IOq3OJ\n\n#권유리 #KWONYURI\n#유리한봄 \n#소녀시대 #GirlsGeneration https://t.co/MgPZTS2OYI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			256
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "권유리",
+					"indices": [
+						212,
+						216
+					]
+				},
+				{
+					"text": "KWONYURI",
+					"indices": [
+						217,
+						226
+					]
+				},
+				{
+					"text": "유리한봄",
+					"indices": [
+						227,
+						232
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						234,
+						239
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						240,
+						256
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ntScrZ3ICf",
+					"expanded_url": "http://instagram.com/kwonyuri_official",
+					"display_url": "instagram.com/kwonyuri_offic…",
+					"indices": [
+						51,
+						74
+					]
+				},
+				{
+					"url": "https://t.co/ykN1IOq3OJ",
+					"expanded_url": "http://facebook.com/212568845457738/posts/5129030663811507/",
+					"display_url": "facebook.com/21256884545773…",
+					"indices": [
+						187,
+						210
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1523995107036635100,
+					"id_str": "1523995107036635141",
+					"indices": [
+						257,
+						280
+					],
+					"media_url": "http://pbs.twimg.com/media/FSZRebJaIAU-ttI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSZRebJaIAU-ttI.jpg",
+					"url": "https://t.co/MgPZTS2OYI",
+					"display_url": "pic.twitter.com/MgPZTS2OYI",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1523996012708515840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1523995107036635100,
+					"id_str": "1523995107036635141",
+					"indices": [
+						257,
+						280
+					],
+					"media_url": "http://pbs.twimg.com/media/FSZRebJaIAU-ttI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSZRebJaIAU-ttI.jpg",
+					"url": "https://t.co/MgPZTS2OYI",
+					"display_url": "pic.twitter.com/MgPZTS2OYI",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1523996012708515840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1523995107141496800,
+					"id_str": "1523995107141496833",
+					"indices": [
+						257,
+						280
+					],
+					"media_url": "http://pbs.twimg.com/media/FSZRebiaMAE2vGm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSZRebiaMAE2vGm.jpg",
+					"url": "https://t.co/MgPZTS2OYI",
+					"display_url": "pic.twitter.com/MgPZTS2OYI",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1523996012708515840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1066,
+							"h": 1600,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1523995107292516400,
+					"id_str": "1523995107292516353",
+					"indices": [
+						257,
+						280
+					],
+					"media_url": "http://pbs.twimg.com/media/FSZRecGakAEgENI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSZRecGakAEgENI.jpg",
+					"url": "https://t.co/MgPZTS2OYI",
+					"display_url": "pic.twitter.com/MgPZTS2OYI",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1523996012708515840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 454,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 801,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1201,
+							"h": 1800,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1523995107061809200,
+					"id_str": "1523995107061809152",
+					"indices": [
+						257,
+						280
+					],
+					"media_url": "http://pbs.twimg.com/media/FSZRebPaQAA5WHv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSZRebPaQAA5WHv.jpg",
+					"url": "https://t.co/MgPZTS2OYI",
+					"display_url": "pic.twitter.com/MgPZTS2OYI",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1523996012708515840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1093456514700828700,
+			"id_str": "1093456514700828672",
+			"name": "SM True",
+			"screen_name": "SMTrueThailand",
+			"location": "Bangkok, Thailand",
+			"description": "SM True Official Twitter Page",
+			"url": "https://t.co/fe1MwqbQdx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fe1MwqbQdx",
+							"expanded_url": "https://www.facebook.com/smtruethailand",
+							"display_url": "facebook.com/smtruethailand",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 93081,
+			"friends_count": 50,
+			"listed_count": 150,
+			"created_at": "Thu Feb 07 10:28:43 +0000 2019",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 11084,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1093456514700828672/1562743620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 36,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "th",
+		"text": "🎉 KWON YURI เปิดบัญชีโซเชียลมีเดียอย่างเป็นทางการ (https://t.co/ntScrZ3ICf) พร้อมโปรเจกต์ภาพถ่าย ‘유리한 봄’!\n\nแฟน ๆ อย่าลืมไปกดติดตามกันเยอะ ๆ และรอชมหลากหลายคอนเทนต์ที่จะปล่อยออกมานะคะ 🥰🌸\n\nhttps://t.co/ykN1IOq3OJ\n\n#권유리 #KWONYURI\n#유리한봄 \n#소녀시대 #GirlsGeneration https://t.co/MgPZTS2OYI"
+	},
+	{
 		"created_at": "Tue May 10 11:07:09 +0000 2022",
 		"id": 1523970757709414400,
 		"id_str": "1523982971212029953",
