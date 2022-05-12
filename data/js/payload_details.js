d7bc004340c96b3165ec563a4890092e979cf3f6
SIGNO <iamsigno@users.noreply.github.com>
2022-05-12 15:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29108,
-	"created_at": "Thu May 12 05:07:48 +0000 2022",
+	"tweets": 29109,
+	"created_at": "Thu May 12 06:07:49 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,324 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Thu May 12 06:07:15 +0000 2022",
+		"id": 1524620119309049900,
+		"id_str": "1524632273794052096",
+		"full_text": "낭만적이면서도 통통 튀는 판타지 로맨스💖\n\n행운의 여신 #슬비 X 불운의 남자 #공수광\n바라만 봐도 가슴 설레는 투 샷!👀\n\nKBS 2TV 새 수목드라마 #징크스의연인\n6월 첫 방송\n\n#KBS #새수목드라마 #징크스의연인 #서현 #나인우 #KBS드라마 #KBSdrama https://t.co/oygJaZ6jIn",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			150
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "슬비",
+					"indices": [
+						31,
+						34
+					]
+				},
+				{
+					"text": "공수광",
+					"indices": [
+						44,
+						48
+					]
+				},
+				{
+					"text": "징크스의연인",
+					"indices": [
+						86,
+						93
+					]
+				},
+				{
+					"text": "KBS",
+					"indices": [
+						103,
+						107
+					]
+				},
+				{
+					"text": "새수목드라마",
+					"indices": [
+						108,
+						115
+					]
+				},
+				{
+					"text": "징크스의연인",
+					"indices": [
+						116,
+						123
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						124,
+						127
+					]
+				},
+				{
+					"text": "나인우",
+					"indices": [
+						128,
+						132
+					]
+				},
+				{
+					"text": "KBS드라마",
+					"indices": [
+						133,
+						140
+					]
+				},
+				{
+					"text": "KBSdrama",
+					"indices": [
+						141,
+						150
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1524620085616279600,
+					"id_str": "1524620085616279553",
+					"indices": [
+						151,
+						174
+					],
+					"media_url": "http://pbs.twimg.com/media/FSiJ49yVgAEK3UT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSiJ49yVgAEK3UT.jpg",
+					"url": "https://t.co/oygJaZ6jIn",
+					"display_url": "pic.twitter.com/oygJaZ6jIn",
+					"expanded_url": "https://twitter.com/MyloveKBS/status/1524620119309049856/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1524620085616279600,
+					"id_str": "1524620085616279553",
+					"indices": [
+						151,
+						174
+					],
+					"media_url": "http://pbs.twimg.com/media/FSiJ49yVgAEK3UT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSiJ49yVgAEK3UT.jpg",
+					"url": "https://t.co/oygJaZ6jIn",
+					"display_url": "pic.twitter.com/oygJaZ6jIn",
+					"expanded_url": "https://twitter.com/MyloveKBS/status/1524620119309049856/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1524620085620400000,
+					"id_str": "1524620085620400128",
+					"indices": [
+						151,
+						174
+					],
+					"media_url": "http://pbs.twimg.com/media/FSiJ49zUYAA3_m-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSiJ49zUYAA3_m-.jpg",
+					"url": "https://t.co/oygJaZ6jIn",
+					"display_url": "pic.twitter.com/oygJaZ6jIn",
+					"expanded_url": "https://twitter.com/MyloveKBS/status/1524620119309049856/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1524620085616267300,
+					"id_str": "1524620085616267265",
+					"indices": [
+						151,
+						174
+					],
+					"media_url": "http://pbs.twimg.com/media/FSiJ49yVUAEAfeR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSiJ49yVUAEAfeR.jpg",
+					"url": "https://t.co/oygJaZ6jIn",
+					"display_url": "pic.twitter.com/oygJaZ6jIn",
+					"expanded_url": "https://twitter.com/MyloveKBS/status/1524620119309049856/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 158284325,
+			"id_str": "158284325",
+			"name": "KBS 한국방송",
+			"screen_name": "MyloveKBS",
+			"location": "",
+			"description": "대한민국 대표 공영방송 KBS 공식 트위터",
+			"url": "https://t.co/QZdiw0Ykps",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/QZdiw0Ykps",
+							"expanded_url": "http://twtkr.com/MyloveKBS",
+							"display_url": "twtkr.com/MyloveKBS",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1106645,
+			"friends_count": 56965,
+			"listed_count": 2114,
+			"created_at": "Tue Jun 22 07:45:48 +0000 2010",
+			"favourites_count": 100,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 54067,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1520993049010065408/83_NUsR2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1520993049010065408/83_NUsR2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/158284325/1651211070",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 170,
+		"favorite_count": 229,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "낭만적이면서도 통통 튀는 판타지 로맨스💖\n\n행운의 여신 #슬비 X 불운의 남자 #공수광\n바라만 봐도 가슴 설레는 투 샷!👀\n\nKBS 2TV 새 수목드라마 #징크스의연인\n6월 첫 방송\n\n#KBS #새수목드라마 #징크스의연인 #서현 #나인우 #KBS드라마 #KBSdrama https://t.co/oygJaZ6jIn"
+	},
+	{
 		"created_at": "Thu May 12 05:07:23 +0000 2022",
 		"id": 1524615420288057300,
 		"id_str": "1524617209678606337",
