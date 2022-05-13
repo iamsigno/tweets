f8760d0343bb1101851135beb3d3dd247583e85e
SIGNO <iamsigno@users.noreply.github.com>
2022-05-13 21:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29119,
-	"created_at": "Fri May 13 10:07:48 +0000 2022",
+	"tweets": 29120,
+	"created_at": "Fri May 13 12:07:49 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,270 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Fri May 13 12:07:09 +0000 2022",
+		"id": 1525082798926995500,
+		"id_str": "1525085236488552448",
+		"full_text": "HYO เปลี่ยนลุคมาเป็น ‘Spider-Woman’ ในเพลงไตเติล ‘DEEP’ ของมินิอัลบั้มแรก!\n\nHoney J หัวหน้าของทีมเต้น Holy Bang มาร่วมออกแบบท่าเต้นด้วย!\n\n🕷 https://t.co/XTNIMnH3n4\n\n#HYO #효연\n#DEEP #HYO_DEEP\n#GirlsGeneration #소녀시대 https://t.co/E6bvOeQrUg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			212
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HYO",
+					"indices": [
+						165,
+						169
+					]
+				},
+				{
+					"text": "효연",
+					"indices": [
+						170,
+						173
+					]
+				},
+				{
+					"text": "DEEP",
+					"indices": [
+						174,
+						179
+					]
+				},
+				{
+					"text": "HYO_DEEP",
+					"indices": [
+						180,
+						189
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						190,
+						206
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						207,
+						212
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/XTNIMnH3n4",
+					"expanded_url": "http://facebook.com/smtruethailand/posts/pfbid022uMbXy8spBy721WvRVuKQNzNG1g7M6m7jDMTvoLUd49ycMq3FHpmwvwHKeTwLMgzl",
+					"display_url": "facebook.com/smtruethailand…",
+					"indices": [
+						140,
+						163
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1525082279642804200,
+					"id_str": "1525082279642804225",
+					"indices": [
+						213,
+						236
+					],
+					"media_url": "http://pbs.twimg.com/media/FSouQMtaMAEoMSM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSouQMtaMAEoMSM.jpg",
+					"url": "https://t.co/E6bvOeQrUg",
+					"display_url": "pic.twitter.com/E6bvOeQrUg",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1525082798926995457/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1525082279642804200,
+					"id_str": "1525082279642804225",
+					"indices": [
+						213,
+						236
+					],
+					"media_url": "http://pbs.twimg.com/media/FSouQMtaMAEoMSM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSouQMtaMAEoMSM.jpg",
+					"url": "https://t.co/E6bvOeQrUg",
+					"display_url": "pic.twitter.com/E6bvOeQrUg",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1525082798926995457/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1525082279672189000,
+					"id_str": "1525082279672188928",
+					"indices": [
+						213,
+						236
+					],
+					"media_url": "http://pbs.twimg.com/media/FSouQM0akAAaAIT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSouQM0akAAaAIT.jpg",
+					"url": "https://t.co/E6bvOeQrUg",
+					"display_url": "pic.twitter.com/E6bvOeQrUg",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1525082798926995457/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 857,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 486,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1347,
+							"h": 1886,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1093456514700828700,
+			"id_str": "1093456514700828672",
+			"name": "SM True",
+			"screen_name": "SMTrueThailand",
+			"location": "Bangkok, Thailand",
+			"description": "SM True Official Twitter Page",
+			"url": "https://t.co/fe1MwqbQdx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fe1MwqbQdx",
+							"expanded_url": "https://www.facebook.com/smtruethailand",
+							"display_url": "facebook.com/smtruethailand",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 93049,
+			"friends_count": 50,
+			"listed_count": 150,
+			"created_at": "Thu Feb 07 10:28:43 +0000 2019",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 11113,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1093456514700828672/1562743620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 24,
+		"favorite_count": 11,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "th",
+		"text": "HYO เปลี่ยนลุคมาเป็น ‘Spider-Woman’ ในเพลงไตเติล ‘DEEP’ ของมินิอัลบั้มแรก!\n\nHoney J หัวหน้าของทีมเต้น Holy Bang มาร่วมออกแบบท่าเต้นด้วย!\n\n🕷 https://t.co/XTNIMnH3n4\n\n#HYO #효연\n#DEEP #HYO_DEEP\n#GirlsGeneration #소녀시대 https://t.co/E6bvOeQrUg"
+	},
+	{
 		"created_at": "Fri May 13 10:07:09 +0000 2022",
 		"id": 1525051222281007000,
 		"id_str": "1525055034609762306",
