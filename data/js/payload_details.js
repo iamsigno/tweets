a809cee0d18c0c1c3cb56fc8f3b270f294d50271
SIGNO <iamsigno@users.noreply.github.com>
2022-05-18 15:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29149,
-	"created_at": "Wed May 18 05:07:49 +0000 2022",
+	"tweets": 29150,
+	"created_at": "Wed May 18 06:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Wed May 18 06:07:15 +0000 2022",
+		"id": 1526804898821550000,
+		"id_str": "1526806601604079616",
+		"full_text": "효연 미니 1집 'DEEP' 발매 기념 대면사인회 이벤트💖\n\n효연 미니 1집 'DEEP' 발매를 기념하는 대면사인회 이벤트가 오늘 17:59에 종료됩니다!💿\n마지막 기회를 놓치지 말고 지금 바로 SMTOWN &amp;STORE 온라인에서 만나보세요~!👉🏻👉🏻\n\n📍 https://t.co/0uhJYaW9WU https://t.co/1TIfcI2ROP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			171
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/0uhJYaW9WU",
+					"expanded_url": "https://bit.ly/3yFcCoq",
+					"display_url": "bit.ly/3yFcCoq",
+					"indices": [
+						148,
+						171
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1526797206048559000,
+					"id_str": "1526797206048559106",
+					"indices": [
+						172,
+						195
+					],
+					"media_url": "http://pbs.twimg.com/media/FTBF-DaVUAIvXjD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTBF-DaVUAIvXjD.jpg",
+					"url": "https://t.co/1TIfcI2ROP",
+					"display_url": "pic.twitter.com/1TIfcI2ROP",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1526804898821550080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1526797206048559000,
+					"id_str": "1526797206048559106",
+					"indices": [
+						172,
+						195
+					],
+					"media_url": "http://pbs.twimg.com/media/FTBF-DaVUAIvXjD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTBF-DaVUAIvXjD.jpg",
+					"url": "https://t.co/1TIfcI2ROP",
+					"display_url": "pic.twitter.com/1TIfcI2ROP",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1526804898821550080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1113723889245278200,
+			"id_str": "1113723889245278209",
+			"name": "SMTOWN &STORE",
+			"screen_name": "SMTOWNandSTORE",
+			"location": "",
+			"description": "",
+			"url": "https://t.co/Ee3t6o418Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Ee3t6o418Q",
+							"expanded_url": "http://www.SMTOWNandSTORE.com",
+							"display_url": "SMTOWNandSTORE.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 218453,
+			"friends_count": 0,
+			"listed_count": 367,
+			"created_at": "Thu Apr 04 08:44:01 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2569,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1113723889245278209/1554705745",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 25,
+		"favorite_count": 43,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "효연 미니 1집 'DEEP' 발매 기념 대면사인회 이벤트💖\n\n효연 미니 1집 'DEEP' 발매를 기념하는 대면사인회 이벤트가 오늘 17:59에 종료됩니다!💿\n마지막 기회를 놓치지 말고 지금 바로 SMTOWN &amp;STORE 온라인에서 만나보세요~!👉🏻👉🏻\n\n📍 https://t.co/0uhJYaW9WU https://t.co/1TIfcI2ROP"
+	},
+	{
 		"created_at": "Wed May 18 05:07:09 +0000 2022",
 		"id": 1526787537540219000,
 		"id_str": "1526791476738502657",
