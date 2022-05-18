bbce4d35cd2351093c22099e883403d737a348d1
SIGNO <iamsigno@users.noreply.github.com>
2022-05-18 18:07:49 +0900

    Added 1 tweets

--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,226 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Wed May 18 09:07:09 +0000 2022",
+		"id": 1526836819634991000,
+		"id_str": "1526851874531549185",
+		"full_text": "[#NEWS] ‘징크스의 연인’ 티저 공개, 서현X나인우 운명을 뛰어넘는 판타지 로맨스\n▶ https://t.co/t9BlmvNZKI\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/DN51ZipCyh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			111
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						76,
+						79
+					]
+				},
+				{
+					"text": "징크스의연인",
+					"indices": [
+						80,
+						87
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						88,
+						96
+					]
+				},
+				{
+					"text": "JinxedatFirst",
+					"indices": [
+						97,
+						111
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/t9BlmvNZKI",
+					"expanded_url": "http://naver.me/59eGuIIu",
+					"display_url": "naver.me/59eGuIIu",
+					"indices": [
+						51,
+						74
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1526836809111457800,
+					"id_str": "1526836809111457792",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/FTBp_QUUUAAZI47.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTBp_QUUUAAZI47.jpg",
+					"url": "https://t.co/DN51ZipCyh",
+					"display_url": "pic.twitter.com/DN51ZipCyh",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1526836819634991105/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 560,
+							"h": 775,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 560,
+							"h": 775,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 491,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1526836809111457800,
+					"id_str": "1526836809111457792",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/FTBp_QUUUAAZI47.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTBp_QUUUAAZI47.jpg",
+					"url": "https://t.co/DN51ZipCyh",
+					"display_url": "pic.twitter.com/DN51ZipCyh",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1526836819634991105/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 560,
+							"h": 775,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 560,
+							"h": 775,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 491,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 60177,
+			"friends_count": 12,
+			"listed_count": 272,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 125,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15594,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 154,
+		"favorite_count": 221,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] ‘징크스의 연인’ 티저 공개, 서현X나인우 운명을 뛰어넘는 판타지 로맨스\n▶ https://t.co/t9BlmvNZKI\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/DN51ZipCyh"
+	},
+	{
 		"created_at": "Wed May 18 06:07:15 +0000 2022",
 		"id": 1526804898821550000,
 		"id_str": "1526806601604079616",
