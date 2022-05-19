7746dfc63fa9f4c16a0347988c90779fe2b387f7
SIGNO <iamsigno@users.noreply.github.com>
2022-05-19 19:07:50 +0900

    Added 1 tweets

--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,288 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Thu May 19 09:44:56 +0000 2022",
+		"id": 1527222028469538800,
+		"id_str": "1527223773400817664",
+		"full_text": "SUB)케미레전드!🔥 눈떴는데 군대첫사랑 소녀시대가 내 앞에 있다면?! 효연의 스뚝파까지ㅋㅋ!! GIRLS' GENERATION HYO!\n\n https://t.co/AsmvTjTvvq\n\n#HYO #효연 @Hyoyeon_djhyo \n#DEEP #HYO_DEEP\n#GirlsGeneration #소녀시대\n#엔조이커플 https://t.co/OaJcO4B5Ss",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			174
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HYO",
+					"indices": [
+						104,
+						108
+					]
+				},
+				{
+					"text": "효연",
+					"indices": [
+						109,
+						112
+					]
+				},
+				{
+					"text": "DEEP",
+					"indices": [
+						129,
+						134
+					]
+				},
+				{
+					"text": "HYO_DEEP",
+					"indices": [
+						135,
+						144
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						145,
+						161
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						162,
+						167
+					]
+				},
+				{
+					"text": "엔조이커플",
+					"indices": [
+						168,
+						174
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Hyoyeon_djhyo",
+					"name": "Hyoyeon",
+					"id": 792687101842186200,
+					"id_str": "792687101842186240",
+					"indices": [
+						113,
+						127
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/AsmvTjTvvq",
+					"expanded_url": "https://youtu.be/QtWF96_Rbec",
+					"display_url": "youtu.be/QtWF96_Rbec",
+					"indices": [
+						79,
+						102
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1527221953525821400,
+					"id_str": "1527221953525821440",
+					"indices": [
+						175,
+						198
+					],
+					"media_url": "http://pbs.twimg.com/media/FTHIRnEVsAAhy_D.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTHIRnEVsAAhy_D.jpg",
+					"url": "https://t.co/OaJcO4B5Ss",
+					"display_url": "pic.twitter.com/OaJcO4B5Ss",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1527222028469538816/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1527221953525821400,
+					"id_str": "1527221953525821440",
+					"indices": [
+						175,
+						198
+					],
+					"media_url": "http://pbs.twimg.com/media/FTHIRnEVsAAhy_D.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTHIRnEVsAAhy_D.jpg",
+					"url": "https://t.co/OaJcO4B5Ss",
+					"display_url": "pic.twitter.com/OaJcO4B5Ss",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1527222028469538816/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1527221953634791400,
+					"id_str": "1527221953634791426",
+					"indices": [
+						175,
+						198
+					],
+					"media_url": "http://pbs.twimg.com/media/FTHIRneUcAIYVKO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTHIRneUcAIYVKO.jpg",
+					"url": "https://t.co/OaJcO4B5Ss",
+					"display_url": "pic.twitter.com/OaJcO4B5Ss",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1527222028469538816/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBV3cSH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBV3cSH",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3768253,
+			"friends_count": 5,
+			"listed_count": 5890,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2841,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1520784620396367872/n-mDvaB1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1520784620396367872/n-mDvaB1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1651417198",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1017,
+		"favorite_count": 2113,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "SUB)케미레전드!🔥 눈떴는데 군대첫사랑 소녀시대가 내 앞에 있다면?! 효연의 스뚝파까지ㅋㅋ!! GIRLS' GENERATION HYO!\n\n https://t.co/AsmvTjTvvq\n\n#HYO #효연 @Hyoyeon_djhyo \n#DEEP #HYO_DEEP\n#GirlsGeneration #소녀시대\n#엔조이커플 https://t.co/OaJcO4B5Ss"
+	},
+	{
 		"created_at": "Thu May 19 05:07:09 +0000 2022",
 		"id": 1527142584698941400,
 		"id_str": "1527153865786904576",
