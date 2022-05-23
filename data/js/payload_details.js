f9a783e85a7f5be40ebc68b96892ab4593b8d4e7
SIGNO <iamsigno@users.noreply.github.com>
2022-05-23 20:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29171,
-	"created_at": "Mon May 23 04:07:49 +0000 2022",
+	"tweets": 29172,
+	"created_at": "Mon May 23 11:07:49 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,262 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Mon May 23 11:07:08 +0000 2022",
+		"id": 1528688439679299600,
+		"id_str": "1528694012185653248",
+		"full_text": "[🎥#페이스캠] 효연 - DEEP \n\n#인기가요 #HYO #효연 #DEEP #소녀시대 #GirlsGeneration @GirlsGeneration @Hyoyeon_djhyo\n\n📌https://t.co/BrhTrkivEb https://t.co/iI2NvVP9bb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			122
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "페이스캠",
+					"indices": [
+						2,
+						7
+					]
+				},
+				{
+					"text": "인기가요",
+					"indices": [
+						21,
+						26
+					]
+				},
+				{
+					"text": "HYO",
+					"indices": [
+						27,
+						31
+					]
+				},
+				{
+					"text": "효연",
+					"indices": [
+						32,
+						35
+					]
+				},
+				{
+					"text": "DEEP",
+					"indices": [
+						36,
+						41
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						42,
+						47
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						48,
+						64
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GirlsGeneration",
+					"name": "Girls' Generation",
+					"id": 391115625,
+					"id_str": "391115625",
+					"indices": [
+						65,
+						81
+					]
+				},
+				{
+					"screen_name": "Hyoyeon_djhyo",
+					"name": "Hyoyeon",
+					"id": 792687101842186200,
+					"id_str": "792687101842186240",
+					"indices": [
+						82,
+						96
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/BrhTrkivEb",
+					"expanded_url": "https://youtu.be/8otMkWY8umc",
+					"display_url": "youtu.be/8otMkWY8umc",
+					"indices": [
+						99,
+						122
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1528681976739205000,
+					"id_str": "1528681976739205120",
+					"indices": [
+						123,
+						146
+					],
+					"media_url": "http://pbs.twimg.com/media/FTb4KJYVEAA-45e.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTb4KJYVEAA-45e.jpg",
+					"url": "https://t.co/iI2NvVP9bb",
+					"display_url": "pic.twitter.com/iI2NvVP9bb",
+					"expanded_url": "https://twitter.com/kpop_sbs/status/1528688439679299587/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1528681976739205000,
+					"id_str": "1528681976739205120",
+					"indices": [
+						123,
+						146
+					],
+					"media_url": "http://pbs.twimg.com/media/FTb4KJYVEAA-45e.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTb4KJYVEAA-45e.jpg",
+					"url": "https://t.co/iI2NvVP9bb",
+					"display_url": "pic.twitter.com/iI2NvVP9bb",
+					"expanded_url": "https://twitter.com/kpop_sbs/status/1528688439679299587/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1131475077650772000,
+			"id_str": "1131475077650771968",
+			"name": "스브스케이팝 / SBS KPOP",
+			"screen_name": "kpop_sbs",
+			"location": "",
+			"description": "♡ 스브스케이팝 / SBS KPOP 공식 트위터 ♡ \nSBS의 예능/음악방송을 여러분께 더 많이 공유하고 소통하기 위한 채널입니다 :)",
+			"url": "https://t.co/dXfdxsnRTm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dXfdxsnRTm",
+							"expanded_url": "https://www.youtube.com/user/SBSMusic1",
+							"display_url": "youtube.com/user/SBSMusic1",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 680161,
+			"friends_count": 79,
+			"listed_count": 754,
+			"created_at": "Thu May 23 08:20:55 +0000 2019",
+			"favourites_count": 5461,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 5429,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1232179341775138818/Ry7b4M3d_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1232179341775138818/Ry7b4M3d_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1131475077650771968/1653192527",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 99,
+		"favorite_count": 183,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[🎥#페이스캠] 효연 - DEEP \n\n#인기가요 #HYO #효연 #DEEP #소녀시대 #GirlsGeneration @GirlsGeneration @Hyoyeon_djhyo\n\n📌https://t.co/BrhTrkivEb https://t.co/iI2NvVP9bb"
+	},
+	{
 		"created_at": "Mon May 23 04:07:09 +0000 2022",
 		"id": 1528577577043996700,
 		"id_str": "1528588316605652992",
