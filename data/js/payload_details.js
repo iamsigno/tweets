9a837f0fb495f8f31bd379a9dfc8ef5b7057b9e0
SIGNO <iamsigno@users.noreply.github.com>
2022-05-25 12:07:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29178,
-	"created_at": "Wed May 25 02:07:50 +0000 2022",
+	"tweets": 29179,
+	"created_at": "Wed May 25 03:07:50 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,226 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Wed May 25 03:07:09 +0000 2022",
+		"id": 1529291160576938000,
+		"id_str": "1529297996013359105",
+		"full_text": "[#NEWS] 서현 “운명처럼 만났어요” 나인우 인생 바꿀 여신 변신(징크스의 연인)\n▶ https://t.co/fwliY4N58d\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/ZLCpxK40cD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			110
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						75,
+						78
+					]
+				},
+				{
+					"text": "징크스의연인",
+					"indices": [
+						79,
+						86
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						87,
+						95
+					]
+				},
+				{
+					"text": "JinxedatFirst",
+					"indices": [
+						96,
+						110
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/fwliY4N58d",
+					"expanded_url": "http://naver.me/xsUSxxiE",
+					"display_url": "naver.me/xsUSxxiE",
+					"indices": [
+						50,
+						73
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1529288965433331700,
+					"id_str": "1529288965433331712",
+					"indices": [
+						111,
+						134
+					],
+					"media_url": "http://pbs.twimg.com/media/FTkgNieUEAAcK1Z.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTkgNieUEAAcK1Z.jpg",
+					"url": "https://t.co/ZLCpxK40cD",
+					"display_url": "pic.twitter.com/ZLCpxK40cD",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1529291160576937984/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 650,
+							"h": 836,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 650,
+							"h": 836,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 529,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1529288965433331700,
+					"id_str": "1529288965433331712",
+					"indices": [
+						111,
+						134
+					],
+					"media_url": "http://pbs.twimg.com/media/FTkgNieUEAAcK1Z.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTkgNieUEAAcK1Z.jpg",
+					"url": "https://t.co/ZLCpxK40cD",
+					"display_url": "pic.twitter.com/ZLCpxK40cD",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1529291160576937984/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 650,
+							"h": 836,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 650,
+							"h": 836,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 529,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 60283,
+			"friends_count": 12,
+			"listed_count": 272,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 125,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15648,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 106,
+		"favorite_count": 137,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] 서현 “운명처럼 만났어요” 나인우 인생 바꿀 여신 변신(징크스의 연인)\n▶ https://t.co/fwliY4N58d\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/ZLCpxK40cD"
+	},
+	{
 		"created_at": "Wed May 25 02:07:09 +0000 2022",
 		"id": 1529275349733081000,
 		"id_str": "1529282892832116744",
