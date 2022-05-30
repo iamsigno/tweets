cbb13aa531a2660eaabaaa8551ebb472c6ef3526
SIGNO <iamsigno@users.noreply.github.com>
2022-05-30 14:07:50 +0900

    Added 1 tweets

--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Mon May 30 05:07:16 +0000 2022",
+		"id": 1531138629028163600,
+		"id_str": "1531140161496555520",
+		"full_text": "💖Happy Birthday YOONA💖\n\nGIRLS' GENERATION YOONA의 생일을 기념하는 ARTIST BIRTHDAY INITIAL RING을 SMTOWN &amp;STORE에서 지금 바로 만나보세요🥳🥳\n\n🎉판매 기간 : 2022. 5. 30 (MON) 14:00~23:59 (KST)\n🎉https://t.co/WXKP6fX4Ld https://t.co/WBGfXsAXJd",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			192
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/WXKP6fX4Ld",
+					"expanded_url": "https://bit.ly/3anQ69b",
+					"display_url": "bit.ly/3anQ69b",
+					"indices": [
+						169,
+						192
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1531136726315712500,
+					"id_str": "1531136726315712513",
+					"indices": [
+						193,
+						216
+					],
+					"media_url": "http://pbs.twimg.com/media/FT-wvYKaQAE412o.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FT-wvYKaQAE412o.jpg",
+					"url": "https://t.co/WBGfXsAXJd",
+					"display_url": "pic.twitter.com/WBGfXsAXJd",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1531138629028163585/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1108,
+							"h": 1478,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1531136726315712500,
+					"id_str": "1531136726315712513",
+					"indices": [
+						193,
+						216
+					],
+					"media_url": "http://pbs.twimg.com/media/FT-wvYKaQAE412o.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FT-wvYKaQAE412o.jpg",
+					"url": "https://t.co/WBGfXsAXJd",
+					"display_url": "pic.twitter.com/WBGfXsAXJd",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1531138629028163585/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1108,
+							"h": 1478,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1113723889245278200,
+			"id_str": "1113723889245278209",
+			"name": "SMTOWN &STORE",
+			"screen_name": "SMTOWNandSTORE",
+			"location": "",
+			"description": "",
+			"url": "https://t.co/Ee3t6o418Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Ee3t6o418Q",
+							"expanded_url": "http://www.SMTOWNandSTORE.com",
+							"display_url": "SMTOWNandSTORE.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 221520,
+			"friends_count": 0,
+			"listed_count": 374,
+			"created_at": "Thu Apr 04 08:44:01 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2607,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1113723889245278209/1554705745",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 35,
+		"favorite_count": 92,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "💖Happy Birthday YOONA💖\n\nGIRLS' GENERATION YOONA의 생일을 기념하는 ARTIST BIRTHDAY INITIAL RING을 SMTOWN &amp;STORE에서 지금 바로 만나보세요🥳🥳\n\n🎉판매 기간 : 2022. 5. 30 (MON) 14:00~23:59 (KST)\n🎉https://t.co/WXKP6fX4Ld https://t.co/WBGfXsAXJd"
+	},
+	{
 		"created_at": "Mon May 30 04:07:08 +0000 2022",
 		"id": 1531121209403773000,
 		"id_str": "1531125031048298500",
