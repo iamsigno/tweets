e0f628edcdda07da446488a4d0aa0e9b14f94c05
SIGNO <iamsigno@users.noreply.github.com>
2022-05-30 11:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29194,
-	"created_at": "Mon May 30 01:07:52 +0000 2022",
+	"tweets": 29195,
+	"created_at": "Mon May 30 02:07:49 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Mon May 30 02:07:09 +0000 2022",
+		"id": 1531090727023046700,
+		"id_str": "1531094832172638209",
+		"full_text": "#HappyYOONADay 🎂🎉\n\n220530 #YOONA #윤아 #소녀시대 #GirlsGeneration https://t.co/t9eXo6W8yt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			59
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HappyYOONADay",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"text": "YOONA",
+					"indices": [
+						26,
+						32
+					]
+				},
+				{
+					"text": "윤아",
+					"indices": [
+						33,
+						36
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						37,
+						42
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						43,
+						59
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1531090575654801400,
+					"id_str": "1531090575654801408",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/FT-GxDjaUAALsf5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FT-GxDjaUAALsf5.jpg",
+					"url": "https://t.co/t9eXo6W8yt",
+					"display_url": "pic.twitter.com/t9eXo6W8yt",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1531090727023046656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1199,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1501,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1531090575654801400,
+					"id_str": "1531090575654801408",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/FT-GxDjaUAALsf5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FT-GxDjaUAALsf5.jpg",
+					"url": "https://t.co/t9eXo6W8yt",
+					"display_url": "pic.twitter.com/t9eXo6W8yt",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1531090727023046656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1199,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1501,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBV3cSH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBV3cSH",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3771011,
+			"friends_count": 5,
+			"listed_count": 5896,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2860,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1520784620396367872/n-mDvaB1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1520784620396367872/n-mDvaB1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1651417198",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2869,
+		"favorite_count": 4442,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "#HappyYOONADay 🎂🎉\n\n220530 #YOONA #윤아 #소녀시대 #GirlsGeneration https://t.co/t9eXo6W8yt"
+	},
+	{
 		"created_at": "Mon May 30 00:44:57 +0000 2022",
 		"id": 1531069910570389500,
 		"id_str": "1531074146586841090",
