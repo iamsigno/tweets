008516180b3a6de8944d656c48ff534f3825e936
SIGNO <iamsigno@users.noreply.github.com>
2022-06-02 21:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29208,
-	"created_at": "Thu Jun 02 06:07:48 +0000 2022",
+	"tweets": 29209,
+	"created_at": "Thu Jun 02 12:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,256 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Thu Jun 02 12:07:08 +0000 2022",
+		"id": 1532319585390645200,
+		"id_str": "1532332990403121152",
+		"full_text": "👑 TAEYEON โชว์ความสง่างามยิ่งใหญ่ในฐานะแกรนด์มาสเตอร์ พร้อมแบ่งปันความคิดเห็นก่อนตอนจบของ ‘Queendom 2’!\n\n💗 https://t.co/JGaOo5qeiq\n\n#태연 #TAEYEON \n#퀸덤2 #Queendom2 https://t.co/bYauzmrhIG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			161
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "태연",
+					"indices": [
+						132,
+						135
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						136,
+						144
+					]
+				},
+				{
+					"text": "퀸덤2",
+					"indices": [
+						146,
+						150
+					]
+				},
+				{
+					"text": "Queendom2",
+					"indices": [
+						151,
+						161
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/JGaOo5qeiq",
+					"expanded_url": "http://facebook.com/smtruethailand/posts/pfbid02PmQztQbG8u53gW22bFMgjnNwpmg4wJW3ePFfEe3EY2fYPbmfLygq8jKBoRuuha5Nl",
+					"display_url": "facebook.com/smtruethailand…",
+					"indices": [
+						107,
+						130
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1532319375750885400,
+					"id_str": "1532319375750885377",
+					"indices": [
+						162,
+						185
+					],
+					"media_url": "http://pbs.twimg.com/media/FUPkWooUcAECYCR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FUPkWooUcAECYCR.jpg",
+					"url": "https://t.co/bYauzmrhIG",
+					"display_url": "pic.twitter.com/bYauzmrhIG",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1532319585390645248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1532319375750885400,
+					"id_str": "1532319375750885377",
+					"indices": [
+						162,
+						185
+					],
+					"media_url": "http://pbs.twimg.com/media/FUPkWooUcAECYCR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FUPkWooUcAECYCR.jpg",
+					"url": "https://t.co/bYauzmrhIG",
+					"display_url": "pic.twitter.com/bYauzmrhIG",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1532319585390645248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1532319375784431600,
+					"id_str": "1532319375784431618",
+					"indices": [
+						162,
+						185
+					],
+					"media_url": "http://pbs.twimg.com/media/FUPkWowUUAIwVxI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FUPkWowUUAIwVxI.jpg",
+					"url": "https://t.co/bYauzmrhIG",
+					"display_url": "pic.twitter.com/bYauzmrhIG",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1532319585390645248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1500,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1093456514700828700,
+			"id_str": "1093456514700828672",
+			"name": "SM True",
+			"screen_name": "SMTrueThailand",
+			"location": "Bangkok, Thailand",
+			"description": "SM True Official Twitter Page",
+			"url": "https://t.co/fe1MwqbQdx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fe1MwqbQdx",
+							"expanded_url": "https://www.facebook.com/smtruethailand",
+							"display_url": "facebook.com/smtruethailand",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 93283,
+			"friends_count": 51,
+			"listed_count": 151,
+			"created_at": "Thu Feb 07 10:28:43 +0000 2019",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 11291,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1093456514700828672/1562743620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 138,
+		"favorite_count": 69,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "th",
+		"text": "👑 TAEYEON โชว์ความสง่างามยิ่งใหญ่ในฐานะแกรนด์มาสเตอร์ พร้อมแบ่งปันความคิดเห็นก่อนตอนจบของ ‘Queendom 2’!\n\n💗 https://t.co/JGaOo5qeiq\n\n#태연 #TAEYEON \n#퀸덤2 #Queendom2 https://t.co/bYauzmrhIG"
+	},
+	{
 		"created_at": "Thu Jun 02 06:07:21 +0000 2022",
 		"id": 1532224873883238400,
 		"id_str": "1532242445173391360",
