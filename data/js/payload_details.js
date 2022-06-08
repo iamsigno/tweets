f1445baa5aa65bbb53d0f1880f594621668af7c2
SIGNO <iamsigno@users.noreply.github.com>
2022-06-08 21:07:48 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29220,
-	"created_at": "Wed Jun 08 10:07:47 +0000 2022",
+	"tweets": 29221,
+	"created_at": "Wed Jun 08 12:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,234 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Wed Jun 08 12:07:08 +0000 2022",
+		"id": 1534505530739372000,
+		"id_str": "1534507317483421696",
+		"full_text": "[Weekly Playlist l 4K캠] LIGHTSUM - Lion Heart (라잇썸 - 라이언 하트) (원곡 : 소녀시대) \n\n📎https://t.co/uUQTTFBRjC\n\n#주간아이돌 #LIGHTSUM #라잇썸 #소녀시대 #SNSD #Lion_Heart https://t.co/R73P74C89E",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			146
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "주간아이돌",
+					"indices": [
+						101,
+						107
+					]
+				},
+				{
+					"text": "LIGHTSUM",
+					"indices": [
+						108,
+						117
+					]
+				},
+				{
+					"text": "라잇썸",
+					"indices": [
+						118,
+						122
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						123,
+						128
+					]
+				},
+				{
+					"text": "SNSD",
+					"indices": [
+						129,
+						134
+					]
+				},
+				{
+					"text": "Lion_Heart",
+					"indices": [
+						135,
+						146
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/uUQTTFBRjC",
+					"expanded_url": "https://youtu.be/re4-eGcEO6w",
+					"display_url": "youtu.be/re4-eGcEO6w",
+					"indices": [
+						76,
+						99
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1534440852411805700,
+					"id_str": "1534440852411805696",
+					"indices": [
+						147,
+						170
+					],
+					"media_url": "http://pbs.twimg.com/media/FUtt01DacAAfeao.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FUtt01DacAAfeao.jpg",
+					"url": "https://t.co/R73P74C89E",
+					"display_url": "pic.twitter.com/R73P74C89E",
+					"expanded_url": "https://twitter.com/ALLTHEKPOP/status/1534505530739372033/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1534440852411805700,
+					"id_str": "1534440852411805696",
+					"indices": [
+						147,
+						170
+					],
+					"media_url": "http://pbs.twimg.com/media/FUtt01DacAAfeao.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FUtt01DacAAfeao.jpg",
+					"url": "https://t.co/R73P74C89E",
+					"display_url": "pic.twitter.com/R73P74C89E",
+					"expanded_url": "https://twitter.com/ALLTHEKPOP/status/1534505530739372033/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 744928926,
+			"id_str": "744928926",
+			"name": "ALL THE K-POP 올더케이팝",
+			"screen_name": "ALLTHEKPOP",
+			"location": "",
+			"description": "✨ALL about K-POP, ALL THE K-POP✨\n\n🎶 24/7 K-POP LIVE STREAMING\n\n💕 POPPIN' WEDNESDAY\n      📌 SHOW CHAMPION, 6PM KST\n      📌 WEEKLY IDOL, 7:30PM KST",
+			"url": "https://t.co/0kbjbCne8R",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0kbjbCne8R",
+							"expanded_url": "http://www.youtube.com/allthekpop",
+							"display_url": "youtube.com/allthekpop",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 123212,
+			"friends_count": 343,
+			"listed_count": 386,
+			"created_at": "Wed Aug 08 10:28:32 +0000 2012",
+			"favourites_count": 7849,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 9207,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035092488409632769/sDzylGOZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035092488409632769/sDzylGOZ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/744928926/1654488893",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[Weekly Playlist l 4K캠] LIGHTSUM - Lion Heart (라잇썸 - 라이언 하트) (원곡 : 소녀시대) \n\n📎https://t.co/uUQTTFBRjC\n\n#주간아이돌 #LIGHTSUM #라잇썸 #소녀시대 #SNSD #Lion_Heart https://t.co/R73P74C89E"
+	},
+	{
 		"created_at": "Wed Jun 08 10:07:08 +0000 2022",
 		"id": 1534472803944083500,
 		"id_str": "1534477118557589504",
