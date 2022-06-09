fc3c9b758ef9cafddcafbf3b265544f298e3986a
SIGNO <iamsigno@users.noreply.github.com>
2022-06-10 05:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29223,
-	"created_at": "Thu Jun 09 09:07:48 +0000 2022",
+	"tweets": 29224,
+	"created_at": "Thu Jun 09 20:07:50 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Thu Jun 09 20:07:09 +0000 2022",
+		"id": 1534983580832907300,
+		"id_str": "1534990501853777921",
+		"full_text": "쿨룩 LIVE ▷ 먼데이,소은(Weeekly) ‘사계’[원곡: 태연] / [비투비의 키스 더 라디오] l KBS 220609 방송\nhttps://t.co/cYilCADa9h https://t.co/vBDH2pyUjc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			96
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/cYilCADa9h",
+					"expanded_url": "https://youtu.be/eoLqnE3uiKg",
+					"display_url": "youtu.be/eoLqnE3uiKg",
+					"indices": [
+						73,
+						96
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1534983571592855600,
+					"id_str": "1534983571592855553",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/FU1bbPjaQAEpu5A.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FU1bbPjaQAEpu5A.jpg",
+					"url": "https://t.co/vBDH2pyUjc",
+					"display_url": "pic.twitter.com/vBDH2pyUjc",
+					"expanded_url": "https://twitter.com/KBS_COOL_FM/status/1534983580832907264/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1534983571592855600,
+					"id_str": "1534983571592855553",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/FU1bbPjaQAEpu5A.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FU1bbPjaQAEpu5A.jpg",
+					"url": "https://t.co/vBDH2pyUjc",
+					"display_url": "pic.twitter.com/vBDH2pyUjc",
+					"expanded_url": "https://twitter.com/KBS_COOL_FM/status/1534983580832907264/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2292392636,
+			"id_str": "2292392636",
+			"name": "KBS Cool FM",
+			"screen_name": "KBS_COOL_FM",
+			"location": "",
+			"description": "KBS 쿨FM 오피셜 계정입니다🆒📻💗\n#쿨FM #89.1MHz",
+			"url": "https://t.co/iWikkJSsdH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/iWikkJSsdH",
+							"expanded_url": "https://www.youtube.com/channel/UCbVRtqsTmYh1xhrDSTfSQLg",
+							"display_url": "youtube.com/channel/UCbVRt…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23686,
+			"friends_count": 3,
+			"listed_count": 124,
+			"created_at": "Wed Jan 15 08:45:26 +0000 2014",
+			"favourites_count": 2,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 8339,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1343472294094462977/nW_jHN92_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1343472294094462977/nW_jHN92_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2292392636/1609143731",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "쿨룩 LIVE ▷ 먼데이,소은(Weeekly) ‘사계’[원곡: 태연] / [비투비의 키스 더 라디오] l KBS 220609 방송\nhttps://t.co/cYilCADa9h https://t.co/vBDH2pyUjc"
+	},
+	{
 		"created_at": "Thu Jun 09 09:07:09 +0000 2022",
 		"id": 1534821585336828000,
 		"id_str": "1534824407339880452",
