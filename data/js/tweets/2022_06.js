cf99386d31d3216942bd1948bb436be1c8291923
SIGNO <iamsigno@users.noreply.github.com>
2022-06-23 14:07:50 +0900

    Added 1 tweets

--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,252 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Thu Jun 23 05:07:09 +0000 2022",
+		"id": 1539835644658061300,
+		"id_str": "1539837439627386880",
+		"full_text": "[#슬비로운_슬비생활🧸]\n\n골라 골라 골라 ୧( ˵ ° ~ ° ˵ )୨\n슬비도 보고 서현 배우도 보고 1⃣➕1⃣\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/UjhLIlgWvr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			99
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "슬비로운_슬비생활",
+					"indices": [
+						1,
+						11
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						64,
+						67
+					]
+				},
+				{
+					"text": "징크스의연인",
+					"indices": [
+						68,
+						75
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						76,
+						84
+					]
+				},
+				{
+					"text": "JinxedatFirst",
+					"indices": [
+						85,
+						99
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1539788269155909600,
+					"id_str": "1539788269155909632",
+					"indices": [
+						100,
+						123
+					],
+					"media_url": "http://pbs.twimg.com/media/FV5tRcxaAAAjnkO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FV5tRcxaAAAjnkO.jpg",
+					"url": "https://t.co/UjhLIlgWvr",
+					"display_url": "pic.twitter.com/UjhLIlgWvr",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1539835644658061312/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 762,
+							"h": 1067,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 486,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 762,
+							"h": 1067,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1539788269155909600,
+					"id_str": "1539788269155909632",
+					"indices": [
+						100,
+						123
+					],
+					"media_url": "http://pbs.twimg.com/media/FV5tRcxaAAAjnkO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FV5tRcxaAAAjnkO.jpg",
+					"url": "https://t.co/UjhLIlgWvr",
+					"display_url": "pic.twitter.com/UjhLIlgWvr",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1539835644658061312/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 762,
+							"h": 1067,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 486,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 762,
+							"h": 1067,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1539788269218852900,
+					"id_str": "1539788269218852864",
+					"indices": [
+						100,
+						123
+					],
+					"media_url": "http://pbs.twimg.com/media/FV5tRdAacAAX_Wi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FV5tRdAacAAX_Wi.jpg",
+					"url": "https://t.co/UjhLIlgWvr",
+					"display_url": "pic.twitter.com/UjhLIlgWvr",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1539835644658061312/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 486,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 762,
+							"h": 1067,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 762,
+							"h": 1067,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 60838,
+			"friends_count": 12,
+			"listed_count": 269,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 125,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15839,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 83,
+		"favorite_count": 109,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#슬비로운_슬비생활🧸]\n\n골라 골라 골라 ୧( ˵ ° ~ ° ˵ )୨\n슬비도 보고 서현 배우도 보고 1⃣➕1⃣\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/UjhLIlgWvr"
+	},
+	{
 		"created_at": "Thu Jun 23 02:07:08 +0000 2022",
 		"id": 1539787521445752800,
 		"id_str": "1539792140573843463",
