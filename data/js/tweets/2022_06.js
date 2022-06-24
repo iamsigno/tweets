8e72477f051ee40caf535dc5ee07ccb28db1ff8d
SIGNO <iamsigno@users.noreply.github.com>
2022-06-24 17:07:49 +0900

    Added 1 tweets

--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,10 +1,139 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Fri Jun 24 07:44:57 +0000 2022",
+		"id": 1540233381090275300,
+		"id_str": "1540239539150721024",
+		"full_text": "[메이킹] 저렇게 예쁘게 노는 언니들 어디 가면 볼 수 있죠?🤷🤷‍ 연기존잘만 8명인 소녀시대의 소시탐탐 티저 촬영 현장 | ＜소시탐탐＞\n\nhttps://t.co/eySs1Psk0P\n\n#소녀시대 #GirlsGeneration \n#소시탐탐",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			132
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "소녀시대",
+					"indices": [
+						103,
+						108
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						109,
+						125
+					]
+				},
+				{
+					"text": "소시탐탐",
+					"indices": [
+						127,
+						132
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/eySs1Psk0P",
+					"expanded_url": "https://youtu.be/9LrIrtSsqT0",
+					"display_url": "youtu.be/9LrIrtSsqT0",
+					"indices": [
+						78,
+						101
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBV3cSH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBV3cSH",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3774933,
+			"friends_count": 5,
+			"listed_count": 5945,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2869,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1520784620396367872/n-mDvaB1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1520784620396367872/n-mDvaB1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1651417198",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1901,
+		"favorite_count": 2763,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[메이킹] 저렇게 예쁘게 노는 언니들 어디 가면 볼 수 있죠?🤷🤷‍ 연기존잘만 8명인 소녀시대의 소시탐탐 티저 촬영 현장 | ＜소시탐탐＞\n\nhttps://t.co/eySs1Psk0P\n\n#소녀시대 #GirlsGeneration \n#소시탐탐"
+	},
+	{
 		"created_at": "Fri Jun 24 03:07:08 +0000 2022",
 		"id": 1540157763636015000,
 		"id_str": "1540169627807977472",
-		"full_text": "[#VIDEO] [메이킹] 저렇게 예쁘게 노는 언니들 어디 가면 볼 수 있죠?🤷🤷‍ 연기존잘만 8명인 소녀시대의 소시탐탐 티저 촬영 현장 | ＜소시탐탐＞\n▶ https://t.co/DfHsX8DnNt\n\n#서현 #소시탐탐 #SEOHYUN",
+		"full_text": "[#VIDEO] [메이킹] 저렇게 예쁘게 노는 언니들 어디 가면 볼 수 있죠?🤷🤷 연기존잘만 8명인 소녀시대의 소시탐탐 티저 촬영 현장 | ＜소시탐탐＞\n▶ https://t.co/DfHsX8DnNt\n\n#서현 #소시탐탐 #SEOHYUN",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -133,7 +262,7 @@ Grailbird.data.tweets_2022_06 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "[#VIDEO] [메이킹] 저렇게 예쁘게 노는 언니들 어디 가면 볼 수 있죠?🤷🤷‍ 연기존잘만 8명인 소녀시대의 소시탐탐 티저 촬영 현장 | ＜소시탐탐＞\n▶ https://t.co/DfHsX8DnNt\n\n#서현 #소시탐탐 #SEOHYUN"
+		"text": "[#VIDEO] [메이킹] 저렇게 예쁘게 노는 언니들 어디 가면 볼 수 있죠?🤷🤷 연기존잘만 8명인 소녀시대의 소시탐탐 티저 촬영 현장 | ＜소시탐탐＞\n▶ https://t.co/DfHsX8DnNt\n\n#서현 #소시탐탐 #SEOHYUN"
 	},
 	{
 		"created_at": "Fri Jun 24 02:07:14 +0000 2022",
