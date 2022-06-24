f8fc9944502453550245f4f7cf8b6c23cd39610d
SIGNO <iamsigno@users.noreply.github.com>
2022-06-24 11:07:50 +0900

    Added 1 tweets

--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,226 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Fri Jun 24 02:07:14 +0000 2022",
+		"id": 1540150025921040400,
+		"id_str": "1540154550576992261",
+		"full_text": "[#NEWS] '징크스의 연인' 자체 최고 시청률…서현, 나인우 위한 행운의 여신\n▶ https://t.co/PTVmpWnMQb\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/QjPLZ4a8Nf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			108
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						73,
+						76
+					]
+				},
+				{
+					"text": "징크스의연인",
+					"indices": [
+						77,
+						84
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						85,
+						93
+					]
+				},
+				{
+					"text": "JinxedatFirst",
+					"indices": [
+						94,
+						108
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/PTVmpWnMQb",
+					"expanded_url": "https://bit.ly/3zYM55Q",
+					"display_url": "bit.ly/3zYM55Q",
+					"indices": [
+						48,
+						71
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1540150015016181800,
+					"id_str": "1540150015016181760",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/FV-2R1HaIAAAg5O.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FV-2R1HaIAAAg5O.jpg",
+					"url": "https://t.co/QjPLZ4a8Nf",
+					"display_url": "pic.twitter.com/QjPLZ4a8Nf",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1540150025921040384/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 600,
+							"h": 1049,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 389,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 600,
+							"h": 1049,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1540150015016181800,
+					"id_str": "1540150015016181760",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/FV-2R1HaIAAAg5O.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FV-2R1HaIAAAg5O.jpg",
+					"url": "https://t.co/QjPLZ4a8Nf",
+					"display_url": "pic.twitter.com/QjPLZ4a8Nf",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1540150025921040384/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 600,
+							"h": 1049,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 389,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 600,
+							"h": 1049,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 60843,
+			"friends_count": 12,
+			"listed_count": 269,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 125,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15845,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 77,
+		"favorite_count": 108,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] '징크스의 연인' 자체 최고 시청률…서현, 나인우 위한 행운의 여신\n▶ https://t.co/PTVmpWnMQb\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/QjPLZ4a8Nf"
+	},
+	{
 		"created_at": "Thu Jun 23 09:07:08 +0000 2022",
 		"id": 1539888027882795000,
 		"id_str": "1539897837110059008",
