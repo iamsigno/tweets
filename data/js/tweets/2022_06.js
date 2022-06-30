59a24c3cf084b809ebf22846e4939fe78aa6854a
SIGNO <iamsigno@users.noreply.github.com>
2022-06-30 16:07:49 +0900

    Added 1 tweets

--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,262 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Thu Jun 30 07:07:08 +0000 2022",
+		"id": 1542399535091179500,
+		"id_str": "1542404352949850112",
+		"full_text": "[#NEWS] '징크스의 연인' 나인우♥서현, 로맨스 시작?..'설렘주의보' 입맞춤 \n▶ https://t.co/H0u4fzkhiH\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/hOvYOwEpTg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			110
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						75,
+						78
+					]
+				},
+				{
+					"text": "징크스의연인",
+					"indices": [
+						79,
+						86
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						87,
+						95
+					]
+				},
+				{
+					"text": "JinxedatFirst",
+					"indices": [
+						96,
+						110
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/H0u4fzkhiH",
+					"expanded_url": "http://naver.me/FZq3xiPf",
+					"display_url": "naver.me/FZq3xiPf",
+					"indices": [
+						50,
+						73
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1542397066042740700,
+					"id_str": "1542397066042740736",
+					"indices": [
+						111,
+						134
+					],
+					"media_url": "http://pbs.twimg.com/media/FWex9agVEAAVSpI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWex9agVEAAVSpI.jpg",
+					"url": "https://t.co/hOvYOwEpTg",
+					"display_url": "pic.twitter.com/hOvYOwEpTg",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1542399535091179520/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 961,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 801,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1542397066042740700,
+					"id_str": "1542397066042740736",
+					"indices": [
+						111,
+						134
+					],
+					"media_url": "http://pbs.twimg.com/media/FWex9agVEAAVSpI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWex9agVEAAVSpI.jpg",
+					"url": "https://t.co/hOvYOwEpTg",
+					"display_url": "pic.twitter.com/hOvYOwEpTg",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1542399535091179520/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 961,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 801,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1542397066218852400,
+					"id_str": "1542397066218852353",
+					"indices": [
+						111,
+						134
+					],
+					"media_url": "http://pbs.twimg.com/media/FWex9bKUUAEeMdw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWex9bKUUAEeMdw.jpg",
+					"url": "https://t.co/hOvYOwEpTg",
+					"display_url": "pic.twitter.com/hOvYOwEpTg",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1542399535091179520/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 801,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 961,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 60943,
+			"friends_count": 12,
+			"listed_count": 269,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 125,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15892,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 112,
+		"favorite_count": 158,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] '징크스의 연인' 나인우♥서현, 로맨스 시작?..'설렘주의보' 입맞춤 \n▶ https://t.co/H0u4fzkhiH\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/hOvYOwEpTg"
+	},
+	{
 		"created_at": "Thu Jun 30 05:07:08 +0000 2022",
 		"id": 1542372357011939300,
 		"id_str": "1542374153164029956",
