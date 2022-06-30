8ba67f26a520a781275d08c3bc26109681a32149
SIGNO <iamsigno@users.noreply.github.com>
2022-06-30 14:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29287,
-	"created_at": "Thu Jun 30 03:07:49 +0000 2022",
+	"tweets": 29288,
+	"created_at": "Thu Jun 30 05:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,252 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Thu Jun 30 05:07:08 +0000 2022",
+		"id": 1542372357011939300,
+		"id_str": "1542374153164029956",
+		"full_text": "[#슬비로운_슬비생활🧸]\n\n나무는 슬비를 좋아한다고!!\n슬비를 좋아해 좋아해 좋아해 100번💗\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/tqzVjm3zKQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			89
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "슬비로운_슬비생활",
+					"indices": [
+						1,
+						11
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						54,
+						57
+					]
+				},
+				{
+					"text": "징크스의연인",
+					"indices": [
+						58,
+						65
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						66,
+						74
+					]
+				},
+				{
+					"text": "JinxedatFirst",
+					"indices": [
+						75,
+						89
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1542359556616900600,
+					"id_str": "1542359556616900608",
+					"indices": [
+						90,
+						113
+					],
+					"media_url": "http://pbs.twimg.com/media/FWeP2FAUUAA_Qw4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWeP2FAUUAA_Qw4.jpg",
+					"url": "https://t.co/tqzVjm3zKQ",
+					"display_url": "pic.twitter.com/tqzVjm3zKQ",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1542372357011939330/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1542359556616900600,
+					"id_str": "1542359556616900608",
+					"indices": [
+						90,
+						113
+					],
+					"media_url": "http://pbs.twimg.com/media/FWeP2FAUUAA_Qw4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWeP2FAUUAA_Qw4.jpg",
+					"url": "https://t.co/tqzVjm3zKQ",
+					"display_url": "pic.twitter.com/tqzVjm3zKQ",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1542372357011939330/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1542359556621119500,
+					"id_str": "1542359556621119489",
+					"indices": [
+						90,
+						113
+					],
+					"media_url": "http://pbs.twimg.com/media/FWeP2FBUsAEhEES.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWeP2FBUsAEhEES.jpg",
+					"url": "https://t.co/tqzVjm3zKQ",
+					"display_url": "pic.twitter.com/tqzVjm3zKQ",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1542372357011939330/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 60936,
+			"friends_count": 12,
+			"listed_count": 269,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 125,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15888,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 106,
+		"favorite_count": 124,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#슬비로운_슬비생활🧸]\n\n나무는 슬비를 좋아한다고!!\n슬비를 좋아해 좋아해 좋아해 100번💗\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/tqzVjm3zKQ"
+	},
+	{
 		"created_at": "Thu Jun 30 03:07:17 +0000 2022",
 		"id": 1542340647151075300,
 		"id_str": "1542343990707716099",
