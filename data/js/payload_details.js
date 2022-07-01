72becdad1063b8b40d58a53bfe264ca005c88403
SIGNO <iamsigno@users.noreply.github.com>
2022-07-01 11:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29291,
-	"created_at": "Thu Jun 30 10:07:49 +0000 2022",
+	"tweets": 29292,
+	"created_at": "Fri Jul 01 02:07:48 +0000 2022",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2022_07.js
@@ -0,0 +1,223 @@
+Grailbird.data.tweets_2022_07 = 
+[
+	{
+		"created_at": "Fri Jul 01 02:07:08 +0000 2022",
+		"id": 1542683657407791000,
+		"id_str": "1542691243439919105",
+		"full_text": "[#NEWS] ‘징크스의 연인’ 서현♥나인우, 뜨거운 입맞춤…수목극 최강자 굳혔다\n▶ https://t.co/Dhs7VrEoBy\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/aXDiPP5fUx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			108
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						73,
+						76
+					]
+				},
+				{
+					"text": "징크스의연인",
+					"indices": [
+						77,
+						84
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						85,
+						93
+					]
+				},
+				{
+					"text": "JinxedatFirst",
+					"indices": [
+						94,
+						108
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Dhs7VrEoBy",
+					"expanded_url": "http://naver.me/GX0jjU79",
+					"display_url": "naver.me/GX0jjU79",
+					"indices": [
+						48,
+						71
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1542681259884744700,
+					"id_str": "1542681259884744707",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/FWi0brEVEAM2o_c.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWi0brEVEAM2o_c.jpg",
+					"url": "https://t.co/aXDiPP5fUx",
+					"display_url": "pic.twitter.com/aXDiPP5fUx",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1542683657407791107/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 499,
+							"h": 378,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 499,
+							"h": 378,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 499,
+							"h": 378,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1542681259884744700,
+					"id_str": "1542681259884744707",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/FWi0brEVEAM2o_c.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWi0brEVEAM2o_c.jpg",
+					"url": "https://t.co/aXDiPP5fUx",
+					"display_url": "pic.twitter.com/aXDiPP5fUx",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1542683657407791107/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 499,
+							"h": 378,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 499,
+							"h": 378,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 499,
+							"h": 378,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 60955,
+			"friends_count": 11,
+			"listed_count": 268,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 125,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15899,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 93,
+		"favorite_count": 128,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] ‘징크스의 연인’ 서현♥나인우, 뜨거운 입맞춤…수목극 최강자 굳혔다\n▶ https://t.co/Dhs7VrEoBy\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/aXDiPP5fUx"
+	}
+]
