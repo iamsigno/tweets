af66667d7f3f8ee2c9ade871b91e950fef6572d5
SIGNO <iamsigno@users.noreply.github.com>
2022-07-05 14:07:49 +0900

    Added 1 tweets

--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,135 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Tue Jul 05 05:07:09 +0000 2022",
+		"id": 1544183718629486600,
+		"id_str": "1544186095751028744",
+		"full_text": "[1회 선공개] ＂아니지 유리야♨＂ 유리 음정 잡아주는 수영X효연 특) 같은 댄서임ㅋㅋ | 〈소시탐탐〉 7/5(화) 오늘 밤 9시 첫 방송\n\n[Ep.1 preview] &lt;소시탐탐&gt;'s 1st episode to air tonight (July 5th) at 9PM KST!\n\nhttps://t.co/4YNxzdTEEz\n\n#소녀시대 #GirlsGeneration \n#소시탐탐",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			214
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "소녀시대",
+					"indices": [
+						185,
+						190
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						191,
+						207
+					]
+				},
+				{
+					"text": "소시탐탐",
+					"indices": [
+						209,
+						214
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/4YNxzdTEEz",
+					"expanded_url": "https://youtu.be/Zi8IaNI4pJI",
+					"display_url": "youtu.be/Zi8IaNI4pJI",
+					"indices": [
+						160,
+						183
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBV3cSH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBV3cSH",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3777252,
+			"friends_count": 5,
+			"listed_count": 5953,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2875,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1520784620396367872/n-mDvaB1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1520784620396367872/n-mDvaB1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1651417198",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 903,
+		"favorite_count": 1219,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[1회 선공개] ＂아니지 유리야♨＂ 유리 음정 잡아주는 수영X효연 특) 같은 댄서임ㅋㅋ | 〈소시탐탐〉 7/5(화) 오늘 밤 9시 첫 방송\n\n[Ep.1 preview] &lt;소시탐탐&gt;'s 1st episode to air tonight (July 5th) at 9PM KST!\n\nhttps://t.co/4YNxzdTEEz\n\n#소녀시대 #GirlsGeneration \n#소시탐탐"
+	},
+	{
 		"created_at": "Tue Jul 05 04:07:08 +0000 2022",
 		"id": 1544170015079153700,
 		"id_str": "1544170993886941188",
