4bb560587d0a5bc64a6a22b04276084f876f3ba7
SIGNO <iamsigno@users.noreply.github.com>
2022-07-08 18:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29314,
-	"created_at": "Fri Jul 08 03:07:48 +0000 2022",
+	"tweets": 29315,
+	"created_at": "Fri Jul 08 09:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Fri Jul 08 09:07:14 +0000 2022",
+		"id": 1545333464094093300,
+		"id_str": "1545333676837408773",
+		"full_text": "여자아이돌 2022년 1학기 성적발표\n2022 K-pop girlgroup first half results\n#KPOP #Girlgroup #RedVelvet #IVE #LeSserafim #GIdle #Taeyeon #STAYC \nhttps://t.co/pSseVGOzmT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			153
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "KPOP",
+					"indices": [
+						61,
+						66
+					]
+				},
+				{
+					"text": "Girlgroup",
+					"indices": [
+						67,
+						77
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						78,
+						88
+					]
+				},
+				{
+					"text": "IVE",
+					"indices": [
+						89,
+						93
+					]
+				},
+				{
+					"text": "LeSserafim",
+					"indices": [
+						94,
+						105
+					]
+				},
+				{
+					"text": "GIdle",
+					"indices": [
+						106,
+						112
+					]
+				},
+				{
+					"text": "Taeyeon",
+					"indices": [
+						113,
+						121
+					]
+				},
+				{
+					"text": "STAYC",
+					"indices": [
+						122,
+						128
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/pSseVGOzmT",
+					"expanded_url": "https://youtu.be/2zxKDB8gppU",
+					"display_url": "youtu.be/2zxKDB8gppU",
+					"indices": [
+						130,
+						153
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3435012852,
+			"id_str": "3435012852",
+			"name": "Gaon Chart",
+			"screen_name": "GaonChart",
+			"location": "대한민국",
+			"description": "Korea Music Content Association\nhttps://t.co/vxaIbW9qBN\nhttps://t.co/0Eibing6CM\nhttps://t.co/fnADI1nzVz",
+			"url": "https://t.co/Rs32cCCXDV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Rs32cCCXDV",
+							"expanded_url": "http://gaonchart.co.kr",
+							"display_url": "gaonchart.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/vxaIbW9qBN",
+							"expanded_url": "http://facebook.com/gaonchart.co.kr",
+							"display_url": "facebook.com/gaonchart.co.kr",
+							"indices": [
+								32,
+								55
+							]
+						},
+						{
+							"url": "https://t.co/0Eibing6CM",
+							"expanded_url": "http://instagram.com/gaonchart_korea",
+							"display_url": "instagram.com/gaonchart_korea",
+							"indices": [
+								56,
+								79
+							]
+						},
+						{
+							"url": "https://t.co/fnADI1nzVz",
+							"expanded_url": "http://www.youtube.com/user/GaonChartKorea",
+							"display_url": "youtube.com/user/GaonChart…",
+							"indices": [
+								80,
+								103
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 22768,
+			"friends_count": 5,
+			"listed_count": 55,
+			"created_at": "Thu Sep 03 08:42:20 +0000 2015",
+			"favourites_count": 22,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 367,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/920171408931483650/6UvNNXDk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/920171408931483650/6UvNNXDk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3435012852/1623995945",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "여자아이돌 2022년 1학기 성적발표\n2022 K-pop girlgroup first half results\n#KPOP #Girlgroup #RedVelvet #IVE #LeSserafim #GIdle #Taeyeon #STAYC \nhttps://t.co/pSseVGOzmT"
+	},
+	{
 		"created_at": "Fri Jul 08 03:07:09 +0000 2022",
 		"id": 1545231948515352600,
 		"id_str": "1545243059641794560",
