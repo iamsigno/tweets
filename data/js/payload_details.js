10d0984b0d7eb1e66597835f9235ede886a85e71
SIGNO <iamsigno@users.noreply.github.com>
2022-07-14 12:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29329,
-	"created_at": "Thu Jul 14 01:07:49 +0000 2022",
+	"tweets": 29330,
+	"created_at": "Thu Jul 14 03:07:49 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,226 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Thu Jul 14 03:07:08 +0000 2022",
+		"id": 1547405520880935000,
+		"id_str": "1547417385325535232",
+		"full_text": "[#NEWS] '징크스의 연인' 서현 나인우, 안타까운 엇갈림\n▶ https://t.co/xmnbm8FfF1\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/49D53iJ9sk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						62,
+						65
+					]
+				},
+				{
+					"text": "징크스의연인",
+					"indices": [
+						66,
+						73
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						74,
+						82
+					]
+				},
+				{
+					"text": "JinxedatFirst",
+					"indices": [
+						83,
+						97
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/xmnbm8FfF1",
+					"expanded_url": "https://naver.me/5AqIJRN7",
+					"display_url": "naver.me/5AqIJRN7",
+					"indices": [
+						37,
+						60
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1547403361783148500,
+					"id_str": "1547403361783148545",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/FXl7KLdUcAEYPhK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXl7KLdUcAEYPhK.jpg",
+					"url": "https://t.co/49D53iJ9sk",
+					"display_url": "pic.twitter.com/49D53iJ9sk",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1547405520880934913/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 703,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 586,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 332,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1547403361783148500,
+					"id_str": "1547403361783148545",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/FXl7KLdUcAEYPhK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXl7KLdUcAEYPhK.jpg",
+					"url": "https://t.co/49D53iJ9sk",
+					"display_url": "pic.twitter.com/49D53iJ9sk",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1547405520880934913/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 703,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 586,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 332,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 61429,
+			"friends_count": 12,
+			"listed_count": 272,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 125,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 16014,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 53,
+		"favorite_count": 84,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] '징크스의 연인' 서현 나인우, 안타까운 엇갈림\n▶ https://t.co/xmnbm8FfF1\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/49D53iJ9sk"
+	},
+	{
 		"created_at": "Thu Jul 14 01:07:16 +0000 2022",
 		"id": 1547387146658451500,
 		"id_str": "1547387218108506112",
