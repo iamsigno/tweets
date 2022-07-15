e9d35d0a233b764ea950fa86fc3737fe7e0cd123
SIGNO <iamsigno@users.noreply.github.com>
2022-07-15 12:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29335,
-	"created_at": "Fri Jul 15 01:07:48 +0000 2022",
+	"tweets": 29336,
+	"created_at": "Fri Jul 15 03:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,226 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Fri Jul 15 03:07:09 +0000 2022",
+		"id": 1547774200164896800,
+		"id_str": "1547779775686578176",
+		"full_text": "[#NEWS] 서현, 운명 스스로 개척…나인우 사랑 깨달았다 (‘징크스의 연인’)[TV종합]\n▶ https://t.co/J8t3ki6EIp\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/47BX83OTmF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			114
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						79,
+						82
+					]
+				},
+				{
+					"text": "징크스의연인",
+					"indices": [
+						83,
+						90
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						91,
+						99
+					]
+				},
+				{
+					"text": "JinxedatFirst",
+					"indices": [
+						100,
+						114
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/J8t3ki6EIp",
+					"expanded_url": "https://naver.me/xueJW4Hs",
+					"display_url": "naver.me/xueJW4Hs",
+					"indices": [
+						54,
+						77
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1547769615761420300,
+					"id_str": "1547769615761420290",
+					"indices": [
+						115,
+						138
+					],
+					"media_url": "http://pbs.twimg.com/media/FXrIQ92aMAItZKL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXrIQ92aMAItZKL.jpg",
+					"url": "https://t.co/47BX83OTmF",
+					"display_url": "pic.twitter.com/47BX83OTmF",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1547774200164896768/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 600,
+							"h": 1092,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 374,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 600,
+							"h": 1092,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1547769615761420300,
+					"id_str": "1547769615761420290",
+					"indices": [
+						115,
+						138
+					],
+					"media_url": "http://pbs.twimg.com/media/FXrIQ92aMAItZKL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXrIQ92aMAItZKL.jpg",
+					"url": "https://t.co/47BX83OTmF",
+					"display_url": "pic.twitter.com/47BX83OTmF",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1547774200164896768/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 600,
+							"h": 1092,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 374,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 600,
+							"h": 1092,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 61520,
+			"friends_count": 12,
+			"listed_count": 271,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 125,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 16028,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 57,
+		"favorite_count": 79,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] 서현, 운명 스스로 개척…나인우 사랑 깨달았다 (‘징크스의 연인’)[TV종합]\n▶ https://t.co/J8t3ki6EIp\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/47BX83OTmF"
+	},
+	{
 		"created_at": "Fri Jul 15 01:07:17 +0000 2022",
 		"id": 1547744010676031500,
 		"id_str": "1547749609216155653",
