4cc01bd846e84104614707cdd99c9cf8e0a08151
SIGNO <iamsigno@users.noreply.github.com>
2022-07-21 15:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29351,
-	"created_at": "Thu Jul 21 04:07:49 +0000 2022",
+	"tweets": 29352,
+	"created_at": "Thu Jul 21 06:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,252 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Thu Jul 21 06:07:09 +0000 2022",
+		"id": 1549987031165575200,
+		"id_str": "1549999399744507905",
+		"full_text": "[#슬비로운_슬비생활🧸]\n\n서현 배우와 슬비는 사실\n필연적으로 만나게 된 운명🔮\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/dQ8kHbQxEC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			81
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "슬비로운_슬비생활",
+					"indices": [
+						1,
+						11
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						46,
+						49
+					]
+				},
+				{
+					"text": "징크스의연인",
+					"indices": [
+						50,
+						57
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						58,
+						66
+					]
+				},
+				{
+					"text": "JinxedatFirst",
+					"indices": [
+						67,
+						81
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1549984594560430000,
+					"id_str": "1549984594560430080",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/FYKmxs4aMAAq6gJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FYKmxs4aMAAq6gJ.jpg",
+					"url": "https://t.co/dQ8kHbQxEC",
+					"display_url": "pic.twitter.com/dQ8kHbQxEC",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1549987031165575168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1549984594560430000,
+					"id_str": "1549984594560430080",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/FYKmxs4aMAAq6gJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FYKmxs4aMAAq6gJ.jpg",
+					"url": "https://t.co/dQ8kHbQxEC",
+					"display_url": "pic.twitter.com/dQ8kHbQxEC",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1549987031165575168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1800,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1549984594703024000,
+					"id_str": "1549984594703024129",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/FYKmxtaaAAE-UCd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FYKmxtaaAAE-UCd.jpg",
+					"url": "https://t.co/dQ8kHbQxEC",
+					"display_url": "pic.twitter.com/dQ8kHbQxEC",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1549987031165575168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 61757,
+			"friends_count": 12,
+			"listed_count": 275,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 125,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 16061,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 257,
+		"favorite_count": 367,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#슬비로운_슬비생활🧸]\n\n서현 배우와 슬비는 사실\n필연적으로 만나게 된 운명🔮\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/dQ8kHbQxEC"
+	},
+	{
 		"created_at": "Thu Jul 21 04:07:11 +0000 2022",
 		"id": 1549960957157183500,
 		"id_str": "1549969209408487424",
