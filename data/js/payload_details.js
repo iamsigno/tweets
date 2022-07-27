1a8e96f3a483b28caa0bdad8b363e452e60e123d
SIGNO <iamsigno@users.noreply.github.com>
2022-07-27 12:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29377,
-	"created_at": "Wed Jul 27 02:07:49 +0000 2022",
+	"tweets": 29378,
+	"created_at": "Wed Jul 27 03:07:49 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,124 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Wed Jul 27 03:07:17 +0000 2022",
+		"id": 1552122394281922600,
+		"id_str": "1552128461673795585",
+		"full_text": "Girls’ Generation celebrate their 15th anniversary with ‘FOREVER 1’,  their 7th full-length album title track about promising lasting, eternal love produced by the hitmaker KENZIE!  \n\n#GirlsGeneration #소녀시대\n#FOREVER1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			216
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						184,
+						200
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						201,
+						206
+					]
+				},
+				{
+					"text": "FOREVER1",
+					"indices": [
+						207,
+						216
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1552122392683905000,
+		"in_reply_to_status_id_str": "1552122392683905024",
+		"in_reply_to_user_id": 391115625,
+		"in_reply_to_user_id_str": "391115625",
+		"in_reply_to_screen_name": "GirlsGeneration",
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBV3cSH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBV3cSH",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3817322,
+			"friends_count": 5,
+			"listed_count": 6062,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2897,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551220686538276865/mAgQFVeM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551220686538276865/mAgQFVeM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1658674834",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1485,
+		"favorite_count": 3238,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Girls’ Generation celebrate their 15th anniversary with ‘FOREVER 1’,  their 7th full-length album title track about promising lasting, eternal love produced by the hitmaker KENZIE!  \n\n#GirlsGeneration #소녀시대\n#FOREVER1"
+	},
+	{
 		"created_at": "Wed Jul 27 02:07:17 +0000 2022",
 		"id": 1552111530044313600,
 		"id_str": "1552113362070388738",
