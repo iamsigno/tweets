1dde224d239fd50332857a7be83c8a526fc518d3
SIGNO <iamsigno@users.noreply.github.com>
2022-07-28 16:07:55 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29389,
-	"created_at": "Thu Jul 28 06:07:49 +0000 2022",
+	"tweets": 29390,
+	"created_at": "Thu Jul 28 07:07:54 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,262 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Thu Jul 28 07:07:18 +0000 2022",
+		"id": 1552548408606396400,
+		"id_str": "1552551253229576192",
+		"full_text": "[#NEWS] 서현, 김난희 등장으로 '위기'…비밀 낱낱이 폭로돼 (징크스의 연인)\n▶ https://t.co/6vKFaQNooR\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/uRZkp9Nj39",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			109
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						74,
+						77
+					]
+				},
+				{
+					"text": "징크스의연인",
+					"indices": [
+						78,
+						85
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						86,
+						94
+					]
+				},
+				{
+					"text": "JinxedatFirst",
+					"indices": [
+						95,
+						109
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/6vKFaQNooR",
+					"expanded_url": "http://naver.me/5okmcz1a",
+					"display_url": "naver.me/5okmcz1a",
+					"indices": [
+						49,
+						72
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1552543735061561300,
+					"id_str": "1552543735061561344",
+					"indices": [
+						110,
+						133
+					],
+					"media_url": "http://pbs.twimg.com/media/FYu-TSKaQAAvYTJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FYu-TSKaQAAvYTJ.jpg",
+					"url": "https://t.co/uRZkp9Nj39",
+					"display_url": "pic.twitter.com/uRZkp9Nj39",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1552548408606396416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1552543735061561300,
+					"id_str": "1552543735061561344",
+					"indices": [
+						110,
+						133
+					],
+					"media_url": "http://pbs.twimg.com/media/FYu-TSKaQAAvYTJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FYu-TSKaQAAvYTJ.jpg",
+					"url": "https://t.co/uRZkp9Nj39",
+					"display_url": "pic.twitter.com/uRZkp9Nj39",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1552548408606396416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1552543735048974300,
+					"id_str": "1552543735048974336",
+					"indices": [
+						110,
+						133
+					],
+					"media_url": "http://pbs.twimg.com/media/FYu-TSHaMAAfj6g.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FYu-TSHaMAAfj6g.jpg",
+					"url": "https://t.co/uRZkp9Nj39",
+					"display_url": "pic.twitter.com/uRZkp9Nj39",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1552548408606396416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 62118,
+			"friends_count": 12,
+			"listed_count": 277,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 125,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 16107,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 100,
+		"favorite_count": 145,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] 서현, 김난희 등장으로 '위기'…비밀 낱낱이 폭로돼 (징크스의 연인)\n▶ https://t.co/6vKFaQNooR\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/uRZkp9Nj39"
+	},
+	{
 		"created_at": "Thu Jul 28 06:07:09 +0000 2022",
 		"id": 1552535579270664200,
 		"id_str": "1552536114287763457",
