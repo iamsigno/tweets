5025b74e15c50f4ea2de60cefbd86e738ea1144b
SIGNO <iamsigno@users.noreply.github.com>
2022-07-30 16:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29399,
-	"created_at": "Sat Jul 30 02:07:49 +0000 2022",
+	"tweets": 29400,
+	"created_at": "Sat Jul 30 07:07:49 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,224 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Sat Jul 30 07:07:09 +0000 2022",
+		"id": 1553272906217508900,
+		"id_str": "1553275990150991873",
+		"full_text": "LIM YOONA makes her first appearance in ‘The Big Mouth’ as a nurse and a newlywed! She naturally captures the emotional sides of her character, ramping up the excitement for how the storyline will unravel. \n\n#임윤아 #LIMYOONA \n#빅마우스 #TheBigMouth\n#소녀시대 #GirlsGeneration https://t.co/QVEJLoyhms",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			265
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "임윤아",
+					"indices": [
+						208,
+						212
+					]
+				},
+				{
+					"text": "LIMYOONA",
+					"indices": [
+						213,
+						222
+					]
+				},
+				{
+					"text": "빅마우스",
+					"indices": [
+						224,
+						229
+					]
+				},
+				{
+					"text": "TheBigMouth",
+					"indices": [
+						230,
+						242
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						243,
+						248
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						249,
+						265
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1553272900991389700,
+					"id_str": "1553272900991389696",
+					"indices": [
+						266,
+						289
+					],
+					"media_url": "http://pbs.twimg.com/media/FY5VeU7VEAAH_T3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FY5VeU7VEAAH_T3.jpg",
+					"url": "https://t.co/QVEJLoyhms",
+					"display_url": "pic.twitter.com/QVEJLoyhms",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1553272906217508865/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 908,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 301,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 532,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1553272900991389700,
+					"id_str": "1553272900991389696",
+					"indices": [
+						266,
+						289
+					],
+					"media_url": "http://pbs.twimg.com/media/FY5VeU7VEAAH_T3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FY5VeU7VEAAH_T3.jpg",
+					"url": "https://t.co/QVEJLoyhms",
+					"display_url": "pic.twitter.com/QVEJLoyhms",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1553272906217508865/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 908,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 301,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 532,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 884378714,
+			"id_str": "884378714",
+			"name": "SMTOWN",
+			"screen_name": "SMTOWNGLOBAL",
+			"location": "",
+			"description": "SMEntertainment Group Official Twitter",
+			"url": "https://t.co/zjb7YW3ApX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zjb7YW3ApX",
+							"expanded_url": "http://www.smtown.com",
+							"display_url": "smtown.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11054498,
+			"friends_count": 56,
+			"listed_count": 12482,
+			"created_at": "Tue Oct 16 12:49:45 +0000 2012",
+			"favourites_count": 2026,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 12567,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "F08BA0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1537993544555560963/XHzYSZjH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1537993544555560963/XHzYSZjH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/884378714/1655521211",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 410,
+		"favorite_count": 1147,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "LIM YOONA makes her first appearance in ‘The Big Mouth’ as a nurse and a newlywed! She naturally captures the emotional sides of her character, ramping up the excitement for how the storyline will unravel. \n\n#임윤아 #LIMYOONA \n#빅마우스 #TheBigMouth\n#소녀시대 #GirlsGeneration https://t.co/QVEJLoyhms"
+	},
+	{
 		"created_at": "Sat Jul 30 02:07:14 +0000 2022",
 		"id": 1553198695474204700,
 		"id_str": "1553200515508539392",
