6e93ff8376c32ab9d6c10647bd61b0434cdb8fca
SIGNO <iamsigno@users.noreply.github.com>
2022-08-05 13:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29451,
-	"created_at": "Fri Aug 05 03:07:48 +0000 2022",
+	"tweets": 29452,
+	"created_at": "Fri Aug 05 04:07:49 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,266 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Fri Aug 05 04:07:19 +0000 2022",
+		"id": 1555393360575492000,
+		"id_str": "1555405061131468800",
+		"full_text": "#공조2 와 #소녀시대 이즈 백💕~!\n#공조의히로인 '민영' #임윤아 소녀시대 컴백을 축하합니다!🎉\n\n윤아는 &lt;#공조2_인터내셔날&gt;의 FOREVER 1...✨\n\n#현빈 #유해진 #다니엘헤니 #진선규 #이석훈감독\n#9월대개봉 #공조이즈백 https://t.co/X6kadgguOA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			137
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "공조2",
+					"indices": [
+						0,
+						4
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						7,
+						12
+					]
+				},
+				{
+					"text": "공조의히로인",
+					"indices": [
+						21,
+						28
+					]
+				},
+				{
+					"text": "임윤아",
+					"indices": [
+						34,
+						38
+					]
+				},
+				{
+					"text": "공조2_인터내셔날",
+					"indices": [
+						65,
+						75
+					]
+				},
+				{
+					"text": "현빈",
+					"indices": [
+						96,
+						99
+					]
+				},
+				{
+					"text": "유해진",
+					"indices": [
+						100,
+						104
+					]
+				},
+				{
+					"text": "다니엘헤니",
+					"indices": [
+						105,
+						111
+					]
+				},
+				{
+					"text": "진선규",
+					"indices": [
+						112,
+						116
+					]
+				},
+				{
+					"text": "이석훈감독",
+					"indices": [
+						117,
+						123
+					]
+				},
+				{
+					"text": "9월대개봉",
+					"indices": [
+						124,
+						130
+					]
+				},
+				{
+					"text": "공조이즈백",
+					"indices": [
+						131,
+						137
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1555392797276254200,
+					"id_str": "1555392797276254209",
+					"indices": [
+						138,
+						161
+					],
+					"media_url": "http://pbs.twimg.com/media/FZXdgh_aMAEbj4c.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZXdgh_aMAEbj4c.jpg",
+					"url": "https://t.co/X6kadgguOA",
+					"display_url": "pic.twitter.com/X6kadgguOA",
+					"expanded_url": "https://twitter.com/CJENMMOVIE/status/1555393360575492096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1555392797276254200,
+					"id_str": "1555392797276254209",
+					"indices": [
+						138,
+						161
+					],
+					"media_url": "http://pbs.twimg.com/media/FZXdgh_aMAEbj4c.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZXdgh_aMAEbj4c.jpg",
+					"url": "https://t.co/X6kadgguOA",
+					"display_url": "pic.twitter.com/X6kadgguOA",
+					"expanded_url": "https://twitter.com/CJENMMOVIE/status/1555393360575492096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 452664114,
+			"id_str": "452664114",
+			"name": "CJ ENM Movie",
+			"screen_name": "CJENMMOVIE",
+			"location": "",
+			"description": "아시아 No.1 스튜디오 CJ ENM Movie 공식 트위터입니다.\n28년 동안 한 우물만 파고 있는 C네마을🏡",
+			"url": "https://t.co/ag1essF3t7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ag1essF3t7",
+							"expanded_url": "https://linktr.ee/cjenmmovie",
+							"display_url": "linktr.ee/cjenmmovie",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 331744,
+			"friends_count": 10668,
+			"listed_count": 258,
+			"created_at": "Mon Jan 02 03:32:49 +0000 2012",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 7277,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1412645930050805762/NLrBIhfv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1412645930050805762/NLrBIhfv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/452664114/1658279103",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 751,
+		"favorite_count": 1084,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#공조2 와 #소녀시대 이즈 백💕~!\n#공조의히로인 '민영' #임윤아 소녀시대 컴백을 축하합니다!🎉\n\n윤아는 &lt;#공조2_인터내셔날&gt;의 FOREVER 1...✨\n\n#현빈 #유해진 #다니엘헤니 #진선규 #이석훈감독\n#9월대개봉 #공조이즈백 https://t.co/X6kadgguOA"
+	},
+	{
 		"created_at": "Fri Aug 05 03:07:44 +0000 2022",
 		"id": 1555388128109637600,
 		"id_str": "1555390067954384897",
