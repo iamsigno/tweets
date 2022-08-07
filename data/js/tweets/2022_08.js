5e07d4aa498ed652205d8ebf8554ff09e9f0e040
SIGNO <iamsigno@users.noreply.github.com>
2022-08-07 17:07:50 +0900

    Added 1 tweets

--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Sun Aug 07 08:07:17 +0000 2022",
+		"id": 1556188628631847000,
+		"id_str": "1556190227588423682",
+		"full_text": "[GIRLS' GENERATION The 7th Album - FOREVER 1]💕\n\n소녀시대 정규 7집 FOREVER 1 앨범 구매 시 필름포토 증정 이벤트는 오늘까지!💕\n마지막 기회를 놓치지말고, 지금 바로 SMTOWN &amp;STORE에서 만나보세요!\n\nhttps://t.co/hUYwOPB8tX https://t.co/8X3eE7roGU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			169
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/hUYwOPB8tX",
+					"expanded_url": "https://bit.ly/3Qllk0q",
+					"display_url": "bit.ly/3Qllk0q",
+					"indices": [
+						146,
+						169
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1556188623686766600,
+					"id_str": "1556188623686766592",
+					"indices": [
+						170,
+						193
+					],
+					"media_url": "http://pbs.twimg.com/media/FZixTuZakAA9Mk-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZixTuZakAA9Mk-.jpg",
+					"url": "https://t.co/8X3eE7roGU",
+					"display_url": "pic.twitter.com/8X3eE7roGU",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1556188628631846912/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1556188623686766600,
+					"id_str": "1556188623686766592",
+					"indices": [
+						170,
+						193
+					],
+					"media_url": "http://pbs.twimg.com/media/FZixTuZakAA9Mk-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZixTuZakAA9Mk-.jpg",
+					"url": "https://t.co/8X3eE7roGU",
+					"display_url": "pic.twitter.com/8X3eE7roGU",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1556188628631846912/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1113723889245278200,
+			"id_str": "1113723889245278209",
+			"name": "SMTOWN &STORE",
+			"screen_name": "SMTOWNandSTORE",
+			"location": "",
+			"description": "",
+			"url": "https://t.co/Ee3t6o418Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Ee3t6o418Q",
+							"expanded_url": "http://www.SMTOWNandSTORE.com",
+							"display_url": "SMTOWNandSTORE.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 249046,
+			"friends_count": 0,
+			"listed_count": 405,
+			"created_at": "Thu Apr 04 08:44:01 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2875,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1113723889245278209/1554705745",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 117,
+		"favorite_count": 208,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[GIRLS' GENERATION The 7th Album - FOREVER 1]💕\n\n소녀시대 정규 7집 FOREVER 1 앨범 구매 시 필름포토 증정 이벤트는 오늘까지!💕\n마지막 기회를 놓치지말고, 지금 바로 SMTOWN &amp;STORE에서 만나보세요!\n\nhttps://t.co/hUYwOPB8tX https://t.co/8X3eE7roGU"
+	},
+	{
 		"created_at": "Sun Aug 07 06:44:57 +0000 2022",
 		"id": 1556167478408986600,
 		"id_str": "1556169507802054656",
