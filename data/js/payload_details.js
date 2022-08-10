12fda532c9b9981ffa0ce13d4a19c2e233a14bbd
SIGNO <iamsigno@users.noreply.github.com>
2022-08-10 23:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29527,
-	"created_at": "Wed Aug 10 12:07:50 +0000 2022",
+	"tweets": 29528,
+	"created_at": "Wed Aug 10 14:07:49 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Wed Aug 10 14:07:09 +0000 2022",
+		"id": 1557366678790344700,
+		"id_str": "1557367953804361730",
+		"full_text": "💗 Girls’ Generation จัดงานอีเวนต์พิเศษฉลองครบรอบเดบิวต์ 15 ปี ในวันที่ 3 กันยายนนี้ ที่ประเทศเกาหลีใต้!\n\nhttps://t.co/uYSjeeNH62\n\n#GirlsGeneration #소녀시대\n#FOREVER1\n#2022_GirlsGeneration_Special_Event_Long_Lasting_Love https://t.co/RgpE2dZkdA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			216
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						130,
+						146
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						147,
+						152
+					]
+				},
+				{
+					"text": "FOREVER1",
+					"indices": [
+						153,
+						162
+					]
+				},
+				{
+					"text": "2022_GirlsGeneration_Special_Event_Long_Lasting_Love",
+					"indices": [
+						163,
+						216
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/uYSjeeNH62",
+					"expanded_url": "https://www.facebook.com/smtruethailand/posts/pfbid02zStHyYWWFJ3LBN7NN7x8ajM5dt81MjKb6Y8aGpuhM5JjyEKuZrHDz8gvBUbXpFSxl",
+					"display_url": "facebook.com/smtruethailand…",
+					"indices": [
+						105,
+						128
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1557366598700130300,
+					"id_str": "1557366598700130306",
+					"indices": [
+						217,
+						240
+					],
+					"media_url": "http://pbs.twimg.com/media/FZzgq5SUYAIgnhi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZzgq5SUYAIgnhi.jpg",
+					"url": "https://t.co/RgpE2dZkdA",
+					"display_url": "pic.twitter.com/RgpE2dZkdA",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1557366678790344704/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 849,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1414,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 481,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1557366598700130300,
+					"id_str": "1557366598700130306",
+					"indices": [
+						217,
+						240
+					],
+					"media_url": "http://pbs.twimg.com/media/FZzgq5SUYAIgnhi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZzgq5SUYAIgnhi.jpg",
+					"url": "https://t.co/RgpE2dZkdA",
+					"display_url": "pic.twitter.com/RgpE2dZkdA",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1557366678790344704/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 849,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1414,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 481,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1093456514700828700,
+			"id_str": "1093456514700828672",
+			"name": "SM True",
+			"screen_name": "SMTrueThailand",
+			"location": "Bangkok, Thailand",
+			"description": "SM True Official Twitter Page",
+			"url": "https://t.co/fe1MwqbQdx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fe1MwqbQdx",
+							"expanded_url": "https://www.facebook.com/smtruethailand",
+							"display_url": "facebook.com/smtruethailand",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 97246,
+			"friends_count": 51,
+			"listed_count": 152,
+			"created_at": "Thu Feb 07 10:28:43 +0000 2019",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 11929,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1093456514700828672/1562743620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 54,
+		"favorite_count": 17,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "th",
+		"text": "💗 Girls’ Generation จัดงานอีเวนต์พิเศษฉลองครบรอบเดบิวต์ 15 ปี ในวันที่ 3 กันยายนนี้ ที่ประเทศเกาหลีใต้!\n\nhttps://t.co/uYSjeeNH62\n\n#GirlsGeneration #소녀시대\n#FOREVER1\n#2022_GirlsGeneration_Special_Event_Long_Lasting_Love https://t.co/RgpE2dZkdA"
+	},
+	{
 		"created_at": "Wed Aug 10 12:07:20 +0000 2022",
 		"id": 1557337406986064000,
 		"id_str": "1557337800504139776",
