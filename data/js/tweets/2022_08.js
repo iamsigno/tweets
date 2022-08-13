cdb15a77f346a5452cff0e28e3cc4a1ed41ab521
SIGNO <iamsigno@users.noreply.github.com>
2022-08-13 13:07:51 +0900

    Added 1 tweets

--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,135 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Sat Aug 13 03:44:57 +0000 2022",
+		"id": 1558298326511935500,
+		"id_str": "1558298536181207040",
+		"full_text": "💗소녀시대 x 아는 형님💗\n오늘 저녁 8시 50분! \nComing soon tonight at 8:50 PM KST!\n\n[선공개] ＂머리 치지 마..🔥＂ 수영의 쪼 때문에 심기 불편 그 잡채인 효연 ㅋㅋ | 아는 형님 345회\nhttps://t.co/su4dTXqLJL\n\n#GirlsGeneration #소녀시대\n#아는형님",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			180
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						152,
+						168
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						169,
+						174
+					]
+				},
+				{
+					"text": "아는형님",
+					"indices": [
+						175,
+						180
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/su4dTXqLJL",
+					"expanded_url": "https://youtu.be/4Akw44-IUIU",
+					"display_url": "youtu.be/4Akw44-IUIU",
+					"indices": [
+						127,
+						150
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBV3cSH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBV3cSH",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3871915,
+			"friends_count": 5,
+			"listed_count": 6125,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 3028,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551220686538276865/mAgQFVeM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551220686538276865/mAgQFVeM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1658674834",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1341,
+		"favorite_count": 2403,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "💗소녀시대 x 아는 형님💗\n오늘 저녁 8시 50분! \nComing soon tonight at 8:50 PM KST!\n\n[선공개] ＂머리 치지 마..🔥＂ 수영의 쪼 때문에 심기 불편 그 잡채인 효연 ㅋㅋ | 아는 형님 345회\nhttps://t.co/su4dTXqLJL\n\n#GirlsGeneration #소녀시대\n#아는형님"
+	},
+	{
 		"created_at": "Fri Aug 12 16:07:17 +0000 2022",
 		"id": 1558109526645424000,
 		"id_str": "1558122962112356360",
