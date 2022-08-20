04bae548df138465edd09f8aa0549f353948ed83
SIGNO <iamsigno@users.noreply.github.com>
2022-08-20 15:07:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29587,
-	"created_at": "Sat Aug 20 04:07:49 +0000 2022",
+	"tweets": 29588,
+	"created_at": "Sat Aug 20 06:07:50 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,235 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Sat Aug 20 06:07:09 +0000 2022",
+		"id": 1560869241930268700,
+		"id_str": "1560871036505186304",
+		"full_text": "#소녀시대 특집 2편💘\n핫걸들의 성지 아는 롤러장✨\n절대 퀸카 '소녀시대' VS 얼짱 집단 '써니'\n양보 없는 절대 미모 대결!\n\n▶️오늘 저녁 𝟴시 𝟱𝟬분\n&lt;#아는형님&gt; 본방사수 👀 https://t.co/4Ht3Ezu0Ap",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			108
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "소녀시대",
+					"indices": [
+						0,
+						5
+					]
+				},
+				{
+					"text": "아는형님",
+					"indices": [
+						92,
+						97
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1560509578860961800,
+					"id_str": "1560509578860961792",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/FagOQXuaUAENpE4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FagOQXuaUAENpE4.jpg",
+					"url": "https://t.co/4Ht3Ezu0Ap",
+					"display_url": "pic.twitter.com/4Ht3Ezu0Ap",
+					"expanded_url": "https://twitter.com/jtbclove/status/1560869241930268672/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1560509578860961800,
+					"id_str": "1560509578860961792",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/FagOQXuaUAENpE4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FagOQXuaUAENpE4.jpg",
+					"url": "https://t.co/4Ht3Ezu0Ap",
+					"display_url": "pic.twitter.com/4Ht3Ezu0Ap",
+					"expanded_url": "https://twitter.com/jtbclove/status/1560869241930268672/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 119052,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1560509578860961792/vid/640x360/ZYj_-LyzG3vDJVKN.mp4?tag=14"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1560509578860961792/pl/j6IFyXL8p7-chF_2.m3u8?tag=14&container=fmp4"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1560509578860961792/vid/1280x720/UYgAH7I-cJpvoUbH.mp4?tag=14"
+							},
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1560509578860961792/vid/480x270/utseZLC-gBRmGEl7.mp4?tag=14"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "[아는 형님] 346회 예고",
+						"description": "",
+						"call_to_actions": {
+							"visit_site": {
+								"url": "https://youtu.be/fR8bOaWoMzU"
+							}
+						},
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 418488729,
+			"id_str": "418488729",
+			"name": "@jtbclove",
+			"screen_name": "jtbclove",
+			"location": "Korea",
+			"description": "JTBC 공식 트위터",
+			"url": "https://t.co/b1UaW0h6ia",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/b1UaW0h6ia",
+							"expanded_url": "http://www.jtbc.co.kr",
+							"display_url": "jtbc.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 472580,
+			"friends_count": 108,
+			"listed_count": 824,
+			"created_at": "Tue Nov 22 07:18:13 +0000 2011",
+			"favourites_count": 585,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 25174,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1476711305403060226/mIeKdvX8_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1476711305403060226/mIeKdvX8_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/418488729/1659577702",
+			"profile_link_color": "2785BB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13,
+		"favorite_count": 17,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#소녀시대 특집 2편💘\n핫걸들의 성지 아는 롤러장✨\n절대 퀸카 '소녀시대' VS 얼짱 집단 '써니'\n양보 없는 절대 미모 대결!\n\n▶️오늘 저녁 𝟴시 𝟱𝟬분\n&lt;#아는형님&gt; 본방사수 👀 https://t.co/4Ht3Ezu0Ap"
+	},
+	{
 		"created_at": "Sat Aug 20 04:07:13 +0000 2022",
 		"id": 1560826409899110400,
 		"id_str": "1560840854251732992",
