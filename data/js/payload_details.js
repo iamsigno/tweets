3117b11cd7e9934a3602e26ab5e3c63452e18934
SIGNO <iamsigno@users.noreply.github.com>
2022-08-22 00:07:53 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29601,
-	"created_at": "Sun Aug 21 10:07:49 +0000 2022",
+	"tweets": 29602,
+	"created_at": "Sun Aug 21 15:07:52 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Sun Aug 21 14:45:06 +0000 2022",
+		"id": 1561363256681189400,
+		"id_str": "1561363771024711682",
+		"full_text": "💜💖🫶🏻⚓✨️\n\n#GirlsGeneration #소녀시대\n#FOREVER1 \n#인기가요 #Inkigayo https://t.co/r01Sk7asLv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			58
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						9,
+						25
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						26,
+						31
+					]
+				},
+				{
+					"text": "FOREVER1",
+					"indices": [
+						32,
+						41
+					]
+				},
+				{
+					"text": "인기가요",
+					"indices": [
+						43,
+						48
+					]
+				},
+				{
+					"text": "Inkigayo",
+					"indices": [
+						49,
+						58
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1561363242001113000,
+					"id_str": "1561363242001113088",
+					"indices": [
+						59,
+						82
+					],
+					"media_url": "http://pbs.twimg.com/media/FasTmJzUIAAs2zh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FasTmJzUIAAs2zh.jpg",
+					"url": "https://t.co/r01Sk7asLv",
+					"display_url": "pic.twitter.com/r01Sk7asLv",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1561363256681189377/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1561363242001113000,
+					"id_str": "1561363242001113088",
+					"indices": [
+						59,
+						82
+					],
+					"media_url": "http://pbs.twimg.com/media/FasTmJzUIAAs2zh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FasTmJzUIAAs2zh.jpg",
+					"url": "https://t.co/r01Sk7asLv",
+					"display_url": "pic.twitter.com/r01Sk7asLv",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1561363256681189377/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBV3cSH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBV3cSH",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3884082,
+			"friends_count": 5,
+			"listed_count": 6163,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 3060,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551220686538276865/mAgQFVeM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551220686538276865/mAgQFVeM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1658674834",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8013,
+		"favorite_count": 14596,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "💜💖🫶🏻⚓✨️\n\n#GirlsGeneration #소녀시대\n#FOREVER1 \n#인기가요 #Inkigayo https://t.co/r01Sk7asLv"
+	},
+	{
 		"created_at": "Sun Aug 21 10:07:09 +0000 2022",
 		"id": 1561285739349889000,
 		"id_str": "1561293822348644354",
