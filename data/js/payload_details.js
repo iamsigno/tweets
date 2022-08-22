1f4cbab2b3b2ca15882c6cb4e999bfee22ead414
SIGNO <iamsigno@users.noreply.github.com>
2022-08-22 10:07:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29602,
-	"created_at": "Sun Aug 21 15:07:52 +0000 2022",
+	"tweets": 29603,
+	"created_at": "Mon Aug 22 01:07:50 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,242 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Mon Aug 22 01:07:09 +0000 2022",
+		"id": 1561518519467929600,
+		"id_str": "1561520315322322944",
+		"full_text": "냅다 '불티' 외친 #효연 에 \n애써 태연(?)🙄\n#태연 의 〈사계〉♬\n\n▶️매주 토요일 저녁 𝟴시 𝟰𝟬분\n&lt;#아는형님&gt; 놓치지 마세요❣️ https://t.co/b1XUugPydl",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			83
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "효연",
+					"indices": [
+						11,
+						14
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						28,
+						31
+					]
+				},
+				{
+					"text": "아는형님",
+					"indices": [
+						64,
+						69
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1561515789613944800,
+					"id_str": "1561515789613944833",
+					"indices": [
+						84,
+						107
+					],
+					"media_url": "http://pbs.twimg.com/media/FauejTcaIAEXy7d.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FauejTcaIAEXy7d.jpg",
+					"url": "https://t.co/b1XUugPydl",
+					"display_url": "pic.twitter.com/b1XUugPydl",
+					"expanded_url": "https://twitter.com/jtbclove/status/1561518519467929600/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1561515789613944800,
+					"id_str": "1561515789613944833",
+					"indices": [
+						84,
+						107
+					],
+					"media_url": "http://pbs.twimg.com/media/FauejTcaIAEXy7d.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FauejTcaIAEXy7d.jpg",
+					"url": "https://t.co/b1XUugPydl",
+					"display_url": "pic.twitter.com/b1XUugPydl",
+					"expanded_url": "https://twitter.com/jtbclove/status/1561518519467929600/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 70036,
+						"variants": [
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1561515789613944833/vid/480x270/6YUNEdeaEfTbx28Q.mp4?tag=14"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1561515789613944833/vid/640x360/ZKkYolhjVHzWVBtH.mp4?tag=14"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1561515789613944833/vid/1280x720/XmhJzFdKD0eO6aTO.mp4?tag=14"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1561515789613944833/pl/d-4S-GOu1STwCkUS.m3u8?tag=14&container=fmp4"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "[아는 형님] 냅다 '불티' 외친 효연에 애써 태연(?) 태연의 〈사계〉♬ | JTBC 220820 방송",
+						"description": "",
+						"call_to_actions": {
+							"visit_site": {
+								"url": "https://tv.naver.com/v/28688895"
+							}
+						},
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 418488729,
+			"id_str": "418488729",
+			"name": "@jtbclove",
+			"screen_name": "jtbclove",
+			"location": "Korea",
+			"description": "JTBC 공식 트위터",
+			"url": "https://t.co/b1UaW0h6ia",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/b1UaW0h6ia",
+							"expanded_url": "http://www.jtbc.co.kr",
+							"display_url": "jtbc.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 472559,
+			"friends_count": 108,
+			"listed_count": 826,
+			"created_at": "Tue Nov 22 07:18:13 +0000 2011",
+			"favourites_count": 585,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 25180,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1476711305403060226/mIeKdvX8_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1476711305403060226/mIeKdvX8_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/418488729/1659577702",
+			"profile_link_color": "2785BB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 14,
+		"favorite_count": 15,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "냅다 '불티' 외친 #효연 에 \n애써 태연(?)🙄\n#태연 의 〈사계〉♬\n\n▶️매주 토요일 저녁 𝟴시 𝟰𝟬분\n&lt;#아는형님&gt; 놓치지 마세요❣️ https://t.co/b1XUugPydl"
+	},
+	{
 		"created_at": "Sun Aug 21 14:45:06 +0000 2022",
 		"id": 1561363256681189400,
 		"id_str": "1561363771024711682",
