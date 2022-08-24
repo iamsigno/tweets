347848c389b0d797ffab0aecc46a6b4d68bc1ecd
SIGNO <iamsigno@users.noreply.github.com>
2022-08-24 18:07:52 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29618,
-	"created_at": "Wed Aug 24 08:07:51 +0000 2022",
+	"tweets": 29619,
+	"created_at": "Wed Aug 24 09:07:51 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,224 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Wed Aug 24 09:07:12 +0000 2022",
+		"id": 1562364338916036600,
+		"id_str": "1562365901080764419",
+		"full_text": "지금은, 앞으로도, 영원히 하나로 반짝일!\n레전드 걸그룹 소녀시대 컴백💕\n\n#소녀시대 #GIRLSGENERATION\n#FOREVER1 https://t.co/OgVwkPHHT8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			74
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "소녀시대",
+					"indices": [
+						42,
+						47
+					]
+				},
+				{
+					"text": "GIRLSGENERATION",
+					"indices": [
+						48,
+						64
+					]
+				},
+				{
+					"text": "FOREVER1",
+					"indices": [
+						65,
+						74
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1562228213371531300,
+					"id_str": "1562228213371531264",
+					"indices": [
+						75,
+						98
+					],
+					"media_url": "http://pbs.twimg.com/media/Fa4tJv4aMAICwBQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fa4tJv4aMAICwBQ.jpg",
+					"url": "https://t.co/OgVwkPHHT8",
+					"display_url": "pic.twitter.com/OgVwkPHHT8",
+					"expanded_url": "https://twitter.com/KBSMusicBank/status/1562364338916036609/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 360,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 360,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 360,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1562228213371531300,
+					"id_str": "1562228213371531264",
+					"indices": [
+						75,
+						98
+					],
+					"media_url": "http://pbs.twimg.com/media/Fa4tJv4aMAICwBQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fa4tJv4aMAICwBQ.jpg",
+					"url": "https://t.co/OgVwkPHHT8",
+					"display_url": "pic.twitter.com/OgVwkPHHT8",
+					"expanded_url": "https://twitter.com/KBSMusicBank/status/1562364338916036609/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 360,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 360,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 360,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 223823,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1562228213371531264/vid/1280x720/KYKzLLD_ctS0UW9Y.mp4?tag=14"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1562228213371531264/pl/UhQR21nHVV-d0fdv.m3u8?tag=14&container=fmp4"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1562228213371531264/vid/640x360/xSmMK61t3mY57PrT.mp4?tag=14"
+							},
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1562228213371531264/vid/480x270/hdXCzCI_AmlSPlp9.mp4?tag=14"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 52661072,
+			"id_str": "52661072",
+			"name": "뮤직뱅크 (Music Bank)🏦",
+			"screen_name": "KBSMusicBank",
+			"location": "대한민국 서울 (Seoul, South Korea)",
+			"description": "Friday 17:10 KBS 2TV 가장 신뢰할 수 있는 음악 차트 K-Chart, 현재 KBS World를 통해 142개국 방송 中",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2099205,
+			"friends_count": 19,
+			"listed_count": 5171,
+			"created_at": "Wed Jul 01 07:17:45 +0000 2009",
+			"favourites_count": 157,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2133,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "010C0D",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1460149723671990272/xqA9TWcb_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1460149723671990272/xqA9TWcb_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/52661072/1558753651",
+			"profile_link_color": "96268B",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FADD4B",
+			"profile_text_color": "030003",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 58,
+		"favorite_count": 222,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "지금은, 앞으로도, 영원히 하나로 반짝일!\n레전드 걸그룹 소녀시대 컴백💕\n\n#소녀시대 #GIRLSGENERATION\n#FOREVER1 https://t.co/OgVwkPHHT8"
+	},
+	{
 		"created_at": "Wed Aug 24 08:07:14 +0000 2022",
 		"id": 1562347771973079000,
 		"id_str": "1562350807982698497",
