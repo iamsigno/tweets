455fd5603a3157cfa28963504583c0e9d208b01f
SIGNO <iamsigno@users.noreply.github.com>
2022-08-28 14:07:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29636,
-	"created_at": "Sun Aug 28 03:07:51 +0000 2022",
+	"tweets": 29637,
+	"created_at": "Sun Aug 28 05:07:50 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,240 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Sun Aug 28 05:07:09 +0000 2022",
+		"id": 1563753243984797700,
+		"id_str": "1563755039797256194",
+		"full_text": "디스와 거친말이 난무하는\n마 !! 이게 바로 15년차 우정이다 !!!\n#소녀시대 거친말 모음💗\n\n[JTBC 봐야지] 구독하기\n☞ https://t.co/CUkJ4lDNZr\n\n📌 영상 속 프로그램은?\n【#소시탐탐】 https://t.co/rR3DLv4Lg4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			118
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "소녀시대",
+					"indices": [
+						39,
+						44
+					]
+				},
+				{
+					"text": "소시탐탐",
+					"indices": [
+						112,
+						117
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/CUkJ4lDNZr",
+					"expanded_url": "http://asq.kr/VgFZ3",
+					"display_url": "asq.kr/VgFZ3",
+					"indices": [
+						72,
+						95
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1562971449916240000,
+					"id_str": "1562971449916239877",
+					"indices": [
+						119,
+						142
+					],
+					"media_url": "http://pbs.twimg.com/media/FbDKzIMUsAMwqoj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FbDKzIMUsAMwqoj.jpg",
+					"url": "https://t.co/rR3DLv4Lg4",
+					"display_url": "pic.twitter.com/rR3DLv4Lg4",
+					"expanded_url": "https://twitter.com/jtbclove/status/1563753243984797696/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1562971449916240000,
+					"id_str": "1562971449916239877",
+					"indices": [
+						119,
+						142
+					],
+					"media_url": "http://pbs.twimg.com/media/FbDKzIMUsAMwqoj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FbDKzIMUsAMwqoj.jpg",
+					"url": "https://t.co/rR3DLv4Lg4",
+					"display_url": "pic.twitter.com/rR3DLv4Lg4",
+					"expanded_url": "https://twitter.com/jtbclove/status/1563753243984797696/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 480112,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1562971449916239877/vid/640x360/5w2MJxVecXpNkiRs.mp4?tag=14"
+							},
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1562971449916239877/vid/480x270/IfK_Tz0NLWa2clyE.mp4?tag=14"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1562971449916239877/vid/1280x720/NoaZSRO3CuEVKSBH.mp4?tag=14"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1562971449916239877/pl/ZqhoJEm_Ytr-BhOM.m3u8?tag=14&container=fmp4"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "[봐야지] \"저 Shake it 때문에!\" 15년 우정 짬, 거침없는 그녀들 소녀시대 거친말 모음",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 418488729,
+			"id_str": "418488729",
+			"name": "@jtbclove",
+			"screen_name": "jtbclove",
+			"location": "Korea",
+			"description": "JTBC 공식 트위터",
+			"url": "https://t.co/b1UaW0h6ia",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/b1UaW0h6ia",
+							"expanded_url": "http://www.jtbc.co.kr",
+							"display_url": "jtbc.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 472553,
+			"friends_count": 108,
+			"listed_count": 825,
+			"created_at": "Tue Nov 22 07:18:13 +0000 2011",
+			"favourites_count": 585,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 25218,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1476711305403060226/mIeKdvX8_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1476711305403060226/mIeKdvX8_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/418488729/1659577702",
+			"profile_link_color": "2785BB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "디스와 거친말이 난무하는\n마 !! 이게 바로 15년차 우정이다 !!!\n#소녀시대 거친말 모음💗\n\n[JTBC 봐야지] 구독하기\n☞ https://t.co/CUkJ4lDNZr\n\n📌 영상 속 프로그램은?\n【#소시탐탐】 https://t.co/rR3DLv4Lg4"
+	},
+	{
 		"created_at": "Sun Aug 28 03:07:09 +0000 2022",
 		"id": 1563721507423268900,
 		"id_str": "1563724840854511618",
