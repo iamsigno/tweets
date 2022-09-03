a1ef369e3a41e6db67b9cf9341a58ea9d9558669
SIGNO <iamsigno@users.noreply.github.com>
2022-09-03 19:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29677,
-	"created_at": "Sat Sep 03 09:07:52 +0000 2022",
+	"tweets": 29678,
+	"created_at": "Sat Sep 03 10:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Sat Sep 03 09:42:44 +0000 2022",
+		"id": 1565998719756955600,
+		"id_str": "1565998719756955648",
+		"full_text": "지금은 소녀시대! \n앞으로도 소녀시대! \n영원히 소녀시대! https://t.co/PWdrWeMGsH",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			32
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1565998708797292500,
+					"id_str": "1565998708797292544",
+					"indices": [
+						33,
+						56
+					],
+					"media_url": "http://pbs.twimg.com/media/FbuLh1SVQAA7Ncb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FbuLh1SVQAA7Ncb.jpg",
+					"url": "https://t.co/PWdrWeMGsH",
+					"display_url": "pic.twitter.com/PWdrWeMGsH",
+					"expanded_url": "https://twitter.com/IAMSIGNO/status/1565998719756955648/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1565998708797292500,
+					"id_str": "1565998708797292544",
+					"indices": [
+						33,
+						56
+					],
+					"media_url": "http://pbs.twimg.com/media/FbuLh1SVQAA7Ncb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FbuLh1SVQAA7Ncb.jpg",
+					"url": "https://t.co/PWdrWeMGsH",
+					"display_url": "pic.twitter.com/PWdrWeMGsH",
+					"expanded_url": "https://twitter.com/IAMSIGNO/status/1565998719756955648/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 270405347,
+			"id_str": "270405347",
+			"name": "SIGNO",
+			"screen_name": "IAMSIGNO",
+			"location": "",
+			"description": "소녀시대(Girls' Generation) S❤NE\n\n#소녀시대 #GirlsGeneration\n@GirlsGeneration @gg_official_jp",
+			"url": "https://t.co/6TifdAFXqL",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6TifdAFXqL",
+							"expanded_url": "https://iamsigno.wordpress.com/",
+							"display_url": "iamsigno.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 487,
+			"friends_count": 445,
+			"listed_count": 4,
+			"created_at": "Tue Mar 22 15:18:36 +0000 2011",
+			"favourites_count": 1914,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 30074,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1201460639044005888/RTQPfRgI_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1201460639044005888/RTQPfRgI_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/270405347/1578714265",
+			"profile_link_color": "777777",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDE9D1",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "지금은 소녀시대! \n앞으로도 소녀시대! \n영원히 소녀시대! https://t.co/PWdrWeMGsH"
+	},
+	{
 		"created_at": "Sat Sep 03 09:06:47 +0000 2022",
 		"id": 1565941625892716500,
 		"id_str": "1565989675160190976",
@@ -377,7 +552,7 @@ Grailbird.data.tweets_2022_09 =
 		"created_at": "Sat Sep 03 08:53:45 +0000 2022",
 		"id": 1565963114826592300,
 		"id_str": "1565986393457061888",
-		"full_text": "PINK COSMIC \n#LongLastingLove #GirlsGeneration​ #GG4EVA #소녀시대 https://t.co/zLd8YiKxau",
+		"full_text": "PINK COSMIC \n#LongLastingLove #GirlsGeneration #GG4EVA #소녀시대 https://t.co/zLd8YiKxau",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -670,7 +845,7 @@ Grailbird.data.tweets_2022_09 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "PINK COSMIC \n#LongLastingLove #GirlsGeneration​ #GG4EVA #소녀시대 https://t.co/zLd8YiKxau"
+		"text": "PINK COSMIC \n#LongLastingLove #GirlsGeneration #GG4EVA #소녀시대 https://t.co/zLd8YiKxau"
 	},
 	{
 		"created_at": "Sat Sep 03 08:53:44 +0000 2022",
