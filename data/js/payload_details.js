3ef25574b4a5782c40706d955fdf6006cf230249
SIGNO <iamsigno@users.noreply.github.com>
2022-09-04 16:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29689,
-	"created_at": "Sun Sep 04 06:07:49 +0000 2022",
+	"tweets": 29690,
+	"created_at": "Sun Sep 04 07:07:49 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,253 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Sun Sep 04 06:44:57 +0000 2022",
+		"id": 1566305646999654400,
+		"id_str": "1566316366587633664",
+		"full_text": "소원 너무 반가웠고 즐거웠어요 😘\n우리 꼭 영원하자💜\n\n#소녀시대 #GirlsGeneration \n#태연 #TAEYEON \n#Long_Lasting_Love https://t.co/FcBB86yRaq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			87
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "소녀시대",
+					"indices": [
+						31,
+						36
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						37,
+						53
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						55,
+						58
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						59,
+						67
+					]
+				},
+				{
+					"text": "Long_Lasting_Love",
+					"indices": [
+						69,
+						87
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1566305638040211500,
+					"id_str": "1566305638040211456",
+					"indices": [
+						88,
+						111
+					],
+					"media_url": "http://pbs.twimg.com/media/Fbyird0UEAABCZv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fbyird0UEAABCZv.jpg",
+					"url": "https://t.co/FcBB86yRaq",
+					"display_url": "pic.twitter.com/FcBB86yRaq",
+					"expanded_url": "https://twitter.com/TAEYEONsmtown/status/1566305646999654401/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1566305638040211500,
+					"id_str": "1566305638040211456",
+					"indices": [
+						88,
+						111
+					],
+					"media_url": "http://pbs.twimg.com/media/Fbyird0UEAABCZv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fbyird0UEAABCZv.jpg",
+					"url": "https://t.co/FcBB86yRaq",
+					"display_url": "pic.twitter.com/FcBB86yRaq",
+					"expanded_url": "https://twitter.com/TAEYEONsmtown/status/1566305646999654401/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1800,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1566305642867880000,
+					"id_str": "1566305642867879936",
+					"indices": [
+						88,
+						111
+					],
+					"media_url": "http://pbs.twimg.com/media/FbyirvzUcAAP3nk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FbyirvzUcAAP3nk.jpg",
+					"url": "https://t.co/FcBB86yRaq",
+					"display_url": "pic.twitter.com/FcBB86yRaq",
+					"expanded_url": "https://twitter.com/TAEYEONsmtown/status/1566305646999654401/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1496499777965822000,
+			"id_str": "1496499777965821956",
+			"name": "TAEYEON Official",
+			"screen_name": "TAEYEONsmtown",
+			"location": "",
+			"description": "TAEYEON The 3rd Album 'INVU'",
+			"url": "https://t.co/c1nmkKMaGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/c1nmkKMaGi",
+							"expanded_url": "https://youtu.be/AbZH7XWDW_k",
+							"display_url": "youtu.be/AbZH7XWDW_k",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 141171,
+			"friends_count": 2,
+			"listed_count": 459,
+			"created_at": "Wed Feb 23 14:59:02 +0000 2022",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 64,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1496687188041678849/ngLIpota_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1496687188041678849/ngLIpota_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1496499777965821956/1645673016",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4693,
+		"favorite_count": 9474,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "소원 너무 반가웠고 즐거웠어요 😘\n우리 꼭 영원하자💜\n\n#소녀시대 #GirlsGeneration \n#태연 #TAEYEON \n#Long_Lasting_Love https://t.co/FcBB86yRaq"
+	},
+	{
 		"created_at": "Sun Sep 04 05:44:57 +0000 2022",
 		"id": 1566293187224031200,
 		"id_str": "1566301268003504128",
