a855bd9b1bdf76d30ef9c3954a6c43d2fd8f829e
SIGNO <iamsigno@users.noreply.github.com>
2022-09-05 15:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29691,
-	"created_at": "Mon Sep 05 04:07:49 +0000 2022",
+	"tweets": 29692,
+	"created_at": "Mon Sep 05 06:07:49 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Mon Sep 05 06:07:11 +0000 2022",
+		"id": 1566667839595233300,
+		"id_str": "1566669251456471041",
+		"full_text": "[Beyond LIVE – 2022 Girls’ Generation Special Event – Long Lasting Love] 💕\n\n소녀시대 Beyond LIVE의 아쉬움을 뒤로 할 2차 단일상품이 공개되었습니다!\n오늘 판매 종료되는 1차 단일상품까지 SMTOWN &amp;STORE 온라인에서 만나보세요!🙌🏻🙌🏻\nhttps://t.co/YCl3AgOwLF https://t.co/PS7o3VqyLP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			201
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/YCl3AgOwLF",
+					"expanded_url": "https://bit.ly/3enHmSL",
+					"display_url": "bit.ly/3enHmSL",
+					"indices": [
+						178,
+						201
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1566667834994065400,
+					"id_str": "1566667834994065409",
+					"indices": [
+						202,
+						225
+					],
+					"media_url": "http://pbs.twimg.com/media/Fb3sGGnaAAEW0gR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fb3sGGnaAAEW0gR.jpg",
+					"url": "https://t.co/PS7o3VqyLP",
+					"display_url": "pic.twitter.com/PS7o3VqyLP",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1566667839595233280/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1050,
+							"h": 1050,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1050,
+							"h": 1050,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1566667834994065400,
+					"id_str": "1566667834994065409",
+					"indices": [
+						202,
+						225
+					],
+					"media_url": "http://pbs.twimg.com/media/Fb3sGGnaAAEW0gR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fb3sGGnaAAEW0gR.jpg",
+					"url": "https://t.co/PS7o3VqyLP",
+					"display_url": "pic.twitter.com/PS7o3VqyLP",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1566667839595233280/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1050,
+							"h": 1050,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1050,
+							"h": 1050,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1113723889245278200,
+			"id_str": "1113723889245278209",
+			"name": "SMTOWN &STORE",
+			"screen_name": "SMTOWNandSTORE",
+			"location": "",
+			"description": "",
+			"url": "https://t.co/Ee3t6o418Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Ee3t6o418Q",
+							"expanded_url": "http://www.SMTOWNandSTORE.com",
+							"display_url": "SMTOWNandSTORE.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 251677,
+			"friends_count": 0,
+			"listed_count": 414,
+			"created_at": "Thu Apr 04 08:44:01 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2953,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1113723889245278209/1554705745",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 17,
+		"favorite_count": 57,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[Beyond LIVE – 2022 Girls’ Generation Special Event – Long Lasting Love] 💕\n\n소녀시대 Beyond LIVE의 아쉬움을 뒤로 할 2차 단일상품이 공개되었습니다!\n오늘 판매 종료되는 1차 단일상품까지 SMTOWN &amp;STORE 온라인에서 만나보세요!🙌🏻🙌🏻\nhttps://t.co/YCl3AgOwLF https://t.co/PS7o3VqyLP"
+	},
+	{
 		"created_at": "Mon Sep 05 04:07:15 +0000 2022",
 		"id": 1566626170409734100,
 		"id_str": "1566639068007616515",
