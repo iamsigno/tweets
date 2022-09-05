a1c791d58cdde8c5d9927e4b5cb70a6b6182f46f
SIGNO <iamsigno@users.noreply.github.com>
2022-09-05 16:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29692,
-	"created_at": "Mon Sep 05 06:07:49 +0000 2022",
+	"tweets": 29693,
+	"created_at": "Mon Sep 05 07:07:49 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,289 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Mon Sep 05 07:07:09 +0000 2022",
+		"id": 1566679438472802300,
+		"id_str": "1566684341085806593",
+		"full_text": "YOONA Tunjukkan Kesuksesannya dengan Bintangi Banyak Pemotretan Majalah di Tahun Ini\n\nCek info lengkapnya di Official Facebook Page SMTOWN Indonesia\n👉🏻https://t.co/7lhf4jiesk\n\n@GirlsGeneration \n#LIMYOONA #GirlsGeneration https://t.co/3wS4Fllk4d",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			220
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LIMYOONA",
+					"indices": [
+						194,
+						203
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						204,
+						220
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GirlsGeneration",
+					"name": "Girls' Generation",
+					"id": 391115625,
+					"id_str": "391115625",
+					"indices": [
+						176,
+						192
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/7lhf4jiesk",
+					"expanded_url": "https://www.facebook.com/smtown.idn/posts/pfbid0rXzwhUcsRAdyCdw2hbVBEQ55YK1Nt9Pb3Wk7AxkmxCDQSoXvUKFLz85vTPcbCLuml",
+					"display_url": "facebook.com/smtown.idn/pos…",
+					"indices": [
+						151,
+						174
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1566677673178304500,
+					"id_str": "1566677673178304512",
+					"indices": [
+						221,
+						244
+					],
+					"media_url": "http://pbs.twimg.com/media/Fb31CwtUEAARgRW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fb31CwtUEAARgRW.jpg",
+					"url": "https://t.co/3wS4Fllk4d",
+					"display_url": "pic.twitter.com/3wS4Fllk4d",
+					"expanded_url": "https://twitter.com/SMTOWN_Idn/status/1566679438472802304/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1229,
+							"h": 1800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 464,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 819,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1566677673178304500,
+					"id_str": "1566677673178304512",
+					"indices": [
+						221,
+						244
+					],
+					"media_url": "http://pbs.twimg.com/media/Fb31CwtUEAARgRW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fb31CwtUEAARgRW.jpg",
+					"url": "https://t.co/3wS4Fllk4d",
+					"display_url": "pic.twitter.com/3wS4Fllk4d",
+					"expanded_url": "https://twitter.com/SMTOWN_Idn/status/1566679438472802304/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1229,
+							"h": 1800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 464,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 819,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1566679384836022300,
+					"id_str": "1566679384836022273",
+					"indices": [
+						221,
+						244
+					],
+					"media_url": "http://pbs.twimg.com/media/Fb32mZIUIAEyMqN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fb32mZIUIAEyMqN.jpg",
+					"url": "https://t.co/3wS4Fllk4d",
+					"display_url": "pic.twitter.com/3wS4Fllk4d",
+					"expanded_url": "https://twitter.com/SMTOWN_Idn/status/1566679438472802304/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 676,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1154,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1566679401198080000,
+					"id_str": "1566679401198080000",
+					"indices": [
+						221,
+						244
+					],
+					"media_url": "http://pbs.twimg.com/media/Fb32nWFVUAAUtVz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fb32nWFVUAAUtVz.jpg",
+					"url": "https://t.co/3wS4Fllk4d",
+					"display_url": "pic.twitter.com/3wS4Fllk4d",
+					"expanded_url": "https://twitter.com/SMTOWN_Idn/status/1566679438472802304/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 676,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1154,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1064828006487609300,
+			"id_str": "1064828006487609345",
+			"name": "SMTOWN Indonesia",
+			"screen_name": "SMTOWN_Idn",
+			"location": "FX Sudirman Mall F5 #18B",
+			"description": "SM Entertainment Indonesia Official Twitter Page",
+			"url": "https://t.co/BGCHmjauKM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BGCHmjauKM",
+							"expanded_url": "http://bit.ly/AngketSM2022",
+							"display_url": "bit.ly/AngketSM2022",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1091823,
+			"friends_count": 38,
+			"listed_count": 670,
+			"created_at": "Tue Nov 20 10:29:15 +0000 2018",
+			"favourites_count": 89,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 3695,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1100927655610744832/D4iRym3b_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1100927655610744832/D4iRym3b_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1064828006487609345/1551316595",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 14,
+		"favorite_count": 103,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "YOONA Tunjukkan Kesuksesannya dengan Bintangi Banyak Pemotretan Majalah di Tahun Ini\n\nCek info lengkapnya di Official Facebook Page SMTOWN Indonesia\n👉🏻https://t.co/7lhf4jiesk\n\n@GirlsGeneration \n#LIMYOONA #GirlsGeneration https://t.co/3wS4Fllk4d"
+	},
+	{
 		"created_at": "Mon Sep 05 06:07:11 +0000 2022",
 		"id": 1566667839595233300,
 		"id_str": "1566669251456471041",
