b3d47c9078f22f7a70f586ffafeceac32794b0a8
SIGNO <iamsigno@users.noreply.github.com>
2022-09-14 12:07:49 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29712,
-	"created_at": "Wed Sep 14 02:07:49 +0000 2022",
+	"tweets": 29713,
+	"created_at": "Wed Sep 14 03:07:48 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,224 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Wed Sep 14 03:07:09 +0000 2022",
+		"id": 1569877805655269400,
+		"id_str": "1569885435958284291",
+		"full_text": "'Good Job' KWON YURI shows off her script packed full of an eclectic mix of transformations! Her colorful visuals as she takes on different jobs are visually interesting to watch!\n\n#권유리 #KWONYURI\n#굿잡 #GoodJob \n#소녀시대 #GirlsGeneration https://t.co/hTM4EwQ47x",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			232
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "권유리",
+					"indices": [
+						181,
+						185
+					]
+				},
+				{
+					"text": "KWONYURI",
+					"indices": [
+						186,
+						195
+					]
+				},
+				{
+					"text": "굿잡",
+					"indices": [
+						196,
+						199
+					]
+				},
+				{
+					"text": "GoodJob",
+					"indices": [
+						200,
+						208
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						210,
+						215
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						216,
+						232
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1569877793047199700,
+					"id_str": "1569877793047199746",
+					"indices": [
+						233,
+						256
+					],
+					"media_url": "http://pbs.twimg.com/media/FclTiQNaQAIkmAN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FclTiQNaQAIkmAN.jpg",
+					"url": "https://t.co/hTM4EwQ47x",
+					"display_url": "pic.twitter.com/hTM4EwQ47x",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1569877805655269377/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 518,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 914,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1560,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1569877793047199700,
+					"id_str": "1569877793047199746",
+					"indices": [
+						233,
+						256
+					],
+					"media_url": "http://pbs.twimg.com/media/FclTiQNaQAIkmAN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FclTiQNaQAIkmAN.jpg",
+					"url": "https://t.co/hTM4EwQ47x",
+					"display_url": "pic.twitter.com/hTM4EwQ47x",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1569877805655269377/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 518,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 914,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1560,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 884378714,
+			"id_str": "884378714",
+			"name": "SMTOWN",
+			"screen_name": "SMTOWNGLOBAL",
+			"location": "",
+			"description": "SMEntertainment Group Official Twitter",
+			"url": "https://t.co/zjb7YW3ApX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zjb7YW3ApX",
+							"expanded_url": "http://www.smtown.com",
+							"display_url": "smtown.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11170832,
+			"friends_count": 55,
+			"listed_count": 12602,
+			"created_at": "Tue Oct 16 12:49:45 +0000 2012",
+			"favourites_count": 2027,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 12882,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "F08BA0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1537993544555560963/XHzYSZjH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1537993544555560963/XHzYSZjH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/884378714/1655521211",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 446,
+		"favorite_count": 1814,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "'Good Job' KWON YURI shows off her script packed full of an eclectic mix of transformations! Her colorful visuals as she takes on different jobs are visually interesting to watch!\n\n#권유리 #KWONYURI\n#굿잡 #GoodJob \n#소녀시대 #GirlsGeneration https://t.co/hTM4EwQ47x"
+	},
+	{
 		"created_at": "Wed Sep 14 02:07:14 +0000 2022",
 		"id": 1569868317778915300,
 		"id_str": "1569870358693412871",
