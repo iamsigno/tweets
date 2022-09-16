adfb1c7db9f1b173f505a30e129183ae4adb8fd7
SIGNO <iamsigno@users.noreply.github.com>
2022-09-16 16:07:49 +0900

    Added 1 tweets

--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Fri Sep 16 07:07:09 +0000 2022",
+		"id": 1570658144711483400,
+		"id_str": "1570670607288733696",
+		"full_text": "[YOONA - CINE21]\nYOONA가 커버를 장식한 &lt;CINE21&gt; 1373호를 지금 SMTOWN &amp;STORE 온라인에서 만나보세요!\nhttps://t.co/YCllnOYcl8 https://t.co/ttlk3t0MoP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			111
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/YCllnOYcl8",
+					"expanded_url": "https://bit.ly/3qF3bQy",
+					"display_url": "bit.ly/3qF3bQy",
+					"indices": [
+						88,
+						111
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1570658139774791700,
+					"id_str": "1570658139774791680",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/FcwZQaTaMAAZQ8Q.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcwZQaTaMAAZQ8Q.jpg",
+					"url": "https://t.co/ttlk3t0MoP",
+					"display_url": "pic.twitter.com/ttlk3t0MoP",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1570658144711483394/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1570658139774791700,
+					"id_str": "1570658139774791680",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/FcwZQaTaMAAZQ8Q.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcwZQaTaMAAZQ8Q.jpg",
+					"url": "https://t.co/ttlk3t0MoP",
+					"display_url": "pic.twitter.com/ttlk3t0MoP",
+					"expanded_url": "https://twitter.com/SMTOWNandSTORE/status/1570658144711483394/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1113723889245278200,
+			"id_str": "1113723889245278209",
+			"name": "SMTOWN &STORE",
+			"screen_name": "SMTOWNandSTORE",
+			"location": "",
+			"description": "",
+			"url": "https://t.co/Ee3t6o418Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Ee3t6o418Q",
+							"expanded_url": "http://www.SMTOWNandSTORE.com",
+							"display_url": "SMTOWNandSTORE.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 252332,
+			"friends_count": 0,
+			"listed_count": 417,
+			"created_at": "Thu Apr 04 08:44:01 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2986,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1113741967370797057/sjT8judc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1113723889245278209/1554705745",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 63,
+		"favorite_count": 174,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[YOONA - CINE21]\nYOONA가 커버를 장식한 &lt;CINE21&gt; 1373호를 지금 SMTOWN &amp;STORE 온라인에서 만나보세요!\nhttps://t.co/YCllnOYcl8 https://t.co/ttlk3t0MoP"
+	},
+	{
 		"created_at": "Fri Sep 16 04:07:09 +0000 2022",
 		"id": 1570612745393021000,
 		"id_str": "1570625311338909701",
