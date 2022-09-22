ad964b8d4cfbe4309262f45a6a8364dbb59353a5
SIGNO <iamsigno@users.noreply.github.com>
2022-09-22 10:07:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29730,
-	"created_at": "Thu Sep 22 00:07:49 +0000 2022",
+	"tweets": 29731,
+	"created_at": "Thu Sep 22 01:07:50 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,290 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Thu Sep 22 01:07:08 +0000 2022",
+		"id": 1572746981495033900,
+		"id_str": "1572754337025167360",
+		"full_text": "[14회 예고]\n두 번째 희생자로… 당첨이야!\n\n힐링드라마 #당신이소원을말하면\n매주 수목 밤 9시 50분 (KBS20220922방송)\n\n#kbs #당신이소원을말하면\n#당소말 #IfYouWishUponMe #지창욱 #성동일 #최수영 #원지안 #kbsdrama #kbs드라마 https://t.co/FZJvCS1fg7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			151
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "당신이소원을말하면",
+					"indices": [
+						33,
+						43
+					]
+				},
+				{
+					"text": "kbs",
+					"indices": [
+						76,
+						80
+					]
+				},
+				{
+					"text": "당신이소원을말하면",
+					"indices": [
+						81,
+						91
+					]
+				},
+				{
+					"text": "당소말",
+					"indices": [
+						92,
+						96
+					]
+				},
+				{
+					"text": "IfYouWishUponMe",
+					"indices": [
+						97,
+						113
+					]
+				},
+				{
+					"text": "지창욱",
+					"indices": [
+						114,
+						118
+					]
+				},
+				{
+					"text": "성동일",
+					"indices": [
+						119,
+						123
+					]
+				},
+				{
+					"text": "최수영",
+					"indices": [
+						124,
+						128
+					]
+				},
+				{
+					"text": "원지안",
+					"indices": [
+						129,
+						133
+					]
+				},
+				{
+					"text": "kbsdrama",
+					"indices": [
+						134,
+						143
+					]
+				},
+				{
+					"text": "kbs드라마",
+					"indices": [
+						144,
+						151
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1572746881452544000,
+					"id_str": "1572746881452544000",
+					"indices": [
+						152,
+						175
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1572746881452544000/pu/img/Ak5Fb19-73q35Pq9.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1572746881452544000/pu/img/Ak5Fb19-73q35Pq9.jpg",
+					"url": "https://t.co/FZJvCS1fg7",
+					"display_url": "pic.twitter.com/FZJvCS1fg7",
+					"expanded_url": "https://twitter.com/MyloveKBS/status/1572746981495033856/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1572746881452544000,
+					"id_str": "1572746881452544000",
+					"indices": [
+						152,
+						175
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1572746881452544000/pu/img/Ak5Fb19-73q35Pq9.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1572746881452544000/pu/img/Ak5Fb19-73q35Pq9.jpg",
+					"url": "https://t.co/FZJvCS1fg7",
+					"display_url": "pic.twitter.com/FZJvCS1fg7",
+					"expanded_url": "https://twitter.com/MyloveKBS/status/1572746981495033856/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 35412,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1572746881452544000/pu/pl/MSsLs5fFNxemg9Bc.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1572746881452544000/pu/vid/1280x720/odhblZ7K4Ygy6jNv.mp4?tag=12"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1572746881452544000/pu/vid/640x360/JE7OJuTeNAw8oDdL.mp4?tag=12"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1572746881452544000/pu/vid/480x270/oNr9k-RtOVKddZWt.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 158284325,
+			"id_str": "158284325",
+			"name": "KBS 한국방송",
+			"screen_name": "MyloveKBS",
+			"location": "",
+			"description": "대한민국 대표 공영방송 KBS 공식 트위터",
+			"url": "https://t.co/QZdiw0Hhns",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/QZdiw0Hhns",
+							"expanded_url": "http://twtkr.com/MyloveKBS",
+							"display_url": "twtkr.com/MyloveKBS",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1105450,
+			"friends_count": 56558,
+			"listed_count": 2119,
+			"created_at": "Tue Jun 22 07:45:48 +0000 2010",
+			"favourites_count": 99,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 55129,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1520993049010065408/83_NUsR2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1520993049010065408/83_NUsR2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/158284325/1663570145",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 16,
+		"favorite_count": 36,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[14회 예고]\n두 번째 희생자로… 당첨이야!\n\n힐링드라마 #당신이소원을말하면\n매주 수목 밤 9시 50분 (KBS20220922방송)\n\n#kbs #당신이소원을말하면\n#당소말 #IfYouWishUponMe #지창욱 #성동일 #최수영 #원지안 #kbsdrama #kbs드라마 https://t.co/FZJvCS1fg7"
+	},
+	{
 		"created_at": "Thu Sep 22 00:07:09 +0000 2022",
 		"id": 1572737986764345300,
 		"id_str": "1572739238692626432",
