1fbdf24fa5b55b1cf5c775dfca37e190e78668e3
SIGNO <iamsigno@users.noreply.github.com>
2022-09-30 20:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29753,
-	"created_at": "Fri Sep 30 06:07:49 +0000 2022",
+	"tweets": 29754,
+	"created_at": "Fri Sep 30 11:07:49 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Fri Sep 30 10:44:57 +0000 2022",
+		"id": 1575792907004485600,
+		"id_str": "1575798848454332416",
+		"full_text": "[LIVE] 제6회 전국 발달장애인 음악축제 GMF(Great Music Festival) 유튜브 생중계 - feat. 소녀시대 효연의 축하공연\n\nhttps://t.co/7Q8hB6A0cj\n\n#HYOYEON #효연 @Hyoyeon_djhyo\n#GirlsGeneration #소녀시대\n#GreatMusicFestival #GMF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			182
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HYOYEON",
+					"indices": [
+						107,
+						115
+					]
+				},
+				{
+					"text": "효연",
+					"indices": [
+						116,
+						119
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						135,
+						151
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						152,
+						157
+					]
+				},
+				{
+					"text": "GreatMusicFestival",
+					"indices": [
+						158,
+						177
+					]
+				},
+				{
+					"text": "GMF",
+					"indices": [
+						178,
+						182
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Hyoyeon_djhyo",
+					"name": "Hyoyeon",
+					"id": 792687101842186200,
+					"id_str": "792687101842186240",
+					"indices": [
+						120,
+						134
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/7Q8hB6A0cj",
+					"expanded_url": "https://youtu.be/tr_1N-4oIrM",
+					"display_url": "youtu.be/tr_1N-4oIrM",
+					"indices": [
+						82,
+						105
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBV3cSH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBV3cSH",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3908129,
+			"friends_count": 5,
+			"listed_count": 6234,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 3120,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551220686538276865/mAgQFVeM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551220686538276865/mAgQFVeM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1658674834",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 768,
+		"favorite_count": 1721,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[LIVE] 제6회 전국 발달장애인 음악축제 GMF(Great Music Festival) 유튜브 생중계 - feat. 소녀시대 효연의 축하공연\n\nhttps://t.co/7Q8hB6A0cj\n\n#HYOYEON #효연 @Hyoyeon_djhyo\n#GirlsGeneration #소녀시대\n#GreatMusicFestival #GMF"
+	},
+	{
 		"created_at": "Fri Sep 30 06:07:17 +0000 2022",
 		"id": 1575727146823258000,
 		"id_str": "1575728973388062720",
