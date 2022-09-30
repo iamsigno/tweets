b6d8f972f5409d2d43af713c41f8be20e5afe4c9
SIGNO <iamsigno@users.noreply.github.com>
2022-09-30 15:07:50 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29752,
-	"created_at": "Fri Sep 30 03:07:53 +0000 2022",
+	"tweets": 29753,
+	"created_at": "Fri Sep 30 06:07:49 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Fri Sep 30 06:07:17 +0000 2022",
+		"id": 1575727146823258000,
+		"id_str": "1575728973388062720",
+		"full_text": "【#SONEJAPAN】先日8月31日(水)にFCサイト内「SPECIAL」にて公開した『SONE NOTE LIVE Vol.39』のメイキング映像を「BLOG」に公開✨ぜひご覧ください♬\n※会員様限定コンテンツのため、転載禁止です。\n#少女時代\n#HYOYEON\n#ヒョヨン\n#SONENOTELIVE https://t.co/iYsHJgxiLn",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			153
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SONEJAPAN",
+					"indices": [
+						1,
+						11
+					]
+				},
+				{
+					"text": "少女時代",
+					"indices": [
+						119,
+						124
+					]
+				},
+				{
+					"text": "HYOYEON",
+					"indices": [
+						125,
+						133
+					]
+				},
+				{
+					"text": "ヒョヨン",
+					"indices": [
+						134,
+						139
+					]
+				},
+				{
+					"text": "SONENOTELIVE",
+					"indices": [
+						140,
+						153
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1575323841626198000,
+					"id_str": "1575323841626198016",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/FdyssAJVUAAoVNN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FdyssAJVUAAoVNN.jpg",
+					"url": "https://t.co/iYsHJgxiLn",
+					"display_url": "pic.twitter.com/iYsHJgxiLn",
+					"expanded_url": "https://twitter.com/gg_official_jp/status/1575727146823258113/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1783,
+							"h": 1003,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1575323841626198000,
+					"id_str": "1575323841626198016",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/FdyssAJVUAAoVNN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FdyssAJVUAAoVNN.jpg",
+					"url": "https://t.co/iYsHJgxiLn",
+					"display_url": "pic.twitter.com/iYsHJgxiLn",
+					"expanded_url": "https://twitter.com/gg_official_jp/status/1575727146823258113/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1783,
+							"h": 1003,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2585025200,
+			"id_str": "2585025200",
+			"name": "少女時代 JAPAN OFFICIAL",
+			"screen_name": "gg_official_jp",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/T70Fo3NBNn",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/T70Fo3NBNn",
+							"expanded_url": "http://www.girls-generation.jp/",
+							"display_url": "girls-generation.jp",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 123516,
+			"friends_count": 0,
+			"listed_count": 542,
+			"created_at": "Tue Jun 24 03:33:35 +0000 2014",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 448,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1555086105711886337/zo-mAiti_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1555086105711886337/zo-mAiti_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2585025200/1659596396",
+			"profile_link_color": "0024B3",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 124,
+		"favorite_count": 239,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "【#SONEJAPAN】先日8月31日(水)にFCサイト内「SPECIAL」にて公開した『SONE NOTE LIVE Vol.39』のメイキング映像を「BLOG」に公開✨ぜひご覧ください♬\n※会員様限定コンテンツのため、転載禁止です。\n#少女時代\n#HYOYEON\n#ヒョヨン\n#SONENOTELIVE https://t.co/iYsHJgxiLn"
+	},
+	{
 		"created_at": "Fri Sep 30 03:07:09 +0000 2022",
 		"id": 1575679795181998000,
 		"id_str": "1575683642155831296",
