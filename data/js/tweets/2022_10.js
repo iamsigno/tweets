0df3456862ee1905165675644502cfa1143cf0d4
SIGNO <iamsigno@users.noreply.github.com>
2022-10-02 20:07:49 +0900

    Added 1 tweets

---
+++ data/js/tweets/2022_10.js
@@ -0,0 +1,217 @@
+Grailbird.data.tweets_2022_10 = 
+[
+	{
+		"created_at": "Sun Oct 02 11:07:09 +0000 2022",
+		"id": 1576527611777659000,
+		"id_str": "1576529211946319872",
+		"full_text": "'2022 Girls’ Generation Special Event - Long Lasting Love' Behind The Scenes\n\nhttps://t.co/Iwl9gv5vDv\n\n#소녀시대 #GirlsGeneration\n#Long_Lasting_Love\n#Behind https://t.co/19AjVTAbwL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			152
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "소녀시대",
+					"indices": [
+						103,
+						108
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						109,
+						125
+					]
+				},
+				{
+					"text": "Long_Lasting_Love",
+					"indices": [
+						126,
+						144
+					]
+				},
+				{
+					"text": "Behind",
+					"indices": [
+						145,
+						152
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Iwl9gv5vDv",
+					"expanded_url": "https://youtu.be/egeLBSUh6-A",
+					"display_url": "youtu.be/egeLBSUh6-A",
+					"indices": [
+						78,
+						101
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1576527387726336000,
+					"id_str": "1576527387726336000",
+					"indices": [
+						153,
+						176
+					],
+					"media_url": "http://pbs.twimg.com/media/FeDzTmwaUAAP3Pt.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FeDzTmwaUAAP3Pt.jpg",
+					"url": "https://t.co/19AjVTAbwL",
+					"display_url": "pic.twitter.com/19AjVTAbwL",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1576527611777658881/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1576527387726336000,
+					"id_str": "1576527387726336000",
+					"indices": [
+						153,
+						176
+					],
+					"media_url": "http://pbs.twimg.com/media/FeDzTmwaUAAP3Pt.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FeDzTmwaUAAP3Pt.jpg",
+					"url": "https://t.co/19AjVTAbwL",
+					"display_url": "pic.twitter.com/19AjVTAbwL",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1576527611777658881/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBV3cSH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBV3cSH",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3908696,
+			"friends_count": 5,
+			"listed_count": 6238,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 3121,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551220686538276865/mAgQFVeM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551220686538276865/mAgQFVeM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1658674834",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2111,
+		"favorite_count": 2966,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "'2022 Girls’ Generation Special Event - Long Lasting Love' Behind The Scenes\n\nhttps://t.co/Iwl9gv5vDv\n\n#소녀시대 #GirlsGeneration\n#Long_Lasting_Love\n#Behind https://t.co/19AjVTAbwL"
+	}
+]
